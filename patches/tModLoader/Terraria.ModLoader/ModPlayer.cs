ad08218347de547a5c894d7e6e50445971ee5ef1
Nathan Kim <bluemagic123@roadrunner.com>
2015-11-14 14:52:23 -0800

    Remove bad merge (try seeing what other people did before blindly merging)

--- patches/tModLoader/Terraria.ModLoader/ModPlayer.cs
+++ patches/tModLoader/Terraria.ModLoader/ModPlayer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.Xna.Framework;
 using Terraria;
 
 namespace Terraria.ModLoader
@@ -133,81 +132,13 @@ namespace Terraria.ModLoader
 		{
 		}
 
-		public virtual void OnHitNPC(NPC target, int damage, float knockBack, bool crit)
+		public virtual void OnHitNPC(Item item, NPC target, int damage, float knockBack, bool crit)
 		{
 		}
 
-		public virtual void ModifyHitNPC(NPC target, int damage, float knockback, bool crit)
+		public virtual void OnHitByNPC(NPC target, int damage, bool crit)
 		{
 		}
-
-		public virtual bool? CanHitNPC(NPC target)
-		{
-			return null;
-		}
-
-		public virtual void OnHitByNPC(NPC npc, int damage, bool crit)
-		{
-		}
-
-		public virtual bool CanBeHitByNPC(NPC npc)
-		{
-			return true;
-		}
-
-		public virtual void ModifyHitByNPC(NPC npc, int damage, bool crit)
-		{
-		}
-
-		public virtual bool Shoot(Vector2 position, float speedX, float speedY, int type, int damage, float knockback)
-		{
-			return true;
-		}
-
-		public virtual void OnHitAnything(float x, float y, Entity victim)
-		{
-		}
-
-		public virtual void PreHurt(int damage, int hitDirection, bool pvp, bool quiet, bool crit)
-		{
-		}
-
-		public virtual void Hurt(int damage, int hitDirection, bool pvp, bool quiet, bool crit)
-		{
-		}
-
-		public virtual void MeleeEffects(Item item, Rectangle hitbox)
-		{
-		}
-
-		public virtual bool CanHitPvp(Player attacked)
-		{
-			return true;
-		}
-
-		public virtual void ModifyHitPvp(Player target, int damage, bool crit)
-		{
-		}
-
-		public virtual void OnHitPvp(Player target, int damage, bool crit)
-		{
-		}
-
-		public virtual int GetWeaponDamage(Item sItem)
-		{
-			return sItem.damage;
-		}
-
-		public virtual bool ConsumeAmmo(Item item)
-		{
-			return true;
-		}
-
-		public virtual float GetWeaponKnockback(Item sItem)
-		{
-			return sItem.knockBack;
-		}
-
 		//TODO
 		//hooks for grappling hooks
 	}
