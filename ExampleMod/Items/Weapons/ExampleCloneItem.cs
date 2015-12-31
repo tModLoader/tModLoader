4cf5e3a421607669d4fdc33648d9298cc6b8d501
JavidPack <javidpack@gmail.com>
2015-12-31 01:26:43 -0700

    CloneDefaults for Item and Projectile

---
+++ ExampleMod/Items/Weapons/ExampleCloneItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Weapons
+{
+	public class ExampleCloneItem : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.Starfury);
+			item.name = "Starfury V2";
+			item.shootSpeed *= 0.75f;
+			item.damage = (int)(item.damage * 1.5);
+		}
+		
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			type = mod.ProjectileType("ExampleCloneProjectile");
+			return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Starfury);
+			recipe.AddCraftGroup(CraftGroup.GetVanillaGroup("IronBar"), 5);
+			recipe.AddTile(TileID.FireflyinaBottle);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ ExampleMod/Projectiles/ExampleCloneProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Projectiles
+{
+	public class ExampleCloneProjectile : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.Starfury);
+			projectile.name = "Starfury V2";
+			aiType = ProjectileID.Starfury;
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				int a = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y - 16f, Main.rand.Next(-10, 11) * .25f, Main.rand.Next(-10, -5) * .25f, ProjectileID.Starfury, (int)(projectile.damage * .5f), 0, projectile.whoAmI);
+				Main.projectile[a].aiStyle = 1;
+				Main.projectile[a].tileCollide = true;
+			}
+			return true;
+		}
+	}
+}
