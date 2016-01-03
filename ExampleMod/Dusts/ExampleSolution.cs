574ed66fdd34edf81f04e30377d7444dcf361714
JavidPack <javidpack@gmail.com>
2016-01-03 03:33:38 -0700

    Example Solution
    btw, we may have a recursion problem with "TileLoader.CheckModTile" and
    "WorldGen.TileFrame"

---
+++ ExampleMod/Dusts/ExampleSolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Dusts
+{
+	public class ExampleSolution : ModDust
+	{
+		public override void SetDefaults()
+		{
+			updateType = 110;
+        }
+	}
+}
---
+++ ExampleMod/Items/ExampleSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items
+{
+	public class ExampleSolution : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Monochromatic Solutiuon";
+			item.shoot = mod.ProjectileType("ExampleSolution") - ProjectileID.PureSpray;
+			item.ammo = ItemID.GreenSolution;
+			item.width = 10;
+			item.height = 12;
+			item.value = Item.buyPrice(0, 0, 25, 0);
+			item.rare = 3;
+			item.maxStack = 999;
+			item.toolTip = "Used by the Clentaminator";
+			item.toolTip2 = "Spreads the example";
+			item.consumable = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "ExampleItem", 10);
+			recipe.SetResult(this, 999);
+			recipe.AddRecipe();
+		}
+	}
+}
