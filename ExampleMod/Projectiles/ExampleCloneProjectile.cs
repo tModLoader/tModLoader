f656c3bb74584d9a5c145665c9628b8a02c392b6
JavidPack <javidpack@gmail.com>
2016-01-06 06:09:13 -0700

    Starfury v2 fix.

--- ExampleMod/Projectiles/ExampleCloneProjectile.cs
+++ ExampleMod/Projectiles/ExampleCloneProjectile.cs
@@ -15,11 +15,17 @@ namespace ExampleMod.Projectiles
 			aiType = ProjectileID.Starfury;
 		}
 
+		public override bool PreKill(int timeLeft)
+		{
+			projectile.type = ProjectileID.Starfury;
+            return true;
+		}
+
 		public override bool OnTileCollide(Vector2 oldVelocity)
 		{
 			for (int i = 0; i < 5; i++)
 			{
-				int a = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y - 16f, Main.rand.Next(-10, 11) * .25f, Main.rand.Next(-10, -5) * .25f, ProjectileID.Starfury, (int)(projectile.damage * .5f), 0, projectile.whoAmI);
+				int a = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y - 16f, Main.rand.Next(-10, 11) * .25f, Main.rand.Next(-10, -5) * .25f, ProjectileID.Starfury, (int)(projectile.damage * .5f), 0, projectile.owner);
 				Main.projectile[a].aiStyle = 1;
 				Main.projectile[a].tileCollide = true;
 			}
