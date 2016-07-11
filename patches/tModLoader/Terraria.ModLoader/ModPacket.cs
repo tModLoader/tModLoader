f3df088262ae738c95c55bf44326a9080720129b
Chicken-Bones <mehvids@gmail.com>
2016-07-11 15:01:49 +1000

    Fix missing broadcast check in ModPacket.Send

--- patches/tModLoader/Terraria.ModLoader/ModPacket.cs
+++ patches/tModLoader/Terraria.ModLoader/ModPacket.cs
@@ -22,7 +22,7 @@ namespace Terraria.ModLoader
 				Netplay.Clients[toClient].Socket.AsyncSend(buf, 0, len, SendCallback);
 			else
 				for (int i = 0; i < 256; i++)
-					if (i != ignoreClient && Netplay.Clients[i].IsConnected())
+					if (i != ignoreClient && Netplay.Clients[i].IsConnected() && NetMessage.buffer[i].broadcast)
 						Netplay.Clients[i].Socket.AsyncSend(buf, 0, len, SendCallback);
 		}
 
