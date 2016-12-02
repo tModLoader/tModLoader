f351f71f861b7046792ff2d42e07a8e9cf4716b5
Chicken-Bones <mehvids@gmail.com>
2016-12-02 21:25:30 +1000

    Update Mac build

--- patches/merged/Terraria/MacLaunch.cs
+++ patches/merged/Terraria/MacLaunch.cs
@@ -8,6 +8,10 @@ namespace Terraria
         private static void Main(string[] args)
         {
             Environment.SetEnvironmentVariable("FNA_WORKAROUND_WINDOW_RESIZABLE", "1");
+			Terraria.Main.OnEnginePreload += delegate()
+			{
+				Terraria.Main.instance.IsMouseVisible = false;
+			};
             Program.LaunchGame(Utils.FixArgs(args));
         }
     }
