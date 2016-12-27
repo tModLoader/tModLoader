158ac77688fdfcb68ebb73e3ee5065863c4882c2
Murphy Angelo <murphycangelo@u.northwestern.edu>
2016-12-27 16:02:57 -0500

    Create ModCommand.cs

---
+++ patches/tModLoader/Terraria.ModLoader/ModCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Terraria.ModLoader
+{
+	public enum CommandType { Chat, Server }
+
+	public abstract class ModCommand
+	{
+		public Mod Mod { get; internal set; }
+		public string Name { get; internal set; }
+		public abstract string Command { get; }
+		public abstract CommandType Type { get; }
+		public abstract string Usage { get; }
+		public abstract bool Show { get; }
+		public virtual bool Autoload(ref string name) => Mod.Properties.Autoload;
+		public abstract bool VerifyArguments(string[] args);
+		public abstract void Action(string[] args);
+	}
+}
