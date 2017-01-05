7083bd46e45ee1c7860d3d7129f108a99628c382
JavidPack <javidpack@gmail.com>
2017-01-04 19:49:24 -0700

    A few comments for TileEntity example.

--- ExampleMod/Tiles/TEElementalPurge.cs
+++ ExampleMod/Tiles/TEElementalPurge.cs
@@ -25,9 +25,10 @@ namespace ExampleMod.Tiles
 
 		public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction)
 		{
+			// i - 1 and j - 2 come from the fact that the origin of the tile is "new Point16(1, 2);", so we need to pass the coordinates back to the top left tile. If using a vanilla TileObjectData.Style, make sure you know the origin value.
 			if (Main.netMode == 1)
 			{
-				NetMessage.SendTileSquare(Main.myPlayer, i - 1, j - 1, 3);
+				NetMessage.SendTileSquare(Main.myPlayer, i - 1, j - 1, 3); // this is -1, -1, however, because -1, -1 places the 3 diameter square over all the tiles, which are sent to other clients as an update.
 				NetMessage.SendData(87, -1, -1, "", i - 1, j - 2, 0f, 0f, 0, 0, 0);
 				return -1;
 			}
