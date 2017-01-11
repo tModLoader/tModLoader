a8cc10015d1bda624a8db782820012e9e1705bf7
gorateron <zond1600@student.nhl.nl>
2017-01-11 19:55:33 +0100

    Fixed LangID static->const // NetmodeID-> static

--- patches/tModLoader/Terraria.ID/LangID.cs
+++ patches/tModLoader/Terraria.ID/LangID.cs
@@ -4,10 +4,10 @@ namespace Terraria.ID
 {
 	public static class LangID
 	{
-		public static int English = 1;
-		public static int German = 2;
-		public static int Italian = 3;
-		public static int French = 4;
-		public static int Spanish = 5;
+		public const int English = 1;
+		public const int German = 2;
+		public const int Italian = 3;
+		public const int French = 4;
+		public const int Spanish = 5;
 	}
 }
--- patches/tModLoader/Terraria.ID/NetmodeID.cs
+++ patches/tModLoader/Terraria.ID/NetmodeID.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Terraria.ID
 {
-	public class NetmodeID
+	public static class NetmodeID
 	{
 		public const int SinglePlayer = 0;
 		public const int MultiplayerClient = 1;
