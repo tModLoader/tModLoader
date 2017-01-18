38d0999ee42b7795e4183096a6879f0dacf7d759
JavidPack <javidpack@gmail.com>
2017-01-18 15:01:17 -0700

    Example Console command.

---
+++ ExampleMod/Commands/VolcanoCommand.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Commands
+{
+	class VolcanoCommand : ModCommand
+	{
+		public override CommandType Type
+		{
+			get { return CommandType.Console; }
+		}
+
+		public override string Command
+		{
+			get { return "volcano"; }
+		}
+
+		public override string Description
+		{
+			get { return "Trigger a volcano"; }
+		}
+
+		public override void Action(CommandCaller caller, string input, string[] args)
+		{
+			string message = "Did you hear something....A Volcano! Find Cover!";
+			Color messageColor = Color.Orange;
+			NetMessage.SendData(25, -1, -1, message, 255, messageColor.R, messageColor.G, messageColor.B, 0);
+			ExampleWorld exampleWorld = Mod.GetModWorld<ExampleWorld>();
+			exampleWorld.VolcanoCountdown = ExampleWorld.DefaultVolcanoCountdown;
+			exampleWorld.VolcanoCooldown = ExampleWorld.DefaultVolcanoCooldown;
+		}
+	}
+}
