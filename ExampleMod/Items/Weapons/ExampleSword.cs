ac1c749637c9d3703ca5760fbf8dd88ad8b06442
CXUtk <2237367067@qq.com>
2017-01-19 12:11:17 +0800

    Documented ExampleSword

--- ExampleMod/Items/Weapons/ExampleSword.cs
+++ ExampleMod/Items/Weapons/ExampleSword.cs
@@ -9,20 +9,20 @@ namespace ExampleMod.Items.Weapons
 	{
 		public override void SetDefaults()
 		{
-			item.name = "Example Sword";
-			item.damage = 50;
-			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.toolTip = "This is a modded sword.";
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
-			item.knockBack = 6;
-			item.value = 10000;
-			item.rare = 2;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
+			item.name = "Example Sword";		//The name of your weapon
+			item.damage = 50;			//The damage of your weapon
+			item.melee = true;			//Is your weapon a melee weapon?
+			item.width = 40;			//Weapon's texture's width
+			item.height = 40;			//Weapon's texture's height
+			item.toolTip = "This is a modded sword.";	//The text showed below your weapon's name
+			item.useTime = 20;			//The time span of using the weapon. Remember in terraria, 60 frames is a second.
+			item.useAnimation = 20;			//The time span of the using animation of the weapon, suggest set it the same as useTime.
+			item.useStyle = 1;			//The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
+			item.knockBack = 6;			//The force of knockback of the weapon. Maxium is 20
+			item.value = 10000;			//The value of the weapon
+			item.rare = 2;				//The rarity of the weapon, from -1 to 13
+			item.UseSound = SoundID.Item1;		//The sound when the weapon is using
+			item.autoReuse = true;			//Whether the weapon can use automaticly by pressing mousebutton
 		}
 
 		public override void AddRecipes()
@@ -39,12 +39,13 @@ namespace ExampleMod.Items.Weapons
 			if (Main.rand.Next(3) == 0)
 			{
 				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("Sparkle"));
+				//Emit dusts when swing the sword
 			}
 		}
 
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
 		{
-			target.AddBuff(BuffID.OnFire, 60);
+			target.AddBuff(BuffID.OnFire, 60);		//Add Onfire buff to the NPC for 1 second
 		}
 	}
 }
