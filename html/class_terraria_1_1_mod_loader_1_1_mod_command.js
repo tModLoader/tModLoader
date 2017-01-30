8e1d2a1ec4cd75a0afa263d38c661ce3c0f5e0b8
Travis CI <travis@travis-ci.org>
2017-01-30 08:53:07 +0000

    Deploy code docs to GitHub Pages Travis build: 3
    Commit: f5e087bfecdc23ccc8827916ae96ffcd151b59ed

--- html/class_terraria_1_1_mod_loader_1_1_mod_command.js
+++ html/class_terraria_1_1_mod_loader_1_1_mod_command.js
@@ -5,7 +5,7 @@ var class_terraria_1_1_mod_loader_1_1_mod_command =
     [ "Description", "class_terraria_1_1_mod_loader_1_1_mod_command.html#ac807de4b57a97d5ee2c6105ceff5d4b9", null ],
     [ "Usage", "class_terraria_1_1_mod_loader_1_1_mod_command.html#ab53c5d3bbea6259e97df8004901d99e4", null ],
     [ "Command", "class_terraria_1_1_mod_loader_1_1_mod_command.html#a0baffa44571dd5dedc9bb278a94fad4e", null ],
-    [ "Mod", "class_terraria_1_1_mod_loader_1_1_mod_command.html#a417edb3a9b3b3e54bbb4de2dfa30a3c2", null ],
+    [ "mod", "class_terraria_1_1_mod_loader_1_1_mod_command.html#af2628918bef8e9d2546bfcca3d331457", null ],
     [ "Name", "class_terraria_1_1_mod_loader_1_1_mod_command.html#a43d8b722b80cd5e7be1ce2188f0421b6", null ],
     [ "Type", "class_terraria_1_1_mod_loader_1_1_mod_command.html#a5691c1b52fc3b7f9f6de2c8cf71fbe27", null ]
 ];
