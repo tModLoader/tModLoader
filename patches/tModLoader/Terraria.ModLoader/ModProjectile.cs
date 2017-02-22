226c0d2aa0d3371f158d800b9a77021d93a245b5
gorateron <zond1600@student.nhl.nl>
2017-02-22 10:54:12 +0100

    First batch ModProjectile documentation
    Will finish it later.

--- patches/tModLoader/Terraria.ModLoader/ModProjectile.cs
+++ patches/tModLoader/Terraria.ModLoader/ModProjectile.cs
@@ -7,22 +7,40 @@ using Terraria;
 
 namespace Terraria.ModLoader
 {
+	/// <summary>
+	/// This class serves as a place for you to place all your properties and hooks for each projectile. Create instances of ModProjectile (preferably overriding this class) to pass as parameters to Mod.AddProjectile.
+	/// </summary>
 	public class ModProjectile
 	{
 		//add modProjectile property to Terraria.Projectile (internal set)
 		//set modProjectile to null at beginning of Terraria.Projectile.SetDefaults
+		/// <summary>
+		/// The projectile object that this ModProjectile controls.
+		/// </summary>
+		/// <value>
+		/// The projectile.
+		/// </value>
 		public Projectile projectile
 		{
 			get;
 			internal set;
 		}
 
+		/// <summary>
+		/// The mod object that this ModProjectile originates from.
+		/// </summary>
+		/// <value>
+		/// The mod.
+		/// </value>
 		public Mod mod
 		{
 			get;
 			internal set;
 		}
 
+		/// <summary>
+		/// The internal name of this ModProjectile.
+		/// </summary>
 		public string Name
 		{
 			get;
@@ -30,11 +48,29 @@ namespace Terraria.ModLoader
 		}
 
 		internal string texture;
+		/// <summary>
+		/// Determines which type of vanilla projectile this ModProjectile will copy the behavior (AI) of. Leave as 0 to not copy any behavior. Defaults to 0.
+		/// </summary>
 		public int aiType = 0;
+		/// <summary>
+		/// Determines which of the player's cooldown counters to use (-1, 0, or 1) when this projectile damages it. Defaults to -1.
+		/// </summary>
 		public int cooldownSlot = -1;
+		/// <summary>
+		/// How far to the right of its position this projectile should be drawn. Defaults to 0.
+		/// </summary>
 		public int drawOffsetX = 0;
+		/// <summary>
+		/// The vertical origin offset from the projectile's center when it is drawn. The origin is essentially the point of rotation. This field will also determine the vertical drawing offset of the projectile.
+		/// </summary>
 		public int drawOriginOffsetY = 0;
+		/// <summary>
+		/// The horizontal origin offset from the projectile's center when it is drawn.
+		/// </summary>
 		public float drawOriginOffsetX = 0f;
+		/// <summary>
+		/// If this projectile is held by the player, determines whether it is drawn in front of or behind the player's arms. Defaults to false.
+		/// </summary>
 		public bool drawHeldProjInFrontOfHeldItemAndArms = false;
 
 		public ModProjectile()
@@ -42,7 +78,12 @@ namespace Terraria.ModLoader
 			projectile = new Projectile();
 			projectile.modProjectile = this;
 		}
-
+		/// <summary>
+		/// Allows you to automatically load a projectile instead of using Mod.AddProjectile. Return true to allow autoloading; by default returns the mod's autoload property. Name is initialized to the overriding class name, and texture is initialized to the namespace and overriding class name with periods replaced with slashes. Use this method to either force or stop an autoload, or to change the default display name and texture path.
+		/// </summary>
+		/// <param name="name">The internal name.</param>
+		/// <param name="texture">The texture path.</param>
+		/// <returns></returns>
 		public virtual bool Autoload(ref string name, ref string texture)
 		{
 			return mod.Properties.Autoload;
@@ -58,130 +99,270 @@ namespace Terraria.ModLoader
 			newProjectile.SetDefaults();
 		}
 
+		/// <summary>
+		/// Whether instances of this ModProjectile are created through a memberwise clone or its constructor. Defaults to false.
+		/// </summary>
 		public virtual bool CloneNewInstances => false;
 
+		/// <summary>
+		/// Allows you to set all your projectile's properties, such as width, damage, aiStyle, penetrate, etc.
+		/// </summary>
 		public virtual void SetDefaults()
 		{
 		}
 
+		/// <summary>
+		/// Allows you to determine how this projectile behaves. Return false to stop the vanilla AI and the AI hook from being run. Returns true by default.
+		/// </summary>
+		/// <returns></returns>
 		public virtual bool PreAI()
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to determine how this projectile behaves. This will only be called if PreAI returns true.
+		/// </summary>
 		public virtual void AI()
 		{
 		}
 
+		/// <summary>
+		/// Allows you to determine how this projectile behaves. This will be called regardless of what PreAI returns.
+		/// </summary>
 		public virtual void PostAI()
 		{
 		}
 
+		/// <summary>
+		/// If you are storing AI information outside of the projectile.ai array, use this to send that AI information between clients and servers.
+		/// </summary>
+		/// <param name="writer"></param>
 		public virtual void SendExtraAI(BinaryWriter writer)
 		{
 		}
 
+		/// <summary>
+		/// Use this to receive information that was sent in SendExtraAI.
+		/// </summary>
+		/// <param name="reader"></param>
 		public virtual void ReceiveExtraAI(BinaryReader reader)
 		{
 		}
 
+		/// <summary>
+		/// Whether or not this projectile should update its position based on factors such as its velocity, whether it is in liquid, etc. Return false to make its velocity have no effect on its position. Returns true by default.
+		/// </summary>
+		/// <returns></returns>
 		public virtual bool ShouldUpdatePosition()
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to determine how this projectile interacts with tiles. Width and height determine the projectile's hitbox for tile collision, and default to -1. Leave them as -1 to use the projectile's real size. Fallthrough determines whether the projectile can fall through platforms, etc., and defaults to true.
+		/// </summary>
+		/// <param name="width">Width of the hitbox.</param>
+		/// <param name="height">Height of the hitbox.</param>
+		/// <param name="fallThrough">If the projectile can fall through platforms etc.</param>
+		/// <returns></returns>
 		public virtual bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to determine what happens when this projectile collides with a tile. OldVelocity is the velocity before tile collision. The velocity that takes tile collision into account can be found with projectile.velocity. Return true to allow the vanilla tile collision code to take place (which normally kills the projectile). Returns true by default.
+		/// </summary>
+		/// <param name="oldVelocity">The velocity of the projectile upon collision.</param>
+		/// <returns></returns>
 		public virtual bool OnTileCollide(Vector2 oldVelocity)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Return true or false to specify if the projectile can cut tiles, like vines. Return null for vanilla decision.
+		/// </summary>
+		/// <returns></returns>
 		public virtual bool? CanCutTiles()
 		{
 			return null;
 		}
 
+		/// <summary>
+		/// Code ran when the projectile cuts tiles. Only runs if CanCutTiles() returns true. Useful when programming lasers and such.
+		/// </summary>
 		public virtual void CutTiles()
 		{
 		}
 
+		/// <summary>
+		/// Allows you to determine whether the vanilla code for Kill and the Kill hook will be called. Return false to stop them from being called. Returns true by default. Note that this does not stop the projectile from dying.
+		/// </summary>
+		/// <param name="timeLeft"></param>
+		/// <returns></returns>
 		public virtual bool PreKill(int timeLeft)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to control what happens when this projectile is killed (for example, creating dust or making sounds). Also useful for creating retrievable ammo. 
+		/// </summary>
+		/// <param name="timeLeft"></param>
 		public virtual void Kill(int timeLeft)
 		{
 		}
 
+		/// <summary>
+		/// Whether or not this projectile is capable of killing tiles (such as grass) and damaging NPCs/players. Return false to prevent it from doing any sort of damage. Returns true by default.
+		/// </summary>
+		/// <returns></returns>
 		public virtual bool CanDamage()
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Whether or not this minion can damage NPCs by touching them. Returns false by default. Note that this will only be used if this projectile is considered a pet.
+		/// </summary>
+		/// <returns></returns>
 		public virtual bool MinionContactDamage()
 		{
 			return false;
 		}
 
+		/// <summary>
+		/// Allows you to determine whether this projectile can hit the given NPC. Return true to allow hitting the target, return false to block this projectile from hitting the target, and return null to use the vanilla code for whether the target can be hit. Returns null by default.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <returns></returns>
 		public virtual bool? CanHitNPC(NPC target)
 		{
 			return null;
 		}
 
+		/// <summary>
+		/// Allows you to modify the damage, knockback, etc., that this projectile does to an NPC.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="knockback"></param>
+		/// <param name="crit"></param>
+		/// <param name="hitDirection"></param>
 		public virtual void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to create special effects when this projectile hits an NPC (for example, inflicting debuffs).
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="knockback"></param>
+		/// <param name="crit"></param>
 		public virtual void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to determine whether this projectile can hit the given opponent player. Return false to block this projectile from hitting the target. Returns true by default.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <returns></returns>
 		public virtual bool CanHitPvp(Player target)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to modify the damage, etc., that this projectile does to an opponent player.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="crit"></param>
 		public virtual void ModifyHitPvp(Player target, ref int damage, ref bool crit)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to create special effects when this projectile hits an opponent player.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="crit"></param>
 		public virtual void OnHitPvp(Player target, int damage, bool crit)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to determine whether this hostile projectile can hit the given player. Return false to block this projectile from hitting the target. Returns true by default.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <returns></returns>
 		public virtual bool CanHitPlayer(Player target)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to modify the damage, etc., that this hostile projectile does to a player.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="crit"></param>
 		public virtual void ModifyHitPlayer(Player target, ref int damage, ref bool crit)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to create special effects when this hostile projectile hits a player.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="damage"></param>
+		/// <param name="crit"></param>
 		public virtual void OnHitPlayer(Player target, int damage, bool crit)
 		{
 		}
 
+		/// <summary>
+		/// Allows you to use custom collision detection between this projectile and a player or NPC that this projectile can damage. Useful for things like diagonal lasers, projectiles that leave a trail behind them, etc.
+		/// </summary>
+		/// <param name="projHitbox"></param>
+		/// <param name="targetHitbox"></param>
+		/// <returns></returns>
 		public virtual bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
 		{
 			return null;
 		}
 
+		/// <summary>
+		/// Allows you to determine the color and transparency in which this projectile is drawn. Return null to use the default color (normally light and buff color). Returns null by default.
+		/// </summary>
+		/// <param name="lightColor"></param>
+		/// <returns></returns>
 		public virtual Color? GetAlpha(Color lightColor)
 		{
 			return null;
 		}
 
+		/// <summary>
+		/// Allows you to draw things behind this projectile. Returns false to stop the game from drawing extras textures related to the projectile (for example, the chains for grappling hooks), useful if you're manually drawing the extras. Returns true by default.
+		/// </summary>
+		/// <param name="spriteBatch"></param>
+		/// <returns></returns>
 		public virtual bool PreDrawExtras(SpriteBatch spriteBatch)
 		{
 			return true;
 		}
 
+		/// <summary>
+		/// Allows you to draw things behind this projectile, or to modify the way this projectile is drawn. Return false to stop the game from drawing the projectile (useful if you're manually drawing the projectile). Returns true by default.
+		/// </summary>
+		/// <param name="spriteBatch"></param>
+		/// <param name="lightColor"></param>
+		/// <returns></returns>
 		public virtual bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
 			return true;
@@ -246,6 +427,14 @@ namespace Terraria.ModLoader
 		{
 		}
 
+		/// <summary>
+		/// When used in conjunction with "projectile.hide = true", allows you to specify that this projectile should be drawn behind certain elements. Add the index to one and only one of the lists. For example, the Nebula Arcanum projectile draws behind NPCs and tiles.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="drawCacheProjsBehindNPCsAndTiles"></param>
+		/// <param name="drawCacheProjsBehindNPCs"></param>
+		/// <param name="drawCacheProjsBehindProjectiles"></param>
+		/// <param name="drawCacheProjsOverWiresUI"></param>
 		public virtual void DrawBehind(int index, List<int> drawCacheProjsBehindNPCsAndTiles, List<int> drawCacheProjsBehindNPCs, List<int> drawCacheProjsBehindProjectiles, List<int> drawCacheProjsOverWiresUI)
 		{
 		}
