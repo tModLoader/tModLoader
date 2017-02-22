ba6e029634091de24e06a688ac036795140c5291
gorateron <zond1600@student.nhl.nl>
2017-02-22 18:37:50 +0100

    EquipType documentation

--- patches/tModLoader/Terraria.ModLoader/EquipType.cs
+++ patches/tModLoader/Terraria.ModLoader/EquipType.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Terraria.ModLoader
 {
+	/// <summary>
+	/// This is an enum of all the types of equipment that exist. An equipment type is defined as a type or layer of texture that is drawn on the player (for example, body and shield and wings).
+	/// </summary>
 	public enum EquipType
 	{
 		Head,
