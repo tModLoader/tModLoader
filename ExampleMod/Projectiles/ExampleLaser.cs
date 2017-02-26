3027422949711c49421c699140757ed8791465d5
CXUtk <2237367067@qq.com>
2017-02-26 12:44:38 +0800

    Added multiplayer support for Example Laser

--- ExampleMod/Projectiles/ExampleLaser.cs
+++ ExampleMod/Projectiles/ExampleLaser.cs
@@ -11,10 +11,20 @@ namespace ExampleMod.Projectiles
 {
 	public class ExampleLaser : ModProjectile
 	{
-		private const int maxCharge = 50;
-		private Vector2 _targetPos;         //Ending position of the laser beam
-		private int _charge;                //The charge level of the weapon
-		private float _moveDist = 60f;       //The distance charge particle from the player center
+		private const int MAX_CHARGE = 50;
+		private const float MOVE_DISTANCE = 60f;       //The distance charge particle from the player center
+
+        public float Distance
+        {
+            get { return projectile.ai[0]; }
+            set { projectile.ai[0] = value; }
+        }
+
+        public float Charge
+        {
+            get { return projectile.localAI[0]; }
+            set { projectile.localAI[0] = value; }
+        }
 
 		public override void SetDefaults()
 		{
@@ -30,11 +40,10 @@ namespace ExampleMod.Projectiles
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			if (_charge == maxCharge)
+			if (Charge == MAX_CHARGE)
 			{
-				Vector2 unit = _targetPos - Main.player[projectile.owner].Center;
-				unit.Normalize();
-				DrawLaser(spriteBatch, Main.projectileTexture[projectile.type], Main.player[projectile.owner].Center, unit, 5, projectile.damage, -1.57f, 1f, 1000f, Color.White, (int)60);
+                Vector2 unit = projectile.velocity;
+				DrawLaser(spriteBatch, Main.projectileTexture[projectile.type], Main.player[projectile.owner].Center, unit, 5, projectile.damage, -1.57f, 1f, 1000f, Color.White, (int)MOVE_DISTANCE);
 			}
 			return false;
 
@@ -49,7 +58,7 @@ namespace ExampleMod.Projectiles
 			float r = unit.ToRotation() + rotation;
 
 			#region Draw laser body
-			for (float i = transDist; i <= _moveDist; i += step)
+			for (float i = transDist; i <= MOVE_DISTANCE; i += step)
 			{
 				Color c = Color.White;
 				origin = start + i * unit;
@@ -65,7 +74,7 @@ namespace ExampleMod.Projectiles
 			#endregion
 
 			#region Draw laser head
-			spriteBatch.Draw(texture, start + (_moveDist + step) * unit - Main.screenPosition,
+			spriteBatch.Draw(texture, start + (MOVE_DISTANCE + step) * unit - Main.screenPosition,
 				new Rectangle(0, 52, 28, 26), Color.White, r, new Vector2(28 / 2, 26 / 2), scale, 0, 0);
 			#endregion
 		}
@@ -75,13 +84,12 @@ namespace ExampleMod.Projectiles
 		/// </summary>
 		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
 		{
-			if (_charge == maxCharge)
+			if (Charge == MAX_CHARGE)
 			{
 				Player p = Main.player[projectile.owner];
-				Vector2 unit = (Main.player[projectile.owner].Center - _targetPos);
-				unit.Normalize();
+                Vector2 unit = projectile.velocity;
 				float point = 0f;
-				if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), p.Center - 95f * unit, p.Center - unit * _moveDist, 22, ref point))
+				if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), p.Center - MOVE_DISTANCE * unit, p.Center - unit * MOVE_DISTANCE, 22, ref point))
 				{
 					return true;
 				}
@@ -111,21 +119,23 @@ namespace ExampleMod.Projectiles
 			{
 				Vector2 diff = mousePos - player.Center;
 				diff.Normalize();
-				projectile.position = player.Center + diff * _moveDist;
-				projectile.timeLeft = 2;
-				int dir = projectile.position.X > player.position.X ? 1 : -1;
-				player.ChangeDir(dir);
-				player.heldProj = projectile.whoAmI;
-				player.itemTime = 2;
-				player.itemAnimation = 2;
-				player.itemRotation = (float)Math.Atan2(diff.Y * dir, diff.X * dir);
-				#endregion
+                projectile.velocity = diff;
+                projectile.netUpdate = true;
 			}
+            projectile.position = player.Center + projectile.velocity * MOVE_DISTANCE;
+            projectile.timeLeft = 2;
+            int dir = projectile.position.X > player.position.X ? 1 : -1;
+            player.ChangeDir(dir);
+            player.heldProj = projectile.whoAmI;
+            player.itemTime = 2;
+            player.itemAnimation = 2;
+            player.itemRotation = (float)Math.Atan2(projectile.velocity.Y * dir, 
+                projectile.velocity.X * dir);
+            #endregion
 
-
-			#region Charging process
-			// Kill the projectile if the player stops channeling
-			if (!player.channel)
+            #region Charging process
+            // Kill the projectile if the player stops channeling
+            if (!player.channel)
 			{
 				projectile.Kill();
 			}
@@ -135,22 +145,21 @@ namespace ExampleMod.Projectiles
 				{
 					projectile.Kill();
 				}
-				Vector2 offset = mousePos - player.Center;
-				offset.Normalize();
-				offset *= _moveDist - 20;
-				Vector2 dustPos = player.Center + offset - new Vector2(10, 10);
-				if (_charge < maxCharge)
+                Vector2 offset = projectile.velocity;
+				offset *= MOVE_DISTANCE - 20;
+				Vector2 pos = player.Center + offset - new Vector2(10, 10);
+				if (Charge < MAX_CHARGE)
 				{
-					_charge++;
+					Charge++;
 				}
-				int chargeFact = _charge / 20;
+                int chargeFact = (int)(Charge / 20f);
 				Vector2 dustVelocity = Vector2.UnitX * 18f;
 				dustVelocity = dustVelocity.RotatedBy(projectile.rotation - 1.57f, default(Vector2));
 				Vector2 spawnPos = projectile.Center + dustVelocity;
 				for (int k = 0; k < chargeFact + 1; k++)
 				{
 					Vector2 spawn = spawnPos + ((float)Main.rand.NextDouble() * 6.28f).ToRotationVector2() * (12f - (chargeFact * 2));
-					Dust dust = Main.dust[Dust.NewDust(dustPos, 20, 20, 226, projectile.velocity.X / 2f,
+					Dust dust = Main.dust[Dust.NewDust(pos, 20, 20, 226, projectile.velocity.X / 2f,
 						projectile.velocity.Y / 2f, 0, default(Color), 1f)];
 					dust.velocity = Vector2.Normalize(spawnPos - spawn) * 1.5f * (10f - chargeFact * 2f) / 10f;
 					dust.noGravity = true;
@@ -161,36 +170,33 @@ namespace ExampleMod.Projectiles
 
 
 			#region Set laser tail position and dusts
-			if (_charge < maxCharge) return;
+			if (Charge < MAX_CHARGE) return;
 			Vector2 start = player.Center;
-			Vector2 unit = (player.Center - mousePos);
+            Vector2 unit = projectile.velocity;
 			unit.Normalize();
 			unit *= -1;
-			for (_moveDist = 95f; _moveDist <= 2200; _moveDist += 5)
+			for (Distance = MOVE_DISTANCE; Distance <= 2200; Distance += 5)
 			{
-				start = player.Center + unit * _moveDist;
+				start = player.Center + unit * Distance;
 				if (!Collision.CanHit(player.Center, 1, 1, start, 1, 1))
 				{
-					_moveDist -= 5f;
+                    Distance -= 5;
 					break;
 				}
 			}
-			_targetPos = player.Center + unit *  _moveDist;
 
+            Vector2 dustPos = player.Center + projectile.velocity * Distance;
 			//Imported dust code from source because I'm lazy
 			for (int i = 0; i < 2; ++i)
 			{
 				float num1 = projectile.velocity.ToRotation() + (Main.rand.Next(2) == 1 ? -1.0f : 1.0f) * 1.57f;
 				float num2 = (float)(Main.rand.NextDouble() * 0.8f + 1.0f);
 				Vector2 dustVel = new Vector2((float)Math.Cos(num1) * num2, (float)Math.Sin(num1) * num2);
-				Dust dust = Main.dust[Dust.NewDust(_targetPos, 0, 0, 226, dustVel.X, dustVel.Y, 0, new Color(), 1f)];
+				Dust dust = Main.dust[Dust.NewDust(dustPos, 0, 0, 226, dustVel.X, dustVel.Y, 0, new Color(), 1f)];
 				dust.noGravity = true;
 				dust.scale = 1.2f;
 				// At this part, I was messing with the dusts going across the laser beam very fast, but only really works properly horizontally now
-				unit = Main.MouseWorld - Main.player[projectile.owner].Center;
-				unit.Normalize();
-				unit *= (projectile.width + projectile.height) / 2f;
-				dust = Main.dust[Dust.NewDust(Main.player[projectile.owner].Center + unit * (5), 0,0, 226, unit.X, unit.Y, 0, new Color(), 1f)];
+				dust = Main.dust[Dust.NewDust(Main.player[projectile.owner].Center + unit * 5f, 0,0, 226, unit.X, unit.Y, 0, new Color(), 1f)];
 				dust.fadeIn = 0f;
 				dust.noGravity = true;
 				dust.scale = 0.88f;
@@ -198,11 +204,11 @@ namespace ExampleMod.Projectiles
 			if (Main.rand.Next(5) == 0)
 			{
 				Vector2 offset = projectile.velocity.RotatedBy(1.57f, new Vector2()) * ((float)Main.rand.NextDouble() - 0.5f) * projectile.width;
-				Dust dust = Main.dust[Dust.NewDust(_targetPos + offset - Vector2.One * 4f, 8, 8, 31, 0.0f, 0.0f, 100, new Color(), 1.5f)];
+				Dust dust = Main.dust[Dust.NewDust(dustPos + offset - Vector2.One * 4f, 8, 8, 31, 0.0f, 0.0f, 100, new Color(), 1.5f)];
 				dust.velocity = dust.velocity * 0.5f;
 				dust.velocity.Y = -Math.Abs(dust.velocity.Y);
 
-			    unit = _targetPos - Main.player[projectile.owner].Center;
+			    unit = dustPos - Main.player[projectile.owner].Center;
 				unit.Normalize();
 				dust = Main.dust[Dust.NewDust(Main.player[projectile.owner].Center + 55 * unit, 8, 8, 31, 0.0f, 0.0f, 100, new Color(), 1.5f)];
 				dust.velocity = dust.velocity * 0.5f;
@@ -219,8 +225,7 @@ namespace ExampleMod.Projectiles
 		public override void CutTiles()
 		{
 			DelegateMethods.tilecut_0 = TileCuttingContext.AttackProjectile;
-			Vector2 unit = (Main.player[projectile.owner].Center - _targetPos);
-			unit.Normalize();
+            Vector2 unit = projectile.velocity;
 			Utils.PlotTileLine(projectile.Center, projectile.Center - unit * 5f, (float)(projectile.width + 16) * projectile.scale, new Utils.PerLinePoint(DelegateMethods.CutTiles));
 		}
 	}
