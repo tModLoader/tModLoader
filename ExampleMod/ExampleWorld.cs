e8b37211de8f3ef377558766897a1fc15c30b0e0
JavidPack <javidpack@gmail.com>
2017-03-04 00:40:55 -0700

    Example usage of WorldGen.PlaceTile (Trap spawn).

--- ExampleMod/ExampleWorld.cs
+++ ExampleMod/ExampleWorld.cs
@@ -118,6 +118,32 @@ namespace ExampleMod
 				}));
 			}
 
+			int TrapsIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Traps"));
+			if (TrapsIndex != -1)
+			{
+				tasks.Insert(TrapsIndex + 1, new PassLegacy("Example Mod Traps", delegate (GenerationProgress progress)
+				{
+					progress.Message = "Example Mod Traps";
+					// Computers are fast, so WorldGen code sometimes looks stupid.
+					// Here, we want to place a bunch of tiles in the world, so we just repeat until success. It might be useful to keep track of attempts and check for attemps > maxattempts so you don't have infinite loops. 
+					// The WorldGen.PlaceTile method returns a bool, but it is useless. Instead, we check the tile after calling it and if it is the desired tile, we know we succeeded.
+					for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
+					{
+						bool placeSuccessful = false;
+						Tile tile;
+						int tileToPlace = mod.TileType<Tiles.ExampleCutTileTile>();
+						while (!placeSuccessful)
+						{
+							int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+							int y = WorldGen.genRand.Next(0, Main.maxTilesY);
+							WorldGen.PlaceTile(x,y,	tileToPlace);
+							tile = Main.tile[x, y];
+							placeSuccessful = tile.active() && tile.type == tileToPlace;
+						}
+					}
+				}));
+			}
+
 			int LivingTreesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Living Trees"));
 			if (LivingTreesIndex != -1)
 			{
