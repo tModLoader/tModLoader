58587b2e9ecd71498170cdd662be4eb72fe1ebae
Trivaxy <itsmeomar2003@yahoo.com>
2017-03-29 00:12:19 +0200

    Add object overload for ErrorLogger.Log (#151)
    * Added overload for Log
    
    This overload allows the user to log objects, it simply retrieves methods, constructors, fields and properties in them.
    (Fields and properties have their name AND value get retrieved)
    
    * fixed tabs, hopefully
    
    * now also fetches private members
    
    * Reflection now optional, default is ToString().

--- patches/tModLoader/Terraria.ModLoader/ErrorLogger.cs
+++ patches/tModLoader/Terraria.ModLoader/ErrorLogger.cs
@@ -3,6 +3,7 @@ using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
 using Terraria.ModLoader.IO;
+using System.Reflection;
 
 namespace Terraria.ModLoader
 {
@@ -210,6 +211,49 @@ namespace Terraria.ModLoader
 			using (StreamWriter writer = File.AppendText(LogPath + Path.DirectorySeparatorChar + "Logs.txt"))
 			{
 				writer.WriteLine(message);
+			}
+		}
+		
+		/// <summary>
+                /// Allows you to log an object for your own testing purposes. The message will be added to the Logs.txt file in the Logs folder. 
+                /// </summary>
+                /// <param name="param">The object to be logged.</param>
+		/// <param name="alternateOutput">If true, the object's data will be manually retrieved and logged. If false, the object's ToString method is logged.</param>
+		public static void Log(object param, bool alternateOutput = false)
+		{
+			Directory.CreateDirectory(LogPath);
+			using (StreamWriter writer = File.AppendText(LogPath + Path.DirectorySeparatorChar + "Logs.txt"))
+			{
+				if (!alternateOutput)
+				{
+					writer.WriteLine(param.ToString());
+				}
+				else
+				{
+					writer.WriteLine("Object type: " + param.GetType());
+					foreach (PropertyInfo property in param.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+					{
+						writer.Write("PROPERTY " + property.Name + " = " + property.GetValue(param, null) + "\n");
+					}
+
+					foreach (FieldInfo field in param.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+					{
+						writer.Write("FIELD " + field.Name + " = " + (field.GetValue(param).ToString() != "" ? field.GetValue(param) : "(Field value not found)") + "\n");
+					}
+
+					foreach (MethodInfo method in param.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+					{
+						writer.Write("METHOD " + method.Name + "\n");
+					}
+
+					int temp = 0;
+
+					foreach (ConstructorInfo constructor in param.GetType().GetConstructors(BindingFlags.Public | BindingFlags.NonPublic))
+					{
+						temp++;
+						writer.Write("CONSTRUCTOR " + temp + " : " + constructor.Name + "\n");
+					}
+				}
 			}
 		}
 
