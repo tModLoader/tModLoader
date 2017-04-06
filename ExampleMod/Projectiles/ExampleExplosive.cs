cb3b8de78508650435c3ed81b3625c991fcfa7ef
JavidPack <javidpack@gmail.com>
2017-04-05 18:59:57 -0600

    Some code comments for exampleMod.

--- ExampleMod/Projectiles/ExampleExplosive.cs
+++ ExampleMod/Projectiles/ExampleExplosive.cs
@@ -14,22 +14,23 @@ namespace ExampleMod.Projectiles
 		public override void SetDefaults()
 		{
 			projectile.name = "Example Explosive";
+			// while the sprite is actually bigger than 15x15, we use 15x15 since it lets the projectile clip into tiles as it bounces. It looks better.
 			projectile.width = 15;
 			projectile.height = 15;
-			//projectile.aiStyle = 16;
 			projectile.friendly = true;
 			projectile.penetrate = -1;
-			// sticky only? how? projectile.tileCollide = false;
 
-			drawOffsetX = 5; // TODO document why projectile.
+			// 5 second fuse.
+			projectile.timeLeft = 300;
+
+			// These 2 help the projectile hitbox be centered on the projectile sprite.
+			drawOffsetX = 5;
 			drawOriginOffsetY = 5;
-
-			projectile.timeLeft = 300; // 5 second timer.
 		}
 
 		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
 		{
-			// Vanilla explisions do less damage to Eater of Worlds for some reason, so we will too.
+			// Vanilla explisions do less damage to Eater of Worlds in expert mode, so we will too.
 			if (Main.expertMode)
 			{
 				if (target.type >= NPCID.EaterofWorldsHead && target.type <= NPCID.EaterofWorldsTail)
@@ -41,16 +42,20 @@ namespace ExampleMod.Projectiles
 
 		public override bool OnTileCollide(Vector2 oldVelocity)
 		{
+			// Die immediately if ai[1] isn't 0 (We set this to 1 for the 5 extra explosives we spawn in Kill)
 			if (projectile.ai[1] != 0)
 			{
 				return true;
 			}
+			// OnTileCollide can trigger quite quickly, so using soundDelay helps prevent the sound from overlapping too much.
 			if (projectile.soundDelay == 0)
 			{
+				// We use WithVolume since the sound is a bit too loud, and WithPitchVariance to give the sound some random pitch variance.
 				Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/BananaImpact").WithVolume(.7f).WithPitchVariance(.5f));
 			}
 			projectile.soundDelay = 10;
 
+			// This code makes the projectile very bouncy.
 			if (projectile.velocity.X != oldVelocity.X && Math.Abs(oldVelocity.X) > 1f)
 			{
 				projectile.velocity.X = oldVelocity.X * -0.9f;
@@ -62,28 +67,26 @@ namespace ExampleMod.Projectiles
 			return false;
 		}
 
-
 		public override void AI()
 		{
 			if (projectile.owner == Main.myPlayer && projectile.timeLeft <= 3)
 			{
 				projectile.tileCollide = false;
-				//projectile.ai[1] = 0f;
+				// Set to transparant. This projectile technically lives as  transparant for about 3 frames
 				projectile.alpha = 255;
-				//if (projectile.type == 29 || projectile.type == 470 || projectile.type == 637)
-				{
-					projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
-					projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
-					projectile.width = 250;
-					projectile.height = 250;
-					projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
-					projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
-					projectile.damage = 250;
-					projectile.knockBack = 10f;
-				}
+				// change the hitbox size, centered about the original projectile center. This makes the projectile damage enemies during the explosion.
+				projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+				projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+				projectile.width = 250;
+				projectile.height = 250;
+				projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+				projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+				projectile.damage = 250;
+				projectile.knockBack = 10f;
 			}
 			else
 			{
+				// Smoke and fuse dust spawn.
 				if (Main.rand.Next(2) == 0)
 				{
 					int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 1f);
@@ -98,14 +101,14 @@ namespace ExampleMod.Projectiles
 				}
 			}
 			projectile.ai[0] += 1f;
-
 			if (projectile.ai[0] > 5f)
 			{
 				projectile.ai[0] = 10f;
+				// Roll speed dampening.
 				if (projectile.velocity.Y == 0f && projectile.velocity.X != 0f)
 				{
 					projectile.velocity.X = projectile.velocity.X * 0.97f;
-					if (projectile.type == 29 || projectile.type == 470 || projectile.type == 637)
+					//if (projectile.type == 29 || projectile.type == 470 || projectile.type == 637)
 					{
 						projectile.velocity.X = projectile.velocity.X * 0.99f;
 					}
@@ -117,43 +120,32 @@ namespace ExampleMod.Projectiles
 				}
 				projectile.velocity.Y = projectile.velocity.Y + 0.2f;
 			}
+			// Rotation increased by velocity.X 
 			projectile.rotation += projectile.velocity.X * 0.1f;
-			if(projectile.velocity.X * 0.1f > 0)
-			{
-				projectile.rotation += Math.Abs(projectile.velocity.Y * 0.01f);
-			}
-			else
-			{
-				projectile.rotation -= Math.Abs(projectile.velocity.Y * 0.01f);
-			}
 			return;
 		}
 
 		public override void Kill(int timeLeft)
 		{
+			// If we are the original projectile, spawn the 5 child projectiles
 			if (projectile.ai[1] == 0)
 			{
 				for (int i = 0; i < 5; i++)
 				{
+					// Random upward vector.
 					Vector2 vel = new Vector2(Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-10, -8));
 					Projectile.NewProjectile(projectile.Center, vel, projectile.type, projectile.damage, projectile.knockBack, projectile.owner, 0, 1);
 				}
 			}
-			Main.PlaySound(SoundID.Item14, projectile.position);
-			if (projectile.type == 29)
-			{
-				projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
-				projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
-				projectile.width = 200;
-				projectile.height = 200;
-				projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
-				projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
-			}
+			// Play explosion sound
+			Main.PlaySound(SoundID.Item15, projectile.position);
+			// Smoke Dust spawn
 			for (int i = 0; i < 50; i++)
 			{
 				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 2f);
 				Main.dust[dustIndex].velocity *= 1.4f;
 			}
+			// Fire Dust spawn
 			for (int i = 0; i < 80; i++)
 			{
 				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 3f);
@@ -162,6 +154,7 @@ namespace ExampleMod.Projectiles
 				dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 2f);
 				Main.dust[dustIndex].velocity *= 3f;
 			}
+			// Large Smoke Gore spawn
 			for (int g = 0; g < 2; g++)
 			{
 				int goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
@@ -181,14 +174,13 @@ namespace ExampleMod.Projectiles
 				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X - 1.5f;
 				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y - 1.5f;
 			}
+			// reset size to normal width and height.
 			projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
 			projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
 			projectile.width = 10;
 			projectile.height = 10;
 			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
 			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
-
-			// 29 470 637
 
 			// TODO, tmodloader helper method
 			{
