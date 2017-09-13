025c50fcbebae45357f46dd455182e645d230dd3
Jofairden <danielzondervan.nl@gmail.com>
2017-09-14 00:09:50 +0200

    Added example damage type, Mundane bow & accessory

---
+++ ExampleMod/Items/ExampleDamageClass/ExampleDamagePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.ExampleDamageClass
+{
+	// This class stores necessary player info for our custom damage class, such as damage multipliers and additions to knockback and crit.
+	public class ExampleDamagePlayer : ModPlayer
+	{
+		public static ExampleDamagePlayer ModPlayer(Player player)
+		{
+			return player.GetModPlayer<ExampleDamagePlayer>();
+		}
+
+		// Vanilla only really has damage multipliers in code
+		// And crit and knockback is usually just added to
+		// As a modder, you could make separate variables for multipliers and simple addition bonuses
+		public float exampleDamage = 1f;
+		public float exampleKnockback = 0f;
+		public int exampleCrit = 0;
+
+		public override void ResetEffects()
+		{
+			ResetVariables();
+		}
+
+		public override void UpdateDead()
+		{
+			ResetVariables();
+		}
+
+		private void ResetVariables()
+		{
+			exampleDamage = 1f;
+			exampleKnockback = 0f;
+			exampleCrit = 0;
+		}
+	}
+}
