3bb5a5eb3901ee3df4e425d668a538b5f8e285e8
Jofairden <danielzondervan.nl@gmail.com>
2017-11-20 19:54:02 +0100

    Update ExampleLaserWeapon (closes #211)

--- ExampleMod/Items/Weapons/ExampleLaserWeapon.cs
+++ ExampleMod/Items/Weapons/ExampleLaserWeapon.cs
@@ -8,7 +8,7 @@ namespace ExampleMod.Items.Weapons
 	{
 		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("Shoot a laser beam that can eliminate everything...");
+			Tooltip.SetDefault("Shoot a laser beam that can eliminate anything...");
 		}
 
 		public override void SetDefaults()
@@ -16,7 +16,7 @@ namespace ExampleMod.Items.Weapons
 			item.damage = 40;
 			item.noMelee = true;
 			item.magic = true;
-			item.channel = true;                            //Channel so that you can held the weapon [Important]
+			item.channel = true; //Channel so that you can held the weapon [Important]
 			item.mana = 5;
 			item.rare = 5;
 			item.width = 28;
@@ -24,10 +24,10 @@ namespace ExampleMod.Items.Weapons
 			item.useTime = 20;
 			item.UseSound = SoundID.Item13;
 			item.useStyle = 5;
-			item.shootSpeed = 14f;				//Speed is not important here
-			item.useAnimation = 20;                         
+			item.shootSpeed = 14f;
+			item.useAnimation = 20;
 			item.shoot = mod.ProjectileType("ExampleLaser");
-			item.value = Item.sellPrice(0, 3, 0, 0);
+			item.value = Item.sellPrice(silver: 3);
 		}
 
 		public override void AddRecipes()
