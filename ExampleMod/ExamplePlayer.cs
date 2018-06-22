af7650d1bd6b3cfb112f8318f288de6d6f6e0b55
JavidPack <javidpack@gmail.com>
2018-06-21 18:43:47 -0600

    SimpleModPlayer to teach ModPlayer basics. Also multiple equip slots for 1 accessory.

--- ExampleMod/ExamplePlayer.cs
+++ ExampleMod/ExamplePlayer.cs
@@ -13,6 +13,8 @@ using Terraria.GameInput;
 
 namespace ExampleMod
 {
+	// ModPlayer classes provide a way to attach data to Players and act on that data. ExamplePlayer has a lot of functionality related to 
+	// several effects and items in ExampleMod. See SimpleModPlayer for a very simple example of how ModPlayer classes work.
 	public class ExamplePlayer : ModPlayer
 	{
 		private const int saveVersion = 0;
