ab57fffa00209fa04c99ef2efdb2c4492e67dd30
Jofairden <danielzondervan.nl@gmail.com>
2018-10-06 13:19:04 +0200

    Made ErrorLogger forward to new Logging system

--- patches/tModLoader/Terraria.ModLoader/ErrorLogger.cs
+++ patches/tModLoader/Terraria.ModLoader/ErrorLogger.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace Terraria.ModLoader
 {
@@ -8,14 +10,14 @@ namespace Terraria.ModLoader
 	/// NOTE: This class is deprecated. Use <see cref="Terraria.ModLoader.Logging"/> instead (see ExampleMod for example)
 	/// This class consists of functions that write error messages to text files for you to read. It also lets you write logs to text files.
 	/// </summary>
-	[Obsolete("This class is deprecated. Use Terraria.ModLoader.Logging instead (see ExampleMod for example)", true)]
+	[Obsolete("This class is deprecated. Use Terraria.ModLoader.Logging instead (see ExampleMod for example)", false)]
 	public static class ErrorLogger
 	{
 		/// <summary>
 		/// NOTE: Deprecated. Use <see cref="Terraria.ModLoader.Logging.LogDir"/> instead
 		/// The file path to which logs are written and stored.
 		/// </summary>
-		[Obsolete("Please use Terraria.ModLoader.Logging.LogDir instead")]
+		[Obsolete("Please use Terraria.ModLoader.Logging.LogDir instead", false)]
 		public static readonly string LogPath = Logging.LogDir;
 
 		private static Object logExceptionLock = new Object();
@@ -25,16 +27,18 @@ namespace Terraria.ModLoader
 		/// NOTE: Deprecated. Please use your own ILog instead, see ExampleMod for an example
 		/// You can use this method for your own testing purposes. The message will be added to the Logs.txt file in the Logs folder.
 		/// </summary>
-		[Obsolete("Please use your own ILog instead, see ExampleMod for an example")]
+		[Obsolete("Please use your own ILog instead, see ExampleMod for an example", false)]
 		public static void Log(string message)
 		{
-			lock (logLock)
+			string callerName = GetCallerName();
+			Mod callerMod = ModLoader.GetMod(callerName);
+			if (callerMod == null)
 			{
-				Directory.CreateDirectory(LogPath);
-				using (StreamWriter writer = File.AppendText(LogPath + Path.DirectorySeparatorChar + "Logs.txt"))
-				{
-					writer.WriteLine(message);
-				}
+				Logging.tML.WarnFormat("Tried to forward ErrorLogger.Log for mod {0} but failed (mod not found!)", callerName);
+			}
+			else
+			{
+				callerMod.Logger.Info(message);
 			}
 		}
 
@@ -44,53 +48,64 @@ namespace Terraria.ModLoader
 		/// </summary>
 		/// <param name="param">The object to be logged.</param>
 		/// <param name="alternateOutput">If true, the object's data will be manually retrieved and logged. If false, the object's ToString method is logged.</param>
-		[Obsolete("Please use your own ILog instead, see ExampleMod for an example")]
+		[Obsolete("Please use your own ILog instead, see ExampleMod for an example", false)]
 		public static void Log(object param, bool alternateOutput = false)
 		{
-			lock (logLock)
+			string getParamString()
 			{
-				Directory.CreateDirectory(LogPath);
-				using (StreamWriter writer = File.AppendText(LogPath + Path.DirectorySeparatorChar + "Logs.txt"))
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine("Object type: " + param.GetType());
+				foreach (PropertyInfo property in param.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
 				{
-					if (!alternateOutput)
-					{
-						writer.WriteLine(param.ToString());
-					}
-					else
-					{
-						writer.WriteLine("Object type: " + param.GetType());
-						foreach (PropertyInfo property in param.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-						{
-							writer.Write("PROPERTY " + property.Name + " = " + property.GetValue(param, null) + "\n");
-						}
-
-						foreach (FieldInfo field in param.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-						{
-							writer.Write("FIELD " + field.Name + " = " + (field.GetValue(param).ToString() != "" ? field.GetValue(param) : "(Field value not found)") + "\n");
-						}
-
-						foreach (MethodInfo method in param.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-						{
-							writer.Write("METHOD " + method.Name + "\n");
-						}
-
-						int temp = 0;
-
-						foreach (ConstructorInfo constructor in param.GetType().GetConstructors(BindingFlags.Public | BindingFlags.NonPublic))
-						{
-							temp++;
-							writer.Write("CONSTRUCTOR " + temp + " : " + constructor.Name + "\n");
-						}
-					}
+					sb.AppendLine("PROPERTY " + property.Name + " = " + property.GetValue(param, null) + "\n");
 				}
+
+				foreach (FieldInfo field in param.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+				{
+					sb.AppendLine("FIELD " + field.Name + " = " + (field.GetValue(param).ToString() != "" ? field.GetValue(param) : "(Field value not found)") + "\n");
+				}
+
+				foreach (MethodInfo method in param.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+				{
+					sb.AppendLine("METHOD " + method.Name + "\n");
+				}
+
+				int temp = 0;
+
+				foreach (ConstructorInfo constructor in param.GetType().GetConstructors(BindingFlags.Public | BindingFlags.NonPublic))
+				{
+					temp++;
+					sb.AppendLine("CONSTRUCTOR " + temp + " : " + constructor.Name + "\n");
+				}
+
+				return sb.ToString();
 			}
+
+			string callerName = GetCallerName();
+			Mod callerMod = ModLoader.GetMod(callerName);
+			if (callerMod == null)
+			{
+				Logging.tML.WarnFormat("Tried to forward ErrorLogger.Log for mod {0} but failed (mod not found!)", callerName);
+			}
+			else
+			{
+				callerMod.Logger.Info(!alternateOutput ? param.ToString() : getParamString());
+			}
+		}
+
+		private static string GetCallerName()
+		{
+			StackTrace stackTrace = new StackTrace();
+			Assembly asm = stackTrace.GetFrame(2).GetMethod().DeclaringType.Assembly;
+			string name = asm.GetName().Name;
+			return name.Contains("_") ? name.Split('_')[0] : name;
 		}
 
 		/// <summary>
 		/// NOTE: Deprecated.
 		/// Deletes all log files.
 		/// </summary>
-		[Obsolete("Please ue Terraria.ModLoader.Logging instead")]
+		[Obsolete("Please ue Terraria.ModLoader.Logging instead", false)]
 		public static void ClearLogs()
 		{
 			lock (logLock)
