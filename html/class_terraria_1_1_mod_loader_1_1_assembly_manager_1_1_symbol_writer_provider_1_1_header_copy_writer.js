17f4976fe78c9fc81d74c0d7be7dcca0630da43c
Travis CI <travis@travis-ci.org>
2018-11-17 09:57:29 +0000

    Deploy code docs to GitHub Pages Travis build: 730
    Commit: 02847a33debe6e08075e36ad76d9221864f74793

--- html/class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.js
+++ html/class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.js
@@ -2,8 +2,8 @@ var class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provide
 [
     [ "HeaderCopyWriter", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#ad91ffef3a09da9170588bacbf44a460b", null ],
     [ "Dispose", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#a7bb3947ac58d5d5b45657658269ebe66", null ],
-    [ "GetDebugHeader", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#a7a9520a343fdce85880ad0497ea197d8", null ],
-    [ "Write", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#a6760b0fc5775172aaa617558d0e54986", null ],
-    [ "Write", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#aa12b55c47de8289b6e42d13bb843f7ad", null ],
+    [ "GetDebugHeader", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#a4c38d90043cced97275c40f8be4764d9", null ],
+    [ "GetReaderProvider", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#ad9d30ae6fd6f60d5cda550521bc75e81", null ],
+    [ "Write", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#ad70ce4e5361b10d30d43bee1ae9b1fdc", null ],
     [ "module", "class_terraria_1_1_mod_loader_1_1_assembly_manager_1_1_symbol_writer_provider_1_1_header_copy_writer.html#a74f54f42aed60cf34d520fcde90d059f", null ]
 ];
