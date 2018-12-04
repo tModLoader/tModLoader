6a5fa28e0a56a411fb2f9672e53ca0f724228e60
JavidPack <javidpack@gmail.com>
2018-12-04 00:22:33 -0700

    ExampleOre and ExampleBar

--- ExampleMod/ExampleWorld.cs
+++ ExampleMod/ExampleWorld.cs
@@ -157,12 +157,12 @@ namespace ExampleMod
 			// "6E-05" is "scientific notation". It simply means 0.00006 but in some ways is easier to read.
 			for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 6E-05); k++) {
 				// The inside of this for loop corresponds to one single splotch of our Ore.
-				// First, we randomly choose any coorinate in the world by choosing a random x and y value.
+				// First, we randomly choose any coordinate in the world by choosing a random x and y value.
 				int x = WorldGen.genRand.Next(0, Main.maxTilesX);
 				int y = WorldGen.genRand.Next((int)WorldGen.worldSurfaceLow, Main.maxTilesY); // WorldGen.worldSurfaceLow is actually the highest surface tile. In practice you might want to use WorldGen.rockLayer or other WorldGen values.
 
 				// Then, we call WorldGen.TileRunner with random "strength" and random "steps", as well as the Tile we wish to place. Feel free to experiment with strength and step to see the shape they generate.
-				WorldGen.TileRunner(x, y, (double)WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 6), mod.TileType("ExampleBlock"), false, 0f, 0f, false, true);
+				WorldGen.TileRunner(x, y, (double)WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 6), mod.TileType("ExampleOre"), false, 0f, 0f, false, true);
 
 				// Alternately, we could check the tile already present in the coordinate we are interested. Wrapping WorldGen.TileRunner in the following condition would make the ore only generate in Snow.
 				// Tile tile = Framing.GetTileSafely(x, y);
