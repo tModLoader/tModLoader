90f9ee239fc66475a274d4fbe0e1bd5978801eb6
JavidPack <javidpack@gmail.com>
2019-01-07 22:02:04 -0700

    Cleaner ExampleTrap Code.

--- ExampleMod/Tiles/ExampleTrap.cs
+++ ExampleMod/Tiles/ExampleTrap.cs
@@ -50,6 +50,7 @@ namespace ExampleMod.Tiles
 			return true;
 		}
 
+		// PlaceInWorld is needed to facilitate styles and alternates since this tile doesn't use a TileObjectData. Placing left and right based on player direction is usually done in the TileObjectData, but the specifics of that didn't work for how we want this tile to work. 
 		public override void PlaceInWorld(int i, int j, Item item)
 		{
 			int style = Main.LocalPlayer.HeldItem.placeStyle;
@@ -65,40 +66,14 @@ namespace ExampleMod.Tiles
 			}
 		}
 
+		// This progression matches vanilla tiles, you don't have to follow it if you don't want. Some vanilla traps don't have 6 states, only 4. This can be implemented with different logic in Slope. Making 8 directions is also easily done in a similar manner.
+		static int[] frameXCycle = { 2, 3, 4, 5, 1, 0 };
 		// We can use the Slope method to override what happens when this tile is hammered.
 		public override bool Slope(int i, int j)
 		{
 			Tile tile = Main.tile[i, j];
-			int nextFrameX = 0;
 			int style = tile.frameY / 18;
-			switch (style)
-			{
-				case 0:
-				case 1:
-					switch (tile.frameX / 18) // This progression matches vanilla tiles, you don't have to follow it if you don't want.
-					{
-						case 0:
-							nextFrameX = 2;
-							break;
-						case 1:
-							nextFrameX = 3;
-							break;
-						case 2:
-							nextFrameX = 4;
-							break;
-						case 3:
-							nextFrameX = 5;
-							break;
-						case 4:
-							nextFrameX = 1;
-							break;
-						case 5:
-							nextFrameX = 0;
-							break;
-					}
-					break;
-					// Some vanilla traps don't have 6 states, only 4. That logic can go here.
-			}
+			int nextFrameX = frameXCycle[tile.frameX / 18];
 			tile.frameX = (short)(nextFrameX * 18);
 			if (Main.netMode == 1)
 			{
@@ -111,45 +86,30 @@ namespace ExampleMod.Tiles
 		{
 			Tile tile = Main.tile[i, j];
 			int style = tile.frameY / 18;
-			Vector2 spawnPosition = Vector2.Zero;
-			float speedX = 0f;
-			float speedY = 0f;
-			int projectileType = 0;
-			int damage = 0;
-			switch (style)
+			Vector2 spawnPosition;
+			// This logic here corresponds to the orientation of the sprites in the spritesheet, change it if your tile is different in design.
+			int horizontalDirection = (tile.frameX == 0) ? -1 : ((tile.frameX == 18) ? 1 : 0);
+			int verticalDirection = (tile.frameX < 36) ? 0 : ((tile.frameX < 72) ? -1 : 1);
+			// Each trap style within this Tile shoots different projectiles.
+			if (style == 0)
 			{
-				// Each trap style within this Tile shoots different projectiles.
-				case 0:
-					if (Wiring.CheckMech(i, j, 10)) // 200 ticks is the cooldown. 200 is the dart/flame cooldown. Spear is 90, spiky ball is 300
-					{
-						int horizontalDirection = (tile.frameX == 0) ? -1 : ((tile.frameX == 18) ? 1 : 0);
-						int verticalDirection = (tile.frameX < 36) ? 0 : ((tile.frameX < 72) ? -1 : 1);
-						spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + verticalDirection * 0); //spawnPosition = new Vector2(i * 16 + 8 + 10 * horizontalDirection, j * 16 + 9 + verticalDirection * 9);
-						float speed = 12f;
-						projectileType = ProjectileID.IchorBullet;
-						damage = 20;
-						speedX = horizontalDirection * speed;
-						speedY = verticalDirection * speed;
-					}
-					break;
-				case 1:
-					if (Wiring.CheckMech(i, j, 200)) // 200 ticks is the cooldown. 200 is the dart/flame cooldown. Spear is 90, spiky ball is 300
-					{
-						int horizontalDirection = (tile.frameX == 0) ? -1 : ((tile.frameX == 18) ? 1 : 0);
-						int verticalDirection = (tile.frameX < 36) ? 0 : ((tile.frameX < 72) ? -1 : 1);
-						spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + verticalDirection * 0); //spawnPosition = new Vector2(i * 16 + 8 + 10 * horizontalDirection, j * 16 + 9 + verticalDirection * 9);
-						float speed = 12f;
-						projectileType = ProjectileID.ChlorophyteBullet;
-						damage = 40;
-						speedX = horizontalDirection * speed;
-						speedY = verticalDirection * speed;
-					}
-					break;
+				// Wiring.CheckMech checks if the wiring cooldown has been reached. Put a longer number here for less frequent projectile spawns. 200 is the dart/flame cooldown. Spear is 90, spiky ball is 300
+				if (Wiring.CheckMech(i, j, 60))
+				{
+					spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + 0 * verticalDirection); // The extra numbers here help center the projectile spawn position if you need to.
+
+					// In reality you should be spawning projectiles that are both hostile and friendly to do damage to both players and NPC.
+					// Make sure to change velocity, projectile, damage, and knockback.
+					Projectile.NewProjectile(spawnPosition, new Vector2(horizontalDirection, verticalDirection) * 6f, ProjectileID.IchorBullet, 20, 2f, Main.myPlayer);
+				}
 			}
-			if (projectileType != 0)
+			else if (style == 1)
 			{
-				// In reality you should be spawning projectiles that are both hostile and friendly to do damage to both players and NPC.
-				Projectile.NewProjectile((int)spawnPosition.X, (int)spawnPosition.Y, speedX, speedY, projectileType, damage, 2f, Main.myPlayer, 0f, 0f);
+				if (Wiring.CheckMech(i, j, 200)) // A longer cooldown for ChlorophyteBullet trap.
+				{
+					spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + 0 * verticalDirection); // The extra numbers here help center the projectile spawn position.
+					Projectile.NewProjectile(spawnPosition, new Vector2(horizontalDirection, verticalDirection) * 8f, ProjectileID.ChlorophyteBullet, 40, 2f, Main.myPlayer);
+				}
 			}
 		}
 	}
