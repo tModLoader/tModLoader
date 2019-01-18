345b7d92238816072335b464f65635e920c11020
Chicken-Bones <mehvids@gmail.com>
2019-01-18 23:06:56 +1000

    Avoid breaking change when overloading TagCompound.Set

--- patches/tModLoader/Terraria.ModLoader.IO/TagCompound.cs
+++ patches/tModLoader/Terraria.ModLoader.IO/TagCompound.cs
@@ -27,6 +27,9 @@ namespace Terraria.ModLoader.IO
 			}
 		}
 
+		// adding default param to Set overload is a breaking changefor now.
+		public void Set(string key, object value) => Set(key, value, false);
+		
 		//if value is null, calls RemoveTag, also performs type checking
 		public void Set(string key, object value, bool replace = false) {
 			if (value == null) {
