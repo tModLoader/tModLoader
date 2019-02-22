61c5f40f6e31619ace59d9678c3ddc6dc8b0ea11
JavidPack <javidpack@gmail.com>
2019-02-21 19:49:41 -0700

    Delayed loading/unloading texture proof of concept. Vanilla NPC add up to 100 MB.

---
+++ patches/tModLoader/Terraria.ModLoader/CustomContentManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria.ModLoader
+{
+	// See https://blogs.msdn.microsoft.com/shawnhar/2007/03/09/contentmanager-readasset/ for ideas on implementing a custom ContentManager class.
+	class CustomContentManager : ContentManager
+	{
+		public CustomContentManager(IServiceProvider serviceProvider)
+			: base(serviceProvider) { }
+
+		public override T Load<T>(string assetName) {
+			return ReadAsset<T>(assetName, null); // We could implement the regular approach and just add a check for Texture.IsDisposed to automatically detect disposed Texture2D, keeping all logic here.
+			// Or we can use Main.OurLoad to track things, if we want to unload TexturePack assets. Lots to consider. 
+		}
+	}
+}
