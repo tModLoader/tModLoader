6cff43598a2b2fcd4ced1ea6bcff1160694787a6
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2019-04-22 13:02:45 -0400

    Traveling Merchant Addition to ExampleMod

--- ExampleMod/ExampleWorld.cs
+++ ExampleMod/ExampleWorld.cs
@@ -27,35 +27,51 @@ namespace ExampleMod
 		public int VolcanoCountdown;
 		public int VolcanoCooldown = DefaultVolcanoCooldown;
 		public int VolcanoTremorTime;
-		public static int exampleTiles;
+        public static int[] travelerItems; // An array of ints to get item types
+        public static bool spawnChance;
+        public static double randomTime;
+        public static int exampleTiles;
 
 		public override void Initialize() {
 			downedAbomination = false;
 			downedPuritySpirit = false;
 			VolcanoCountdown = 0;
 			VolcanoTremorTime = 0;
-		}
+            travelerItems = new int[3]; // Initializing to set 3 random items (Starts off with zeros)
+            for (int i = 0; i < travelerItems.Length; i++)
+            {
+                travelerItems[i] = 0;
+            }
+            spawnChance = false;
+            randomTime = 0;
+        }
 
-		public override TagCompound Save() {
-			var downed = new List<string>();
-			if (downedAbomination) {
-				downed.Add("abomination");
-			}
+        public override TagCompound Save()
+        {
+            var downed = new List<string>();
+            if (downedAbomination)
+            {
+                downed.Add("abomination");
+            }
 
-			if (downedPuritySpirit) {
-				downed.Add("puritySpirit");
-			}
+            if (downedPuritySpirit)
+            {
+                downed.Add("puritySpirit");
+            }
 
-			return new TagCompound {
-				["downed"] = downed,
-			};
-		}
+            return new TagCompound
+            {
+                ["downed"] = downed,
+                ["travelerShopItems"] = travelerItems
+            };
+        }
 
-		public override void Load(TagCompound tag) {
+        public override void Load(TagCompound tag) {
 			var downed = tag.GetList<string>("downed");
 			downedAbomination = downed.Contains("abomination");
 			downedPuritySpirit = downed.Contains("puritySpirit");
-		}
+            travelerItems = tag.GetIntArray("travelerShopItems");
+        }
 
 		public override void LoadLegacy(BinaryReader reader) {
 			int loadVersion = reader.ReadInt32();
@@ -401,7 +417,96 @@ namespace ExampleMod
 			exampleTiles = tileCounts[mod.TileType("ExampleBlock")];
 		}
 
-		public override void PostUpdate() {
+        public override void PreUpdate()
+        {
+            bool travelerExists = false; // Find an Explorer
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                if (Main.npc[i].type != mod.NPCType("ExampleTravelingMerchant") || !Main.npc[i].active) continue; // We dont care about NPCs that arent the Example Traveling Merchant or not active
+                travelerExists = true; // We found one, so set it to true
+            }
+            
+            if (Main.dayTime && randomTime == 0) // Each day we will set a new SpawnChance and TimeVariation
+            {
+                if (Main.rand.Next(4) == 0) // 25% chance
+                {
+                    spawnChance = true;
+                }
+                else
+                {
+                    spawnChance = false;
+                }
+                randomTime = RandomizeSpawnTime(5400, 8100); // minTime = 6:00am, maxTime = 7:30am
+            }
+
+            if (travelerExists)
+            {
+                if (!Main.dayTime || Main.time > 48600.0) // If its becomes night or the time passes your desired point, despawn the traveler
+                {
+                    for (int i = 0; i < Main.maxNPCs; i++)
+                    {
+                        if (Main.npc[i].type != mod.NPCType("ExampleTravelingMerchant")) continue;
+                        if (Main.npc[i].active)
+                        {
+                            // Here we despawn the NPC and send a message stating that the NPC has despawned
+                            string fullName = Main.npc[i].FullName;
+                            if (Main.netMode == NetmodeID.SinglePlayer) Main.NewText(Main.npc[i].FullName + " has departed!", 50, 125, 255);
+                            else NetMessage.BroadcastChatMessage(NetworkText.FromLiteral(Main.npc[i].FullName + " has departed!"), new Color(50, 125, 255));
+                            Main.npc[i].active = false;
+                            Main.npc[i].netSkip = -1;
+                            Main.npc[i].life = 0;
+                            randomTime = 0; // Set to 0 so the reset can happen
+                        }
+                    }
+                }
+            }
+            else if (spawnChance) // If a traveler does not exist, and the spawnchance is successful, spawn a traveler
+            {
+                // Here we can make it so the NPC doesnt spawn at the EXACT same time every time it does spawn
+                if (!Main.fastForwardTime && Main.dayTime && Main.time > randomTime) // Should spawn between 4:30am and 7:30am (Do not spawn if using the Sundial)
+                {
+                    if (!Main.eclipse && (Main.invasionType <= 0 || Main.invasionDelay != 0 || Main.invasionSize <= 0)) // Make sure no events are occuring
+                    {
+                        // Here we are randomizing our chances of items. With this we can interchange between 2 items
+                        // This code line is here and not with the spawnChance and randomTime because this is saved data. We dont want it to randomize when we join back in a world
+                        travelerItems = RandomizeTravelerShop();
+
+                        int newExplorer = NPC.NewNPC(Main.spawnTileX * 16, Main.spawnTileY * 16, mod.NPCType("ExampleTravelingMerchant"), 1); // Spawning at the world spawn
+                        Main.npc[newExplorer].homeless = true;
+                        if (Main.spawnTileX < WorldGen.bestX) Main.npc[newExplorer].direction = 1;
+                        else if (Main.spawnTileX > WorldGen.bestX) Main.npc[newExplorer].direction = -1;
+                        Main.npc[newExplorer].netUpdate = true;
+
+                        // Annouce that the traveler has spawned in!
+                        string fullName = Main.npc[newExplorer].FullName;
+                        if (Main.netMode == NetmodeID.SinglePlayer) Main.NewText(Language.GetTextValue("Announcement.HasArrived", fullName), 50, 125, 255);
+                        else NetMessage.BroadcastChatMessage(NetworkText.FromKey("Announcement.HasArrived", Main.npc[newExplorer].GetFullNetName()), new Color(50, 125, 255));
+                    }
+                }
+            }
+            else if (!Main.dayTime)
+            {
+                // In this case the traveler does NOT exist and the spanChance is false, meaning they never spawn. So we have to reset the timeVar to attempt a new chance
+                randomTime = 0; // Set to 0 so the reset can happen
+            }
+        }
+
+        public static double RandomizeSpawnTime(double minTime, double maxTime)
+        {
+            return (maxTime - minTime) * Main.rand.NextDouble() + minTime;
+        }
+
+        public int[] RandomizeTravelerShop()
+        {
+            int[] randomizedInts = new int[3];
+            randomizedInts[0] = Main.rand.Next(2); // Randomize between 2 items
+            randomizedInts[1] = Main.rand.Next(3); // 3 items
+            randomizedInts[2] = Main.rand.Next(4); // 4 items
+
+            return randomizedInts;
+        }
+
+        public override void PostUpdate() {
 			if (Main.dayTime && VolcanoCountdown == 0) {
 				if (VolcanoCooldown > 0) {
 					VolcanoCooldown--;
---
+++ ExampleMod/NPCs/ExampleTravelingMerchant.cs
@@ -0,0 +1,195 @@
+﻿using ExampleMod.Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ExampleMod.NPCs
+{
+    [AutoloadHead]
+    class ExampleTravelingMerchant : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Example Traveler");
+            Main.npcFrameCount[npc.type] = 25;
+            NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+            NPCID.Sets.AttackFrameCount[npc.type] = 4;
+            NPCID.Sets.DangerDetectRange[npc.type] = 700;
+            NPCID.Sets.AttackType[npc.type] = 0;
+            NPCID.Sets.AttackTime[npc.type] = 90;
+            NPCID.Sets.AttackAverageChance[npc.type] = 30;
+            NPCID.Sets.HatOffsetY[npc.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.townNPC = true; // This will be changed once the NPC is spawned
+            npc.friendly = true;
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 7;
+            npc.damage = 10;
+            npc.defense = 15;
+            npc.lifeMax = 250;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.knockBackResist = 0.5f;
+            animationType = NPCID.Guide;
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            int num = npc.life > 0 ? 1 : 5;
+            for (int k = 0; k < num; k++)
+            {
+                Dust.NewDust(npc.position, npc.width, npc.height, mod.DustType("Sparkle"));
+            }
+        }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs, int money)
+        {
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                if (Main.npc[i].type != mod.NPCType("ExamplePerson")) continue; // If the npc is not the Example Person, move onto the next npc
+                return true; // If one Example Person exists, Example traveling Merchant can spawn
+            }
+            return false;
+        }
+
+        public override string TownNPCName()
+        {
+            switch (WorldGen.genRand.Next(4))
+            {
+                case 0:
+                    return "Someone";
+                case 1:
+                    return "Somebody";
+                case 2:
+                    return "Blockster";
+                default:
+                    return "Colorful";
+            }
+        }
+
+        public override string GetChat()
+        {
+            int partyGirl = NPC.FindFirstNPC(NPCID.PartyGirl);
+            if (partyGirl >= 0 && Main.rand.NextBool(4))
+            {
+                return "Can you please tell " + Main.npc[partyGirl].GivenName + " to stop decorating my cousin's house with colors?";
+            }
+            switch (Main.rand.Next(4))
+            {
+                case 0:
+                    return "Sometimes my cousin feels like they're different from everyone else here.";
+                case 1:
+                    return "What's your favorite color? My cousin's favorite colors are white and black.";
+                case 2:
+                    {
+                        // Main.npcChatCornerItem shows a single item in the corner, like the Angler Quest chat.
+                        Main.npcChatCornerItem = ItemID.HiveBackpack;
+                        return $"Hey, if you find a [i:{ItemID.HiveBackpack}], my cousin can upgrade it for you.";
+                    }
+                default:
+                    return "What? My cousin doesn't have any arms or legs? Oh, don't be ridiculous!";
+            }
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = Language.GetTextValue("LegacyInterface.28");
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref bool shop)
+        {
+            if (firstButton)
+            {
+                shop = true;
+            }
+        }
+
+        public override void SetupShop(Chest shop, ref int nextSlot)
+        {
+            // Here we get the random ints that were declared in ExampleWorld, and based on them make randomzied shop items
+            // Remember that we saved the int array so even if we revisit the shop or leave the world, the items will remain the same until the traveler despawns
+            // You can of course add to the Main.rand.Next(#) number to increase the amount of items that can randomly generate
+            if (ExampleWorld.travelerItems[0] == 0)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleItem"));
+            }
+            else if (ExampleWorld.travelerItems[0] == 1)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("EquipMaterial"));
+            }
+            nextSlot++;
+
+            if (ExampleWorld.travelerItems[1] == 0)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("BossItem"));
+            }
+            else if (ExampleWorld.travelerItems[1] == 1)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleWorkbench"));
+            }
+            else if (ExampleWorld.travelerItems[1] == 2)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleChair"));
+            }
+            nextSlot++;
+
+            if (ExampleWorld.travelerItems[2] == 0)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleDoor"));
+            }
+            else if (ExampleWorld.travelerItems[2] == 1)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleBed"));
+            }
+            else if (ExampleWorld.travelerItems[2] == 2)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleChest"));
+            }
+            else if (ExampleWorld.travelerItems[2] == 3)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("ExamplePickaxe"));
+            }
+            nextSlot++;
+        }
+
+        public override void AI()
+        {
+            npc.homeless = true; // Make sure it stays homeless
+            base.AI();
+        }
+
+        public override void NPCLoot()
+        {
+            Item.NewItem(npc.getRect(), mod.ItemType<Items.Armor.ExampleCostume>());
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 20;
+            knockback = 4f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 30;
+            randExtraCooldown = 30;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = mod.ProjectileType("SparklingBall");
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 12f;
+            randomOffset = 2f;
+        }
+    }
+}
