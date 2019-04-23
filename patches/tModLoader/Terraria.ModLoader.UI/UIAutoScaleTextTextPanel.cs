81199d7a1f285e1aa3ecd2c2e87442a927ad1e1f
JavidPack <javidpack@gmail.com>
2019-04-23 15:41:06 -0600

    Support chat tags in UITextPanel and DrawBorderStringBig

--- patches/tModLoader/Terraria.ModLoader.UI/UIAutoScaleTextTextPanel.cs
+++ patches/tModLoader/Terraria.ModLoader.UI/UIAutoScaleTextTextPanel.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Graphics;
 using Terraria.GameContent.UI.Elements;
+using Terraria.UI.Chat;
 
 namespace Terraria.ModLoader.UI
 {
@@ -49,8 +50,7 @@ namespace Terraria.ModLoader.UI
 
 				TextScaleMax = textScaleMax;
 				DynamicSpriteFont dynamicSpriteFont = large ? Main.fontDeathText : Main.fontMouseText;
-				//Vector2 textSize = new Vector2(dynamicSpriteFont.MeasureString(text.ToString()).X, large ? 32f : 16f) * TextScaleMax;
-				Vector2 textSize = dynamicSpriteFont.MeasureString(text.ToString()) * TextScaleMax;
+				Vector2 textSize = ChatManager.GetStringSize(dynamicSpriteFont, text.ToString(), new Vector2(TextScaleMax));
 
 				innerDimensionsRectangle.Inflate(-4, 0);
 
@@ -59,7 +59,7 @@ namespace Terraria.ModLoader.UI
 				if (textSize.X > availableSpace.X || textSize.Y > availableSpace.Y) {
 					float scale = (textSize.X / availableSpace.X > textSize.Y / availableSpace.Y) ? availableSpace.X / textSize.X : availableSpace.Y / textSize.Y;
 					TextScale = scale;
-					textSize = dynamicSpriteFont.MeasureString(text.ToString()) * TextScale;
+					textSize = ChatManager.GetStringSize(dynamicSpriteFont, text.ToString(), new Vector2(TextScale));
 				}
 				else {
 					TextScale = TextScaleMax;
@@ -74,7 +74,7 @@ namespace Terraria.ModLoader.UI
 				// offset off left corner for centering
 				drawOffsets = new Vector2[textStrings.Length];
 				for (int i = 0; i < textStrings.Length; i++) {
-					Vector2 size = dynamicSpriteFont.MeasureString(textStrings[i]) * TextScale;
+					Vector2 size = ChatManager.GetStringSize(dynamicSpriteFont, textStrings[i], new Vector2(TextScale));
 					//size.Y = size.Y * 0.9f;
 					float x = (innerDimensionsRectangle.Width - size.X) * 0.5f;
 					float y = (-textStrings.Length * size.Y * 0.5f) + i * size.Y + innerDimensionsRectangle.Height * 0.5f;
