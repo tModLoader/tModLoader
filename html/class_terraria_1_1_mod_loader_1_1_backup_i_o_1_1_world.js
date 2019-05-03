6c928fdb71d6a0398f0aa17b1286fbefbd03d203
Travis CI <travis@travis-ci.org>
2019-05-03 18:59:20 +0000

    Deploy code docs to GitHub Pages Travis build: 990
    Commit: 1954c590650d1466f353007bae41f8eb590c556a

--- html/class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.js
+++ html/class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.js
@@ -1,8 +1,7 @@
 var class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world =
 [
-    [ "GetTime", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a7b90282d59055a7ffd59269f0d59c9a3", null ],
-    [ "IsArchiveOlder", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a64e9627d74474a9054e72db2e9ec79ca", null ],
-    [ "TodaysBackup", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a9c91295cbe789a4e1c4dc563ba03c0df", null ],
+    [ "WriteArchive", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a3fd538c7547872090827a8b549651baa", null ],
+    [ "_lock", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a0ea82f5c5e4b30c7d2a9cbd3c2fa4b18", null ],
     [ "WorldBackupDir", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a6a053334bd28fec706a1adf209e83939", null ],
     [ "WorldDir", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#aec291cedf9025d618d24714bd43f712b", null ],
     [ "WorldName", "class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a9155500afcb6a566b4c6af0c748a6126", null ],
--- html/search/variables_0.js
+++ html/search/variables_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['_5fcurrentrequest',['_currentRequest',['../class_terraria_1_1_mod_loader_1_1_mod_net.html#ac7306ad061e3b075f6035877dddd1083',1,'Terraria::ModLoader::ModNet']]],
   ['_5fenabledmods',['_enabledMods',['../class_terraria_1_1_mod_loader_1_1_mod_loader.html#a90cd23cb256c959b05d8a943f71fff5a',1,'Terraria::ModLoader::ModLoader']]],
+  ['_5flock',['_lock',['../class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_world.html#a0ea82f5c5e4b30c7d2a9cbd3c2fa4b18',1,'Terraria.ModLoader.BackupIO.World._lock()'],['../class_terraria_1_1_mod_loader_1_1_backup_i_o_1_1_player.html#ac9fd9ee6f6774278ce779c243b67338f',1,'Terraria.ModLoader.BackupIO.Player._lock()']]],
   ['_5foriginalx',['_originalX',['../class_terraria_1_1_mod_loader_1_1_drawable_tooltip_line.html#ade82e21849572b0d564a84074e3d26f7',1,'Terraria::ModLoader::DrawableTooltipLine']]],
   ['_5foriginaly',['_originalY',['../class_terraria_1_1_mod_loader_1_1_drawable_tooltip_line.html#a6963c50aebf00653d02d96ad70016b64',1,'Terraria::ModLoader::DrawableTooltipLine']]]
 ];
