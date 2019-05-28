277d4668833eafba767e50a9d6f690cc5b312d68
JavidPack <javidpack@gmail.com>
2019-05-28 17:51:10 -0600

    UIModInfo fix

--- patches/tModLoader/Terraria.ModLoader.UI/UIModInfo.cs
+++ patches/tModLoader/Terraria.ModLoader.UI/UIModInfo.cs
@@ -227,8 +227,8 @@ namespace Terraria.ModLoader.UI
 				if (_localMod != null) {
 					_uIElement.AddOrRemoveChild(_deleteButton, ModLoader.Mods.All(x => x.Name != _localMod.Name));
 					_uIElement.Append(_extractButton);
-					Recalculate();
 				}
+				Recalculate();
 				_modInfo.RemoveChild(_loaderElement);
 				_ready = false;
 			}
