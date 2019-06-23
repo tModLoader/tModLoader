aaecf0eed4c406e4f8619e9597bd6a189717a2ee
DarkLight66 <wence_409@yahoo.com>
2019-06-23 10:19:10 -0400

    Fixed ExampleMagicMissile mana usage

--- ExampleMod/Items/Weapons/ExampleMagicMissile.cs
+++ ExampleMod/Items/Weapons/ExampleMagicMissile.cs
@@ -32,28 +32,17 @@ namespace ExampleMod.Items.Weapons
 		}
 
 		// This item's mana usage changes through the day, peaking at 1.5x mana usage at noon, and 0.5x mana usage at midnight.
+		// Thanks to chikenbones for the help in the calculations
 		public override void ModifyManaCost(Player player, ref float reduce, ref float mult) {
-			float currentTime = (float)Main.time;
+			double currentTime = Main.time;
 			// The time at which it changes from day to night and vice versa.
-			int maxTime = Main.dayTime ? 54000 : 32400;
-			// The time at which it is 12pm or 12am, which isn't exactly half the values above.
-			int time12 = Main.dayTime ? 28800 : 18000;
-			// If the time it after 12, recalculate the current time to account for the length of the second half of the day instead of the first.
-			if (currentTime > time12) {
-				currentTime -= time12;
-				time12 = maxTime - time12;
-				currentTime += time12;
-			}
-			// If it is night time, add twice the duration of the current half of the night.
-			if (!Main.dayTime) {
-				currentTime += time12 * 2;
-			}
-			// We divide the current time by the current half of the day or night's time, then by 2.
-			// The result would gradually go from 0 at 4:30am, 0.5 at 12pm, 1 at 7:30pm and 1.5f at 12 am.
-			float timeMult = currentTime / time12 / 2;
-			// Then we multiply the result above by PI, and calculate the sine of that, then multiply it by 0.5f and add 1.
-			// The result would go from 1 at 4:30am, 1.5 at 12pm, 1 at 7:30pm and 0.5f at 12 am, in a sine wave pattern.
-			timeMult = 1 + (float)Math.Sin(timeMult * Math.PI) * 0.5f;
+			double maxTime = Main.dayTime ? Main.dayLength : Main.nightLength;
+			// More mana during day, less at night
+			int direction = Main.dayTime ? 1 : -1;
+			// Sine goes from 0 to 1 to 0 over a period of pi, so we match that to the length of the day/night.
+			float timeMult = (float)Math.Sin(currentTime / maxTime * Math.PI);
+			// Then we multiply by direction so it goes between 1 and -1 through the entire day, then multiply by 0.5 and add 1 to make it go between 1.5 and 0.5.
+			timeMult = 1 + timeMult * direction * 0.5f;
 			// Last, we multiply the current mana cost multiplier of the item by our multiplier.
 			mult *= timeMult;
 		}
