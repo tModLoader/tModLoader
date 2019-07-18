ce22b2511a8708b9c8cd4a440ff71579b4e504f0
Jofairden <jofairden@gmail.com>
2019-07-18 11:53:09 +0200

    Separate MysteryTileFrame and MysteryTileInfo into own class

---
+++ patches/tModLoader/Terraria.ModLoader.Default/MysteryTileFrame.cs
@@ -0,0 +1,30 @@
+﻿namespace Terraria.ModLoader.Default
+{
+	internal class MysteryTileFrame
+	{
+		private short frameX;
+		private short frameY;
+
+		public short FrameX => frameX;
+		public short FrameY => frameY;
+
+		public int FrameID {
+			get {
+				return frameY * (short.MaxValue + 1) + frameX;
+			}
+			set {
+				frameX = (short)(value % (short.MaxValue + 1));
+				frameY = (short)(value / (short.MaxValue + 1));
+			}
+		}
+
+		public MysteryTileFrame(int value) {
+			FrameID = value;
+		}
+
+		public MysteryTileFrame(short frameX, short frameY) {
+			this.frameX = frameX;
+			this.frameY = frameY;
+		}
+	}
+}
