051b3d1d33c659968cd5ad6cfdb75365af155423
JavidPack <javidpack@gmail.com>
2019-08-16 00:01:09 -0600

    Correct steam api paths, mac and linux hash check, MessageBox removed from non windows

--- patches/tModLoader/Terraria.ModLoader.Engine/GoGVerifier.cs
+++ patches/tModLoader/Terraria.ModLoader.Engine/GoGVerifier.cs
@@ -26,20 +26,23 @@ namespace Terraria.ModLoader.Engine
 			return retval;
 		}
 
-		private static void GetPlatformCheckInfo(out string steamAPIpath, out byte[] vanillaGoGhash) {
+		private static void GetPlatformCheckInfo(out string steamAPIpath, out byte[] steamAPIhash, out byte[] vanillaGoGhash) {
 			// I'd like it if you couldn't just open tML in a hex editor and replace the hash
 			// but whether I make it byte array or string doesn't change that. I could obfuscate the hash a bit
 			// but I can't really make it more effort than just nuking the steam check with dnspy (which could be done before we added GoG support)
 			if (Platform.IsWindows) {
 				steamAPIpath = "steam_api.dll";
+				steamAPIhash = ToByteArray("7B857C897BC69313E4936DC3DCCE5193");
 				vanillaGoGhash = ToByteArray("81ef4a9337ae6d7a1698fdeb3137580d");
 			}
 			else if (Platform.IsOSX) {
-				steamAPIpath = "osx/libCSteamworks";
+				steamAPIpath = "osx/libsteam_api.dylib";
+				steamAPIhash = ToByteArray("4EECD26A0CDF89F90D4FF26ECAD37BE0");
 				vanillaGoGhash = ToByteArray("e8dfb127721edc4ceb32381f41ece7b8");
 			}
 			else if (Platform.IsLinux) {
-				steamAPIpath = "lib/libCSteamworks.so";
+				steamAPIpath = "lib/libsteam_api.so";
+				steamAPIhash = ToByteArray("7B74FD4C207D22DB91B4B649A44467F6");
 				vanillaGoGhash = ToByteArray("942ab061e854c74db3a6b1efe2dc24d0");
 			}
 			else {
@@ -48,22 +51,30 @@ namespace Terraria.ModLoader.Engine
 		}
 
 		private static bool GoGCheck() {
-			GetPlatformCheckInfo(out var steamAPIpath, out var vanillaGoGhash);
+			GetPlatformCheckInfo(out var steamAPIpath, out var steamAPIhash, out var vanillaGoGhash);
 			if (File.Exists(steamAPIpath)) {
-				VerifySteamAPI();
+				VerifySteamAPI(steamAPIpath, steamAPIhash);
 				return false;
 			}
 
 			var vanillaPath = Path.GetFileName(Assembly.GetExecutingAssembly().Location) != "Terraria.exe" ? "Terraria.exe" : "Terraria_v1.3.5.3.exe";
 			if (!File.Exists(vanillaPath)) {
 				Logging.tML.Info("Vanilla Terraria.exe not found.");
-				System.Windows.Forms.MessageBox.Show($"{vanillaPath} not found.\n\nGoG installs must have the unmodified Terraria exe to function.\n\nPlease restore your Terraria install, then install tModLoader using the provided tModLoaderInstaller.jar or by following the README.txt instructions.", "Terraria: Error" + $" ({ModLoader.versionedName})");
+				string message = $"{vanillaPath} not found.\n\nGoG installs must have the unmodified Terraria exe to function.\n\nPlease restore your Terraria install, then install tModLoader using the provided tModLoaderInstaller.jar or by following the README.txt instructions.";
+				Logging.tML.Fatal(message);
+#if WINDOWS
+				System.Windows.Forms.MessageBox.Show(message, "Terraria: Error" + $" ({ModLoader.versionedName})");
+#endif
 				Environment.Exit(1);
 				return false;
 			}
 
 			if (!HashMatchesFile(vanillaGoGhash, vanillaPath)) {
-				System.Windows.Forms.MessageBox.Show($"{vanillaPath} is not the unmodified Terraria executable.\n\nGoG installs must have the unmodified Terraria executable to function.\n\nPlease restore your Terraria install, then install tModLoader using the provided tModLoaderInstaller.jar or by following the README.txt instructions.", "Terraria: Error" + $" ({ModLoader.versionedName})");
+				string message = $"{vanillaPath} is not the unmodified Terraria executable.\n\nGoG installs must have the unmodified Terraria executable to function.\n\nPlease restore your Terraria install, then install tModLoader using the provided tModLoaderInstaller.jar or by following the README.txt instructions.";
+				Logging.tML.Fatal(message);
+#if WINDOWS
+				System.Windows.Forms.MessageBox.Show(message, "Terraria: Error" + $" ({ModLoader.versionedName})");
+#endif
 				Environment.Exit(1);
 				return false;
 			}
@@ -72,14 +83,13 @@ namespace Terraria.ModLoader.Engine
 			return true;
 		}
 
-		private static void VerifySteamAPI() {
-			if (!Platform.IsWindows)
-				return;
-
-			var steamAPIhash = new byte[] { 123, 133, 124, 137, 123, 198, 147, 19, 228, 147, 109, 195, 220, 206, 81, 147 };
-			if (!HashMatchesFile(steamAPIhash, "steam_api.dll")) {
-				Logging.tML.Fatal("Steam API hash mismatch, assumed pirated");
-				System.Windows.Forms.MessageBox.Show("Steam API hash mismatch, assumed pirated.\n\ntModLoader requires a legitimate Terraria install to work.", "Terraria: Error" + $" ({ModLoader.versionedName})");
+		private static void VerifySteamAPI(string steamAPIpath, byte[] steamAPIhash) {
+			if (!HashMatchesFile(steamAPIhash, steamAPIpath)) {
+				string message = "Steam API hash mismatch, assumed pirated.\n\ntModLoader requires a legitimate Terraria install to work.";
+				Logging.tML.Fatal(message);
+#if WINDOWS
+				System.Windows.Forms.MessageBox.Show(message, "Terraria: Error" + $" ({ModLoader.versionedName})");
+#endif
 				Process.Start(@"https://terraria.org");
 				Environment.Exit(1);
 			}
