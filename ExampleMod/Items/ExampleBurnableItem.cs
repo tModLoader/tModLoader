d32185885ed80afd429469e3b847eb32a1b232bb
GoodPro712 <45357714+GoodPro712@users.noreply.github.com>
2019-11-30 22:44:46 +1100

    Add files via upload

---
+++ ExampleMod/Items/ExampleBurnableItem.cs
@@ -0,0 +1,39 @@
+using ExampleMod.Dusts;
+using ExampleMod.Tiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Items
+{
+	public class ExampleBurnableItem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("This item burns in lava. Test it out!");
+		}
+
+		public override void SetDefaults() {
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 15;
+			item.useAnimation = 15;
+			item.useStyle = 1;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemType<ExampleItem>(), 10);
+			recipe.AddTile(TileType<ExampleWorkbench>());
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+		
+		public override bool CanBurnInLava() {
+			return true;
+		}
+	}
+}
