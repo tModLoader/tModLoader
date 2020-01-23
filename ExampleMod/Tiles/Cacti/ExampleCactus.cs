bc1156e782519402630a8146bad8be14cc8940b1
Charlie <45357714+GoodPro712@users.noreply.github.com>
2020-01-23 21:24:03 +1100

    ExampleCactus

---
+++ ExampleMod/Tiles/Cacti/ExampleCactus.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Tiles.Cacti
+{
+	public class ExampleCactus : ModCactus
+	{
+		public override Texture2D GetTexture() => ModContent.GetTexture("ExampleMod/Tiles/Cacti/ExampleCactus");
+	}
+}
