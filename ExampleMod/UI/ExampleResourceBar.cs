ce2df084a7468b61dad64b8d035f67fcfc9d2e9c
JavidPack <javidpack@gmail.com>
2020-02-06 01:21:12 -0700

    Fix ExampleResourceBar drawing

--- ExampleMod/UI/ExampleResourceBar.cs
+++ ExampleMod/UI/ExampleResourceBar.cs
@@ -10,41 +10,41 @@ namespace ExampleMod.UI
 {
 	internal class ExampleResourceBar : UIState
 	{
-		// For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler to do it while still looking decent.
-		// Once this is all set up make sure to go and do the required stuff for most UI's in the mod class.
+		// For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler approaches while still looking decent.
+		// Once this is all set up make sure to go and do the required stuff for most UI's in the Mod class.
 		private UIText text;
-		private UIPanel backPanel;
-		public UIImage barFrame;
+		private UIElement area;
+		private UIImage barFrame;
 		private Color gradientA;
 		private Color gradientB;
 
 		public override void OnInitialize() {
-			backPanel = new UIPanel(); // Create a backpanel for all the elements to sit on top of.
-			backPanel.Left.Set(-backPanel.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.
-			backPanel.Top.Set(30, 0f); // Placing it just a bit below the top of the screen.
-			backPanel.Width.Set(138, 0f);
-			backPanel.Height.Set(34, 0f);
-			backPanel.BackgroundColor = Color.Transparent; // Make it invisible.
-			backPanel.BorderColor = Color.Transparent; // Hide the black border that things automatically get.
+			// Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
+			// UIElement is invisible and has no padding. You can use a UIPanel if you wish for a background.
+			area = new UIElement(); 
+			area.Left.Set(-area.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.
+			area.Top.Set(30, 0f); // Placing it just a bit below the top of the screen.
+			area.Width.Set(182, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
+			area.Height.Set(60, 0f);
 
 			barFrame = new UIImage(GetTexture("ExampleMod/UI/ExampleResourceFrame"));
-			barFrame.Left.Set(-12, 0f);
-			barFrame.Top.Set(-12, 0f);
+			barFrame.Left.Set(22, 0f);
+			barFrame.Top.Set(0, 0f);
 			barFrame.Width.Set(138, 0f);
 			barFrame.Height.Set(34, 0f);
 
 			text = new UIText("0/0", 0.8f); // text to show stat
 			text.Width.Set(138, 0f);
 			text.Height.Set(34, 0f);
-			text.Top.Set(30, 0f);
-			text.Left.Set(-15, 0f);
+			text.Top.Set(40, 0f);
+			text.Left.Set(0, 0f);
 
 			gradientA = new Color(123, 25, 138); // A dark purple
 			gradientB = new Color(187, 91, 201); // A light purple
 
-			backPanel.Append(text);
-			backPanel.Append(barFrame);
-			Append(backPanel);
+			area.Append(text);
+			area.Append(barFrame);
+			Append(area);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch) {
@@ -62,13 +62,15 @@ namespace ExampleMod.UI
 			// Calculate quotient
 			float quotient = (float)modPlayer.exampleResourceCurrent / modPlayer.exampleResourceMax2; // Creating a quotient that represents the difference of your currentResource vs your maximumResource, resulting in a float of 0-1f.
 			quotient = Utils.Clamp(quotient, 0f, 1f); // Clamping it to 0-1f so it doesn't go over that.
-			Rectangle hitbox = GetInnerDimensions().ToRectangle();
-			// Set the offset for the actual gradient bar, in case you need it somewhere else.
-			hitbox.X += (int)backPanel.Left.Pixels + 10;
-			hitbox.Y += (int)backPanel.Top.Pixels + 2;
-			// Set the bounds of the gradient bar, adjusting it with pixel offsets if needed.
-			hitbox.Width = (int)backPanel.Width.Pixels - 20;
-			hitbox.Height = (int)backPanel.Height.Pixels - 4;
+
+			// Here we get the screen dimensions of the barFrame element, then tweak the resulting rectangle to arrive at a rectangle within the barFrame texture that we will draw the gradient. These values were measured in a drawing program.
+			Rectangle hitbox = barFrame.GetInnerDimensions().ToRectangle();
+			hitbox.X += 12;
+			hitbox.Width -= 24;
+			hitbox.Y += 8;
+			hitbox.Height -= 16;
+
+			// Now, using this hitbox, we draw a gradient by drawing vertical lines while slowly interpolating between the 2 colors.
 			int left = hitbox.Left;
 			int right = hitbox.Right;
 			int steps = (int)((right - left) * quotient);
