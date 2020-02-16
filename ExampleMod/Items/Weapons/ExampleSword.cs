c2867ed7628a14ee47973e4d980fd2a128b7bf1c
Charlie <45357714+GoodPro712@users.noreply.github.com>
2020-02-17 02:23:58 +1100

    Updated ExampleSword comments (#796)
    * Updated ExampleSword comments
    
    * Update ExampleSword.cs
    
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- ExampleMod/Items/Weapons/ExampleSword.cs
+++ ExampleMod/Items/Weapons/ExampleSword.cs
@@ -15,37 +15,47 @@ namespace ExampleMod.Items.Weapons
 		}
 
 		public override void SetDefaults() {
-			item.damage = 50;           //The damage of your weapon
-			item.melee = true;          //Is your weapon a melee weapon?
-			item.width = 40;            //Weapon's texture's width
-			item.height = 40;           //Weapon's texture's height
-			item.useTime = 20;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			item.useAnimation = 20;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
-			item.useStyle = 1;          //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
-			item.knockBack = 6;         //The force of knockback of the weapon. Maximum is 20
-			item.value = Item.buyPrice(gold: 1);           //The value of the weapon
-			item.rare = 2;              //The rarity of the weapon, from -1 to 13
-			item.UseSound = SoundID.Item1;      //The sound when the weapon is using
-			item.autoReuse = true;          //Whether the weapon can use automatically by pressing mousebutton
+			item.damage = 50; // The damage your item deals
+			item.melee = true; // Whether your item is part of the melee class
+			item.width = 40; // The item texture's width
+			item.height = 40; // The item texture's height
+			item.useTime = 20; // The time span of using the weapon. Remember in terraria, 60 frames is a second.
+			item.useAnimation = 20; // The time span of the using animation of the weapon, suggest setting it the same as useTime.
+			item.knockBack = 6; // The force of knockback of the weapon. Maximum is 20
+			item.value = Item.buyPrice(gold: 1); // The value of the weapon in copper coins
+			item.rare = 2; // The rarity of the weapon, from -1 to 13. You can also use ItemRarityID.TheColorRarity
+			item.UseSound = SoundID.Item1; // The sound when the weapon is being used
+			item.autoReuse = true; // Whether the weapon can be used more than once automatically by holding the use button
+			item.crit = 6; // The critical strike chance the weapon has. The player, by default, has 4 critical strike chance
+			
+			//The useStyle of the item. 
+			//Use useStyle 1 for normal swinging or for throwing
+			//use useStyle 2 for an item that drinks such as a potion,
+			//use useStyle 3 to make the sword act like a shortsword
+			//use useStyle 4 for use like a life crystal,
+			//and use useStyle 5 for staffs or guns
+			item.useStyle = 1; // 1 is the useStyle
 		}
 
 		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
+			// ItemType<ExampleItem>() is how to get the ExampleItem item, 10 is the amount of that item you need to craft the recipe
 			recipe.AddIngredient(ItemType<ExampleItem>(), 10);
-			recipe.AddTile(TileType<ExampleWorkbench>());
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			// You can use recipe.AddIngredient(ItemID.TheItemYouWantToUse, the amount of items needed); for a vanilla item.
+			recipe.AddTile(TileType<ExampleWorkbench>()); // Set the crafting tile to ExampleWorkbench
+			recipe.SetResult(this); // Set the result to this item (ExampleSword)
+			recipe.AddRecipe(); // When your done, add the recipe
 		}
 
 		public override void MeleeEffects(Player player, Rectangle hitbox) {
 			if (Main.rand.NextBool(3)) {
-				//Emit dusts when swing the sword
+				//Emit dusts when the sword is swung
 				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustType<Sparkle>());
 			}
 		}
 
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit) {
-			// Add Onfire buff to the NPC for 1 second
+			// Add the Onfire buff to the NPC for 1 second when the weapon hits an NPC
 			// 60 frames = 1 second
 			target.AddBuff(BuffID.OnFire, 60);
 		}
