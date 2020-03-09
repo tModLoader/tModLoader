32a1f13aae0f835e4d03684adc9971bae3e53fd6
aberna01 <45322920+aberna01@users.noreply.github.com>
2020-03-09 13:35:42 -0500

    Add files via upload
    Maybe merge the config or start a Settings folder

---
+++ ExampleMod/Backgrounds/ExampleCloud.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Utilities;
+using static Terraria.Cloud;
+using static Terraria.WorldGen;
+
+namespace ExampleMod
+{
+	class ExampleCloud
+	{
+		public static UnifiedRandom Rand = new UnifiedRandom();
+
+		/// <summary>
+		/// As the name implies, clears all the draw data from any currently active clouds.
+		/// </summary>
+		public static void Reset()
+			=> resetClouds();
+
+		/// <summary>
+		/// This encapsulates all the data needed to draw the clouds.
+		/// </summary>
+		/// <returns>A DrawData object that can be modified or drawn immediately.</returns>
+		public static DrawData CloudToDrawData(Cloud C)
+			=> new DrawData(Main.cloudTexture[C.type], C.position, null, C.cloudColor(Main.bgColor), C.rotation, Vector2.Zero, C.scale, C.spriteDir, 1);
+
+		/// <summary>
+		/// A simple command to let you add a bunch of vanilla clouds.
+		/// </summary>
+		/// <param name="Amount"></param>
+		public static void AddClouds(int Amount)
+		{
+			for(int Index = 0; Index < Amount; Index++)
+			{
+				addCloud();
+			}
+		}
+
+
+		
+
+		/////  BONUS INFORMATION	//////////
+		///
+		///
+		///   .Clouds are stored like entities and other objects in terraria in an array.
+		///   
+		///   .The vanilla limit for clouds in terraria is 200.
+		///   .They have bool fields that when marked false can have their value overwritten.
+		///   .There is also a field bool for a flag of whether or not the cloud is going to be removed this update cycle.
+		///   
+		///   .These concepts are important if you go further in video game design as it's the basic system of iterating
+		///      through limited objects to conserve resources.
+		///
+		///   .That being said, lets make our own clouds.
+		///   .Wind Speed is like the cloud's velocity for an entity.
+
+	}
+}
---
+++ ExampleMod/Backgrounds/ExampleCloudConfig.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader.Config;
+
+namespace ExampleMod
+{
+	class ExampleCloudConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+
+		[Label("Click this to enable custom wind speeds.")]
+		public bool UseCustomWindSpeed;
+
+
+		[Range(-1f, 1f)]
+		[Increment(0.1f)]
+		[Label("Use this to change the directiono the clouds in the sky are moving and the rate at which they move.")]
+		public static float SetWindSpeed;
+
+		[Label("Enable Clouds to take on custom color values below")]
+		public static bool UseCustomCloudColor;
+
+		[Range(0f, 255f)]
+		[Increment(1f)]
+		[Label("Shift the red value of the clouds.")]
+		public static byte RedHueValue;
+
+		[Range(0f, 255f)]
+		[Increment(1f)]
+		[Label("Shift the green value of the clouds.")]
+		public static byte GreenHueValue;
+
+		[Range(0f, 255f)]
+		[Increment(1f)]
+		[Label("Shift the blue value of the clouds.")]
+		public static byte BlueHueValue;
+
+		[Range(0f, 255f)]
+		[Increment(1f)]
+		[Label("Shift the transparency of the clouds.")]
+		public static byte CloudOpacity;
+
+		public static void SetAllCloudColor()
+		{
+			if (UseCustomCloudColor) {
+				for (int Index = 0; Index < Main.cloudLimit; Index++) {
+					Main.cloud[Index].cloudColor(new Color(RedHueValue, GreenHueValue, BlueHueValue, CloudOpacity));
+				}
+			}
+		}
+	}
+
+}
