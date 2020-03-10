91e2d3d16d6432104b9bcec019a73e5d9e4c8401
Panini <7970069+xPanini@users.noreply.github.com>
2020-03-10 17:49:22 -0400

    Stabilize ExampleSign anchors, add search terms (#813)
    - Add a comment containing "ExampleSign" and "ExampleCommandCaller" so it can be searched by either term
    - Adjust sign anchors so they're more stable

--- ExampleMod/Tiles/ExampleCommandSign.cs
+++ ExampleMod/Tiles/ExampleCommandSign.cs
@@ -9,6 +9,7 @@ using Terraria.ObjectData;
 
 namespace ExampleMod.Tiles
 {
+	// This is an example mod for ExampleSign and ExampleCommandCaller combined into one
 	public class ExampleCommandSign : ModTile
 	{
 		public override void SetDefaults()
@@ -24,39 +25,55 @@ namespace ExampleMod.Tiles
 			Main.tileFrameImportant[Type] = true;
 			Main.tileLavaDeath[Type] = true;
 
-			// Allow hanging from ceilings
+			// Use a 2x2 style as our foundation
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+
+			// Allow hanging from ceilings
 			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
 			TileObjectData.newAlternate.StyleHorizontal = true;
+			TileObjectData.newAlternate.AnchorAlternateTiles = new int[] { 124 };
 			TileObjectData.newAlternate.Origin = new Point16(0, 0);
-			TileObjectData.newAlternate.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, 2, 0);
+			TileObjectData.newAlternate.AnchorLeft = AnchorData.Empty;
+			TileObjectData.newAlternate.AnchorRight = AnchorData.Empty;
+			TileObjectData.newAlternate.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidBottom, TileObjectData.newTile.Width, 0);
 			TileObjectData.newAlternate.AnchorBottom = AnchorData.Empty;
 			TileObjectData.addAlternate(1);
 
 			// Allow attaching to a solid object that is to the left of the sign
 			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
 			TileObjectData.newAlternate.StyleHorizontal = true;
+			TileObjectData.newAlternate.AnchorAlternateTiles = new int[] { 124 };
 			TileObjectData.newAlternate.Origin = new Point16(0, 0);
-			TileObjectData.newAlternate.AnchorLeft = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, 2, 0);
+			TileObjectData.newAlternate.AnchorLeft = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide | AnchorType.Tree, TileObjectData.newTile.Width, 0);
 			TileObjectData.newAlternate.AnchorBottom = AnchorData.Empty;
 			TileObjectData.addAlternate(2);
 
 			// Allow attaching to a solid object that is to the right of the sign
 			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
 			TileObjectData.newAlternate.StyleHorizontal = true;
+			TileObjectData.newAlternate.AnchorAlternateTiles = new int[] { 124 };
 			TileObjectData.newAlternate.Origin = new Point16(0, 0);
-			TileObjectData.newAlternate.AnchorRight = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, 2, 0);
+			TileObjectData.newAlternate.AnchorRight = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide | AnchorType.Tree, TileObjectData.newTile.Width, 0);
 			TileObjectData.newAlternate.AnchorBottom = AnchorData.Empty;
 			TileObjectData.addAlternate(3);
 
 			// Allow attaching to a wall behind the sign
 			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
 			TileObjectData.newAlternate.StyleHorizontal = true;
+			TileObjectData.newAlternate.AnchorAlternateTiles = new int[] { 124 };
 			TileObjectData.newAlternate.Origin = new Point16(0, 0);
 			TileObjectData.newAlternate.AnchorWall = true;
 			TileObjectData.newAlternate.AnchorBottom = AnchorData.Empty;
 			TileObjectData.addAlternate(4);
+
+			// Allow attaching sign to the ground
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.StyleHorizontal = true;
+			TileObjectData.newAlternate.AnchorAlternateTiles = new int[] { 124 };
+			TileObjectData.newAlternate.Origin = new Point16(0, 0);
+			TileObjectData.addAlternate(5);
 			TileObjectData.addTile(Type);
+
 			ModTranslation name = CreateMapEntryName();
 			name.SetDefault("Example Command Sign");
 			AddMapEntry(new Color(200, 200, 200), name);
