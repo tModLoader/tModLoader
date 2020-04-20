cf29afc7149bfcf5c83ca3bbb046e94eddc312a3
Charlie <45357714+GoodPro712@users.noreply.github.com>
2020-04-21 00:32:54 +1000

    Sparkling ExampleHerb code

--- ExampleMod/Tiles/ExampleHerb.cs
+++ ExampleMod/Tiles/ExampleHerb.cs
@@ -1,3 +1,5 @@
+using ExampleMod.Items.Placeable;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
@@ -9,76 +11,70 @@ namespace ExampleMod.Tiles
 {
 	public class ExampleHerb : ModTile
 	{
-		public override void SetDefaults() {
+		private static readonly byte pixelsPerStage = 18; //a field for readibilty and to kick out those magic numbers
+
+		public override void SetDefaults()
+		{
 			Main.tileFrameImportant[Type] = true;
 			Main.tileCut[Type] = true;
-			//Main.tileAlch[Type] = true;
 			Main.tileNoFail[Type] = true;
-			//Main.tileLavaDeath[Type] = true;
-			//dustType = -1;
-			//disableSmartCursor = true;
-			//AddMapEntry(new Color(13, 88, 130), "Banner");
-			//TileObjectData.newTile.Width = 1;
-			//TileObjectData.newTile.Height = 1;
-			//TileObjectData.newTile.Origin = Point16.Zero;
-			//TileObjectData.newTile.UsesCustomCanPlace = true;
-			//TileObjectData.newTile.CoordinateHeights = new int[]
-			//{
-			//	20
-			//};
-			//TileObjectData.newTile.CoordinateWidth = 16;
-			//TileObjectData.newTile.CoordinatePadding = 2;
-			//TileObjectData.newTile.DrawYOffset = -1;
-			//TileObjectData.newTile.StyleHorizontal = true;
-			//TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.AlternateTile, TileObjectData.newTile.Width, 0);
-			//TileObjectData.newTile.WaterPlacement = LiquidPlacement.NotAllowed;
-			//TileObjectData.newTile.LavaDeath = true;
-			//TileObjectData.newTile.LavaPlacement = LiquidPlacement.NotAllowed;
-			//TileObjectData.addBaseTile(out TileObjectData.StyleAlch);
+
 			TileObjectData.newTile.CopyFrom(TileObjectData.StyleAlch);
-			TileObjectData.newTile.AnchorValidTiles = new[]
+
+			TileObjectData.newTile.AnchorValidTiles = new int[]
 			{
-				2, //TileID.Grass
-				109, // TileId.HallowedGrass
+				TileID.Grass,
+				TileID.HallowedGrass,
 				TileType<ExampleBlock>()
 			};
-			TileObjectData.newTile.AnchorAlternateTiles = new[]
+
+			TileObjectData.newTile.AnchorAlternateTiles = new int[]
 			{
-				78, //ClayPot
+				TileID.ClayPot,
 				TileID.PlanterBox
 			};
+
 			TileObjectData.addTile(Type);
-			//drop = ItemType()
-		}
-		//public override bool CanPlace(int i, int j)
-		//{
-		//	return base.CanPlace(i, j);
-		//}
-		public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects) {
-			if (i % 2 == 1) {
-				spriteEffects = SpriteEffects.FlipHorizontally;
-			}
 		}
 
-		public override bool Drop(int i, int j) {
-			int stage = Main.tile[i, j].frameX / 18;
-			if (stage == 2) {
-				Item.NewItem(i * 16, j * 16, 0, 0, ItemType<Items.Placeable.ExampleHerbSeeds>());
-			}
+		public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects)
+		{
+			if (i % 2 == 1)
+				spriteEffects = SpriteEffects.FlipHorizontally;
+		}
+
+		public override bool Drop(int i, int j)
+		{
+			Stage stage = GetStage(i, j);
+
+			if (stage == Stage.Grown)
+				Item.NewItem(new Vector2(i, j).ToWorldCoordinates(), ItemType<ExampleHerbSeeds>());
+
 			return false;
 		}
 
-		public override void RandomUpdate(int i, int j) {
-			if (Main.tile[i, j].frameX == 0) {
-				Main.tile[i, j].frameX += 18;
-			}
-			else if (Main.tile[i, j].frameX == 18) {
-				Main.tile[i, j].frameX += 18;
+		public override void RandomUpdate(int i, int j)
+		{
+			Tile tile = Framing.GetTileSafely(i, j);
+			Stage stage = GetStage(i, j);
+
+			if (stage == Stage.Planted || stage == Stage.Growing) {
+				tile.frameX += pixelsPerStage;
+				NetMessage.SendTileSquare(-1, i, j, 1);
 			}
 		}
-		//public override void RightClick(int i, int j)
-		//{
-		//	base.RightClick(i, j);
-		//}
+
+		private Stage GetStage(int i, int j)
+		{
+			Tile tile = Framing.GetTileSafely(i, j); //Always use Framing.GetTileSafely instead of Main.tile as it prevents any errors caused from other mods
+			return (Stage)(tile.frameX / pixelsPerStage);
+		}
+
+		private enum Stage : byte
+		{
+			Planted,
+			Growing,
+			Grown
+		}
 	}
 }
