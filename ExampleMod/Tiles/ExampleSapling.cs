4b951b966f751c420c0352b2b0a98c79246ac08c
Charlie <45357714+GoodPro712@users.noreply.github.com>
2020-04-20 22:53:30 +1000

    Cleaned code and added comments

--- ExampleMod/Tiles/ExampleSapling.cs
+++ ExampleMod/Tiles/ExampleSapling.cs
@@ -13,10 +13,12 @@ namespace ExampleMod.Tiles
 {
 	public class ExampleSapling : ModTile
 	{
-		public override void SetDefaults() {
+		public override void SetDefaults()
+		{
 			Main.tileFrameImportant[Type] = true;
 			Main.tileNoAttach[Type] = true;
 			Main.tileLavaDeath[Type] = true;
+
 			TileObjectData.newTile.Width = 1;
 			TileObjectData.newTile.Height = 2;
 			TileObjectData.newTile.Origin = new Point16(0, 1);
@@ -36,38 +38,46 @@ namespace ExampleMod.Tiles
 			TileObjectData.newSubTile.AnchorValidTiles = new int[] { TileType<ExampleSand>() };
 			TileObjectData.addSubTile(1);
 			TileObjectData.addTile(Type);
-			sapling = true;
+
 			ModTranslation name = CreateMapEntryName();
-			name.SetDefault("Sapling");
+			name.SetDefault("Example Sapling");
 			AddMapEntry(new Color(200, 200, 200), name);
+
+			sapling = true;
 			dustType = DustType<Sparkle>();
 			adjTiles = new int[] { TileID.Saplings };
 		}
 
-		public override void NumDust(int i, int j, bool fail, ref int num) {
-			num = fail ? 1 : 3;
-		}
+		public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 
-		public override void RandomUpdate(int i, int j) {
-			if (WorldGen.genRand.Next(20) == 0) {
-				bool isPlayerNear = WorldGen.PlayerLOS(i, j);
-				Tile tile = Framing.GetTileSafely(i, j);
-				bool success;
+		public override void RandomUpdate(int i, int j)
+		{
+			// A random chance to slow down growth
+			if (WorldGen.genRand.Next(20) == 0)
+			{
+				Tile tile = Framing.GetTileSafely(i, j); // Safely get the tile at the given coordinates
+				bool growSucess; // A bool to see if the tree growing was sucessful.
+
 				// Style 0 is for the ExampleTree sapling, and style 1 is for ExamplePalmTree, so here we check frameX to call the correct method.
+				// Any pixels before 54 on the tilesheet are for ExampleTree while any pixels above it are for ExamplePalmTree
 				if (tile.frameX < 54)
-					success = WorldGen.GrowTree(i, j);
+					growSucess = WorldGen.GrowTree(i, j);
 				else
-					success = WorldGen.GrowPalmTree(i, j);
-				if (success && isPlayerNear) {
+					growSucess = WorldGen.GrowPalmTree(i, j);
+
+				// A flag to check if a player is near the sapling
+				bool isPlayerNear = WorldGen.PlayerLOS(i, j);
+
+				//If growing the tree was a sucess and the player is near, show growing effects
+				if (growSucess && isPlayerNear)
 					WorldGen.TreeGrowFXCheck(i, j);
-				}
 			}
 		}
 
-		public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects) {
-			if (i % 2 == 1) {
+		public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects)
+		{
+			if (i % 2 == 1)
 				effects = SpriteEffects.FlipHorizontally;
-			}
 		}
 	}
 }
