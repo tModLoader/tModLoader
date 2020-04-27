958d24331a9dab621d8d1c5d3008a6ddec29c961
termigator1359 <45545085+termigator1359@users.noreply.github.com>
2020-04-27 15:46:54 -0400

    spacing and bracket fixes

--- ExampleMod/Items/ExampleFishingRod.cs
+++ ExampleMod/Items/ExampleFishingRod.cs
@@ -14,17 +14,15 @@ namespace ExampleMod.Items
 		public override string Texture => "Terraria/Item_" + ItemID.WoodFishingPole;
 		public static Color OverrideColor = Color.Coral;
 
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Example Fishing Rod");
-            Tooltip.SetDefault("Fires multiple lines at once. Can fish in lava.\n" +
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Fishing Rod");
+			Tooltip.SetDefault("Fires multiple lines at once. Can fish in lava.\n" +
 				"The fishing line never snaps.");
 			//Allows the pole to fish in lava
 			ItemID.Sets.CanFishInLava[item.type] = true;
-        }
+		}
 
-        public override void SetDefaults()
-        {
+		public override void SetDefaults() {
 			//These are copied through the CloneDefaults method
 			//item.useStyle = 1;
 			//item.useAnimation = 8;
@@ -45,36 +43,32 @@ namespace ExampleMod.Items
 
 			// Change the item's draw color so that it is visually distinct from the vanilla Wooden Fishing Rod.
 			item.color = OverrideColor;
-        }
+		}
 
 		//Grants the High Test Fishing Line bool if holding the item
 		//NOTE: Only triggers through the hotbar, not if you hold the item by hand outside of the inventory
-		public override void HoldItem(Player player)
-        {
-            player.accFishingLine = true;
-        }
+		public override void HoldItem(Player player) {
+			player.accFishingLine = true;
+		}
 
 		//Overrides the default shooting method to fire multiple bobbers
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
 			int bobberAmount = Main.rand.Next(3,6); //3 to 5 bobbers
 			float spreadAmount = 75f;
-            for (int index = 0; index < bobberAmount; ++index)
-            {
-                float SpeedX = speedX + Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f;
-                float SpeedY = speedY + Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f;
-                Projectile.NewProjectile(position.X, position.Y, SpeedX, SpeedY, type, 0, 0f, player.whoAmI, 0f, 0f);
-            }
-            return false;
-        }
+			for (int index = 0; index < bobberAmount; ++index) {
+				float SpeedX = speedX + Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f;
+				float SpeedY = speedY + Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f;
+				Projectile.NewProjectile(position.X, position.Y, SpeedX, SpeedY, type, 0, 0f, player.whoAmI, 0f, 0f);
+			}
+			return false;
+		}
 
-		public override void AddRecipes()
-		{
+		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(ItemType<ExampleItem>(), 10);
 			recipe.AddTile(TileType<ExampleWorkbench>());
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
-    }
+	}
 }
