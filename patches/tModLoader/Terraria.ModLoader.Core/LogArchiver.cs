f503002d0979191c1f28e11f1208ea2901e7306e
Charlie <45357714+GoodPro712@users.noreply.github.com>
2020-05-09 14:13:03 +1000

    i did an oopsie

--- patches/tModLoader/Terraria.ModLoader.Core/LogArchiver.cs
+++ patches/tModLoader/Terraria.ModLoader.Core/LogArchiver.cs
@@ -13,6 +13,9 @@ namespace Terraria.ModLoader.Core
 	internal static class LogArchiver
 	{
 		internal static void ArchiveLogs() {
+			if (!Directory.Exists(Logging.LogArchiveDir))
+				Directory.CreateDirectory(Logging.LogArchiveDir);
+
 			foreach (var logFile in Directory.GetFiles(Logging.LogDir, "*.old*"))
 				Archive(logFile, Path.GetFileNameWithoutExtension(logFile));
 
@@ -24,11 +27,11 @@ namespace Terraria.ModLoader.Core
 			int n = 1;
 
 			var pattern = new Regex($"{time:yyyy-MM-dd}-(\\d+)\\.zip");
-			var existingLogs = Directory.GetFiles(Logging.LogDir).Where(s => pattern.IsMatch(Path.GetFileName(s))).ToList();
+			var existingLogs = Directory.GetFiles(Logging.LogArchiveDir).Where(s => pattern.IsMatch(Path.GetFileName(s))).ToList();
 			if (existingLogs.Count > 0)
 				n = existingLogs.Select(s => int.Parse(pattern.Match(Path.GetFileName(s)).Groups[1].Value)).Max() + 1;
 
-			using (var zip = new ZipFile(Path.Combine(Logging.LogDir, $"{time:yyyy-MM-dd}-{n}.zip"), Encoding.UTF8)) {
+			using (var zip = new ZipFile(Path.Combine(Logging.LogArchiveDir, $"{time:yyyy-MM-dd}-{n}.zip"), Encoding.UTF8)) {
 				using (var stream = File.OpenRead(logFile)) {
 					zip.AddEntry(entryName, stream);
 					zip.Save();
@@ -41,8 +44,15 @@ namespace Terraria.ModLoader.Core
 		private const int MAX_LOGS = 20;
 		private static void DeleteOldArchives() {
 			var pattern = new Regex(".*\\.zip");
-			var existingLogs = Directory.GetFiles(Logging.LogDir).Where(s => pattern.IsMatch(Path.GetFileName(s))).OrderBy(File.GetCreationTime).ToList();
+			var existingLogs = Directory.GetFiles(Logging.LogArchiveDir).Where(s => pattern.IsMatch(Path.GetFileName(s))).OrderBy(File.GetCreationTime).ToList();
+			var existingOldLogs = Directory.GetFiles(Logging.LogDir).Where(s => pattern.IsMatch(Path.GetFileName(s))).ToList();
 			foreach (var f in existingLogs.Take(existingLogs.Count - MAX_LOGS)) {
+				try {
+					File.Delete(f);
+				}
+				catch (IOException) { }
+			}
+			foreach (var f in existingOldLogs) {
 				try {
 					File.Delete(f);
 				}
