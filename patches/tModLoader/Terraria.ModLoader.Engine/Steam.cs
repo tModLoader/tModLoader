31a3ffc4afb435260b2e5c981e5d93a913241921
JavidPack <javidpack@gmail.com>
2020-05-09 07:12:44 -0600

    Fix server again

--- patches/tModLoader/Terraria.ModLoader.Engine/Steam.cs
+++ patches/tModLoader/Terraria.ModLoader.Engine/Steam.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Steamworks;
+using Terraria.Social;
 
 namespace Terraria.ModLoader.Engine
 {
 	internal class Steam
 	{
 		public const uint TMLAppID = 1281930;
-		public static bool IsSteamApp = SteamApps.BIsAppInstalled(new AppId_t(TMLAppID));
+		public static bool IsSteamApp = SocialAPI.Mode == SocialMode.Steam && Steamworks.SteamAPI.Init() && SteamApps.BIsAppInstalled(new AppId_t(TMLAppID));
 	}
 }
