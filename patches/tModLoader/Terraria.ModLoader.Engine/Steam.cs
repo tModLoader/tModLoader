9c2a8be3f8ed7fe5dc1ac6521b3286173eeb1583
Jofairden <jofairden@gmail.com>
2020-05-09 12:56:03 +0200

    Add steam app check for patreon and achievements

---
+++ patches/tModLoader/Terraria.ModLoader.Engine/Steam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks;
+
+namespace Terraria.ModLoader.Engine
+{
+	internal class Steam
+	{
+		public const uint TMLAppID = 1281930;
+		public static bool IsSteamApp = SteamApps.BIsAppInstalled(new AppId_t(TMLAppID));
+	}
+}
