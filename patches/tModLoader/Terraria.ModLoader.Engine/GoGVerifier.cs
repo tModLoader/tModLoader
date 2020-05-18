a132275d7b6660d43dd97fa51d67df1182752dcd
Jofairden <danielzondervan.nl@gmail.com>
2020-05-18 21:32:06 +0200

    Add Content directory check and Terraria install check

--- patches/tModLoader/Terraria.ModLoader.Engine/GoGVerifier.cs
+++ patches/tModLoader/Terraria.ModLoader.Engine/GoGVerifier.cs
@@ -1,10 +1,12 @@
 ﻿using ReLogic.OS;
+using Steamworks;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
+using Terraria.Social;
 
 namespace Terraria.ModLoader.Engine
 {
@@ -13,20 +15,23 @@ namespace Terraria.ModLoader.Engine
 		private static bool? isGoG;
 		public static bool IsGoG => isGoG ?? (isGoG = GoGCheck()).Value;
 
-		private static bool HashMatchesFile(byte[] hash, string path) {
+		private static bool HashMatchesFile(byte[] hash, string path)
+		{
 			using (var md5 = MD5.Create())
 			using (var stream = File.OpenRead(path))
 				return hash.SequenceEqual(md5.ComputeHash(stream));
 		}
 
-		private static byte[] ToByteArray(string hexString) {
+		private static byte[] ToByteArray(string hexString)
+		{
 			byte[] retval = new byte[hexString.Length / 2];
 			for (int i = 0; i < hexString.Length; i += 2)
 				retval[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
 			return retval;
 		}
 
-		private static void GetPlatformCheckInfo(out string steamAPIpath, out byte[] steamAPIhash, out byte[] vanillaGoGhash) {
+		private static void GetPlatformCheckInfo(out string steamAPIpath, out byte[] steamAPIhash, out byte[] vanillaGoGhash)
+		{
 			// I'd like it if you couldn't just open tML in a hex editor and replace the hash
 			// but whether I make it byte array or string doesn't change that. I could obfuscate the hash a bit
 			// but I can't really make it more effort than just nuking the steam check with dnspy (which could be done before we added GoG support)
@@ -50,67 +55,66 @@ namespace Terraria.ModLoader.Engine
 			}
 		}
 
-		private static bool GoGCheck() {
+		private static bool GoGCheck()
+		{
 			GetPlatformCheckInfo(out var steamAPIpath, out var steamAPIhash, out var vanillaGoGhash);
 
 			const string ContentDirectory = "Content";
-			const string InstallInstructions = "Please restore your Terraria install, then install tModLoader using the provided installer or by following the README.txt instructions.";
+			const string InstallInstructions = "Please restore your Terraria install, then install tModLoader on Steam or by following the README.txt instructions for manual installation.";
 
 			void Exit(string errorMessage)
 			{
+				errorMessage += $"\r\n\r\n{InstallInstructions}";
 				Logging.tML.Fatal(errorMessage);
 				UI.Interface.MessageBoxShow(errorMessage);
 				Environment.Exit(1);
 			}
 
-			// TODO: Fix all these messages to the new installation pattern
-			/* 
+			// TODO These steam checks should not be here, but I'm afraid to move stuff around right now -Jof
+			bool checkSteam = File.Exists(steamAPIpath);
 #if !SERVER
-			if(!Directory.Exists(ContentDirectory)) {
-				Logging.tML.Info($"{ContentDirectory} could not be found.");
-
-				Exit($"{ContentDirectory} directory could not be found.\r\n\r\nDid you forget to extract tModLoader into Terraria's install directory?\r\n\r\n{InstallInstructions}");
-
+			if (!Directory.Exists(ContentDirectory)) {
+				Exit($"{ContentDirectory} directory could not be found.\r\n\r\nDid you forget to extract tModLoader's Content directory into the tModLoader folder?\r\n\r\nEnsure tModLoader is installed in a separate folder from Terraria.");
 				return false;
+			}
+			if (checkSteam) {
+				SocialAPI.LoadSteam();
+				SteamApps.GetAppInstallDir(Steam.TerrariaAppId_t, out string terrariaInstallLocation, 1000);
+				if (!Directory.Exists(Path.Combine(terrariaInstallLocation, ContentDirectory))) {
+					Exit($"Terraria Steam installation or Terraria Content directory not found.\r\n\r\nPlease ensure Terraria 1.4 is installed through Steam.");
+					return false;
+				}
 			}
 #endif
-			*/
-
-			if (File.Exists(steamAPIpath)) {
+			if (checkSteam) {
 				VerifySteamAPI(steamAPIpath, steamAPIhash);
-
 				return false;
 			}
+			Logging.tML.Info("GoG detected. Disabled steam check.");
 
 			const string DefaultExe = "Terraria.exe";
-			const string CheckExe = "Terraria_v1.3.5.3.exe"; //TODO: Use Main.versionNumber here after porting to 1.3.5.3
-
+			string CheckExe = $"Terraria_{Main.versionNumber}.exe";
 			string vanillaPath = File.Exists(CheckExe) ? CheckExe : DefaultExe;
 
 			if (!File.Exists(vanillaPath)) {
 #if SERVER
 				return false;
 #else
-				Logging.tML.Info($"{vanillaPath} could not be found.");
-
-				Exit($"{vanillaPath} could not be found.\r\n\r\nGoG installs must have the unmodified Terraria executable to function.\r\n\r\n{InstallInstructions}");
-
+				Exit($"{vanillaPath} could not be found.\r\n\r\nGoG installs must have the unmodified Terraria executable to function.");
 				return false;
 #endif
 			}
 
 			if (!HashMatchesFile(vanillaGoGhash, vanillaPath)) {
-				Exit($"{vanillaPath} is not the unmodified Terraria executable.\r\n\r\nGoG installs must have the unmodified Terraria executable to function.\r\n\r\n{InstallInstructions}");
-				
+				Exit($"{vanillaPath} is not the unmodified Terraria executable.\r\n\r\nGoG installs must have the unmodified Terraria executable to function.");
 				return false;
 			}
-
-			Logging.tML.Info("GoG detected. Disabled steam check.");
 
 			return true;
 		}
 
-		private static void VerifySteamAPI(string steamAPIpath, byte[] steamAPIhash) {
+		private static void VerifySteamAPI(string steamAPIpath, byte[] steamAPIhash)
+		{
 			if (!HashMatchesFile(steamAPIhash, steamAPIpath)) {
 				string message = "Steam API hash mismatch, assumed pirated.\n\ntModLoader requires a legitimate Terraria install to work.";
 				Logging.tML.Fatal(message);
--- patches/tModLoader/Terraria.ModLoader.Engine/Steam.cs
+++ patches/tModLoader/Terraria.ModLoader.Engine/Steam.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,25 @@ namespace Terraria.ModLoader.Engine
 	internal class Steam
 	{
 		public const uint TMLAppID = 1281930;
-		public static bool IsSteamApp = SocialAPI.Mode == SocialMode.Steam && Steamworks.SteamAPI.Init() && SteamApps.BIsAppInstalled(new AppId_t(TMLAppID));
+		public const uint TerrariaAppID = 105600;
+
+		public static AppId_t TMLAppID_t = new AppId_t(TMLAppID);
+		public static AppId_t TerrariaAppId_t = new AppId_t(TerrariaAppID);
+
+		public static bool IsSteamApp = SocialAPI.Mode == SocialMode.Steam && SteamAPI.Init() && SteamApps.BIsAppInstalled(new AppId_t(TMLAppID));
+
+		public static bool EnsureSteamAppIdTMLFile()
+		{
+			bool exists = File.Exists("steam_appid.txt");
+			File.WriteAllText("steam_appid.txt", TMLAppID.ToString());
+			return exists;
+		}
+
+		public static bool EnsureSteamAppIdTerrariaFile()
+		{
+			bool exists = File.Exists("steam_appid.txt");
+			File.WriteAllText("steam_appid.txt", TerrariaAppID.ToString());
+			return exists;
+		}
 	}
 }
