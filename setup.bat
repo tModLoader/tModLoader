07237a0703566f6836b89fdacde566bcb16ee62d
Chicken-Bones <mehvids@gmail.com>
2020-05-21 20:06:17 +1000

    setup.bat is back!

--- setup.bat
+++ setup.bat
@@ -1,1 +1,15 @@
-start setup\bin\Debug\netcoreapp3.1\setup.exe
+git submodule update --init --recursive
+@echo off
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+    exit /b %errorlevel%
+)
+@echo on
+
+dotnet run --project setup/setup.csproj
+
+@echo off
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+    exit /b %errorlevel%
+)
