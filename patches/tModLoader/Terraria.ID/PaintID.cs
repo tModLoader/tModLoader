4c6f6b6ca29cab8fc1a3462f5070a5ed4128c5dd
direwolf420 <amakejk1@web.de>
2020-05-29 05:02:34 +0200

    Add PaintID.cs (#938)

---
+++ patches/tModLoader/Terraria.ID/PaintID.cs
@@ -0,0 +1,38 @@
+﻿namespace Terraria.ID
+{
+	public static class PaintID
+	{
+		// Name derived from the item name associated with the paint
+		public const byte Red = 1;
+		public const byte Orange = 2;
+		public const byte Yellow = 3;
+		public const byte Lime = 4;
+		public const byte Green = 5;
+		public const byte Teal = 6;
+		public const byte Cyan = 7;
+		public const byte SkyBlue = 8;
+		public const byte Blue = 9;
+		public const byte Purple = 10;
+		public const byte Violet = 11;
+		public const byte Pink = 12;
+		public const byte DeepRed = 13;
+		public const byte DeepOrange = 14;
+		public const byte DeepYellow = 15;
+		public const byte DeepLime = 16;
+		public const byte DeepGreen = 17;
+		public const byte DeepTeal = 18;
+		public const byte DeepCyan = 19;
+		public const byte DeepSkyBlue = 20;
+		public const byte DeepBlue = 21;
+		public const byte DeepPurple = 22;
+		public const byte DeepViolet = 23;
+		public const byte DeepPink = 24;
+		public const byte Black = 25;
+		public const byte White = 26;
+		public const byte Gray = 27;
+		public const byte Brown = 28;
+		public const byte Shadow = 29;
+		public const byte Negative = 30;
+		public const byte Count = 30;
+	}
+}
