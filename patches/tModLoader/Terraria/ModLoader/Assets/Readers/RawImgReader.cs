c9f159b2a5f493ef308da03df9288e6d46a7503e
Mirsario <me@mirsar.io>
2020-07-06 19:00:53 +0300

    Cleanups for the last commit.

--- patches/tModLoader/Terraria/ModLoader/Assets/Readers/RawImgReader.cs
+++ patches/tModLoader/Terraria/ModLoader/Assets/Readers/RawImgReader.cs
@@ -16,36 +16,16 @@ namespace Terraria.ModLoader.Assets
 	public class RawImgReader : IAssetReader, IDisposable
 	{
 		private readonly GraphicsDevice _graphicsDevice;
-		//private readonly ThreadLocal<Color[]> _colorProcessingCache;
-		//private bool _disposedValue;
 
 		public RawImgReader(GraphicsDevice graphicsDevice) {
 			_graphicsDevice = graphicsDevice;
-			//_colorProcessingCache = new ThreadLocal<Color[]>();
 		}
 
 		public T FromStream<T>(Stream stream) where T : class {
 			if (typeof(T) != typeof(Texture2D))
 				throw AssetLoadException.FromInvalidReader<RawImgReader, T>();
 
-			Texture2D texture2D = ImageIO.RawToTexture2D(_graphicsDevice, stream);
-
-			/*int num = texture2D.Width * texture2D.Height;
-
-			if (!_colorProcessingCache.IsValueCreated || _colorProcessingCache.Value.Length < num)
-				_colorProcessingCache.Value = new Color[num];
-
-			Color[] value = _colorProcessingCache.Value;
-			
-			texture2D.GetData(value, 0, num);
-
-			for (int i = 0;i != num;i++) {
-				value[i] = Color.FromNonPremultiplied(value[i].ToVector4());
-			}
-
-			texture2D.SetData(value, 0, num);*/
-
-			return texture2D as T;
+			return ImageIO.RawToTexture2D(_graphicsDevice, stream) as T;
 		}
 
 		public void Dispose() {
