34808985f7d6c0326e50d30f122158d402b539d4
Mirsario <me@mirsar.io>
2020-07-06 17:50:31 +0300

    Fixed FNA not compiling because I'm dumb.

--- patches/tModLoader/Terraria/ModLoader/Core/XnaTitleContainerRelativePathFix.cs
+++ patches/tModLoader/Terraria/ModLoader/Core/XnaTitleContainerRelativePathFix.cs
@@ -1,10 +1,12 @@
-﻿using Ionic.Zip;
+﻿#if XNA
+using Ionic.Zip;
 using Microsoft.Xna.Framework;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour.HookGen;
 using MonoMod.Utils;
 using System;
 using System.IO;
+#endif
 
 namespace Terraria.ModLoader.Core
 {
@@ -13,11 +15,12 @@ namespace Terraria.ModLoader.Core
 		/// <summary>
 		/// TitleContainer.OpenStream throws an error if it's fed a path that starts with '..\', treating paths like that as absolute, for some reason.
 		/// </summary>
-		public static void Init() =>
+		public static void Init() {
 #if XNA
 			//Can't believe that this is enough, lmao.
 			HookEndpointManager.Add(typeof(TitleContainer).FindMethod("IsCleanPathAbsolute"), new Func<string, bool>(path => false));
 #endif
+		}
 
 	}
 }
