175862455f1a8fcc61125369de0b1997a187990f
Itorius <itorius.yt@gmail.com>
2020-07-18 21:04:46 +0200

    Include WinServerDebug in the batch file

--- solutions/buildDebugAndMac.bat
+++ solutions/buildDebugAndMac.bat
@@ -1,5 +1,8 @@
 msbuild tModLoader.sln /restore /p:Configuration=WindowsDebug
 @IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
 
+msbuild tModLoader.sln /restore /p:Configuration=WindowsServerDebug
+@IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
+
 msbuild tModLoader.sln /restore /p:Configuration=MacRelease
 @IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
