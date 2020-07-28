281cc2ceeb8f08c8768c2fcae6fd69a83d862d49
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 11:12:25 -0500

    fixed comments

--- ExampleMod/Content/Items/Ammo/ExampleBullet.cs
+++ ExampleMod/Content/Items/Ammo/ExampleBullet.cs
@@ -8,24 +8,24 @@ namespace ExampleMod.Content.Items.Ammo
 	public class ExampleBullet : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("This is a modded bullet ammo."); //item description, can be set to whatever you want.
+			Tooltip.SetDefault("This is a modded bullet ammo."); // The item's description, can be set to whatever you want.
 		}
 
 		public override void SetDefaults() {
-			item.damage = 12; //The damage for projectiles isn't actually 12, it actually is the damage combined with the projectile and the item together
+			item.damage = 12; // The damage for projectiles isn't actually 12, it actually is the damage combined with the projectile and the item together.
 			item.ranged = true;
 			item.width = 8;
 			item.height = 8;
 			item.maxStack = 999;
-			item.consumable = true; //You need to set the item consumable so that the ammo would automatically consumed
+			item.consumable = true; // You need to set the item consumable so that the ammo would automatically consumed.
 			item.knockBack = 1.5f;
 			item.value = 10;
 			item.rare = ItemRarityID.Green;
-			item.shoot = ProjectileType<Projectiles.ExampleBullet>(); //The projectile shoot when your weapon using this ammo
-			item.shootSpeed = 16f; //The speed of the projectile
+			item.shoot = ProjectileType<Projectiles.ExampleBullet>(); //The projectile shoot when your weapon using this ammo.
+			item.shootSpeed = 16f; // The speed of the projectile.
 			item.ammo = AmmoID.Bullet; //The ammo class this ammo belongs to.
 		}
-
+		// Refer to ExampleItem.cs for how to create recipes.
 		public override void AddRecipes() {
 			CreateRecipe(50)
 				.AddIngredient(ItemID.MusketBall, 50)
