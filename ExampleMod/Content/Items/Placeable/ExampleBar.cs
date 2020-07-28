5b0915828d8c9e6e18ed678aa92b1a6a87bd7e40
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 18:33:45 -0500

    comment changes

--- ExampleMod/Content/Items/Placeable/ExampleBar.cs
+++ ExampleMod/Content/Items/Placeable/ExampleBar.cs
@@ -14,14 +14,14 @@ namespace ExampleMod.Content.Items.Placeable
 			item.width = 20;
 			item.height = 20;
 			item.maxStack = 99;
-			item.value = 750; // Reminder: each time you increase the value by one adds 1 copper to the sell price. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
+			item.value = 750; // The cost of the item in copper coins. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
 			item.useStyle = ItemUseStyleID.Swing;
 			item.useTurn = true;
 			item.useAnimation = 15;
 			item.useTime = 10;
 			item.autoReuse = true;
 			item.consumable = true;
-			item.createTile = TileType<Tiles.ExampleBar>(); // Place the tile that is put inside the <>. Modded tiles will use this format -> 'TileType<TileName>();'.
+			item.createTile = TileType<Tiles.ExampleBar>(); // The ID of the wall that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets)..
 			item.placeStyle = 0;
 		}
 		// Check ExampleItem.cs to see how recipes work.
