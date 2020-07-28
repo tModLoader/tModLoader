6d4aaa2a5a65599019533b32e605ee1977bf2895
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 11:40:16 -0500

    Made all comments generally better

--- ExampleMod/Content/Items/ExampleSoul.cs
+++ ExampleMod/Content/Items/ExampleSoul.cs
@@ -12,28 +12,28 @@ namespace ExampleMod.Content.Items
 			DisplayName.SetDefault("Soul of Exampleness");
 			Tooltip.SetDefault("'The essence of example creatures'");
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4)); // reminder, (4, 6) is an example of an item that draws a new frame every 6 ticks
-			ItemID.Sets.AnimatesAsSoul[item.type] = true; // auto defines the item having 4 frames.
-			ItemID.Sets.ItemIconPulse[item.type] = true; // the item pulses while in the player's inventory
-			ItemID.Sets.ItemNoGravity[item.type] = true; // makes the item have no gravity
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4)); // Reminder, (4, 6) is an example of an item that draws a new frame every 6 ticks
+			ItemID.Sets.AnimatesAsSoul[item.type] = true; // Makes the item have 4 animation frames by default.
+			ItemID.Sets.ItemIconPulse[item.type] = true; // The item pulses while in the player's inventory
+			ItemID.Sets.ItemNoGravity[item.type] = true; // Makes the item have no gravity
 		}
 
 		public override void SetDefaults() {
 			item.width = 18;
 			item.height = 18;
 			item.maxStack = 999;
-			item.value = 1000; //
+			item.value = 1000; // Makes the item worth 1 gold.
 			item.rare = ItemRarityID.Orange;
 		}
 
-		public override void PostUpdate() => Lighting.AddLight(item.Center, Color.WhiteSmoke.ToVector3() * 0.55f * Main.essScale); // make this item glow when thrown out of inventory
+		public override void PostUpdate() => Lighting.AddLight(item.Center, Color.WhiteSmoke.ToVector3() * 0.55f * Main.essScale); // Makes this item glow when thrown out of inventory.
 	}
 
 	// todo: implement
 	// public class SoulGlobalNPC : GlobalNPC
 	// {
 	// 	public override void NPCLoot(NPC npc) {
-	// 		if (Main.player[Player.FindClosest(npc.position, npc.width, npc.height)].GetModPlayer<ExamplePlayer>().ZoneExample) { //  drop this item only in the ExampleBiome
+	// 		if (Main.player[Player.FindClosest(npc.position, npc.width, npc.height)].GetModPlayer<ExamplePlayer>().ZoneExample) { // Drop this item only in the ExampleBiome.
 	// 			Item.NewItem(npc.getRect(), ItemType<ExampleSoul>()); // get the npc's hitbox rectangle and spawn an item of choice
 	// 		}
 	// 	}
