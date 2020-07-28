f310e4c16b234174a44414d9d5370beef221028a
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 12:17:00 -0500

    added a ton of more descriptive comments

--- ExampleMod/Content/Dusts/Sparkle.cs
+++ ExampleMod/Content/Dusts/Sparkle.cs
@@ -6,13 +6,13 @@ namespace ExampleMod.Content.Dusts
 	public class Sparkle : ModDust
 	{
 		public override void OnSpawn(Dust dust) {
-			dust.velocity *= 0.4f;
-			dust.noGravity = true;
-			dust.noLight = true;
-			dust.scale *= 1.5f;
+			dust.velocity *= 0.4f; // Velocity of the dust when spawned by anything. Remember: Positive floats make it go downward. Negative floats make it go upward.
+			dust.noGravity = true; // Makes the dust have no gravity.
+			dust.noLight = true; // Makes the dust emit no light.
+			dust.scale *= 1.5f; // The scale of the dust.
 		}
 
-		public override bool Update(Dust dust) {
+		public override bool Update(Dust dust) { // Calls every frame the dust is active
 			dust.position += dust.velocity;
 			dust.rotation += dust.velocity.X * 0.15f;
 			dust.scale *= 0.99f;
@@ -25,7 +25,7 @@ namespace ExampleMod.Content.Dusts
 				dust.active = false;
 			}
 
-			return false;
+			return false; // Return false to prevent the vanilla behaviors to overrwrite the dust
 		}
 	}
 }
