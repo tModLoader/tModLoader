9a8b7d66dc83f6c2834cd3c87a2b0488ce927c79
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 11:48:53 -0500

    made comments overall better.

--- ExampleMod/Content/Items/Placeable/ExampleBar.cs
+++ ExampleMod/Content/Items/Placeable/ExampleBar.cs
@@ -7,24 +7,24 @@ namespace ExampleMod.Content.Items.Placeable
 	public class ExampleBar : ModItem
 	{
 		public override void SetStaticDefaults() {
-			ItemID.Sets.SortingPriorityMaterials[item.type] = 59; // influences the inventory sort order. 59 is PlatinumBar, higher is more valuable.
+			ItemID.Sets.SortingPriorityMaterials[item.type] = 59; // Influences the inventory sort order. 59 is PlatinumBar, higher is more valuable.
 		}
 
 		public override void SetDefaults() {
 			item.width = 20;
 			item.height = 20;
 			item.maxStack = 99;
-			item.value = 750; // reminder: each time you increase the value by one is adding 1 copper. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
+			item.value = 750; // Reminder: each time you increase the value by one adds 1 copper to the sell price. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
 			item.useStyle = ItemUseStyleID.Swing;
 			item.useTurn = true;
 			item.useAnimation = 15;
 			item.useTime = 10;
 			item.autoReuse = true;
 			item.consumable = true;
-			item.createTile = TileType<Tiles.ExampleBar>(); // place this tile
+			item.createTile = TileType<Tiles.ExampleBar>(); // Place the tile that is put inside the <>. Modded tiles will use this format -> 'TileType<TileName>();'.
 			item.placeStyle = 0;
 		}
-		// check ExampleItem.cs to see how recipes work
+		// Check ExampleItem.cs to see how recipes work.
 		public override void AddRecipes() {
 			CreateRecipe()
 				.AddIngredient<ExampleOre>(4)
