2d0da5281964e079c2d4bb49e10804142d758f07
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 18:40:40 -0500

    Update ExampleItem.cs

--- ExampleMod/Content/Items/ExampleItem.cs
+++ ExampleMod/Content/Items/ExampleItem.cs
@@ -15,7 +15,7 @@ namespace ExampleMod.Content.Items
 			item.height = 20; //The item texture's height
 
 			item.maxStack = 999; //The item's max stack value
-			item.value = Item.buyPrice(silver: 1); //The value of the item in copper/silver/gold/platinum coins
+			item.value = Item.buyPrice(silver: 1); //The value of the item in copper coins. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
 			item.rare = ItemRarityID.Blue; // The rarity of the weapon.
 		}
 
