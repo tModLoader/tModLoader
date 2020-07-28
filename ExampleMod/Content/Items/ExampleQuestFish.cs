b98057c20e9233402ca90e82831dc4cb25b3df03
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-07-28 11:43:47 -0500

    made better comments altogether

--- ExampleMod/Content/Items/ExampleQuestFish.cs
+++ ExampleMod/Content/Items/ExampleQuestFish.cs
@@ -15,17 +15,19 @@ namespace ExampleMod.Content.Items
 			item.maxStack = 1;
 			item.width = 26;
 			item.height = 26;
-			item.uniqueStack = true;
-			item.rare = ItemRarityID.Quest; //
+			item.uniqueStack = true; // Make this item only stack one time.
+			item.rare = ItemRarityID.Quest; // Also known as -11
 		}
 
-		public override bool IsQuestFish() => true; // makes the item a quest fish
+		public override bool IsQuestFish() => true; // Makes the item a quest fish
 
-		public override bool IsAnglerQuestAvailable() => Main.hardMode; // makes the quest only appear in hard mode
+		public override bool IsAnglerQuestAvailable() => Main.hardMode; // Makes the quest only appear in hard mode. Adding a '!' before Main.hardMode makes it ONLY available in pre-hardmode.
 
 		public override void AnglerQuestChat(ref string description, ref string catchLocation) {
-			/* how the angler describes the fish to the player*/ description = "I've heard stories of a fish that swims upside-down. Supposedly you have the stand upside-down yourself to even find one. One of those would go great on my ceiling. Go fetch!";
-			/* what it says on the bottom of the angler's text box of how to catch the fish */ catchLocation = "Caught anywhere while standing upside-down.";
+			// How the angler describes the fish to the player.
+			description = "I've heard stories of a fish that swims upside-down. Supposedly you have the stand upside-down yourself to even find one. One of those would go great on my ceiling. Go fetch!";
+			// What it says on the bottom of the angler's text box of how to catch the fish.
+			catchLocation = "Caught anywhere while standing upside-down.";
 		}
 	}
 }
