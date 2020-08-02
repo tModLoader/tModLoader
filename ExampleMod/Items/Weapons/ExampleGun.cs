925ad5a260970436d9aeb41d5410127a4f3f7fc6
MojoDallin <56805376+MojoDallin@users.noreply.github.com>
2020-08-01 21:23:38 -0400

    ExampleGun comments
    A few extra comments in the SetDefaults, fixed a typo

--- ExampleMod/Items/Weapons/ExampleGun.cs
+++ ExampleMod/Items/Weapons/ExampleGun.cs
@@ -12,22 +12,22 @@ namespace ExampleMod.Items.Weapons
 		}
 
 		public override void SetDefaults() {
-			item.damage = 20;
-			item.ranged = true;
-			item.width = 40;
-			item.height = 20;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.damage = 20; // item damage
+			item.ranged = true; // sets the damage type to ranged
+			item.width = 40; // hitbox width of the item
+			item.height = 20; // hitbox height of the item
+			item.useTime = 20; //how fast the item can be used (measured in ticks)
+			item.useAnimation = 20; // how long it takes for the item to be used (measured in ticks)
+			item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
 			item.noMelee = true; //so the item's animation doesn't do damage
-			item.knockBack = 4;
-			item.value = 10000;
-			item.rare = ItemRarityID.Green;
-			item.UseSound = SoundID.Item11;
-			item.autoReuse = true;
+			item.knockBack = 4; // how far back enemies will be pushed
+			item.value = 10000; // how much the item sells for (measured in copper)
+			item.rare = ItemRarityID.Green; // the color that the item's name will be in-game
+			item.UseSound = SoundID.Item11; // when you use an item, the sound the item makes
+			item.autoReuse = true; // if you can hold click to automatically use it again
 			item.shoot = 10; //idk why but all the guns in the vanilla source have this
-			item.shootSpeed = 16f;
-			item.useAmmo = AmmoID.Bullet;
+			item.shootSpeed = 16f; // the speed of the projectile (measured in pixels per frame)
+			item.useAmmo = AmmoID.Bullet; // what ammo it uses
 		}
 
 		public override void AddRecipes() {
@@ -49,7 +49,7 @@ namespace ExampleMod.Items.Weapons
 		}*/
 
 		// What if I wanted it to work like Uzi, replacing regular bullets with High Velocity Bullets?
-		// Uzi/Molten Fury style: Replace normal Bullets with Highvelocity
+		// Uzi/Molten Fury style: Replace normal Bullets with High Velocity
 		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
 			if (type == ProjectileID.Bullet) // or ProjectileID.WoodenArrowFriendly
@@ -126,7 +126,7 @@ namespace ExampleMod.Items.Weapons
 			item.reuseDelay = 14;
 		public override bool ConsumeAmmo(Player player)
 		{
-			// Because of how the game works, player.itemAnimation will be 11, 7, and finally 3. (UseAmination - 1, then - useTime until less than 0.) 
+			// Because of how the game works, player.itemAnimation will be 11, 7, and finally 3. (useAnimation - 1, then - useTime until less than 0.) 
 			// We can get the Clockwork Assault Riffle Effect by not consuming ammo when itemAnimation is lower than the first shot.
 			return !(player.itemAnimation < item.useAnimation - 2);
 		}*/
