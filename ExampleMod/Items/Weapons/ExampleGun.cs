f4736f44c730db61b7bb60235f6c6ca6d6e95b61
MojoDallin <56805376+MojoDallin@users.noreply.github.com>
2020-08-05 09:24:48 -0400

    Update ExampleMod/Items/Weapons/ExampleGun.cs
    Co-authored-by: Mirsario <me@mirsar.io>

--- ExampleMod/Items/Weapons/ExampleGun.cs
+++ ExampleMod/Items/Weapons/ExampleGun.cs
@@ -12,7 +12,7 @@ namespace ExampleMod.Items.Weapons
 		}
 
 		public override void SetDefaults() {
-			item.damage = 20; // item damage
+			item.damage = 20; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
 			item.ranged = true; // sets the damage type to ranged
 			item.width = 40; // hitbox width of the item
 			item.height = 20; // hitbox height of the item
