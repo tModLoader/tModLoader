d82070d357feea3e3010b8f111d31156632777e9
Mirsario <me@mirsar.io>
2020-08-05 17:25:23 +0300

    Major cleanup of Pet & LightPet examples.

---
+++ ExampleMod/Content/Pets/ExamplePet/ExamplePetBuff.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Content.Pets.ExamplePet
+{
+	public class ExamplePetBuff : ModBuff
+	{
+		public override void SetDefaults() {
+			DisplayName.SetDefault("Paper Airplane");
+			Description.SetDefault("\"Let this pet be an example to you!\"");
+
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.vanityPet[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex) { // This method gets called every frame your buff is active on your player.
+			player.buffTime[buffIndex] = 18000;
+
+			int projType = ProjectileType<ExamplePetProjectile>();
+
+			//If the player is local, and there hasn't been a pet projectile spawned yet - spawn it.
+			if (player.whoAmI == Main.myPlayer && player.ownedProjectileCounts[projType] <= 0) {
+				Projectile.NewProjectile(player.position.X + player.width * 0.5f, player.position.Y + player.height * 0.5f, 0f, 0f, projType, 0, 0f, player.whoAmI);
+			}
+		}
+	}
+}
---
+++ ExampleMod/Content/Pets/ExamplePet/ExamplePetItem.cs
@@ -0,0 +1,37 @@
+﻿using ExampleMod.Content.Items;
+using ExampleMod.Content.Tiles.Furniture;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Content.Pets.ExamplePet
+{
+	public class ExamplePetItem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Paper Airplane");
+			Tooltip.SetDefault("Summons a Paper Airplane to follow aimlessly behind you");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.ZephyrFish); // Copy the Defaults of the Zephyr Fish item.
+
+			item.shoot = ProjectileType<ExamplePetProjectile>(); // "Shoot" your pet projectile.
+			item.buffType = BuffType<ExamplePetBuff>(); // Apply buff upon usage of the item.
+		}
+
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>(10)
+				.AddTile<ExampleWorkbench>()
+				.Register();
+		}
+
+		public override void UseStyle(Player player) {
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) {
+				player.AddBuff(item.buffType, 3600);
+			}
+		}
+	}
+}
