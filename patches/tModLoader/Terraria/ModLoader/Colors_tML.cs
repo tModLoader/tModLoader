2b760963322fb402bc5a76a9bf1b838b9a33221a
Oliver Heamon <oliverheamon@gmail.com>
2020-08-13 22:07:13 +0100

    final changes

--- patches/tModLoader/Terraria/ModLoader/Colors_tML.cs
+++ patches/tModLoader/Terraria/ModLoader/Colors_tML.cs
@@ -2,7 +2,7 @@
 
 namespace Terraria.ID
 {
-	public static partial class Colors
+	partial class Colors
 	{
 		public static readonly Color RarityDarkRed = new Color(255, 40, 100);
 		public static readonly Color RarityDarkPurple = new Color(180, 40, 255);
