5e642b999dc6184f1f65bc6cf0e60757a0a9abc6
kittenchilly <mario.pikmin.zelda@gmail.com>
2020-08-23 13:58:37 -0500

    Update GlobalWall.cs

--- patches/tModLoader/Terraria/ModLoader/GlobalWall.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalWall.cs
@@ -65,7 +65,6 @@ namespace Terraria.ModLoader
 		public virtual void ModifyLight(int i, int j, int type, ref float r, ref float g, ref float b) {
 		}
 
-
 		/// <summary>
 		/// Called for every wall the world randomly decides to update in a given tick. Useful for things such as growing or spreading.
 		/// </summary>
