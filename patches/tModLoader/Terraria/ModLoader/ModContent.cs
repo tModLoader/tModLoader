1c4a92364d7ad28fe81e1568bf19ed98641cd2c8
Mirsario <me@mirsar.io>
2020-08-25 18:55:30 +0300

    Removed an unnecessary using.

--- patches/tModLoader/Terraria/ModLoader/ModContent.cs
+++ patches/tModLoader/Terraria/ModLoader/ModContent.cs
@@ -15,7 +15,6 @@ using Terraria.ModLoader.Core;
 using Terraria.ModLoader.Engine;
 using Terraria.ModLoader.Exceptions;
 using Terraria.ModLoader.IO;
-using Terraria.ModLoader.Tags;
 using Terraria.ModLoader.UI;
 using Terraria.UI;
 
