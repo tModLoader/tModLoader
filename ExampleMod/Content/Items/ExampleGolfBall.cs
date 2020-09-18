ef85872bf9e281c111322c72a0640f5fc0a0ed8f
pollen__ <36719558+41D4N@users.noreply.github.com>
2020-09-17 21:47:04 -0500

    Added ExampleGolfBall and ExampleGolfBallProjectile

---
+++ ExampleMod/Content/Items/ExampleGolfBall.cs
@@ -0,0 +1,27 @@
+﻿using ExampleMod.Content.Projectiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items
+{
+	public class ExampleGolfBall : ModItem
+	{
+		public override void SetDefaults() {
+			item.shoot = ModContent.ProjectileType<ExampleGolfBallProjectile>(); // Determines what projectile is placed on the golf tee.
+			item.useStyle = ItemUseStyleID.Swing;
+			item.shootSpeed = 12f;
+			item.width = 18;
+			item.height = 20;
+			item.maxStack = 1;
+			item.UseSound = SoundID.Item1;
+			item.useAnimation = 15;
+			item.useTime = 15;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+			item.value = 0;
+			item.accessory = true;
+			item.rare = ItemRarityID.Green;
+			item.canBePlacedInVanityRegardlessOfConditions = true; // Allows the golf ball to be placed in vanity.
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleGolfBallProjectile.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	public class ExampleGolfBallProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			ProjectileID.Sets.IsAGolfBall[Type] = true; // Allows the projectile to be placed on the tee.
+			ProjectileID.Sets.TrailingMode[Type] = 0; // Creates a trail behind the golf ball.
+			ProjectileID.Sets.TrailCacheLength[Type] = 20; // Sets the length of the trail.
+		}
+
+		public override void SetDefaults() {
+			projectile.netImportant = true;
+			projectile.width = 7;
+			projectile.height = 7;
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.aiStyle = 149; // 149 is the golf ball AI.
+			projectile.tileCollide = false;
+		}
+	}
+}
