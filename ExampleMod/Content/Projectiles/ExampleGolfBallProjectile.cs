68c926b68a3f817db4e50b192c0173c5d66a777f
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-09-18 22:39:50 +0300

    Update ExampleGolfBallProjectile.cs

--- ExampleMod/Content/Projectiles/ExampleGolfBallProjectile.cs
+++ ExampleMod/Content/Projectiles/ExampleGolfBallProjectile.cs
@@ -12,13 +12,13 @@ namespace ExampleMod.Content.Projectiles
 		}
 
 		public override void SetDefaults() {
-			projectile.netImportant = true;
-			projectile.width = 7;
-			projectile.height = 7;
-			projectile.friendly = true;
-			projectile.penetrate = -1;
+			projectile.netImportant = true; // Indicates that this projectile will be synced to a joining player (by default, any projectiles active before the player joins (besides pets) are not synced over)
+			projectile.width = 7; // the width of the projectile's hitbox
+			projectile.height = 7; // the height of the projectile's hitbox
+			projectile.friendly = true; // Can the projectile damage enemies?
+			projectile.penetrate = -1; // Can the projectile penetrate enemies? -1 sets it to infinite penetration.
 			projectile.aiStyle = 149; // 149 is the golf ball AI.
-			projectile.tileCollide = false;
+			projectile.tileCollide = false; // Can the projectile collide with tiles?
 		}
 	}
 }
