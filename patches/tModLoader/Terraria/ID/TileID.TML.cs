fbb2b5d715aa50f3989a7848d4cdfe968a59b6e7
Mirsario <me@mirsar.io>
2020-09-19 12:12:36 +0300

    Moved some ID.Sets additions to partial files.

--- patches/tModLoader/Terraria/ID/TileID.TML.cs
+++ patches/tModLoader/Terraria/ID/TileID.TML.cs
@@ -5,6 +5,9 @@
 		partial class Sets
 		{
 			public static bool[] CanDropFromRightClick = Factory.CreateBoolSet(4);
+			public static bool[] Stone = Factory.CreateBoolSet(1, 117, 25, 203);
+			public static bool[] Grass = Factory.CreateBoolSet(2, 23, 109, 199, 477, 492);
+			public static bool[] CanBeClearedDuringOreRunner = Factory.CreateBoolSet(0, 1, 23, 25, 40, 53, 57, 59, 60, 70, 109, 112, 116, 117, 147, 161, 163, 164, 199, 200, 203, 234);
 		}
 	}
 }
