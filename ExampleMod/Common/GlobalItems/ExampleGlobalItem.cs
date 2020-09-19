2bb9ea378d6a7d7c366d4de5acb61dacf9e81577
Mirsario <me@mirsar.io>
2020-09-19 15:00:03 +0300

    Moved and renamed CopperShortsword.cs to ExampleGlobalItem.cs

--- ExampleMod/Common/GlobalItems/ExampleGlobalItem.cs
+++ ExampleMod/Common/GlobalItems/ExampleGlobalItem.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ExampleMod.Content.Items.Weapons
+namespace ExampleMod.Common.GlobalItems
 {
 	// This file shows a very simple example of a GlobalItem class. GlobalItem hooks are called on all items in the game and are suitable for sweeping changes like 
 	// adding additional data to all items in the game. Here we simply adjust the damage of the Copper Shortsword item, as it is simple to understand. 
@@ -11,7 +11,7 @@ namespace ExampleMod.Content.Items.Weapons
 	{
 		public override void SetDefaults(Item item) {
 			if (item.type == ItemID.CopperShortsword) { // Here we make sure to only change Copper Shortsword by checking item.type in an if statement
-				item.damage = 50;	// Changed original CopperShortsword's damage to 50!
+				item.damage = 50; // Changed original CopperShortsword's damage to 50!
 			}
 		}
 	}
