8a6c33aaf9b05269ee763b18e4a952c9260a91f9
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-09-20 19:04:11 +0300

    Ported ExampleSpear + Added Comments

---
+++ ExampleMod/Content/Items/Weapons/ExampleSpear.cs
@@ -0,0 +1,48 @@
+﻿using ExampleMod.Content.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	public class ExampleSpear : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("An example spear");
+		}
+
+		public override void SetDefaults() { 
+			item.damage = 40; // The damage inflicted by the item.
+			item.useStyle = ItemUseStyleID.Shoot; // The use style of your item.
+			item.useAnimation = 18; // The time span of the using animation for the item. 
+			item.useTime = 24; // The time span of using the item in frames.
+			item.shootSpeed = 3.7f; // The velocity in pixels the projectile fired by the item will have.
+			item.knockBack = 6.5f; // The force of the knock back. Max value is 20.
+			item.width = 32; // The width of the item's hitbox in pixels.
+			item.height = 32; // The height of the item's hitbox in pixels.
+			item.scale = 1f; // The size multiplier of the item's sprite while the item is being used. Also increases range for melee weapons.
+			item.rare = ItemRarityID.Pink; // Rarity/Color of the item you can look at the rest at https://terraria.gamepedia.com/Rarity.
+			item.value = Item.sellPrice(silver: 10); // Value is the number of coins the item is worth.
+
+			item.melee = true; //Whether or not your item is part of the melee class.
+			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+			item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+			item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+			item.UseSound = SoundID.Item1; // The sound that your item makes when used.
+			item.shoot = ProjectileType<ExampleSpearProjectile>(); // The projectile that is fired by the item on use. 
+		}
+
+		public override bool CanUseItem(Player player) {
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[item.shoot] < 1;
+		}
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleSpearProjectile.cs
@@ -0,0 +1,94 @@
+﻿using ExampleMod.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Content.Projectiles
+{
+	public class ExampleSpearProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Spear");
+		}
+
+		public override void SetDefaults() {
+			projectile.width = 18; // The width of the projectile's hitbox in pixels.
+			projectile.height = 18; // The height of the projectile's hitbox in pixels.
+			projectile.aiStyle = 19; // Selects which vanilla code to use for the AI method. Modders can use vanilla aiStyle to mimic AI code already in the game.
+			projectile.penetrate = -1; // The Peneration of the projectile. -1 is infinite peneration.
+			projectile.scale = 1.3f; // The size multiplier of the projectile's sprite while the item is being used.
+			projectile.alpha = 0; // How transparent to draw this projectile. 0 to 255. 255 is completely transparent.
+
+			projectile.hide = true; 
+			projectile.ownerHitCheck = true;
+			projectile.melee = true; // Determines which crit chance will influence the damage of this projectile
+			projectile.tileCollide = false; // The projectile will collide with tiles.
+			projectile.friendly = true; // If True, this projectile will hurt enemies.
+		}
+
+		// In here the AI uses this example, to make the code more organized and readable
+		public float movementFactor // Change this value to alter how fast the spear moves
+		{
+			get => projectile.ai[0];
+			set => projectile.ai[0] = value;
+		}
+
+		// It appears that for this AI, only the ai0 field is used!
+		public override void AI() {
+			// Since we access the owner player instance so much, it's useful to create a helper local variable for this
+			// Sadly, Projectile/ModProjectile does not have its own
+			Player projOwner = Main.player[projectile.owner];
+			// Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+			Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+			projectile.direction = projOwner.direction;
+			projOwner.heldProj = projectile.whoAmI;
+			projOwner.itemTime = projOwner.itemAnimation;
+			projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+			projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+			// As long as the player isn't frozen, the spear can move
+			if (!projOwner.frozen) {
+				if (movementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+				{
+					movementFactor = 3f; // Make sure the spear moves forward when initially thrown out
+					projectile.netUpdate = true; // Make sure to netUpdate this spear
+				}
+				if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3) // Somewhere along the item animation, make sure the spear moves back
+				{
+					movementFactor -= 2.4f;
+				}
+				else // Otherwise, increase the movement factor
+				{
+					movementFactor += 2.1f;
+				}
+			}
+			// Change the spear position based off of the velocity and the movementFactor
+			projectile.position += projectile.velocity * movementFactor;
+			// When we reach the end of the animation, we can kill the spear projectile
+			if (projOwner.itemAnimation == 0) {
+				projectile.Kill();
+			}
+			// Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation, notice the usage of MathHelper, use this class!
+			// MathHelper.ToRadians(xx degrees here)
+			projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+			// Offset by 90 degrees here
+			if (projectile.spriteDirection == -1) {
+				projectile.rotation -= MathHelper.ToRadians(90f);
+			}
+
+			// These dusts are added later, for the 'ExampleMod' effect
+			if (Main.rand.NextBool(3)) {
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.height, projectile.width, DustType<Sparkle>(),
+					projectile.velocity.X * .2f, projectile.velocity.Y * .2f, 200, Scale: 1.2f);
+				dust.velocity += projectile.velocity * 0.3f;
+				dust.velocity *= 0.2f;
+			}
+			if (Main.rand.NextBool(4)) {
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.height, projectile.width, DustType<Sparkle>(),
+					0, 0, 254, Scale: 0.3f);
+				dust.velocity += projectile.velocity * 0.5f;
+				dust.velocity *= 0.5f;
+			}
+		}
+	}
+}
