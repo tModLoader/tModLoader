80cec23ffff225601bcd48e7a4af60828d16237d
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2020-09-21 01:37:01 -0400

    Run 64-bit .NET Core 3.1 on vanilla 1.4
    WeGame support needed; using RailSDK.NET will crash (BadImageFormatException).

--- patches/Terraria/Terraria/MonoLaunch.cs
+++ patches/Terraria/Terraria/MonoLaunch.cs
@@ -1,7 +1,11 @@
 #if !WINDOWS
+using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Terraria;
 
 internal static class MonoLaunch
@@ -19,9 +23,45 @@ internal static class MonoLaunch
 				return Assembly.Load(array);
 			}
 		};
-
+#if NETCORE
+		NativeLibrary.SetDllImportResolver(typeof(Game).Assembly, resolver);
+		NativeLibrary.SetDllImportResolver(typeof(Steamworks.SteamAPI).Assembly, resolver);
+#endif
 		Environment.SetEnvironmentVariable("FNA_WORKAROUND_WINDOW_RESIZABLE", "1");
 		Program.LaunchGame(args, monoArgs: true);
 	}
+
+#if NETCORE
+	private static IntPtr resolver(string name, Assembly assembly, DllImportSearchPath? searchPath) {
+		try {
+			if (assemblies.TryGetValue(name, out var handle)) {
+				return handle;
+			}
+			var path = Path.Combine(Environment.CurrentDirectory, "Libraries", "Native", getNativeDir(name));
+			var files = Directory.GetFiles(path, name + "*", SearchOption.AllDirectories);
+			var match = files.FirstOrDefault(s => File.Exists(s));
+			if (match != null && NativeLibrary.TryLoad(match, out handle)) {
+				return assemblies[name] = handle;
+			}
+			return assemblies[name] = IntPtr.Zero;
+		}
+		catch (DirectoryNotFoundException e) {
+			throw new DirectoryNotFoundException("A needed library file was missing from the tModLoader directory. " + e.Message, e);
+		}
+	}
+	private static readonly Dictionary<string, IntPtr> assemblies = new Dictionary<string, IntPtr>();
+
+	private static string getNativeDir(string name) {
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			return "Windows";
+		if (name.Contains("steam", StringComparison.OrdinalIgnoreCase))
+			return "Linux";
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			return "Linux";
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			return "OSX";
+		throw new InvalidOperationException("Unknown OS.");
+	}
+#endif
 }
 #endif
