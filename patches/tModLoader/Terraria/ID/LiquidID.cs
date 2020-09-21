7545bf0aebada11fdd9189db52a4d685376de813
RighteousRyan <60587654+RyanMakesMods@users.noreply.github.com>
2020-09-21 18:21:54 -0500

    Update LiquidID.cs

--- patches/tModLoader/Terraria/ID/LiquidID.cs
+++ patches/tModLoader/Terraria/ID/LiquidID.cs
@@ -1,10 +1,10 @@
 namespace Terraria.ID
 {
-  /// <summary> Enumerates liquid values to make them easier to see/read </summary>
-  public static class LiquidID
-  {
-    public const int Water = 0;
-    public const int Lava = 1;
-    public const int Honey = 2;
-  }
+	/// <summary> Enumerates liquid values to make them easier to see/read </summary>
+	public static class LiquidID
+	{
+		public const int Water = 0;
+		public const int Lava = 1;
+		public const int Honey = 2;
+	}
 }
