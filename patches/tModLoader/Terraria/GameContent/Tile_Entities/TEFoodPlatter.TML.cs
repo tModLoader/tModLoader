23f1c96abd162a118036c1b3e06853668e9a4c30
Mirsario <me@mirsar.io>
2020-09-28 21:59:18 +0300

    Fixed all remaining tileentities.

---
+++ patches/tModLoader/Terraria/GameContent/Tile_Entities/TEFoodPlatter.TML.cs
@@ -0,0 +1,13 @@
+using Terraria.ModLoader.IO;
+
+namespace Terraria.GameContent.Tile_Entities
+{
+	public partial class TEFoodPlatter
+	{
+		public override TagCompound Save() => new TagCompound {
+			{ "item", ItemIO.Save(item) }
+		};
+
+		public override void Load(TagCompound tag) => item = ItemIO.Load(tag.GetCompound("item"));
+	}
+}
