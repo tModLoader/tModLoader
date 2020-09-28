742274e0ed91f71ca3c71e833052b7c79469c83a
Mirsario <me@mirsar.io>
2020-09-28 20:18:11 +0300

    Moved ModTileEntity.Save/Load up to TileEntity.

---
+++ patches/tModLoader/Terraria/DataStructures/TileEntity.TML.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ModLoader.IO;
+
+namespace Terraria.DataStructures
+{
+	public partial class TileEntity
+	{
+		/// <summary>
+		/// Allows you to save custom data for this tile entity.
+		/// </summary>
+		/// <returns></returns>
+		public virtual TagCompound Save() {
+			return null;
+		}
+
+		/// <summary>
+		/// Allows you to load the custom data you have saved for this tile entity.
+		/// </summary>
+		public virtual void Load(TagCompound tag) {
+		}
+	}
+}
