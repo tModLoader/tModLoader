5ba577c3e9d9417143860c1d387131186fa58d31
kittenchilly <mario.pikmin.zelda@gmail.com>
2020-09-29 18:21:08 -0500

    Update ExampleMod/Common/GlobalItems/ExampleResearchSorting.cs
    Co-authored-by: Mirsario <me@mirsar.io>

--- ExampleMod/Common/GlobalItems/ExampleResearchSorting.cs
+++ ExampleMod/Common/GlobalItems/ExampleResearchSorting.cs
@@ -4,9 +4,9 @@ using Terraria.ModLoader;
 
 namespace ExampleMod.Common.GlobalItems
 {
-		//This file shows examples of creating and setting your own sorting group in Journey mode's Duplication menu, as well as changing the sorting groups of existing items.
-		//Creating your own research sorting group can be useful if your mod has a specific custom item type, or the vanilla sorting method doesn't assign the right group to your item.
-		//While you can do this in ModItem, there are benefits to adding all modded items to sorting groups in bulk using GlobalItem, as shown here.
+	//This file shows examples of creating and setting your own sorting group in Journey mode's Duplication menu, as well as changing the sorting groups of existing items.
+	//Creating your own research sorting group can be useful if your mod has a specific custom item type, or the vanilla sorting method doesn't assign the right group to your item.
+	//While you can do this in ModItem, there are benefits to adding all modded items to sorting groups in bulk using GlobalItem, as shown here.
 	public class ExampleResearchSorting : GlobalItem
 	{
 		//Here we add both every item in this mod to a single custom sorting group, as well as add an existing item, the copper shortsword, to a vanilla sorting group.
