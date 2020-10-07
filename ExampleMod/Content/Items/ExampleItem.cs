e1cd421886d66442a7ec5646426ffafb2ee17cd6
Itorius <itorius.yt@gmail.com>
2020-10-07 09:10:42 +0200

    Add examples for modded item with data (clone and new instance)

--- ExampleMod/Content/Items/ExampleItem.cs
+++ ExampleMod/Content/Items/ExampleItem.cs
@@ -18,12 +18,11 @@ namespace ExampleMod.Content.Items
 
 			item.maxStack = 999; //The item's max stack value
 			item.value = Item.buyPrice(silver: 1); //The value of the item in copper coins. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
-			item.rare = ItemRarityID.Blue; // The rarity of the weapon.
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
 		public override void AddRecipes() {
-			CreateRecipe()
+			CreateRecipe(999)
 				.AddIngredient(ItemID.DirtBlock, 10)
 				.AddTile(TileID.WorkBenches)
 				.Register();
