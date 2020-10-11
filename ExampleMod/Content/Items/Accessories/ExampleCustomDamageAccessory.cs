3df9a57849eb7c489cc140a0c42b85ba084a9b2c
Mirsario <me@mirsar.io>
2020-10-11 15:10:08 +0300

    Fixed EM derp.

--- ExampleMod/Content/Items/Accessories/ExampleCustomDamageAccessory.cs
+++ ExampleMod/Content/Items/Accessories/ExampleCustomDamageAccessory.cs
@@ -19,9 +19,9 @@ namespace ExampleMod.Content.Items.Accessories
 		public override void UpdateAccessory(Player player, bool hideVisual) {
 			// GetDamage returns a reference to the specified damage class' damage Modifier.
 			// Since it doesn't return a value, but a reference to it, you can freely modify it with +-*/ operators.
-			// In this case, we're multiplying it by 0.25f, which means a 25% damage increase.
+			// In this case, we're multiplying it by 1.25f, which means a 25% damage increase.
 			// If you want to add or subtract a non-multiplicative damage value, you can use + or - operators on it instead.
-			player.GetDamage<ExampleDamageClass>() *= 0.25f;
+			player.GetDamage<ExampleDamageClass>() *= 1.25f;
 		}
 	}
 }
