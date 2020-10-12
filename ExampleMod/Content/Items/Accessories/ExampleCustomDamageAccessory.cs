91dc7a3f79618c1b7d5f3ddb530f2e4656fec746
Mirsario <me@mirsar.io>
2020-10-12 17:46:52 +0300

    EM updates.

--- ExampleMod/Content/Items/Accessories/ExampleCustomDamageAccessory.cs
+++ ExampleMod/Content/Items/Accessories/ExampleCustomDamageAccessory.cs
@@ -19,12 +19,14 @@ namespace ExampleMod.Content.Items.Accessories
 		public override void UpdateAccessory(Player player, bool hideVisual) {
 			// GetDamage returns a reference to the specified damage class' damage Modifier.
 			// Since it doesn't return a value, but a reference to it, you can freely modify it with +-*/ operators.
-			// In this case, we're multiplying it by 1.25f, which means a 25% damage increase.
-			// If you want to add or subtract a non-multiplicative damage value, you can use + or - operators on it instead.
+			// Modifier is a structure that separately holds float additive and multiplicative modifiers.
+			// When Modifier is applied to a value, its additive modifiers are applied before multiplicative ones.
+
+			// In this case, we're multiplying by 1.25f, which will mean a 25% damage increase after every other additive modifier is applied.
 			player.GetDamage<ExampleDamageClass>() *= 1.25f;
 
 			// GetCrit, similarly to GetDamage, returns a reference to the specified damage class' crit Modifier.
-			// In this case, we're adding 10% crit chance without multiplying others' bonuses.
+			// In this case, we're adding 10% additive crit chance without multiplying others' bonuses.
 			player.GetCrit<ExampleDamageClass>() += 10;
 		}
 	}
