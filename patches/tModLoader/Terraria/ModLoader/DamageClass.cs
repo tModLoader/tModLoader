cdb3fa803e945b3b0ba04d7c54b4ec79aaa84d7a
OliHeamon <52383843+OliHeamon@users.noreply.github.com>
2020-10-12 15:47:52 +0100

    Added DamageClass (#1056)
    * Added ModDamageClass
    
    * Fixed up examplemod items to be more relevant
    
    * Mir's requested changes
    
    * More requested changes
    
    Made DamageType an item field to allow conversion of vanilla items, changed SetX to ModifyX, etc
    
    * Ported vanilla damage types to less hardcoded new system
    
    * Update patches/tModLoader/Terraria/Item_tML.cs
    
    Co-authored-by: Mirsario <me@mirsar.io>
    
    * Renamed to DamageClass, moved default classes
    
    * Ported to damagemodifier system
    
    Adidtionally, changed damage modification hooks to reflect this
    
    * fixed issue with alldamage calculation
    
    * Shortened some code.
    
    * Renamed DamageModifier to Modifier (1/2)
    
    * Renamed DamageModifier to Modifier (2/2)
    
    * Removed AddDamage methods, made GetDamage return references.
    
    * Oops, now generic GetDamage returns references too.
    
    * Updated the example.
    
    * Added a texture for ExampleCustomDamageAccessory.
    
    * Made DamageClass use ModTranslation. Not yet tested.
    
    * Fixed damageData indexoutofrange exceptions.
    
    * Fixed localization issues.
    
    * EM comment fix
    
    * Fixed EM derp.
    
    * Made crits use Modifier.
    
    * Fixed behavior of melee/magic/ranged/summon setters.
    
    * EM updates.
    
    Co-authored-by: Mirsario <me@mirsar.io>

---
+++ patches/tModLoader/Terraria/ModLoader/DamageClass.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.Localization;
+
+namespace Terraria.ModLoader
+{
+	public abstract class DamageClass : ModType
+	{
+		public static Melee Melee { get; private set; } = new Melee();
+		public static Ranged Ranged { get; private set; } = new Ranged();
+		public static Magic Magic { get; private set; } = new Magic();
+		public static Summon Summon { get; private set; } = new Summon();
+
+		internal int index;
+
+		/// <summary> This is the translation that is used behind <see cref="DisplayName"/>. The translation will show up when an item tooltip displays 'X [ClassName]'. This should include the 'damage' part. </summary>
+		public ModTranslation ClassName { get; internal set; }
+
+		/// <summary> This is the name that will show up when an item tooltip displays 'X [ClassName]'. This should include the 'damage' part. </summary>
+		public string DisplayName => DisplayNameInternal;
+
+		internal protected virtual string DisplayNameInternal => ClassName.GetTranslation(Language.ActiveCulture);
+
+		protected override void Register() {
+			index = DamageClassLoader.Add(this);
+
+			ClassName = Mod.GetOrCreateTranslation($"Mods.{Mod.Name}.DamageClassName.{Name}");
+
+			ModTypeLookup<DamageClass>.Register(this);
+		}
+	}
+}
