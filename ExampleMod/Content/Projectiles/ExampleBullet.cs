cbd59b5143fedc776f362a237d847af325ce79c2
Itorius <itorius.yt@gmail.com>
2020-10-21 17:24:42 +0200

    ExampleMod fixes (should be part of 1.4 branch)

--- ExampleMod/Content/Projectiles/ExampleBullet.cs
+++ ExampleMod/Content/Projectiles/ExampleBullet.cs
@@ -23,7 +23,7 @@ namespace ExampleMod.Content.Projectiles
 			projectile.aiStyle = 1; //The ai style of the projectile, please reference the source code of Terraria
 			projectile.friendly = true; //Can the projectile deal damage to enemies?
 			projectile.hostile = false; //Can the projectile deal damage to the player?
-			projectile.ranged = true; //Is the projectile shoot by a ranged weapon?
+			// projectile.ranged = true; //Is the projectile shoot by a ranged weapon?
 			projectile.penetrate = 5; //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
 			projectile.timeLeft = 600; //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
 			projectile.alpha = 255; //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in) Make sure to delete this if you aren't using an aiStyle that fades in. You'll wonder why your projectile is invisible.
