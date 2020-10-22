5d807e58d95d08cdf5ec72325b5fb6cbaa469eed
pollen__ <36719558+41D4N@users.noreply.github.com>
2020-10-21 19:23:33 -0500

    Created SlopeIDs

---
+++ patches/tModLoader/Terraria/ID/SlopeID.cs
@@ -0,0 +1,11 @@
+﻿namespace Terraria.ID
+{
+	public static class SlopeID
+	{
+		public static byte None = 0;
+		public static byte DownLeft = 1;
+		public static byte DownRight = 2;
+		public static byte UpLeft = 3;
+		public static byte UpRight = 4;
+	}
+}
