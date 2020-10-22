82a9ff51d0eddb532520ef595b52d1d4ef690414
pollen__ <36719558+41D4N@users.noreply.github.com>
2020-10-21 19:46:20 -0500

    Added Summaries

--- patches/tModLoader/Terraria/ID/SlopeID.cs
+++ patches/tModLoader/Terraria/ID/SlopeID.cs
@@ -2,10 +2,25 @@
 {
 	public static class SlopeID
 	{
+		/// <summary>
+		/// A full tile.
+		/// </summary>
 		public static byte None = 0;
+		/// <summary>
+		/// A tile with the bottom left corner solid.
+		/// </summary>
 		public static byte DownLeft = 1;
+		/// <summary>
+		/// A tile with the bottom right corner solid.
+		/// </summary>
 		public static byte DownRight = 2;
+		/// <summary>
+		/// A tile with the top left corner solid.
+		/// </summary>
 		public static byte UpLeft = 3;
+		/// <summary>
+		/// A tile with the top right corner solid.
+		/// </summary>
 		public static byte UpRight = 4;
 	}
 }
