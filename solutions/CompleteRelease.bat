82eda99d48736ff192ebd8ed22690153987a4c89
JavidPack <javidpack@gmail.com>
2020-11-10 23:34:54 -0700

    v0.11.7.8 Beta 1, test fix for 1.4.1.2 music change

--- solutions/CompleteRelease.bat
+++ solutions/CompleteRelease.bat
@@ -4,7 +4,7 @@
 @ECHO off
 :: Compile/Build exe 
 echo "Building Release"
-set tModLoaderVersion=v0.11.7.7
+set tModLoaderVersion=v0.11.7.8 Beta 1
 call buildRelease.bat
 
 set destinationFolder=.\tModLoader %tModLoaderVersion% Release
