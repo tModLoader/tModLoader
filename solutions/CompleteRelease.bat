5525a4e953897c8c0620b5285df7bde4c468724e
JavidPack <javidpack@gmail.com>
2020-11-11 15:55:21 -0700

    v0.11.7.8 release

--- solutions/CompleteRelease.bat
+++ solutions/CompleteRelease.bat
@@ -4,7 +4,7 @@
 @ECHO off
 :: Compile/Build exe 
 echo "Building Release"
-set tModLoaderVersion=v0.11.7.8 Beta 1
+set tModLoaderVersion=v0.11.7.8
 call buildRelease.bat
 
 set destinationFolder=.\tModLoader %tModLoaderVersion% Release
