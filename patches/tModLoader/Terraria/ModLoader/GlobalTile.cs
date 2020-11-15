45a1d89d78495f784eeb8ab4b236ef1453e93a27
RighteousRyan <60587654+RighteousRyan1@users.noreply.github.com>
2020-11-15 16:27:56 -0600

    Fixed typo with paramref (#1207)
    (cherry picked from commit a646b7d5aedb662184013ec847307cb7b07877bf)

--- patches/tModLoader/Terraria/ModLoader/GlobalTile.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalTile.cs
@@ -127,7 +127,7 @@ namespace Terraria.ModLoader
 		}
 
 		/// <summary>
-		/// Allows you to determine what happens when the tile at the given coordinates is killed or hit with a pickaxe. If <paramref name="fail"/> is true, the tile will not be mined; <paramref name="effectsOnly"/> makes it so that only dust is created; <paramref name="noItem"/> stops items from dropping.
+		/// Allows you to determine what happens when the tile at the given coordinates is killed or hit with a pickaxe. If <paramref name="fail"/> is true, the tile will not be mined; <paramref name="effectOnly"/> makes it so that only dust is created; <paramref name="noItem"/> stops items from dropping.
 		/// </summary>
 		/// <param name="i"></param>
 		/// <param name="j"></param>
