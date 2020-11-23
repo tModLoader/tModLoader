671bac1a6b1aaf257a98382e6dc7882bf11801e5
Chicken-Bones <mehvids@gmail.com>
2020-11-23 22:21:22 +1000

    Fix extra space in vanilla damage class tooltips from previous commit

--- patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
+++ patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
@@ -4,26 +4,26 @@ namespace Terraria.ModLoader
 {
 	public class Melee : DamageClass
 	{
-		internal protected override string DisplayNameInternal => " "+Language.GetTextValue("LegacyTooltip.2");
+		internal protected override string DisplayNameInternal => Language.GetTextValue("LegacyTooltip.2").Substring(1);
 	}
 
 	public class Ranged : DamageClass
 	{
-		internal protected override string DisplayNameInternal => " " + Language.GetTextValue("LegacyTooltip.3");
+		internal protected override string DisplayNameInternal => Language.GetTextValue("LegacyTooltip.3").Substring(1);
 	}
 
 	public class Magic : DamageClass
 	{
-		internal protected override string DisplayNameInternal => " " + Language.GetTextValue("LegacyTooltip.4");
+		internal protected override string DisplayNameInternal => Language.GetTextValue("LegacyTooltip.4").Substring(1);
 	}
 
 	public class Summon : DamageClass
 	{
-		internal protected override string DisplayNameInternal => " " + Language.GetTextValue("LegacyTooltip.53");
+		internal protected override string DisplayNameInternal => Language.GetTextValue("LegacyTooltip.53").Substring(1);
 	}
 
 	public class Throwing : DamageClass
 	{
-		internal protected override string DisplayNameInternal => " " + Language.GetTextValue("LegacyTooltip.58");
+		internal protected override string DisplayNameInternal => Language.GetTextValue("LegacyTooltip.58").Substring(1);
 	}
 }
