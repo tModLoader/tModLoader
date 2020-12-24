c54f5db61112595b296568204735dc4b593b1f92
Jofairden <Jofairden@users.noreply.github.com>
2020-12-24 11:20:30 +0100

    Update SlopeID.cs

--- patches/tModLoader/Terraria/ID/SlopeID.cs
+++ patches/tModLoader/Terraria/ID/SlopeID.cs
@@ -5,22 +5,22 @@
 		/// <summary>
 		/// A full tile.
 		/// </summary>
-		public static byte None = 0;
+		public const byte None = 0;
 		/// <summary>
 		/// A tile with the bottom left corner solid.
 		/// </summary>
-		public static byte DownLeft = 1;
+		public const byte DownLeft = 1;
 		/// <summary>
 		/// A tile with the bottom right corner solid.
 		/// </summary>
-		public static byte DownRight = 2;
+		public const byte DownRight = 2;
 		/// <summary>
 		/// A tile with the top left corner solid.
 		/// </summary>
-		public static byte UpLeft = 3;
+		public const byte UpLeft = 3;
 		/// <summary>
 		/// A tile with the top right corner solid.
 		/// </summary>
-		public static byte UpRight = 4;
+		public const byte UpRight = 4;
 	}
 }
