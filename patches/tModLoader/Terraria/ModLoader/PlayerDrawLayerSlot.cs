e88ac3d10bb92f639b01596c699482ab9d5fc4fc
Mirsario <me@mirsar.io>
2020-12-27 16:13:31 +0300

    Repaired and overhauled player draw layers. (#1047)
    * Squashed '1.4_playerdrawing' branch.
    
    * Merged head draw layers into normal draw layers.
    
    * Renamed PlayerLayer to PlayerDrawLayer
    
    * PlayerLayer.cs -> PlayerDrawLayer.cs.
    
    * Reverted a debugging leftover.
    
    * Updated a comment.
    
    * Fixed issues with ArmorIDs.Wings.Stats not getting resized.
    
    * ExamplePlayerDrawLayer.
    
    * Force integer coordinates for drawdata rendering, to avoid quivering of modded draw layers' sprites..
    
    * Comment typo fixed.
    
    * Changed lists in ModPlayer.ModifyDrawLayers to readonly, + minor things.
    
    * Dumbed down ExamplePlayerDrawLayer.Draw
    
    * Replaced Setup method with GetDefaults, which should not alter layers' state on its own. Also made Parent virtual.
    
    * Removed an unneeded parameter in PlayerLayerHooks.AddDrawLayers
    
    * Added a default depth value for vanilla layers.
    
    * Update ExampleMod/Common/ExamplePlayerDrawLayer.cs
    
    Co-authored-by: Cameron Ewell <spazeglitch@gmail.com>
    
    * Removed a comment in EquipLoader.
    
    * Removed ModPlayer.AddDrawLayers, made jof's set's layers OOP.
    
    * Made LegacyPlayerDrawLayer internal.
    
    * Removed a duplicate description.
    
    * Fixed layer visibility not being checked against and reset for vanilla layers.
    
    * Reverted changes to drawdatainfo in jof's set.
    
    * Reverted forced "pixel-perfection".
    
    * Moved short-circuits in jof's layers to visibility setting.
    
    * Moved some patches to new PlayerLayerHooks.GetDrawLayers
    
    * Renamed PlayerLayerHooks to PlayerDrawLayerHooks.
    
    * Improved the example.
    
    * Replaced layer depth with constraints (unfinished!)
    
    * Back Accessory BugFix (#1082)
    
    Also modified shield for consistency.
    
    * update Example Player Draw Layer (#1121)
    
    * update example player draw layer
    
    * re-add comment
    
    * Fixed post-merge compilation.
    
    * Fixed runtime issues due to bad ArmorIDs patches.
    
    * Changed Player parameters to PlayerDrawSet. May need to clear draw data because of this.
    
    * Remove redundant factory for LegacyPlayerDrawLayer
    
    * Vanilla layers don't move anymore!
    
    * Implement player layer constraint based ordering. Layers can no longer move during the game.
    
    * Update ExamplePlayerDrawLayer and add missing ModContent patch from last commit
    
    * Fixed ModPlayer.ModifyDrawInfo never being called.
    
    * Rediff, oops.
    
    * Fixed compilation.
    
    * Applied ordering changes from 1.4.1.2
    
    * Replace PlayerLayer constraints with positions, handling static, moving and child layers.
    
    * Improve naming of PlayerDrawLayer features and conform to code style
    
    Co-authored-by: Cameron Ewell <spazeglitch@gmail.com>
    Co-authored-by: Marcus Kertesz <ModernMAK@Hotmail.com>
    Co-authored-by: Agrair <42445050+Agrair@users.noreply.github.com>
    Co-authored-by: Chicken-Bones <mehvids@gmail.com>

---
+++ patches/tModLoader/Terraria/ModLoader/PlayerDrawLayerSlot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Terraria.DataStructures;
+
+namespace Terraria.ModLoader
+{
+	[Autoload(false)]
+	public class PlayerDrawLayerSlot : PlayerDrawLayer
+	{
+		public PlayerDrawLayer Layer { get; }
+		public Multiple.Condition Condition { get; }
+
+		private readonly int _slot;
+
+		public override string Name => $"{Layer.Name}_slot{_slot}";
+
+		internal PlayerDrawLayerSlot(PlayerDrawLayer layer, Multiple.Condition cond, int slot) {
+			Layer = layer;
+			Condition = cond;
+			_slot = slot;
+		}
+
+		public override Position GetDefaultPosition() => throw new NotImplementedException();
+
+		protected override void Draw(ref PlayerDrawSet drawInfo) => Layer.DrawWithTransformationAndChildren(ref drawInfo);
+
+		public override bool GetDefaultVisiblity(PlayerDrawSet drawInfo) => Condition(drawInfo);
+	}
+}
