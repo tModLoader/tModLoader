25a0896407f43cb65f941de90b2694c3d2ed21a1
direwolf420 <amakejk1@web.de>
2020-12-30 13:19:08 +0100

    make BigProgressBarSystem partial

---
+++ patches/tModLoader/Terraria/GameContent/UI/BigProgressBar/BigProgressBarSystem.TML.cs
@@ -0,0 +1,11 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Terraria.GameContent.UI.BigProgressBar
+{
+	public partial class BigProgressBarSystem
+	{
+		public static NeverValidProgressBar NeverValid => _neverValid;
+	}
+}
