481c771516ab1f4430bbc1ed0050110b56330104
Chicken-Bones <mehvids@gmail.com>
2021-01-02 22:50:23 +1000

    Build TerrariaNetCore without any WindowsDesktop references

--- patches/Terraria/Terraria/MonoLaunch.cs
+++ patches/Terraria/Terraria/MonoLaunch.cs
@@ -1,5 +1,4 @@
 #if !WINDOWS
-using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,35 +10,41 @@ using Terraria;
 internal static class MonoLaunch
 {
 	private static void Main(string[] args) {
+		// FNA is requested by both Terraria and ReLogic.dll
+		var loaded = new Dictionary<string, Assembly>();
+
 		AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs sargs) {
 			string resourceName = new AssemblyName(sargs.Name).Name + ".dll";
 			string text = Array.Find(typeof(Program).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
 			if (text == null)
 				return null;
 
+			if (loaded.TryGetValue(text, out var assembly))
+				return assembly;
+
 			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(text)) {
 				byte[] array = new byte[stream.Length];
 				stream.Read(array, 0, array.Length);
-				return Assembly.Load(array);
+				loaded[text] = assembly = Assembly.Load(array);
+#if NETCORE
+				NativeLibrary.SetDllImportResolver(assembly, ResolveNativeLibrary);
+#endif
+				return assembly;
 			}
 		};
-#if NETCORE
-		NativeLibrary.SetDllImportResolver(typeof(Game).Assembly, resolver);
-		NativeLibrary.SetDllImportResolver(typeof(Steamworks.SteamAPI).Assembly, resolver);
-#endif
 		Environment.SetEnvironmentVariable("FNA_WORKAROUND_WINDOW_RESIZABLE", "1");
 		Program.LaunchGame(args, monoArgs: true);
 	}
 
 #if NETCORE
-	private static IntPtr resolver(string name, Assembly assembly, DllImportSearchPath? searchPath) {
+	private static IntPtr ResolveNativeLibrary(string name, Assembly assembly, DllImportSearchPath? searchPath) {
 		try {
 			if (assemblies.TryGetValue(name, out var handle)) {
 				return handle;
 			}
-			var path = Path.Combine(Environment.CurrentDirectory, "Libraries", "Native", getNativeDir(name));
-			var files = Directory.GetFiles(path, name + "*", SearchOption.AllDirectories);
-			var match = files.FirstOrDefault(s => File.Exists(s));
+			var dir = Path.Combine(Environment.CurrentDirectory, "Libraries", "Native", getNativeDir(name));
+			var files = Directory.GetFiles(dir, $"*{name}*", SearchOption.AllDirectories);
+			var match = files.FirstOrDefault();
 			if (match != null && NativeLibrary.TryLoad(match, out handle)) {
 				return assemblies[name] = handle;
 			}
