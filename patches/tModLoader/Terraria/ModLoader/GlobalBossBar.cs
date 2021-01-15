61044a4661a1aad4e2e6e8d08ec87d0e75b4611c
direwolf420 <amakejk1@web.de>
2021-01-15 12:41:05 +0100

    fix GlobalBossBar docs

--- patches/tModLoader/Terraria/ModLoader/GlobalBossBar.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalBossBar.cs
@@ -10,10 +10,10 @@ namespace Terraria.ModLoader
 		protected sealed override void Register() => BossBarLoader.AddGlobalBossBar(this);
 
 		/// <summary>
-		/// Allows you to draw things before the default draw code is ran. Return false to prevent drawing the ModBossBar. Returns true by default.
+		/// Allows you to draw things before the default draw code is ran. Return false to prevent drawing the bar. Returns true by default.
 		/// </summary>
 		/// <param name="spriteBatch">The spriteBatch that is drawn on</param>
-		/// <param name="npc">The NPC this ModBossBar is focused on</param>
+		/// <param name="npc">The NPC this bar is focused on</param>
 		/// <param name="drawParams">The draw parameters for the boss bar</param>
 		/// <returns><see langword="true"/> for allowing drawing, <see langword="false"/> for preventing drawing</returns>
 		public virtual bool PreDraw(SpriteBatch spriteBatch, NPC npc, ref BossBarDrawParams drawParams) => true;
@@ -23,7 +23,7 @@ namespace Terraria.ModLoader
 		/// </summary>
 		/// <param name="skipped"><see langword="true"/> if you or another mod has skipped drawing the bar in PreDraw (possibly hiding it or in favor of new visuals)</param>
 		/// <param name="spriteBatch">The spriteBatch that is drawn on</param>
-		/// <param name="npc">The NPC this ModBossBar is focused on</param>
+		/// <param name="npc">The NPC this bar is focused on</param>
 		/// <param name="drawParams">The draw parameters for the boss bar</param>
 		public virtual void PostDraw(bool skipped, SpriteBatch spriteBatch, NPC npc, BossBarDrawParams drawParams) {
 		}
