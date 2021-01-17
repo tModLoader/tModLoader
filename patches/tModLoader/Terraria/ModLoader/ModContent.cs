a03e660795c52a671a21afcc4cc1b0f436623c0b
ThomasThePencil <tailsguy24@gmail.com>
2021-01-16 21:48:10 -0500

    fixed CountsAsCache not being called
    also fixed a couple stray incorrect StatModifier combining cases

--- patches/tModLoader/Terraria/ModLoader/ModContent.cs
+++ patches/tModLoader/Terraria/ModLoader/ModContent.cs
@@ -389,6 +389,7 @@ namespace Terraria.ModLoader
 			Interface.loadMods.SetLoadStage("tModLoader.MSSettingUp");
 			ResizeArrays();
 			RecipeGroupHelper.FixRecipeGroupLookups();
+			DamageClassLoader.CountsAsCache();
 
 			Interface.loadMods.SetLoadStage("tModLoader.MSLoading", ModLoader.Mods.Length);
 			LoadModContent(token, mod => {
