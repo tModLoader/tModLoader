4abb6b21d8ef50945ac08bbbb629ae2f48d433c8
direwolf420 <amakejk1@web.de>
2021-01-18 16:58:19 +0100

    ExampleMod: fix array length

--- ExampleMod/Common/Systems/DownedBossSystem.cs
+++ ExampleMod/Common/Systems/DownedBossSystem.cs
@@ -94,7 +94,7 @@ namespace ExampleMod.Common.Systems
 				downedOtherBoss,
 			};
 			BitArray bitArray = new BitArray(flags);
-			byte[] bytes = new byte[bitArray.Length / 8]; //8 because that's the amount of bits in a byte
+			byte[] bytes = new byte[bitArray.Length / 8 + 1]; //8 because that's the amount of bits in a byte
 			bitArray.CopyTo(bytes, 0);
 
 			writer.Write(bytes.Length);
