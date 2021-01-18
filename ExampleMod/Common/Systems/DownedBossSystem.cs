e50daa8afdacec4ecbbd46ccb26141a11b31ff96
direwolf420 <amakejk1@web.de>
2021-01-18 16:02:47 +0100

    ExampleMod: more comments for DownedBossSystem, BitArray alternative

--- ExampleMod/Common/Systems/DownedBossSystem.cs
+++ ExampleMod/Common/Systems/DownedBossSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using Terraria;
 using Terraria.ModLoader;
@@ -9,23 +10,33 @@ namespace ExampleMod.Common.Systems
 	//Acts as a container for "downed boss" flags.
 	//Set a flag like this in your bosses OnKill hook:
 	//    NPC.SetEventFlagCleared(ref DownedBossSystem.downedMinionBoss, -1);
+
+	//Saving and loading these flags requires TagCompounds, a guide exists on the wiki: https://github.com/tModLoader/tModLoader/wiki/Saving-and-loading-using-TagCompound
 	public class DownedBossSystem : ModSystem
 	{
 		public static bool downedMinionBoss = false;
+		//public static bool downedOtherBoss = false;
 
 		public override void OnWorldLoad() {
 			downedMinionBoss = false;
+			//downedOtherBoss = false;
 		}
 
 		public override void OnWorldUnload() {
 			downedMinionBoss = false;
+			//downedOtherBoss = false;
 		}
 
 		public override TagCompound SaveWorldData() {
 			var downed = new List<string>();
+
 			if (downedMinionBoss) {
 				downed.Add("downedMinionBoss");
 			}
+
+			//if (downedOtherBoss) {
+			//	downed.Add("downedOtherBoss");
+			//}
 
 			return new TagCompound {
 				["downed"] = downed,
@@ -34,16 +45,20 @@ namespace ExampleMod.Common.Systems
 
 		public override void LoadWorldData(TagCompound tag) {
 			var downed = tag.GetList<string>("downed");
+
 			downedMinionBoss = downed.Contains("downedMinionBoss");
+			//downedOtherBoss = downed.Contains("downedOtherBoss");
 		}
 
 		public override void NetSend(BinaryWriter writer) {
+			//Order of operations is important and has to match that of NetReceive
 			var flags = new BitsByte();
 			flags[0] = downedMinionBoss;
+			//flags[1] = downedOtherBoss;
 			writer.Write(flags);
 
 			/*
-			Remember that Bytes/BitsByte only have 8 entries. If you have more than 8 flags you want to sync, use multiple BitsByte:
+			Remember that Bytes/BitsByte only have up to 8 entries. If you have more than 8 flags you want to sync, use multiple BitsByte:
 				This is wrong:
 			flags[8] = downed9thBoss; // an index of 8 is nonsense. 
 				This is correct:
@@ -56,13 +71,11 @@ namespace ExampleMod.Common.Systems
 			*/
 
 			//If you prefer, you can use the BitsByte constructor approach as well.
-			//writer.Write(saveVersion);
 			//BitsByte flags = new BitsByte(downedMinionBoss, downedOtherBoss);
 			//writer.Write(flags);
 
 			// This is another way to do the same thing, but with bitmasks and the bitwise OR assignment operator (the |=)
 			// Note that 1 and 2 here are bit masks. The next values in the pattern are 4,8,16,32,64,128. If you require more than 8 flags, make another byte.
-			//writer.Write(saveVersion);
 			//byte flags = 0;
 			//if (downedMinionBoss)
 			//{
@@ -73,14 +86,41 @@ namespace ExampleMod.Common.Systems
 			//	flags |= 2;
 			//}
 			//writer.Write(flags);
+
+			//If you plan on having more than 8 of these flags and don't want to use multiple BitsByte, an alternative is using a System.Collections.BitArray
+			/*
+			bool[] flags = new bool[] {
+				downedMinionBoss,
+				downedOtherBoss,
+			};
+			BitArray bitArray = new BitArray(flags);
+			byte[] bytes = new byte[bitArray.Length / 8]; //8 because that's the amount of bits in a byte
+			bitArray.CopyTo(bytes, 0);
+
+			writer.Write(bytes.Length);
+			writer.Write(bytes);
+			*/
 		}
 
 		public override void NetReceive(BinaryReader reader) {
+			//Order of operations is important and has to match that of NetSend
 			BitsByte flags = reader.ReadByte();
 			downedMinionBoss = flags[0];
-			// As mentioned in NetSend, BitBytes can contain 8 values. If you have more, be sure to read the additional data:
+			//downedOtherBoss = flags[1];
+
+			// As mentioned in NetSend, BitBytes can contain up to 8 values. If you have more, be sure to read the additional data:
 			// BitsByte flags2 = reader.ReadByte();
 			// downed9thBoss = flags[0];
+
+			//System.Collections.BitArray approach:
+			/*
+			int length = reader.ReadInt32();
+			byte[] bytes = reader.ReadBytes(length);
+
+			BitArray bitArray = new BitArray(bytes);
+			downedMinionBoss = bitArray[0];
+			downedOtherBoss = bitArray[1];
+			*/
 		}
 	}
 }
