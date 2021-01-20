f001aed2ef779beba95948325455991c2f6c1e79
direwolf420 <amakejk1@web.de>
2021-01-20 16:47:14 +0100

    documentation for TryGetSpecialVanillaBossBar

--- patches/tModLoader/Terraria/GameContent/UI/BigProgressBar/BigProgressBarSystem.TML.cs
+++ patches/tModLoader/Terraria/GameContent/UI/BigProgressBar/BigProgressBarSystem.TML.cs
@@ -3,7 +3,14 @@ namespace Terraria.GameContent.UI.BigProgressBar
 	public partial class BigProgressBarSystem
 	{
 		//If any mods need to fetch special vanilla bars
-		public bool TryGetSpecialVanillaBossBar(int netID, out IBigProgressBar bossBar) =>
-			_bossBarsByNpcNetId.TryGetValue(netID, out bossBar);
+		/// <summary>
+		/// Gets the special IBigProgressBar associated with this vanilla NPCs netID (usually the type).
+		/// <para> Reminder: If no special or modded bar exists, any NPC with a boss head index will automatically display a common boss bar shared among all simple bosses.</para>
+		/// </summary>
+		/// <param name="netID">The NPC netID (usually the type)</param>
+		/// <param name="bossBar">When this method returns, contains the IBigProgressBar associated with the specified NPC netID</param>
+		/// <returns><see langword="true"/> if IBigProgressBar exists; otherwise, <see langword="false"/>.</returns>
+		public bool TryGetSpecialVanillaBossBar(int netID, out IBigProgressBar bossBar)
+			 => _bossBarsByNpcNetId.TryGetValue(netID, out bossBar);
 	}
 }
