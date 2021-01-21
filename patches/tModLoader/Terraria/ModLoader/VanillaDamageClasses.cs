9e35fee1860b5511a516c3db28a3114a4c06d664
Chicken-Bones <mehvids@gmail.com>
2021-01-21 21:54:36 +1000

    Use arrow syntax in VanillaDamageClasses.GetBenefitsFrom

--- patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
+++ patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
@@ -27,59 +27,34 @@ namespace Terraria.ModLoader
 	{
 		protected override string LangKey => "LegacyTooltip.2";
 
-		protected override float GetBenefitsFrom(DamageClass damageClass) {
-			if (damageClass == Generic)
-				return 1f;
-
-			return 0f;
-		}
+		protected override float GetBenefitsFrom(DamageClass damageClass) => damageClass == Generic ? 1f : 0f;
 	}
 
 	public class Ranged : VanillaDamageClass
 	{
 		protected override string LangKey => "LegacyTooltip.3";
 
-		protected override float GetBenefitsFrom(DamageClass damageClass) {
-			if (damageClass == Generic)
-				return 1f;
-
-			return 0f;
-		}
+		protected override float GetBenefitsFrom(DamageClass damageClass) => damageClass == Generic ? 1f : 0f;
 	}
 
 	public class Magic : VanillaDamageClass
 	{
 		protected override string LangKey => "LegacyTooltip.4";
 
-		protected override float GetBenefitsFrom(DamageClass damageClass) {
-			if (damageClass == Generic)
-				return 1f;
-
-			return 0f;
-		}
+		protected override float GetBenefitsFrom(DamageClass damageClass) => damageClass == Generic ? 1f : 0f;
 	}
 
 	public class Summon : VanillaDamageClass
 	{
 		protected override string LangKey => "LegacyTooltip.53";
 
-		protected override float GetBenefitsFrom(DamageClass damageClass) {
-			if (damageClass == Generic)
-				return 1f;
-
-			return 0f;
-		}
+		protected override float GetBenefitsFrom(DamageClass damageClass) => damageClass == Generic ? 1f : 0f;
 	}
 
 	public class Throwing : VanillaDamageClass
 	{
 		protected override string LangKey => "LegacyTooltip.58";
 
-		protected override float GetBenefitsFrom(DamageClass damageClass) {
-			if (damageClass == Generic)
-				return 1f;
-
-			return 0f;
-		}
+		protected override float GetBenefitsFrom(DamageClass damageClass) => damageClass == Generic ? 1f : 0f;
 	}
 }
