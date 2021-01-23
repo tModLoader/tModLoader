89ebf4fe731e469c3b0405c345ce79b4ee25cc9e
ThomasThePencil <tailsguy24@gmail.com>
2021-01-23 13:43:07 -0500

    fixed DamageClass throwing an exception before the game can even start
    if this gets undone heads are gonna fucking roll

--- patches/tModLoader/Terraria/Item.TML.cs
+++ patches/tModLoader/Terraria/Item.TML.cs
@@ -17,11 +17,13 @@ namespace Terraria
 
 		private DamageClass _damageClass = DamageClass.Generic;
 		/// <summary>
-		/// The damage type of this Item. Assign to DamageClass.Melee/Ranged/Magic/Summon/Throwing for vanilla classes, or ModContent.GetInstance<T>() for custom damage types.
+		/// The damage type of this Item.
+		/// Assign to DamageClass.Melee/Ranged/Magic/Summon/Throwing for vanilla classes, or ModContent.GetInstance<T>() for custom damage types.
+		/// Set to DamageClass.Generic to not have any one damage class, but still have the weapon be boosted by universal bonuses.
 		/// </summary>
 		public DamageClass DamageType {
 			get => _damageClass;
-			set => _damageClass = value ?? throw new ArgumentException("DamageType cannot be null");
+			set => _damageClass = value;
 		}
 
 		/// <summary> Gets the instance of the specified GlobalItem type. This will throw exceptions on failure. </summary>
@@ -84,6 +86,11 @@ namespace Terraria
 		public static int NewItem(Vector2 position, int Type, int Stack = 1, bool noBroadcast = false, int prefixGiven = 0, bool noGrabDelay = false, bool reverseLookup = false) => 
 			NewItem((int)position.X, (int)position.Y, 0, 0, Type, Stack, noBroadcast, prefixGiven, noGrabDelay, reverseLookup);
 
-		public bool CountsAsClass(DamageClass damageClass) => DamageClassLoader.countsAs[DamageType.Type, damageClass.Type];
+		public bool CountsAsClass(DamageClass damageClass) {
+			if (DamageType != null)
+				return DamageClassLoader.countsAs[DamageType.Type, damageClass.Type];
+
+			return false;
+		}
 	}
 }
