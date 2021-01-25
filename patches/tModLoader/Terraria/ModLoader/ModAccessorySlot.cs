601d503f2812126dda6dbc7e6a65b88ab336800d
nmoll <nmoll@NMOLLER-DESK>
2021-01-25 16:36:21 -0700

    Pushing a stable version. Doesn't do anything; Need to add to ModAccessorySlot class: 1) an overridable method for custom conditions. Need to add to Player.cs: 1) Full support for custom conditions Need to add to Main.cs: 1) Complete the ModAccessorySlot Draw call. Need to create an instance of ModAccessorySlot in exampleMod to test

--- patches/tModLoader/Terraria/ModLoader/ModAccessorySlot.cs
+++ patches/tModLoader/Terraria/ModLoader/ModAccessorySlot.cs
@@ -18,108 +18,119 @@ namespace Terraria.ModLoader
 	/// <summary>
 	/// A ModAccessorySlot instance represents a net new accessory slot instance. You can store fields in the ModAccessorySlot class. 
 	/// </summary>
-	public abstract class ModAccessorySlot : ModType
+	public abstract class ModAccessorySlot : ModPlayer
 	{
-		public Player Player { get; internal set; }
-
-		internal int slot;
 		internal bool overrideVanillaConditions = false;
 
-		public void GetModdedAccessorySlot() {
+		internal int slot;
+		private int num20;
+		private Microsoft.Xna.Framework.Color color2;
+		private int yLoc;
+		private int xLoc;
+		private bool flag3;
+
+		protected void GetModdedAccessorySlot() {
 			int pendingID = Player.moddedAccSlots.IndexOf(this.FullName);
 			if (pendingID < 0) {
 				pendingID = Player.moddedAccSlots.Count;
 				Player.moddedAccSlots.Add(this.FullName);
 			}
-			slot = pendingID + Player.vanillaEquipSlots;
+			this.slot = pendingID + Player.vanillaEquipSlots;
 		}
 
-		public TagCompound Save() { // Needs to revisit and fix up.
+		public override TagCompound Save() { // Needs to revisit and fix up.
 			return new TagCompound {
-				["functional"] = Player.armor[slot],
-				["dyes"] = Player.dye[slot],
-				["vanity"] = Player.armor[slot + Player.totalEquipSlots],
+				["slot"] = ItemIO.Save(Player.armor[slot]),
+				["dyes"] = ItemIO.Save(Player.dye[slot]),
+				["vanity"] = ItemIO.Save(Player.armor[slot + Player.totalEquipSlots]),
 				["visible"] = (bool) Player.hideVisibleAccessory[slot]
 			};
 		}
 
-		internal void Load(TagCompound tag) {
-			Player.armor[slot] = tag.Get<Item>("functional");
+		public override void Load(TagCompound tag) {
+			Player.armor[slot] = tag.Get<Item>("slot");
 			Player.dye[slot] = tag.Get<Item>("dyes");
-			Player.armor[slot + Player.totalEquipSlots] = tag.Get<Item>("functional");
+			Player.armor[slot + Player.totalEquipSlots] = tag.Get<Item>("slot");
 			Player.hideVisibleAccessory[slot] = tag.Get<bool>("visible");
 		}
 
-		internal void Draw(int num20) {
+		public void Draw(int num20) { 
+			this.PreDraw(num20);
+			this.DrawFunctional();
+			this.DrawVanity();
+			this.DrawDye();
+		}
+
+		internal void PreDraw(int num20) {
+			this.num20 = num20;
+			this.flag3 = !Player.IsAValidEquipmentSlotForIteration(slot);
 			//int num20 = 174 + Main.mh; 
-			Microsoft.Xna.Framework.Color color2 = new Microsoft.Xna.Framework.Color(80, 80, 80, 80);
-			Microsoft.Xna.Framework.Color color = Main.inventoryBack;
-			int dyes = slot + Player.vanillaEquipSlots;
-			int functional = dyes;
-			int vanity = dyes + Player.totalEquipSlots;
-
-			int yLoc = (int)((float)(num20) + (float)(slot * 56) * Main.inventoryScale);
-			int yLoc2 = (int)((float)(num20 - 2) + (float)(slot * 56) * Main.inventoryScale);
-			bool flag3 = !Player.IsAValidEquipmentSlotForIteration(dyes);
-
-			int xLoc = Main.screenWidth - 64 - 28 - 47 * 3;
-
-			int xLoc2 = Main.screenWidth - 58 - 47 * 3;
-			
-			new Microsoft.Xna.Framework.Color(100, 100, 100, 100);
+			this.PreDrawCustom();
 			if (flag3)
 				Main.inventoryBack = color2;
+		}
 
-			/////////////////////////////////////////////////// Functional
+		// Override Method for setting custom colour and/or custom position
+		public void PreDrawCustom() {
+			this.color2 = new Microsoft.Xna.Framework.Color(80, 80, 80, 80);
+			this.yLoc = (int)((float)(num20) + (float)(slot * 56) * Main.inventoryScale);
+			this.xLoc = Main.screenWidth - 64 - 28 - 47 * 3;
+		}
+
+		internal void DrawFunctional() {
+			int yLoc2 = yLoc + (int)((float)(-2) + (float)(slot * 56) * Main.inventoryScale);
+			int xLoc2 = xLoc - 58 + 64 + 28;
 			int context = 8;
 
 			Texture2D value4 = TextureAssets.InventoryTickOn.Value;
-			if (Player.hideVisibleAccessory[functional])
+			if (Player.hideVisibleAccessory[slot])
 				value4 = TextureAssets.InventoryTickOff.Value;
 
 			Microsoft.Xna.Framework.Rectangle rectangle = new Microsoft.Xna.Framework.Rectangle(xLoc2, yLoc2, value4.Width, value4.Height);
 			int num45 = 0;
-			if (functional > Player.totalArmorSlots - 1 && rectangle.Contains(new Microsoft.Xna.Framework.Point(Main.mouseX, Main.mouseY)) && !PlayerInput.IgnoreMouseInterface) {
+			if (rectangle.Contains(new Microsoft.Xna.Framework.Point(Main.mouseX, Main.mouseY)) && !PlayerInput.IgnoreMouseInterface) {
 				Player.mouseInterface = true;
 				if (Main.mouseLeft && Main.mouseLeftRelease) {
-					Player.hideVisibleAccessory[functional] = !Player.hideVisibleAccessory[functional];
+					Player.hideVisibleAccessory[slot] = !Player.hideVisibleAccessory[slot];
 					SoundEngine.PlaySound(12);
 					if (Main.netMode == 1)
 						NetMessage.SendData(4, -1, -1, null, Player.whoAmI);
 				}
 
-				num45 = ((!Player.hideVisibleAccessory[functional]) ? 1 : 2);
+				num45 = ((!Player.hideVisibleAccessory[slot]) ? 1 : 2);
 			}
-			else if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc 
+			else if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc
 				&& (float)Main.mouseY <= (float)yLoc + (float)TextureAssets.InventoryBack.Height() * Main.inventoryScale && !PlayerInput.IgnoreMouseInterface) {
-					
+
 				Main.armorHide = true;
 				Player.mouseInterface = true;
-				ItemSlot.OverrideHover(Player.armor, context, functional);
+				ItemSlot.OverrideHover(Player.armor, context, slot);
 				if (!flag3 || Main.mouseItem.IsAir)
-					ItemSlot.LeftClick(Player.armor, context, functional);
+					ItemSlot.LeftClick(Player.armor, context, slot);
 
-				ItemSlot.MouseHover(Player.armor, context, functional);
+				ItemSlot.MouseHover(Player.armor, context, slot);
 			}
 
-			this.DrawModded(Player.armor, context, functional, new Vector2(xLoc, yLoc));
+			this.DrawModded(Player.armor, context, slot, new Vector2(xLoc, yLoc));
 
-			//TODO: figure out if below code is actually needed.
+			//TODO: figure out below code include or not.
 			Main.spriteBatch.Draw(value4, new Vector2(xLoc2, yLoc2), Microsoft.Xna.Framework.Color.White * 0.7f);
 			if (num45 > 0) {
 				Main.HoverItem = new Item();
 				Main.hoverItemName = Lang.inter[58 + num45].Value;
 			}
-			
-			///////////////////////////////////////////////// Vanity
+		}
+
+		internal void DrawVanity() {
+			int vanity = slot + Player.totalEquipSlots;
 			bool flag7 = flag3 && !Main.mouseItem.IsAir;
 			xLoc -= 47;
-				
-			context = 9;
+
+			int context = 9;
 			if (vanity > Player.totalEquipSlots + Player.totalArmorSlots - 1)
 				context = 11;
 
-			if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc 
+			if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc
 				&& (float)Main.mouseY <= (float)yLoc + (float)TextureAssets.InventoryBack.Height() * Main.inventoryScale && !PlayerInput.IgnoreMouseInterface) {
 				Player.mouseInterface = true;
 				Main.armorHide = true;
@@ -133,31 +144,32 @@ namespace Terraria.ModLoader
 			}
 
 			this.DrawModded(Player.armor, context, vanity, new Vector2(xLoc, yLoc));
-			
-			//////////////////////////////////////////////// Dyes
+		}
+
+		internal void DrawDye() {
 			bool flag8 = flag3 && !Main.mouseItem.IsAir;
 			xLoc -= 47;
 
-			if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc 
+			if (Main.mouseX >= xLoc && (float)Main.mouseX <= (float)xLoc + (float)TextureAssets.InventoryBack.Width() * Main.inventoryScale && Main.mouseY >= yLoc
 				&& (float)Main.mouseY <= (float)yLoc + (float)TextureAssets.InventoryBack.Height() * Main.inventoryScale && !PlayerInput.IgnoreMouseInterface) {
 				Player.mouseInterface = true;
 				Main.armorHide = true;
-				ItemSlot.OverrideHover(Player.dye, 12, dyes);
+				ItemSlot.OverrideHover(Player.dye, 12, slot);
 				if (!flag8) {
 					if (Main.mouseRightRelease && Main.mouseRight)
-						ItemSlot.RightClick(Player.dye, 12, dyes);
+						ItemSlot.RightClick(Player.dye, 12, slot);
 
-					ItemSlot.LeftClick(Player.dye, 12, dyes);
+					ItemSlot.LeftClick(Player.dye, 12, slot);
 				}
 
-				ItemSlot.MouseHover(Player.dye, 12, dyes);
+				ItemSlot.MouseHover(Player.dye, 12, slot);
 			}
-			this.DrawModded(Player.armor, 12, dyes, new Vector2(xLoc, yLoc));
+			this.DrawModded(Player.armor, 12, slot, new Vector2(xLoc, yLoc));
 		}
-		
-		//TODO: Make this overrideable... somehow. This is where custom draw can be done. Should default execute as written,
-		public void DrawModded(Item[] armor, int context, int slot, Vector2 position) {
-			ItemSlot.Draw(Main.spriteBatch, armor, context, slot, position);
+
+		// Override here for creating custom drawing at generated position
+		public void DrawModded(Item[] inv, int context, int slot, Vector2 position) {
+			ItemSlot.Draw(Main.spriteBatch, inv, context, slot, position);
 		}
 	}
 }
