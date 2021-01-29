4a84674a09c30a5a0fe80df16c088f917065d04c
Mirsario <me@mirsar.io>
2021-01-29 03:15:11 +0300

    Questionably expanded GlobalLoot.

--- patches/tModLoader/Terraria/ModLoader/GlobalLoot.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalLoot.cs
@@ -1,8 +1,10 @@
-﻿using Terraria.GameContent.ItemDropRules;
+﻿using System;
+using System.Collections.Generic;
+using Terraria.GameContent.ItemDropRules;
 
 namespace Terraria.ModLoader
 {
-	/// <summary> This readonly struct is a simple shortcut to add global drop rules to <see cref="ItemDropDatabase"/>'s methods. </summary>
+	/// <summary> This readonly struct is a simple shortcut for modifying global drop rules in an <see cref="ItemDropDatabase"/>. </summary>
 	public readonly struct GlobalLoot
 	{
 		private readonly ItemDropDatabase itemDropDatabase;
@@ -11,6 +13,24 @@ namespace Terraria.ModLoader
 			this.itemDropDatabase = itemDropDatabase;
 		}
 
+		//A whole new list is created here to avoid enumeration issues and direct edits. This is, of course, lame for the GC.
+		//Should this return an IReadOnlyList wrapper?
+		public List<IItemDropRule> Get() => new List<IItemDropRule>(itemDropDatabase._globalEntries);
+
 		public IItemDropRule Add(IItemDropRule entry) => itemDropDatabase.RegisterToGlobal(entry);
+
+		public IItemDropRule Remove(IItemDropRule entry) {
+			itemDropDatabase._globalEntries.Remove(entry);
+
+			return entry;
+		}
+
+		public void RemoveWhere(Predicate<IItemDropRule> predicate) {
+			foreach(var entry in Get()) {
+				if (predicate(entry)) {
+					Remove(entry);
+				}
+			}
+		}
 	}
 }
