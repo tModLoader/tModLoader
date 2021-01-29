0da86c1218ef23d5a53c7ee23acca742ae311834
direwolf420 <amakejk1@web.de>
2021-01-29 15:58:54 +0100

    ExampleMod: expand ExampleDefenseBuff

--- ExampleMod/Content/Buffs/ExampleDefenseBuff.cs
+++ ExampleMod/Content/Buffs/ExampleDefenseBuff.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace ExampleMod.Content.Buffs
@@ -9,11 +11,18 @@ namespace ExampleMod.Content.Buffs
 			DisplayName.SetDefault("Defensive Buff");
 			Description.SetDefault("Grants +4 defense.");
 			Main.buffNoTimeDisplay[Type] = false;
-			Main.debuff[Type] = false; //Add this so the nurse doesn't remove the buff when healing
+			Main.debuff[Type] = false; //Set this to true so the nurse doesn't remove the buff when healing
 		}
 
 		public override void Update(Player player, ref int buffIndex) {
 			player.statDefense += 4; //Grant a +4 defense boost to the player while the buff is active.
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, int buffIndex, ref Vector2 drawPosition, ref Rectangle sourceRectangle, ref Rectangle mouseRectangle, ref Color drawColor) {
+
+			drawColor = Color.LimeGreen * (Main.buffAlpha[buffIndex] / 255f);
+
+			return true;
 		}
 	}
 }
