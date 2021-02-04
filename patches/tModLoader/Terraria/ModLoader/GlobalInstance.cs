daa2e119ef20b21b6bfa9186ca601035602c8f25
Mirsario <me@mirsar.io>
2021-02-04 05:04:10 +0300

    StructLayout for GlobalInstance struct

--- patches/tModLoader/Terraria/ModLoader/GlobalInstance.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalInstance.cs
@@ -1,6 +1,9 @@
-﻿namespace Terraria.ModLoader
+﻿using System.Runtime.InteropServices;
+
+namespace Terraria.ModLoader
 {
 	/// <summary> The purpose of this struct is to micro-optimize lookups of GlobalX indices by providing these associations without additional retrievals from the heap. </summary>
+	[StructLayout(LayoutKind.Sequential, Pack = 2)]
 	internal readonly struct GlobalInstance<T> where T : class
 	{
 		public readonly short index;
