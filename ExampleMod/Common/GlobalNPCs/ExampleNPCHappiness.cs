b63f57cbdc331608128d2198954d63b80c28a842
kittenchilly <mario.pikmin.zelda@gmail.com>
2021-02-08 17:49:55 -0600

    ExampleNPCHappiness

---
+++ ExampleMod/Common/GlobalNPCs/ExampleNPCHappiness.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Common.GlobalNPCs
+{
+	//TODO: documentation
+	public class ExampleNPCHappiness : GlobalNPC 
+	{
+		public override void NPCHappiness(NPC npc, ref ShopHelper shopHelperInstance, ref int primaryPlayerBiome, ref bool[] nearbyNPCsByType) 
+		{
+			int ExamplePersonType = ModContent.NPCType<Content.NPCs.ExamplePerson>();
+			switch (npc.type) 
+			{
+				case NPCID.Guide:
+					if (nearbyNPCsByType[ExamplePersonType]) {
+						shopHelperInstance.LikeNPC(ExamplePersonType);
+					}
+					break;
+			}
+		}
+	}
+}
