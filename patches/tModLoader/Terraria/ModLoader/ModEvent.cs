3252499a0ce870037fd6a2a718a3f178be73dd43
Trivaxy <itsmeomar2003@yahoo.com>
2021-02-14 19:02:55 +0200

    start work on ModEvent

---
+++ patches/tModLoader/Terraria/ModLoader/ModEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Terraria.ModLoader
+{
+	public class ModEvent : ModType
+	{
+		public int Type { get; internal set; }
+
+		public bool Active { get; internal set; }
+
+		protected virtual void StartEvent() { }
+
+		protected virtual void StopEvent() { }
+
+		protected internal virtual void Update() { }
+
+		public void Start() {
+			Active = true;
+		}
+
+		protected override void Register() {
+			ModTypeLookup<ModEvent>.Register(this);
+			Type = ModEventLoader.Register(this);
+		}
+	}
+}
---
+++ patches/tModLoader/Terraria/ModLoader/ModEventLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Terraria.ModLoader
+{
+	public static class ModEventLoader
+	{
+		internal static readonly IList<ModEvent> ModEvents = new List<ModEvent>();
+
+		internal static int Register(ModEvent modEvent) {
+			ModEvents.Add(modEvent);
+			return ModEvents.Count - 1;
+		}
+
+		internal static void UpdateActiveEvents() {
+			foreach (ModEvent modEvent in ModEvents) {
+				if (modEvent.Active)
+					modEvent.Update();
+			}
+		}
+
+		internal static void Unload() {
+			ModEvents.Clear();
+		}
+	}
+}
