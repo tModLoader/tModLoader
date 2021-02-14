fe4baa1382088aace02fe3afa0f61964c6d25fdc
Mellozx <67687232+Mellozx@users.noreply.github.com>
2021-02-15 00:47:08 +0300

    Create ModInvasion

---
+++ patches/tModLoader/Terraria/ModLoader/ModInvasion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Terraria.ModLoader
+{
+	public class ModInvasion : ModEvent
+	{
+		public virtual Texture2D Icon { get; }
+
+		public float Progress { get; set; }
+
+		IDictionary<int, float> spawnPool = new Dictionary<int, float>();
+
+
+	}
+}
