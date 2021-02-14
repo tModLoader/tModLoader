78d823ab486108d9d27fbe1912d699970b149906
RighteousRyan <60587654+RighteousRyan1@users.noreply.github.com>
2021-02-14 11:48:02 -0600

    Summary for Update()

--- patches/tModLoader/Terraria/ModLoader/ModEvent.cs
+++ patches/tModLoader/Terraria/ModLoader/ModEvent.cs
@@ -3,13 +3,15 @@
 	public class ModEvent : ModType
 	{
 		public int Type { get; internal set; }
-
+		
 		public bool Active { get; internal set; }
-
+		
 		protected virtual void StartEvent() { }
 
 		protected virtual void StopEvent() { }
-
+		/// <summary>
+		/// An method that runs every tick while the event is activated.
+		/// </summary>
 		protected internal virtual void Update() { }
 
 		public void Start() {
