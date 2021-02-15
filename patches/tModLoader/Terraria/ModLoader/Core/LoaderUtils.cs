94794c29c7c486adf0f77ef65df48aeccd5aed42
Mirsario <me@mirsar.io>
2021-02-15 18:44:29 +0300

    Fixed ResetStaticMembers doing nothing on .net core.

--- patches/tModLoader/Terraria/ModLoader/Core/LoaderUtils.cs
+++ patches/tModLoader/Terraria/ModLoader/Core/LoaderUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Terraria.ModLoader.Core
 {
@@ -6,7 +7,26 @@ namespace Terraria.ModLoader.Core
 	{
 		/// <summary> Calls static constructors on the provided type and, optionally, its nested types. </summary>
 		public static void ResetStaticMembers(Type type, bool recursive) {
-			type.TypeInitializer?.Invoke(null, null);
+#if NETCORE
+			var typeInitializer = type.TypeInitializer;
+
+			if (typeInitializer != null) {
+				var field = typeInitializer
+					.GetType()
+					.GetField("m_invocationFlags", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+
+				object previousValue = field.GetValue(typeInitializer);
+
+				//.NET Core uses invocation flags on static constructor to ensure that they're never called twice. We'll have to ignore the law, and remove these flags temporarily.
+				field.SetValue(typeInitializer, (uint)0x00000001); //INVOCATION_FLAGS_INITIALIZED
+
+				typeInitializer.Invoke(null, null);
+
+				field.SetValue(typeInitializer, previousValue);
+			}
+#else
+			type.TypeInitializer.Invoke(null, null);
+#endif
 
 			if (recursive) {
 				foreach (var nestedType in type.GetNestedTypes()) {
