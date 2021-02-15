50cc44f90af3bd654b27d878e11e080eae08ed4b
Trivaxy <itsmeomar2003@yahoo.com>
2021-02-16 01:54:03 +0200

    make draw code a little more readable

--- patches/tModLoader/Terraria/Main.TML.cs
+++ patches/tModLoader/Terraria/Main.TML.cs
@@ -54,45 +54,53 @@ namespace Terraria
 				if (progressDisplay.Alpha >= 1f)
 					progressDisplay.Alpha = 1f;
 
-				float scale = 0.5f + progressDisplay.Alpha * 0.5f;
 				Texture2D icon = modInvasion.Icon;
+
 				string title = modInvasion.Title;
+				float alpha = progressDisplay.Alpha;
+				float scale = 0.5f + alpha * 0.5f;
 
 				int width = (int)(200f * scale);
 				int height = (int)(45f * scale);
-				Vector2 offset = (height + 50) * i * Vector2.UnitY;
+				Vector2 uiOffset = (height + 50) * i * Vector2.UnitY;
 
 				if (invasionType != 0 && invasionProgressAlpha > 0f)
-					offset.Y += height + 50;
+					uiOffset.Y += height + 50;
 
-				Vector2 uiPosition = new Vector2(screenWidth - 120, screenHeight - 40) - offset;
+				Vector2 uiPosition = new Vector2(screenWidth - 120, screenHeight - 40) - uiOffset;
 
-				Utils.DrawInvBG(R: new Rectangle((int)uiPosition.X - width / 2, (int)uiPosition.Y - height / 2, width, height), sb: spriteBatch, c: modInvasion.ProgressUIColor);
+				Utils.DrawInvBG(spriteBatch, new Rectangle((int)uiPosition.X - width / 2, (int)uiPosition.Y - height / 2, width, height), modInvasion.ProgressUIColor);
 
+				Texture2D progressBarOutline = TextureAssets.ColorBar.Value;
 				string progressText = modInvasion.ProgressText;
-				Texture2D value2 = TextureAssets.ColorBar.Value;
-
 				float progress = modInvasion.Progress;
 				float progressBarWidth = 169f * scale;
 				float progressBarHeight = 8f * scale;
 				Vector2 progressBarPos = uiPosition + Vector2.UnitY * progressBarHeight + Vector2.UnitX * 1f;
 
-				Utils.DrawBorderString(spriteBatch, progressText, progressBarPos, Color.White * progressDisplay.Alpha, scale, 0.5f, 1f);
-				spriteBatch.Draw(value2, uiPosition, null, Color.White * progressDisplay.Alpha, 0f, new Vector2(value2.Width / 2, 0f), scale, SpriteEffects.None, 0f);
+				Utils.DrawBorderString(spriteBatch, progressText, progressBarPos, Color.White * alpha, scale, 0.5f, 1f);
+
+				spriteBatch.Draw(progressBarOutline, uiPosition, null, Color.White * alpha, 0f, new Vector2(progressBarOutline.Width / 2, 0f), scale, SpriteEffects.None, 0f);
+
 				progressBarPos += Vector2.UnitX * (progress - 0.5f) * progressBarWidth;
-				spriteBatch.Draw(TextureAssets.MagicPixel.Value, progressBarPos, new Rectangle(0, 0, 1, 1), modInvasion.ProgressBarColor * progressDisplay.Alpha, 0f, new Vector2(1f, 0.5f), new Vector2(progressBarWidth * progress, progressBarHeight), SpriteEffects.None, 0f);
-				spriteBatch.Draw(TextureAssets.MagicPixel.Value, progressBarPos, new Rectangle(0, 0, 1, 1), new Color(255, 165, 0, 127) * progressDisplay.Alpha, 0f, new Vector2(1f, 0.5f), new Vector2(2f, progressBarHeight), SpriteEffects.None, 0f);
-				spriteBatch.Draw(TextureAssets.MagicPixel.Value, progressBarPos, new Rectangle(0, 0, 1, 1), Color.Black * progressDisplay.Alpha, 0f, new Vector2(0f, 0.5f), new Vector2(progressBarWidth * (1f - progress), progressBarHeight), SpriteEffects.None, 0f);
+
+				Texture2D magicPixel = TextureAssets.MagicPixel.Value;
+				Rectangle sourceRect = new Rectangle(0, 0, 1, 1);
+
+				spriteBatch.Draw(magicPixel, progressBarPos, sourceRect, modInvasion.ProgressBarColor * alpha, 0f, new Vector2(1f, 0.5f), new Vector2(progressBarWidth * progress, progressBarHeight), SpriteEffects.None, 0f);
+				spriteBatch.Draw(magicPixel, progressBarPos, sourceRect, new Color(255, 165, 0, 127) * alpha, 0f, new Vector2(1f, 0.5f), new Vector2(2f, progressBarHeight), SpriteEffects.None, 0f);
+				spriteBatch.Draw(magicPixel, progressBarPos, sourceRect, Color.Black * alpha, 0f, new Vector2(0f, 0.5f), new Vector2(progressBarWidth * (1f - progress), progressBarHeight), SpriteEffects.None, 0f);
 
 				Vector2 invasionTitleSize = FontAssets.MouseText.Value.MeasureString(title);
-				float num13 = 120f;
-				if (invasionTitleSize.X > 200f)
-					num13 += invasionTitleSize.X - 200f;
+				float titleUIWidth = 120f;
 
-				Rectangle r3 = Utils.CenteredRectangle(new Vector2(screenWidth - num13, screenHeight - 80) - offset, (invasionTitleSize + new Vector2(icon.Width + 12, 6f)) * scale);
-				Utils.DrawInvBG(spriteBatch, r3, modInvasion.TitleUIColor);
-				spriteBatch.Draw(icon, r3.Left() + Vector2.UnitX * scale * 8f, null, Color.White * progressDisplay.Alpha, 0f, new Vector2(0f, icon.Height / 2), scale * 0.8f, SpriteEffects.None, 0f);
-				Utils.DrawBorderString(spriteBatch, title, r3.Right() + Vector2.UnitX * scale * -22f, Color.White * progressDisplay.Alpha, scale * 0.9f, 1f, 0.4f);
+				if (invasionTitleSize.X > 200f)
+					titleUIWidth += invasionTitleSize.X - 200f;
+
+				Rectangle titleUIRect = Utils.CenteredRectangle(new Vector2(screenWidth - titleUIWidth, screenHeight - 80) - uiOffset, (invasionTitleSize + new Vector2(icon.Width + 12, 6f)) * scale);
+				Utils.DrawInvBG(spriteBatch, titleUIRect, modInvasion.TitleUIColor);
+				spriteBatch.Draw(icon, titleUIRect.Left() + Vector2.UnitX * scale * 8f, null, Color.White * alpha, 0f, new Vector2(0f, icon.Height / 2), scale * 0.8f, SpriteEffects.None, 0f);
+				Utils.DrawBorderString(spriteBatch, title, titleUIRect.Right() + Vector2.UnitX * scale * -22f, Color.White * alpha, scale * 0.9f, 1f, 0.4f);
 
 				i++;
 			}
