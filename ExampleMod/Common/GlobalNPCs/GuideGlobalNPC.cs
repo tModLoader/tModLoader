9442ad2b569c051ae0d0966eeac79fc522896aea
Mirsario <me@mirsar.io>
2021-02-19 22:09:31 +0300

    Improved EM tests.

--- ExampleMod/Common/GlobalNPCs/GuideGlobalNPC.cs
+++ ExampleMod/Common/GlobalNPCs/GuideGlobalNPC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -12,11 +13,9 @@ namespace ExampleMod.Common.GlobalNPCs
 		}
 
 		public override void AI(NPC npc) {
-			if (npc.velocity.Y == 0f) {
-				Main.NewText($"NPC {npc.GetTypeNetName()}: I am a guide.");
-
-				npc.velocity.Y = Math.Min(npc.velocity.Y, -10f);
-			}
+			//Make the guide giant and green.
+			npc.scale = 1.5f;
+			npc.color = Color.ForestGreen;
 		}
 	}
 }
