760c2d178e79276ac5c2b9a300c7d62714238965
Mello <67687232+Mellozx@users.noreply.github.com>
2021-03-08 07:22:28 +0300

    Change ExampleSpear according to CB's comments

--- ExampleMod/Content/Items/Weapons/ExampleSpear.cs
+++ ExampleMod/Content/Items/Weapons/ExampleSpear.cs
@@ -13,20 +13,17 @@ namespace ExampleMod.Content.Items.Weapons
 		}
 
 		public override void SetDefaults() { 
-			item.damage = 40; // The damage inflicted by the item.
-			item.useStyle = ItemUseStyleID.Shoot; // The use style of your item.
-			item.useAnimation = 18; // The time span of the using animation for the item. 
-			item.useTime = 24; // The time span of using the item in frames.
-			item.shootSpeed = 3.7f; // The velocity in pixels the projectile fired by the item will have.
+			item.damage = 40; 
+			item.useStyle = ItemUseStyleID.Shoot; // The use style of your item, UseStyle shoot draws the item sprite in the direction of the cursor.
+			item.useAnimation = 18; // Animation Duration (In ticks).
+			item.useTime = 24; // Usetime Duration (In ticks).
+			item.shootSpeed = 3.7f; // The velocity in pixels per frame the projectile fired by the item will have.
 			item.knockBack = 6.5f; // The force of the knock back. Max value is 20.
-			item.width = 32; // The width of the item's hitbox in pixels.
-			item.height = 32; // The height of the item's hitbox in pixels.
-			item.scale = 1f; // The size multiplier of the item's sprite while the item is being used. Also increases range for melee weapons.
 			item.rare = ItemRarityID.Pink; // Rarity/Color of the item you can look at the rest at https://terraria.gamepedia.com/Rarity.
 			item.value = Item.sellPrice(silver: 10); // Value is the number of coins the item is worth.
 
-			item.melee = true; //Whether or not your item is part of the melee class.
-			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+			item.melee = true; // The type of damage this item deals, if it is a weapon. Setting more than one is useless.
+			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item, Needed to mention that the projectile also has its own sprite.
 			item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
 			item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
 
