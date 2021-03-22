600f0bb0d83e8f93ea2b594aefe332bff3747d76
FullOfXP <71927028+FullOfXP@users.noreply.github.com>
2021-03-22 22:21:30 +0100

    ExampleMinecartBuff

---
+++ ExampleMod/Buffs/ExampleMinecartBuff.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Buffs
+{
+	public class ExampleMinecartBuff : ModBuff
+	{ 
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Example Minecart");
+			Description.SetDefault("Example your rails");
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.buffNoSave[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.mount.SetMount(ModContent.MountType<Mounts.ExampleMinecart>(), player);
+			player.buffTime[buffIndex] = 10;
+		}
+	}
+}
