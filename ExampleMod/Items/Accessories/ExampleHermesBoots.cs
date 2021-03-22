5f783120b1124dacb092f76f51241b6ce826410b
Naka <jaonakamura@gmail.com>
2021-03-22 03:29:52 -0300

    Example Hermes Boots Implementation
    ExampleHermesBoots.cs added

---
+++ ExampleMod/Items/Accessories/ExampleHermesBoots.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Accessories
+{
+	public class ExampleHermesBoots : ModItem
+	{
+		public override string Texture => "Terraria/Item_" + ItemID.HermesBoots;
+
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Hermes Boots");
+			Tooltip.SetDefault("The wearer can run super fast");
+		}
+
+		public override void SetDefaults() {
+			item.width = 28; 
+			item.height = 24;
+			item.accessory = true; // Makes this item an accessory.
+			item.color = Color.Orange; // Makes the item's sprite color orange.
+			item.rare = ItemRarityID.Blue;
+			item.value = Item.sellPrice(gold: 1); // Sets the item sell price to one gold coin.
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			player.accRunSpeed = 6f; // Max movement speed for mounts.
+			player.moveSpeed += 0.05f; // Increases the player movement speed, making it reach the max run speed faster.
+		}
+	}
+}
