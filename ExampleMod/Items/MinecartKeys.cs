e14d27f6ae23cbc43292a6d473fde9b0e9b74836
FullOfXP <71927028+FullOfXP@users.noreply.github.com>
2021-03-24 09:38:45 +0100

    added sprite bcs I felt like i wantedto

--- ExampleMod/Items/MinecartKeys.cs
+++ ExampleMod/Items/MinecartKeys.cs
@@ -8,7 +8,6 @@ namespace ExampleMod.Items
 {
 	public class MinecartKeys : ModItem
 	{
-		public override string Texture => "Terraria/Item_" + ItemID.MechanicalWheelPiece; //use a vanilla item texture
 		public override void SetStaticDefaults()
 		{
 			Tooltip.SetDefault("This is a modded minecart.");
@@ -16,7 +15,6 @@ namespace ExampleMod.Items
 
 		public override void SetDefaults()
 		{
-			item.color = Color.Purple; //changing vanilla item color
 			item.width = 20;
 			item.height = 30;
 			item.useTime = 20;
@@ -26,14 +24,13 @@ namespace ExampleMod.Items
 			item.rare = ItemRarityID.Green;
 			item.UseSound = SoundID.Item79;
 			item.noMelee = true;
-			MountID.Sets.Cart[ModContent.MountType<ExampleMinecart>()] = true; //this line makies it automatically equip into aminecart slot
 			item.mountType = ModContent.MountType<ExampleMinecart>();
 		}
 
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ModContent.ItemType<ExampleItem>(), 10);
+			recipe.AddIngredient(ModContent.ItemType<ExampleItem>(), 420);
 			recipe.AddTile(ModContent.TileType<ExampleWorkbench>());
 			recipe.SetResult(this);
 			recipe.AddRecipe();
