e7e85011a95f7a499741780c9659318e660a0214
JavidPack <javidpack@gmail.com>
2021-03-24 14:51:03 -0600

    Updated some comments, added custom sparks

---
+++ ExampleMod/Dusts/ExampleMinecartDust.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Dusts
+{
+	public class ExampleMinecartDust : ModDust
+	{
+		public override void SetDefaults()
+		{
+			updateType = 213;
+		}
+
+		public override Color? GetAlpha(Dust dust, Color lightColor)
+		{
+			int fade = (int)(dust.scale / 2.5f * 255f);
+			return new Color(fade, fade, fade, fade);
+		}
+	}
+}
--- ExampleMod/Mounts/ExampleMinecart.cs
+++ ExampleMod/Mounts/ExampleMinecart.cs
@@ -17,33 +17,34 @@ namespace ExampleMod.Mounts
 	{
 		public override void SetDefaults()
 		{
-			//what separates mounts and minecarts are these 3 lines
+			// What separates mounts and minecarts are these 3 lines
 			mountData.Minecart = true;
-			mountData.MinecartDust = new Action<Vector2>(DelegateMethods.Minecart.Sparks); // this will make the minecart spawn dust when slowing down
-			MountID.Sets.Cart[ModContent.MountType<ExampleMinecart>()] = true; //this makes the minecarts item autoequip in the minecart slot
+			// This makes the minecarts item autoequip in the minecart slot
+			MountID.Sets.Cart[ModContent.MountType<ExampleMinecart>()] = true; 
+			// The specified method takes care of spawning dust when stopping or jumping. Use DelegateMethods.Minecart.Sparks for normal sparks.
+			mountData.MinecartDust = GreenSparks; 
 
-			int[] array = new int[3];
-			for (int i = 0; i < array.Length; i++) 
-			{
-				array[i] = 9; //edit this to edit the playerYoffset
-			}
 			mountData.spawnDust = 16;
-			mountData.buff = ModContent.BuffType<ExampleMinecartBuff>(); //serves the same purpose as for Car.cs
-			mountData.flightTimeMax = 0; //always set flight time to 0 for minecarts
-			mountData.fallDamage = 1f; //hwo much fall dmg will the player take in the minecart
-			mountData.runSpeed = 10f; //how fast can the minecart go
-			mountData.acceleration = 0.04f; //how fast does the minecart accelerate
-			mountData.jumpHeight = 15; //how far does the minecart jump
-			mountData.jumpSpeed = 5.15f; //how fast does the minecart jump
-			mountData.blockExtraJumps = true; //Can the player use a could in a bottle when in the minecart?
-			mountData.heightBoost = 12;
-			mountData.playerYOffsets = array; //where is the players Y position on the mount
-			mountData.xOffset = 2; //the X offset of the minecarts sprite
-			mountData.yOffset = 9; //the Y offset of the minecarts sprite
-			mountData.bodyFrame = 3; //which body frame is being used from the player when the player is boarded on the minecart
+			mountData.buff = ModContent.BuffType<ExampleMinecartBuff>(); // serves the same purpose as for Car.cs
 
-			#region Sheeting
-			//sheeting for the minecart is already done, edit this only if your minecart doesnt match vanilla minecart sheets
+			// Movement fields:
+			mountData.flightTimeMax = 0; // always set flight time to 0 for minecarts
+			mountData.fallDamage = 1f; // how much fall damage will the player take in the minecart
+			mountData.runSpeed = 10f; // how fast can the minecart go
+			mountData.acceleration = 0.04f; // how fast does the minecart accelerate
+			mountData.jumpHeight = 15; // how far does the minecart jump
+			mountData.jumpSpeed = 5.15f; // how fast does the minecart jump
+			mountData.blockExtraJumps = true; // Can the player not use a could in a bottle when in the minecart?
+			mountData.heightBoost = 12;
+
+			// Drawing fields:
+			mountData.playerYOffsets = new int[] { 9, 9, 9 }; // where is the players Y position on the mount for each frame of animation
+			mountData.xOffset = 2; // the X offset of the minecarts sprite
+			mountData.yOffset = 9; // the Y offset of the minecarts sprite
+			mountData.bodyFrame = 3; // which body frame is being used from the player when the player is boarded on the minecart
+			mountData.playerHeadOffset = 14; // Affects where the player head is drawn on the map
+
+			// Animation fields: The following is the mount animation values shared by vanilla minecarts. It can be edited if you know what you are doing.
 			mountData.totalFrames = 3;
 			mountData.standingFrameCount = 1;
 			mountData.standingFrameDelay = 12;
@@ -65,8 +66,24 @@ namespace ExampleMod.Mounts
 				mountData.textureWidth = mountData.frontTexture.Width;
 				mountData.textureHeight = mountData.frontTexture.Height;
 			}
+		}
 
-			#endregion
+		// This code adapted from DelegateMethods.Minecart.Sparks. Custom sparks are just and example and are not required.
+		private void GreenSparks(Vector2 dustPosition)
+		{
+			dustPosition += new Vector2((Main.rand.Next(2) == 0) ? 13 : (-13), 0f).RotatedBy(DelegateMethods.Minecart.rotation);
+			int num = Dust.NewDust(dustPosition, 1, 1, ModContent.DustType<Dusts.ExampleMinecartDust>(), Main.rand.Next(-2, 3), Main.rand.Next(-2, 3));
+			Main.dust[num].noGravity = true;
+			Main.dust[num].fadeIn = Main.dust[num].scale + 1f + 0.01f * (float)Main.rand.Next(0, 51);
+			Main.dust[num].noGravity = true;
+			Main.dust[num].velocity *= (float)Main.rand.Next(15, 51) * 0.01f;
+			Main.dust[num].velocity.X *= (float)Main.rand.Next(25, 101) * 0.01f;
+			Main.dust[num].velocity.Y -= (float)Main.rand.Next(15, 31) * 0.1f;
+			Main.dust[num].position.Y -= 4f;
+			if (Main.rand.Next(3) != 0)
+				Main.dust[num].noGravity = false;
+			else
+				Main.dust[num].scale *= 0.6f;
 		}
 	}
 }
