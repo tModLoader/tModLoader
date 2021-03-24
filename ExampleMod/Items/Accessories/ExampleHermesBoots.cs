5185b9f385b846c771dc9f2d57901146e6c69622
Naka <60052047+naakaamura@users.noreply.github.com>
2021-03-24 17:28:27 -0300

    Example Hermes Boots (#1376)
    * Example Hermes Boots Implementation
    
    ExampleHermesBoots.cs added
    
    * Example Cloud in a Bottle Implemenation
    
    Added ExampleCloudinaBottle.cs
    
    * Changed a few comments
    
    * Removed Example Cloud in a Bottle
    
    Deleted ExampleCloudinaBottle.cs
    
    * Added a sprite
    
    Co-authored-by: Naka <jaonakamura@gmail.com>

---
+++ ExampleMod/Items/Accessories/ExampleHermesBoots.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Accessories
+{
+	public class ExampleHermesBoots : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Hermes Boots");
+			Tooltip.SetDefault("The wearer can run super fast");
+		}
+
+		public override void SetDefaults() {
+			item.width = 28; 
+			item.height = 24;
+			item.accessory = true; // Makes this item an accessory.
+			item.rare = ItemRarityID.Blue; 
+			item.value = Item.sellPrice(gold: 1); // Sets the item sell price to one gold coin.
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			player.accRunSpeed = 6f; // The player's maximum run speed with accessories
+			player.moveSpeed += 0.05f; // The acceleration multiplier of the player's movement speed
+		}
+	}
+}
