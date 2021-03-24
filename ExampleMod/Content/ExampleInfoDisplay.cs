503fa7b756ba11b399f00d940ce6126849fa22b8
JavidPack <javidpack@gmail.com>
2021-03-23 18:19:35 -0600

    Fix hideInfo save load bug, support modded save load. ExampleInfoDisplay

---
+++ ExampleMod/Content/ExampleInfoDisplay.cs
@@ -0,0 +1,23 @@
+﻿using ExampleMod.Common.Players;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content
+{
+	class ExampleInfoDisplay : InfoDisplay
+	{
+		public override void SetupContent() {
+			InfoName.SetDefault("Minion Count");
+		}
+
+		public override bool Active() {
+			return Main.LocalPlayer.GetModPlayer<ExamplePlayer>().ShowMinionCount;
+		}
+
+		public override string DisplayValue() {
+			int minionCount = Main.projectile.Count(x =>x.active && x.minion && x.owner == Main.LocalPlayer.whoAmI);
+			return minionCount > 0 ? $"{minionCount} minions." : "No minions";
+		}
+	}
+}
