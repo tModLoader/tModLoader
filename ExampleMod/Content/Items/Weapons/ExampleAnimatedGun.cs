710151f371075147af090676a037b9fa0c18c023
andropov_a <andropov@geogracom.com>
2021-03-29 09:58:57 +0300

    * Added Example Animated Gun

---
+++ ExampleMod/Content/Items/Weapons/ExampleAnimatedGun.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	//This example show how to made an Animated Gun(like Vortex Beater)
+	//The main idea is to create in this script an animated Projectile that will behave like a Gun(ExampleAnimatedGunProjectile)  
+	//Therefore, most of the properties of the weapon are not needed, but we can still use them in the projectile.
+	public class ExampleAnimatedGun : ModItem
+	{
+		public override string Texture => "Terraria/Images/Item_" + ItemID.VortexBeater;
+
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("This is a modded animated gun.");
+		}
+
+		public override void SetDefaults() {
+			//Important properties for Animation
+			Item.channel = true; //Set wapon to channel mode. When the attack button is held, we create only one projectile(animated gun projectile).
+			Item.noUseGraphic = true; //The item's sprite will not be visible while the item is in use
+
+			// This is common properties for Guns like in ExampleGun (https://github.com/tModLoader/tModLoader/wiki/Item-Class-Documentation)
+			// Common Properties
+			Item.width = 66; // Hitbox width of the item.
+			Item.height = 28; // Hitbox height of the item.
+			Item.rare = ItemRarityID.Green; // The color that the item's name will be in-game.
+
+			// Use Properties
+			Item.useTime = 8; // The item's use time in ticks (60 ticks == 1 second.). Not necessary, but we can use it for inner projectiles.
+			Item.useAnimation = 8; // The length of the item's use animation in ticks (60 ticks == 1 second.). Not necessary same as previosly.
+			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
+			Item.autoReuse = true; // Whether or not you can hold click to automatically use it again. Not necessary, but if we want to repeat sound or create multiple projectiles.
+			Item.UseSound = SoundID.Item13; // The sound that this item plays when used. 
+
+			// Weapon Properties
+			Item.DamageType = DamageClass.Ranged; // Sets the damage type to ranged.
+			Item.damage = 50; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			Item.knockBack = 2f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
+			Item.noMelee = true; // So the item's animation doesn't do damage.
+
+			// Gun Properties
+			Item.shoot = ModContent.ProjectileType<Projectiles.ExampleAnimatedGunProjectile>(); //It must not be null, but it can be anything. Can be use for creatin multiple projectiles. 
+			Item.shootSpeed = 20f; // The speed of the inner projectile (measured in pixels per frame.)
+			Item.useAmmo = AmmoID.Bullet; // The "ammo Id" of the ammo item that this weapon uses. Ammo IDs are magic numbers that usually correspond to the item id of one item that most commonly represent the ammo type.
+		}
+
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+			// Create animated gun projectile(ExampleAnimatedGunProjectile). Speed not used because it manualy changed inside projectile script.
+			// Item.useTime used as cooldown for shooting bullets inside Projectile script. Timers good choice for that stuff. (https://github.com/tModLoader/tModLoader/wiki/Basic-Projectile) 
+			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ModContent.ProjectileType<Projectiles.ExampleAnimatedGunProjectile>(), damage, knockBack, player.whoAmI, Item.useTime, Item.useTime);
+			// We manually create the projectiles, so we don't need the default behavior 
+			return false;
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleAnimatedGunProjectile.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	// This Animated Gun based on Projectile
+	public class ExampleAnimatedGunProjectile: ModProjectile
+	{
+		public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.VortexBeater;
+		public override void SetDefaults() {
+			Projectile.width = 68; // The width of projectile hitbox
+			Projectile.height = 30; // The height of projectile hitbox
+			Projectile.aiStyle = 0; // The ai style of the projectile, please reference the source code of Terraria (0 - to manually change behavior )
+			Projectile.friendly = true; // Can the projectile deal damage to enemies?
+			Projectile.hostile = false;  // Can the projectile deal damage to the player?
+			Projectile.tileCollide = false; // Can the projectile collide with tiles?
+			Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+
+
+			Projectile.penetrate = -1; //How many monsters the projectile can penetrate. This is a gun, so it cannot penetrate anything. 
+			Main.projFrames[Projectile.type] = 7; // How many frames of animation 
+
+			Projectile.hide = true; // This property is required for special rendering of projectiles. Without it, the rendered gun can be a little laggy. 
+		}
+
+
+		public override void AI() {
+			// Get current player and mounted point(The point around which the hand rotates)
+			Player player = Main.player[Projectile.owner];
+			Vector2 mountedCenter = player.RotatedRelativePoint(player.MountedCenter);
+
+			// Looping animation
+			if (++Projectile.frameCounter >= 4) {
+				Projectile.frameCounter = 0;
+				if (++Projectile.frame >= Main.projFrames[Projectile.type])
+					Projectile.frame = 0;
+			}
+
+			//Using Timers for implementing shoot cooldowns
+			bool cooldown = true;
+			Projectile.ai[1] += 1f;
+			if (Projectile.ai[1] >= Projectile.ai[0]) {
+				Projectile.ai[1] = 0;
+				cooldown = false;
+			}
+
+			//If cooldown is over and we inside current player(for multiplayer)
+			if (!cooldown && Main.myPlayer == Projectile.owner) {
+
+				// We can shoot if the button is pressed and there is still ammunition, and the player has items.
+				bool canShoot = player.channel && player.HasAmmo(player.inventory[player.selectedItem], canUse: true) && !player.noItems && !player.CCed;
+
+				if (canShoot) {
+					//Setting properties for bullets. This can be moved to the fields of the class
+					int projToShoot = ProjectileID.Bullet;
+					int damage = player.GetWeaponDamage(player.inventory[player.selectedItem]);
+					float knockBack = player.GetWeaponKnockback(player.inventory[player.selectedItem], player.inventory[player.selectedItem].knockBack);
+					float speed = player.inventory[player.selectedItem].shootSpeed;
+					
+					//Using ammo
+					player.PickAmmo(player.inventory[player.selectedItem], ref projToShoot, ref speed, ref canShoot, ref damage, ref knockBack);
+					
+					Vector2 rayFromMountedCenterToMouseCoordinates = Main.MouseWorld - mountedCenter;
+					//We normalize the ray so it coordinates from 0 to 1
+					Vector2 rotatingPoint = Vector2.Normalize(rayFromMountedCenterToMouseCoordinates);
+					//Offset the sprite from the ray (it cannot be (0, 0)). If you need to correct the position of the sprite
+					rotatingPoint *= new Vector2(x: 20, y: 20);
+
+					//Sync in mulriplayer when velocity is change
+					if (rotatingPoint.X != Projectile.velocity.X || rotatingPoint.Y != Projectile.velocity.Y)
+						Projectile.netUpdate = true;
+					Projectile.velocity = rotatingPoint;
+
+					//Create projectile of bullet
+					Projectile.NewProjectile(mountedCenter, rayFromMountedCenterToMouseCoordinates, projToShoot, damage, knockBack, Projectile.owner);
+				}
+				else {
+					Projectile.Kill();
+				}
+			}
+			// Updating projectile parameters
+			Projectile.position = player.RotatedRelativePoint(player.MountedCenter, false, false) - Projectile.Size / 2f;
+			Projectile.rotation = Projectile.velocity.ToRotation() + (Projectile.spriteDirection == -1 ? (float)Math.PI : 0f);
+			Projectile.spriteDirection = Projectile.direction;
+			
+			player.ChangeDir(Projectile.direction); // Update player direction
+			player.heldProj = Projectile.whoAmI; // The sprite is drawn in hand (If disabled, the sprite will be behind the player)
+			Projectile.timeLeft = 2; // As long as the weapon can shoot, we update the life time
+			player.SetDummyItemTime(2); // Always setting Item.useTime, Item.useAnimation to 2 frames so the Shoot method will never be called once again
+			// It is necessary for the player to understand the position of the item. (for example, how to rotate hand)
+			player.itemRotation = MathHelper.WrapAngle((float)Math.Atan2(Projectile.velocity.Y * (float)Projectile.direction, Projectile.velocity.X * (float)Projectile.direction)); 
+		}
+	}
+}
