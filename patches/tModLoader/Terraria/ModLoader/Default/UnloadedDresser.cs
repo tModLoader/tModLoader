b7bd8ec51eac7f202ae08d4026039b67f213adf8
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-03-30 20:19:57 -0600

    Make Furniture Roomy Again

--- patches/tModLoader/Terraria/ModLoader/Default/UnloadedDresser.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/UnloadedDresser.cs
@@ -19,7 +19,8 @@ namespace Terraria.ModLoader.Default
 			Main.tileSolidTop[Type] = true;
 			Main.tileContainer[Type] = true;
 			TileID.Sets.BasicDresser[Type] = true;
-			
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2); // Disables hammering
 			TileObjectData.addTile(Type);
 
