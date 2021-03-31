dfe30aae2a3ecc8f5f7bc45db2132e386ddb90f5
JavidPack <javidpack@gmail.com>
2021-03-31 16:43:35 -0600

    v0.11.8.3 Release, gog support for 1.4.2.1 hotfix. Backup vanilla exe.

--- solutions/CompleteRelease.bat
+++ solutions/CompleteRelease.bat
@@ -4,7 +4,7 @@
 @ECHO off
 :: Compile/Build exe 
 echo "Building Release"
-set tModLoaderVersion=v0.11.8.2
+set tModLoaderVersion=v0.11.8.3
 call buildRelease.bat
 
 set destinationFolder=.\tModLoader %tModLoaderVersion% Release
