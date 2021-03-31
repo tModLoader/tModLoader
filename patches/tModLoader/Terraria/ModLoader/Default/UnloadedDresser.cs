a1f7d3005d5b533837e3646d3c5994fa43dcb183
Solxanich <59670736+Solxanich@users.noreply.github.com>
2021-03-30 21:02:28 -0600

    Unloaded Dresser now has all properties needed (I believe) (#1409)
    * Unloaded Dresser now properly is a table, container, and has correct adjTiles
    
    Minor Bug fix.
    We really need to get a proper example dresser at some point working, on a different note.
    
    * Update UnloadedDresser.cs
    
    * Make Furniture Roomy Again

--- patches/tModLoader/Terraria/ModLoader/Default/UnloadedDresser.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/UnloadedDresser.cs
@@ -17,10 +17,14 @@ namespace Terraria.ModLoader.Default
 			Main.tileNoAttach[Type] = true;
 			Main.tileTable[Type] = true;
 			Main.tileSolidTop[Type] = true;
+			Main.tileContainer[Type] = true;
 			TileID.Sets.BasicDresser[Type] = true;
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
 
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2); // Disables hammering
 			TileObjectData.addTile(Type);
+
+			AdjTiles = new int[] { TileID.Dressers };
 		}
 	}
 }
