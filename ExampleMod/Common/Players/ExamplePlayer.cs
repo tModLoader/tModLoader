8b070f9805ea7f028e0708923db6ab7b44714aea
RayRelax <www.ordandrew1@gmail.com>
2021-04-04 23:48:16 +0300

    + Import ExampleEtherealFlames

--- ExampleMod/Common/Players/ExamplePlayer.cs
+++ ExampleMod/Common/Players/ExamplePlayer.cs
@@ -5,14 +5,32 @@ namespace ExampleMod.Common.Players
 	class ExamplePlayer : ModPlayer
 	{
 		public bool ShowMinionCount;
+		// debuffs
+		public bool eFlames;
 
 		public override void ResetEffects() {
 			ShowMinionCount = false;
+			eFlames = false;
 		}
 
 		public override void UpdateEquips() {
 			if (Player.accThirdEye)
 				ShowMinionCount = true;
+		}
+
+		// Allows you to give the player a negative life regeneration based on its state(for example, the "On Fire!" debuff makes the player take damage-over-time)
+		// This is typically done by setting player.lifeRegen to 0 if it is positive, setting player.lifeRegenTime to 0, and subtracting a number from player.lifeRegen
+		// The player will take damage at a rate of half the number you subtract per second
+		public override void UpdateBadLifeRegen() {
+			if (eFlames) {
+				// These lines zero out any positive lifeRegen. This is expected for all bad life regeneration effects.
+				if (Player.lifeRegen > 0)
+					Player.lifeRegen = 0;
+				
+				Player.lifeRegenTime = 0;
+				// lifeRegen is measured in 1/2 life per second. Therefore, this effect causes 8 life lost per second.
+				Player.lifeRegen -= 16;
+			}
 		}
 	}
 }
---
+++ ExampleMod/Content/Buffs/ExampleEtherealFlames.cs
@@ -0,0 +1,26 @@
+using ExampleMod.Common.Players;
+using ExampleMod.NPCs;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Buffs
+{
+	// Ethereal Flames is an example of a buff that causes constant loss of life.
+	// See ExamplePlayer.UpdateBadLifeRegen for more information.
+	public class ExampleEtherealFlames : ModBuff
+	{
+		public override void SetDefaults() {
+			DisplayName.SetDefault("Ethereal Flames"); // Buff display name
+			Description.SetDefault("Losing life"); // Buff description
+			Main.debuff[Type] = true;  // Is it a debuff?
+			Main.pvpBuff[Type] = true; // Players can give other players buffs, which are listed as pvpBuff
+			Main.buffNoSave[Type] = true; // It means the buff won't save when you exit the world
+			LongerExpertDebuff = true; // If this buff is a debuff, setting this to true will make this buff last twice as long on players in expert mode
+		}
+
+		// Allows you to make this buff give certain effects to the given player
+		public override void Update(Player player, ref int buffIndex) {
+			player.GetModPlayer<ExamplePlayer>().eFlames = true;
+		}
+	}
+}
