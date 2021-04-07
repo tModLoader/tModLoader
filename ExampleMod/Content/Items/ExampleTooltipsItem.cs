d527b3bb4f8e1b063a6750e49eb941bc1fbb963c
NotLe0n <mr.sheep75@gmail.com>
2021-04-07 22:31:22 +0200

    Ported more EM (#1425)
    * Ported ExampleCarMount (formally Car),
    Ported ExampleHood, ExampleCustomAISlimeNPC (formally Flutter Slime),
    Ported ExampleTooltipsItem (formally Face)
    Ported ExampleFishingRod
    Ported ExampleSimpleMinion
    Moved MinionBossEye to new "Minions" Folder
    
    * deleted left over old files
    
    * unmoved MinionBossEye; whoops
    
    * added mount comments
    
    * a few more comments + fixed typo in ExampleArmor (yes, this was bothering me)
    
    * cleanup
    
    * Renamed ExampleCarMount to ExampleMount + Added Mount description.
    
    * Resolve requested changes
    
    * Cleanups.
    
    * Main.spriteBatch -> spriteBatch
    
    Co-authored-by: Mirsario <me@mirsar.io>

--- ExampleMod/Content/Items/ExampleTooltipsItem.cs
+++ ExampleMod/Content/Items/ExampleTooltipsItem.cs
@@ -1,30 +1,28 @@
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
-using System.Linq;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
-namespace ExampleMod.Items
+namespace ExampleMod.Content.Items
 {
-	public class Face : ModItem
+	public class ExampleTooltipsItem : ModItem
 	{
 		public override void SetStaticDefaults() {
 			// See here for help on using Tags: http://terraria.gamepedia.com/Chat#Tags
 			Tooltip.SetDefault("How are you feeling today?"
-				+ $"\n[c/FF0000:Colors ][c/00FF00:are ][c/0000FF:fun ]and so are items: [i:{item.type}][i:{ItemType<CarKey>()}][i/s123:{ItemID.Ectoplasm}]");
+				+ $"\n[c/FF0000:Colors ][c/00FF00:are ][c/0000FF:fun ]and so are items: [i:{Item.type}][i:{ModContent.ItemType<ExampleMountItem>()}][i/s123:{ItemID.Ectoplasm}]");
 
-			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(30, 4));
-			ItemID.Sets.ItemNoGravity[item.type] = true;
+			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(30, 4));
+			ItemID.Sets.ItemNoGravity[Item.type] = true;
 		}
 
 		public override void SetDefaults() {
-			item.width = 20;
-			item.height = 20;
-			item.value = 100;
-			item.rare = ItemRarityID.Blue;
+			Item.width = 20;
+			Item.height = 20;
+			Item.sellPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
 		}
 
 		public override Color? GetAlpha(Color lightColor) {
@@ -33,16 +31,18 @@ namespace ExampleMod.Items
 
 		public override void ModifyTooltips(List<TooltipLine> tooltips) {
 			// Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
-			var line = new TooltipLine(mod, "Verbose:RemoveMe", "This tooltip won't show in-game");
+			var line = new TooltipLine(Mod, "Verbose:RemoveMe", "This tooltip won't show in-game");
 			tooltips.Add(line);
 
-			line = new TooltipLine(mod, "Face", "I'm feeling just fine!") {
+			line = new TooltipLine(Mod, "Face", "I'm feeling just fine!") {
 				overrideColor = new Color(100, 100, 255)
 			};
 			tooltips.Add(line);
+
+			// Here we give the item name a rainbow effect.
 			foreach (TooltipLine line2 in tooltips) {
 				if (line2.mod == "Terraria" && line2.Name == "ItemName") {
-					line2.overrideColor = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB);
+					line2.overrideColor = Main.DiscoColor;
 				}
 			}
 
@@ -57,11 +57,12 @@ namespace ExampleMod.Items
 			//tooltips.Remove(tooltipLine);
 		}
 
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
 		public override void AddRecipes() {
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemType<ExampleItem>());
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
 		}
 	}
 }
