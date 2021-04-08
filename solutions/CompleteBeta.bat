374b198f829882e9940c4066c46073a3ace9f53c
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-04-08 13:07:12 -0600

    Finish minor updates coming out of cross-plat testing. Running Linux from Build need testing more

---
+++ solutions/CompleteBeta.bat
@@ -0,0 +1,59 @@
+:: After Pulling, Patching, and making sure the version number is changed in src, this bat will compile and create zips for all release.
+:: It will also create a zip for ExampleMod
+
+@ECHO off
+:: Compile/Build exe 
+echo "Building Beta"
+set tModLoaderVersion=v0.11.9.0
+call buildDebug.bat
+
+set destinationFolder=.\tModLoader %tModLoaderVersion% Beta
+@IF %ERRORLEVEL% NEQ 0 (
+	pause
+	EXIT /B %ERRORLEVEL%
+)
+
+:: Folder for release
+mkdir "%destinationFolder%"
+
+:: Temp Folders
+set rls=%destinationFolder%\tModLoader %tModLoaderVersion%
+mkdir "%rls%"
+
+set steam=%destinationFolder%\tModLoaderSteam %tModLoaderVersion%
+mkdir "%steam%"
+
+:: Generic release
+robocopy /S ReleaseExtras\Content "%rls%\Content"
+robocopy /S ..\src\tModLoader\Terraria\bin\Debug\net5.0 "%rls%"
+robocopy /S ..\src\tModLoader\Terraria\bin\ServerDebug\net5.0 "%rls%"
+robocopy /S ReleaseExtras\RuntimeFiles "%rls%"
+
+call python ZipAndMakeExecutable.py "%rls%" "%rls%.zip"
+call python ZipAndMakeExecutable.py "%rls%" "%rls%.tar.gz"
+
+:: Steam release
+robocopy /S "%rls%" "%steam%"
+robocopy /S ReleaseExtras\SteamFiles "%steam%"
+
+call python ZipAndMakeExecutable.py "%steam%" "%steam%.zip"
+call python ZipAndMakeExecutable.py "%steam%" "%steam%.tar.gz"
+
+:: CleanUp, Delete temp Folders
+:: rmdir "%rls%" /S /Q
+:: rmdir "%steam%" /S /Q
+
+:: ExampleMod.zip (TODO, other parts of ExampleMod release)
+::rmdir ..\ExampleMod\bin /S /Q
+::rmdir ..\ExampleMod\obj /S /Q
+::rmdir ..\ExampleMod\.vs /S /Q
+::call python ZipAndMakeExecutable.py "..\ExampleMod" "%destinationFolder%\ExampleMod %tModLoaderVersion%.zip" ExampleMod\
+
+echo(
+echo(
+echo(
+echo tModLoader %tModLoaderVersion% ready to release.
+echo Upload the 2ish zip files to github/discord.
+echo(
+echo(
+pause
--- solutions/buildDebug.bat
+++ solutions/buildDebug.bat
@@ -1,5 +1,8 @@
 @echo off
 
+dotnet clean tModLoader.sln -c Debug --nologo -v q
+dotnet clean tModLoader.sln -c ServerDebug --nologo -v q
+
 echo Building Debug (1/2)
 dotnet build tModLoader.sln -c Debug --nologo -v q /clp:ErrorsOnly
 @IF %ERRORLEVEL% NEQ 0 (pause && EXIT /B %ERRORLEVEL%)
@@ -7,5 +10,5 @@ dotnet build tModLoader.sln -c Debug --nologo -v q /clp:ErrorsOnly
 echo.
 
 echo Building ServerDebug (2/2)
-dotnet build tModLoader.sn -c ServerDebug --nologo -v q /clp:ErrorsOnly
+dotnet build tModLoader.sln -c ServerDebug --nologo -v q /clp:ErrorsOnly
 @IF %ERRORLEVEL% NEQ 0 (pause && EXIT /B %ERRORLEVEL%)
