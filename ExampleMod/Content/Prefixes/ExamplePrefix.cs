5374d16f14c94bacf77a21e24c4431d7515b890a
valjean42 <noams1187@gmail.com>
2021-04-10 23:19:32 +0300

    Updated ExamplePrefix
    It turns out the code is really against loading 2 similar prefixes in 1 class. Also one of the methods I was using was deprecated.

--- ExampleMod/Content/Prefixes/ExamplePrefix.cs
+++ ExampleMod/Content/Prefixes/ExamplePrefix.cs
@@ -1,4 +1,3 @@
-using ExampleMod.Items;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -6,7 +5,9 @@ namespace ExampleMod.Prefixes
 {
     public class ExamplePrefix : ModPrefix
     {
-        private readonly byte _power;
+
+        // The constructor. It's needed for autoloading.
+        public ExamplePrefix() { }
 
         // See documentation for vanilla weights and more information.
         // Note: a weight of 0f might still be rolled. See CanRoll to exclude prefixes.
@@ -23,41 +24,21 @@ namespace ExampleMod.Prefixes
         public override PrefixCategory Category
             => PrefixCategory.AnyWeapon;
 
-        // This is needed for the tModLoader to attempt Autoloading.
-        public ExamplePrefix()
-        {
-        }
-
-        // The actual constructor which is used in the Autoload.
-        public ExamplePrefix(byte power)
-        {
-            _power = power;
-        }
-
-        // Allow multiple prefix autoloading this way (Different versions of the same prefix).
-        public override bool Autoload(ref string name)
-        {
-            if (base.Autoload(ref name))
-            {
-                mod.AddPrefix("Awesome", new ExamplePrefix(1));
-                mod.AddPrefix("ReallyAwesome", new ExamplePrefix(2));
-            }
-
-            return false;
-        }
+		// Set the display name translation default for this prefix.
+		public override void SetDefaults() => DisplayName.SetDefault("Exemplary");
 
         // Use this function to modify these stats for items which have this prefix: Damage Multiplier, Knockback Multiplier, Use Time Multiplier,
         //			Scale Multiplier (Size), Shoot Speed Multiplier, Mana Multiplier (Mana cost), Crit Bonus.
         public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult,
             ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
         {
-            damageMult *= 1.05;
+            damageMult *= 1.20f;
         }
 
         // Modify the cost of items with this modifier with this function.
         public override void ModifyValue(ref float valueMult)
         {
-            float multiplier = 1f + 0.05f * _power;
+            float multiplier = 1.05f;
             valueMult *= multiplier;
         }
 
