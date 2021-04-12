3b7638509f903fc3fa93811af0e6e7a10bc4ed31
Chicken-Bones <mehvids@gmail.com>
2021-04-12 22:54:31 +1000

    Revert change to AutoloadAttribute.cs comment

--- patches/tModLoader/Terraria/ModLoader/AutoloadAttribute.cs
+++ patches/tModLoader/Terraria/ModLoader/AutoloadAttribute.cs
@@ -5,7 +5,7 @@ namespace Terraria.ModLoader
 {
 	/// <summary>
 	/// Allows for types to be autoloaded and unloaded.
-	/// True to always autoload, false to never autoload.
+	/// True to always autoload, false to never autoload, null to use mod default.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true, Inherited = true)]
 	public sealed class AutoloadAttribute : Attribute
