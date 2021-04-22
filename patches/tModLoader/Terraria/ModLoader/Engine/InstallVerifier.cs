4ec825d1cbc81ac215a65a0a7d9816d22ef25727
JavidPack <javidpack@gmail.com>
2021-04-21 22:15:58 -0600

    Update github actions, latest steam libs,

--- patches/tModLoader/Terraria/ModLoader/Engine/InstallVerifier.cs
+++ patches/tModLoader/Terraria/ModLoader/Engine/InstallVerifier.cs
@@ -30,19 +30,19 @@ namespace Terraria.ModLoader.Engine
 		static InstallVerifier() {
 			if (Platform.IsWindows) {
 				steamAPIPath = "Libraries/Native/Windows32/steam_api.dll";
-				steamAPIHash = ToByteArray("73688FFCBC2E5F0980B055C1D93B2FB2");
+				steamAPIHash = ToByteArray("9084E3645F0601810362084DA53203DB");
 				gogHash = ToByteArray("ff61b96a07894a9e65f880fb9608fb37"); // Don't forget to update CheckExe in CheckGoG
 				steamHash = ToByteArray("4fd8072ca82ded3d9da1be577a478788");
 			}
 			else if (Platform.IsOSX) {
 				steamAPIPath = "Libraries/Native/OSX/libsteam_api.dylib";
-				steamAPIHash = ToByteArray("FB32124B2E07ED2AAE54FE8823D069B3");
+				steamAPIHash = ToByteArray("69C98EC9C3179D52A7FE75BD8405D8FC");
 				gogHash = ToByteArray("181c586d0fe64156adb0ecd4b9fabf9d");
 				steamHash = ToByteArray("12c8d2ac5af6c8505bd1a9339dc75231");
 			}
 			else if (Platform.IsLinux) {
 				steamAPIPath = "Libraries/Native/Linux/libsteam_api.so";
-				steamAPIHash = ToByteArray("8915306857EEF2A516956A2398845EA2");
+				steamAPIHash = ToByteArray("25BF523623B5EE8E82B047327F9C1341");
 				gogHash = ToByteArray("4a051352dd6ecc323c5a0a15e5b598fb");
 				steamHash = ToByteArray("debcc318ca4e14295e3ac22e380e289b");
 			}
--- solutions/CompleteRelease.bat
+++ solutions/CompleteRelease.bat
@@ -8,6 +8,9 @@ set tModLoaderVersion=v0.11.9.0
 call buildRelease.bat
 
 set destinationFolder=.\tModLoader %tModLoaderVersion% Release
+If "%1"=="skipzip" (
+	set destinationFolder=.\Release
+)
 @IF %ERRORLEVEL% NEQ 0 (
 	pause
 	EXIT /B %ERRORLEVEL%
@@ -16,31 +19,44 @@ set destinationFolder=.\tModLoader %tModLoaderVersion% Release
 :: Folder for release
 mkdir "%destinationFolder%"
 
+:: Temp Folders
+set shared=%destinationFolder%\Shared
+set win=%destinationFolder%\tModLoader Windows %tModLoaderVersion%
+
+mkdir "%shared%"
+
 :: Generic release
-robocopy /S ..\src\tModLoader\Terraria\bin\Release\net5.0 "%destinationFolder%"
-del "%destinationFolder%\tModLoader.exe"
-rmdir /S /Q "%destinationFolder%\ref"
-robocopy /S ReleaseExtras\RuntimeFiles "%destinationFolder%"
-robocopy /S ReleaseExtras\SteamFiles "%destinationFolder%"
-copy ReleaseExtras\tModLoader.png "%destinationFolder%\Libraries\Native\tModLoader.png"
+robocopy /S ..\src\tModLoader\Terraria\bin\Release\net5.0 "%shared%"
+del "%shared%\tModLoader.exe"
+rmdir /S /Q "%shared%\ref"
+robocopy /S ReleaseExtras\RuntimeFiles "%shared%"
+robocopy /S ReleaseExtras\SteamFiles "%shared%"
+copy ReleaseExtras\tModLoader.png "%shared%\Libraries\Native\tModLoader.png"
 
-call python ZipAndMakeExecutable.py "%destinationFolder%" "%destinationFolder%.zip"
-call python ZipAndMakeExecutable.py "%destinationFolder%" "%destinationFolder%.tar.gz"
+If "%1"=="skipzip" (
+	echo Skipping zipping.
+) Else (
+	:: Windows
+	call python ZipAndMakeExecutable.py "%shared%" "%win%.zip"
 
-:: CleanUp, Delete temp Folders
-:: rmdir "%destinationFolder%" /S /Q
+	:: CleanUp, Delete temp Folders
+	:: rmdir "%shared%" /S /Q
 
-:: ExampleMod.zip (TODO, other parts of ExampleMod release)
-::rmdir ..\ExampleMod\bin /S /Q
-::rmdir ..\ExampleMod\obj /S /Q
-::rmdir ..\ExampleMod\.vs /S /Q
-::call python ZipAndMakeExecutable.py "..\ExampleMod" "%destinationFolder%\ExampleMod %tModLoaderVersion%.zip" ExampleMod\
+	:: ExampleMod.zip (TODO, other parts of ExampleMod release)
+	rmdir ..\ExampleMod\bin /S /Q
+	rmdir ..\ExampleMod\obj /S /Q
+	rmdir ..\ExampleMod\.vs /S /Q
+	call python ZipAndMakeExecutable.py "..\ExampleMod" "%destinationFolder%\ExampleMod %tModLoaderVersion%.zip" ExampleMod\
+)
+
 
 echo(
 echo(
 echo(
 echo tModLoader %tModLoaderVersion% ready to release.
-echo Upload the 1ish zip files to github/discord.
+echo Upload the 2 zip files to github/discord.
 echo(
 echo(
-pause
+If NOT "%1"=="skipzip" (
+	pause
+)
