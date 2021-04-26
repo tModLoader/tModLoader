4c2381a8947a9f796c5cb9fd80d4af508f985156
Mirsario <me@mirsar.io>
2021-04-26 19:23:26 +0300

    Update patches/tModLoader/Terraria/ModLoader/UI/UIModSources.cs

--- patches/tModLoader/Terraria/ModLoader/UI/UIModSources.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/UIModSources.cs
@@ -198,7 +198,7 @@ namespace Terraria.ModLoader.UI
 
 			var font = FontAssets.MouseText.Value;
 			Vector2 sizes = font.MeasureString(versionUpgradeMessage);
-			Color color = Color.Red;
+			Color color = Color.IndianRed;
 
 			int xLoc = (int)(Main.screenWidth / 2 + 134);
 			int yLoc = (int)(sizes.Y + 244f);
