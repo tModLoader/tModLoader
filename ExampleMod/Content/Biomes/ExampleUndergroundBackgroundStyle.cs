bfabb1454b7fd550b9bb878e73558c33a115baa3
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-05-07 16:08:54 -0600

    Fix errors tossed from ExampleMod, put a temp fix for DrawFarTexture

--- ExampleMod/Content/Biomes/ExampleUndergroundBackgroundStyle.cs
+++ ExampleMod/Content/Biomes/ExampleUndergroundBackgroundStyle.cs
@@ -4,12 +4,11 @@ namespace ExampleMod.Backgrounds
 {
 	public class ExampleUndergroundBackgroundStyle : ModUndergroundBackgroundStyle
 	{
-		//TODO: This currently doesn't work
 		public override void FillTextureArray(int[] textureSlots) {
-			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot("Assets/Textures/Backgrounds/ExampleBiomeUnderground0");
-			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot("Assets/Textures/Backgrounds/ExampleBiomeUnderground1");
-			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("Assets/Textures/Backgrounds/ExampleBiomeUnderground2");
-			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("Assets/Textures/Backgrounds/ExampleBiomeUnderground3");
+			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground0");
+			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground1");
+			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground2");
+			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground3");
 		}
 	}
 }
