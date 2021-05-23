de7778a5d9075710516e7cb8b4e02cf1257c5cbb
jack bond-preston <jackbondpreston@outlook.com>
2021-05-23 17:26:41 +0100

    Move update checking logic outside of ModLoader.cs

---
+++ patches/tModLoader/Terraria/ModLoader/Core/GOGModUpdateChecker.cs
@@ -0,0 +1,101 @@
+﻿using Ionic.Zlib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terraria.ModLoader.Core
+{
+	internal static class GOGModUpdateChecker
+	{
+		public static int ModUpdatesAvailable = 0;
+
+		internal static void CheckModUpdates() {
+			ModUpdatesAvailable = 0;
+
+			try {
+				Task.Factory.StartNew(() => {
+					ServicePointManager.Expect100Continue = false;
+
+					const string Url = "http://javid.ddns.net/tModLoader/listmods.php";
+
+					var values = new NameValueCollection {
+						{ "modloaderversion", BuildInfo.versionedName },
+						{ "platform", ModLoader.CompressedPlatformRepresentation },
+						{ "netversion", FrameworkVersion.Version.ToString() },
+						{ "EarlyAutoUpdate", UI.ModBrowser.UIModBrowser.EarlyAutoUpdate.ToString() }
+					};
+
+					using var client = new WebClient();
+
+					ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, policyErrors) => true;
+					client.UploadValuesCompleted += UpdateCheckComplete;
+
+					client.UploadValuesAsync(new Uri(Url), "POST", values);
+				});
+			}
+			catch (Exception e) {
+				// Fail silently, it doesn't really matter if the update indicator is missing
+				Logging.tML.Debug("Error checking for mod updates: " + e.Message);
+			}
+		}
+
+		private static void UpdateCheckComplete(object sender, UploadValuesCompletedEventArgs e) {
+			if (e.Error != null) {
+				// Fail silently, it doesn't really matter if the update indicator is missing
+				Logging.tML.Debug("Error checking for mod updates.");
+			}
+			else if (!e.Cancelled) {
+				var result = e.Result;
+				string response = Encoding.UTF8.GetString(result);
+				Task.Factory
+					.StartNew(ModOrganizer.FindMods)
+					.ContinueWith(task => {
+						var installedMods = task.Result;
+
+						JObject jsonObject;
+						try {
+							jsonObject = JObject.Parse(response);
+						}
+						catch (Exception e) {
+							throw new Exception($"Bad JSON: {response}", e);
+						}
+
+						JArray modlist;
+						string modlist_compressed = (string)jsonObject["modlist_compressed"];
+						if (modlist_compressed != null) {
+							byte[] data = Convert.FromBase64String(modlist_compressed);
+							using var zip = new GZipStream(new MemoryStream(data), CompressionMode.Decompress);
+							using var reader = new StreamReader(zip);
+
+							modlist = JArray.Parse(reader.ReadToEnd());
+						}
+						else {
+							// Fallback if needed.
+							modlist = (JArray)jsonObject["modlist"];
+						}
+
+						// Iterate through all mods, and increment modUpdatesAvailable where a mod
+						// has a higher available version than the installed version
+						foreach (var mod in modlist.Children<JObject>()) {
+							string version = (string)mod["version"];
+							var installed = installedMods.FirstOrDefault(m => m.Name == (string)mod["name"]);
+
+							if (installed != null) {
+								var cVersion = new Version(version.Substring(1));
+								if (cVersion > installed.modFile.Version) {
+									ModUpdatesAvailable++;
+								}
+							}
+						}
+					}, TaskScheduler.Current);
+			}
+		}
+
+	}
+}
--- patches/tModLoader/Terraria/ModLoader/ModLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/ModLoader.cs
@@ -1,17 +1,13 @@
-using Ionic.Zlib;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
-using Newtonsoft.Json.Linq;
 using ReLogic.OS;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -29,8 +25,6 @@ using ReLogic.Content;
 using ReLogic.Graphics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Net;
-using System.IO;
 #if NETCORE
 using System.Runtime.Loader;
 #endif
@@ -46,7 +40,6 @@ namespace Terraria.ModLoader
 		public static Version LastLaunchedTModLoaderVersion;
 		// public static bool ShowWhatsNew;
 		public static bool ShowFirstLaunchWelcomeMessage;
-		public static int ModUpdatesAvailable;
 
 		public static string versionedName => ModCompile.DeveloperMode ? BuildInfo.versionedNameDevFriendly : BuildInfo.versionedName;
 
@@ -203,91 +196,7 @@ namespace Terraria.ModLoader
 				OnSuccessfulLoad = null;
 				skipLoad = false;
 				ModNet.NetReloadActive = false;
-				CheckModUpdates();
-			}
-		}
-
-		internal static void CheckModUpdates()
-		{
-			ModUpdatesAvailable = 0;
-
-			try {
-				Task.Factory.StartNew(() => {
-					ServicePointManager.Expect100Continue = false;
-					
-					const string Url = "http://javid.ddns.net/tModLoader/listmods.php";
-					
-					var values = new NameValueCollection {
-						{ "modloaderversion", BuildInfo.versionedName },
-						{ "platform", ModLoader.CompressedPlatformRepresentation },
-						{ "netversion", FrameworkVersion.Version.ToString() },
-						{ "EarlyAutoUpdate", UI.ModBrowser.UIModBrowser.EarlyAutoUpdate.ToString() }
-					};
-					
-					using var client = new WebClient();
-					
-					ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, policyErrors) => true;
-					client.UploadValuesCompleted += UpdateCheckComplete;
-					
-					client.UploadValuesAsync(new Uri(Url), "POST", values);
-				});
-			}
-			catch (Exception e) {
-				// Fail silently, it doesn't really matter if the update indicator is missing
-				Logging.tML.Debug("Error checking for mod updates.");
-			}
-		}
-
-		private static void UpdateCheckComplete(object sender, UploadValuesCompletedEventArgs e)
-		{
-			if (e.Error != null) {
-				// Fail silently, it doesn't really matter if the update indicator is missing
-				Logging.tML.Debug("Error checking for mod updates.");
-			}
-			else if (!e.Cancelled) {
-				var result = e.Result;
-				string response = Encoding.UTF8.GetString(result);
-				Task.Factory
-					.StartNew(ModOrganizer.FindMods)
-					.ContinueWith(task => {
-						var installedMods = task.Result;
-
-						JObject jsonObject;
-						try {
-							jsonObject = JObject.Parse(response);
-						}
-						catch (Exception e) {
-							throw new Exception($"Bad JSON: {response}", e);
-						}
-
-						JArray modlist;
-						string modlist_compressed = (string)jsonObject["modlist_compressed"];
-						if (modlist_compressed != null) {
-							byte[] data = Convert.FromBase64String(modlist_compressed);
-							using var zip = new GZipStream(new MemoryStream(data), CompressionMode.Decompress);
-							using var reader = new StreamReader(zip);
-							
-							modlist = JArray.Parse(reader.ReadToEnd());
-						}
-						else {
-							// Fallback if needed.
-							modlist = (JArray)jsonObject["modlist"];
-						}
-
-						// Iterate through all mods, and increment modUpdatesAvailable where a mod
-						// has a higher available version than the installed version
-						foreach (var mod in modlist.Children<JObject>()) {
-							string version = (string)mod["version"];
-							var installed = installedMods.FirstOrDefault(m => m.Name == (string)mod["name"]);
-
-							if (installed != null) {
-								var cVersion = new Version(version.Substring(1));
-								if (cVersion > installed.modFile.Version) {
-									ModUpdatesAvailable++;
-								}
-							}
-						}
-					}, TaskScheduler.Current);
+				GOGModUpdateChecker.CheckModUpdates();
 			}
 		}
 
