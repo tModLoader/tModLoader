e8796ffb013a2f10c2dd215496f7bc7ead97a577
VaemaVoid <78240180+Vaema@users.noreply.github.com>
2021-05-26 16:49:11 -0400

    Updated ExampleCustomCurrency.cs

--- ExampleMod/Content/Currencies/ExampleCustomCurrency.cs
+++ ExampleMod/Content/Currencies/ExampleCustomCurrency.cs
@@ -7,7 +7,7 @@ namespace ExampleMod
 {
 	public class ExampleCustomCurrency : CustomCurrencySingleCoin
 	{
-		public static readonly Color ExampleCustomCurrencyTextColor = Color.BlueViolet; // This variable is located in the GetPriceText hook.
+		public static readonly Color ExampleCustomCurrencyTextColor = Color.BlueViolet;
 
 		public ExampleCustomCurrency(int coinItemID, long currencyCap) : base(coinItemID, currencyCap)
 		{
@@ -16,7 +16,7 @@ namespace ExampleMod
 		public override void GetPriceText(string[] lines, ref int currentLine, int price)
 		{
 			Color color = ExampleCustomCurrencyTextColor * (Main.mouseTextColor / 255f); // The text color is blue-violet, mentioned with the variable above.
-			lines[currentLine++] = $"[c/{color.Hex3()}:{Language.GetTextValue("LegacyTooltip.50")} {price} faces]"; // Replaces the format item in a specified string with the string representation.
+			lines[currentLine++] = $"[c/{color.Hex3()}:{Language.GetTextValue("LegacyTooltip.50")} {price} faces]";
 			// Note that color.Hex3() changes a color like {R: 255, G: 0, B: 0} to FF0000.
 		}
 	}
