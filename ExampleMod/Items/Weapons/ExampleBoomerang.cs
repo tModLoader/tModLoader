6043fcfea5b7155ac475ab2c4de5661958eefb60
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2021-06-02 18:08:19 -0700

    made exampleboomerang

---
+++ ExampleMod/Items/Weapons/ExampleBoomerang.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Weapons
+{
+    public class ExampleBoomerang : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Example Boomerang");
+        }
+
+        public override void SetDefaults()
+        {
+            item.melee = true; //Makes the item do melee damage
+            item.useStyle = ItemUseStyleID.SwingThrow; //Makes the player swing
+            item.noUseGraphic = true; //Turns off the boomerang from showing in the player's hand
+
+            item.shoot = ModContent.ProjectileType<ExampleBoomerangProj>(); //Shoots the boomerang
+
+            item.autoReuse = true; //Determines whether the player can hold left click to continue throwing or not, set to false to make them not able to
+            item.useTime = 10; //Sets how fast the player can switch to another item after throwing, or how fast they can throw another boomerang after throwing the previous
+            item.useAnimation = 10; //Sets how fast the player's hand finishes its throwing animation in ticks(60 ticks in a second)
+            item.value = Item.buyPrice(0, 10, 0, 0); //Sets price(platinum, gold, silver, copper)
+            item.rare = ItemRarityID.Blue; //Sets rarity
+            item.damage = 100; //Sets damage
+            item.knockBack = 4f; //Sets knockback
+            item.width = 18; //Sets width of item
+            item.height = 32; //Sets height of item
+            item.UseSound = SoundID.Item1; //Sets the sound to the default thrown sound
+            item.shootSpeed = 10f; //Sets how fast/far the boomerang is thrown, increase for more range and decrease for less
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[item.shoot] < 1; //Ensures there are no more than 1 boomerang out at once, you can set this to 2 or 3 if you want more boomerangs to be able to be thrown at once
+        }
+    }
+}
---
+++ ExampleMod/Projectiles/ExampleBoomerangProjectile.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Weapons
+{
+    public class ExampleBoomerangProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Example Boomerang");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.friendly = true; //Makes the projectile hurt enemies
+            projectile.hostile = false; //Makes the projectile not hurt friendly creatures
+            projectile.aiStyle = 3; //Gives the projectile boomerang behavior - return after being thrown, and die when touching player after returning
+
+            projectile.width = 18; //Sets the width
+            projectile.height = 32; //Sets the height
+            projectile.melee = true; //Makes the projectile deal melee damage
+            projectile.timeLeft = 300; //Sets the projectile's lifespan in ticks(60 ticks in a second), you can set this very high if you only want it to die after reaching the player
+            projectile.tileCollide = true; //Makes the projectile die on contact with tiles - you can set this to false if you want it to pass through tiles
+        }
+
+        public override void AI()
+        {
+            projectile.rotation += 0.1f; //Dictates how fast the projectile rotates, set higher for faster rotation and lower for slower rotation, and make -= instead of += for rotation in the other direction
+        }
+    }
+}
