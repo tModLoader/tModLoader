280669f4fe8d980eabc0b4219c98170cd3f621e3
Mirsario <me@mirsar.io>
2021-06-07 18:43:12 +0300

    Address #1559 via an overload for RegisterHotkey.

--- patches/tModLoader/Terraria/ModLoader/HotkeyLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/HotkeyLoader.cs
@@ -20,6 +20,15 @@ namespace Terraria.ModLoader
 		/// <param name="mod"> The mod that this hotkey will belong to. Usually, this would be your mod instance. </param>
 		/// <param name="name"> The name of the keybind. </param>
 		/// <param name="defaultBinding"> The default binding. </param>
+		public static ModHotkey RegisterHotkey(Mod mod, string name, Keys defaultBinding)
+			=> RegisterHotkey(mod, name, defaultBinding.ToString());
+
+		/// <summary>
+		/// Registers a hotkey with a <paramref name="name"/> and <paramref name="defaultBinding"/>. Use the returned <see cref="ModHotkey"/> to detect when buttons are pressed.
+		/// </summary>
+		/// <param name="mod"> The mod that this hotkey will belong to. Usually, this would be your mod instance. </param>
+		/// <param name="name"> The name of the keybind. </param>
+		/// <param name="defaultBinding"> The default binding. </param>
 		public static ModHotkey RegisterHotkey(Mod mod, string name, string defaultBinding) {
 			if (mod == null)
 				throw new ArgumentNullException(nameof(mod));
