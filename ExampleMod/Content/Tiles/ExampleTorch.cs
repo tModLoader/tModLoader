8896c2878853c1c798d4ff3697aaa28d3d47fb4c
direwolf420 <amakejk1@web.de>
2021-06-08 13:07:54 +0200

    Add two parameters to ModTile.SetDrawPositions for controlling the drawn frame (for non-animated tiles)

--- ExampleMod/Content/Tiles/ExampleTorch.cs
+++ ExampleMod/Content/Tiles/ExampleTorch.cs
@@ -94,7 +94,7 @@ namespace ExampleMod.Content.Tiles
 			}
 		}
 
-		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height) {
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
 			offsetY = 0;
 
 			if (WorldGen.SolidTile(i, j - 1)) {
