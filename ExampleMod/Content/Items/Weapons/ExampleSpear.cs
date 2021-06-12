ce2bed048ec82656862c7c6c51f0f80d8fe2f4e7
Naginif <danielfinigan@gmail.com>
2021-06-12 15:55:10 -0400

    Move ExampleSpear and ExampleSpearProjectile out of Old directory

---
+++ ExampleMod/Content/Items/Weapons/ExampleSpear.cs
@@ -0,0 +1,57 @@
+﻿using ExampleMod.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Items.Weapons
+{
+	public class ExampleSpear : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("This is a modded spear");
+		}
+
+		public override void SetDefaults() {
+			// Common Properties
+			item.width = 32; // The item texture's width.
+			item.height = 32; // The item texture's height.
+			item.scale = 1f; // The item texture's scale.
+			item.rare = ItemRarityID.Pink; // Assign this item a rarity level of Pink
+			item.value = Item.sellPrice(silver: 10); // The number and type of coins item can be sold for to an NPC
+
+			// Use Properties
+			item.useStyle = ItemUseStyleID.HoldingOut; // How you use the item (swinging, holding out, etc.)
+			item.useAnimation = 18; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			item.useTime = 24; // The length of the item's use time in ticks (60 ticks == 1 second.)
+			item.UseSound = SoundID.Item1; // The sound that this item plays when used.
+			item.autoReuse = true; // Whether or not you can hold click to automatically use it again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()		
+
+			// Weapon Properties
+			item.damage = 40;
+			item.knockBack = 6.5f;	
+			item.noUseGraphic = true; // If true, the item's sprite will not be visible while the item is in use. This is true because a spear is technically a projectile so we do not want to show a second spear while using.
+
+			// Melee Properties
+			item.melee = true;
+			item.noMelee = true; // Whether or not the item's animation will do damage Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+
+			// Projectile Properties
+			item.shootSpeed = 3.7f; // The speed of the projectile (measured in pixels per frame.)
+			item.shoot = ProjectileType<ExampleSpearProjectile>(); //The projectile that is fired from this weapon
+		}
+
+		public override bool CanUseItem(Player player) {
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[item.shoot] < 1;
+		}
+		
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleSpearProjectile.cs
@@ -0,0 +1,83 @@
+﻿using ExampleMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Projectiles
+{
+	public class ExampleSpearProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Spear");
+		}
+
+		public override void SetDefaults() {
+			Projectile.CloneDefaults(ProjectileID.Spear); // Clone the default values for a vanilla spear. Spear specific values set for width, height, aiStyle, friendly, penetrate, tileCollide, scale, hide, ownerHitCheck, and melee.  
+
+		}
+
+		// In here the AI uses this example, to make the code more organized and readable
+		// Also showcased in ExampleJavelinProjectile.cs
+		public float movementFactor // Change this value to alter how fast the spear moves
+		{
+			get => projectile.ai[0];
+			set => projectile.ai[0] = value;
+		}
+
+		// It appears that for this AI, only the ai0 field is used!
+		public override void AI() {
+			// Since we access the owner player instance so much, it's useful to create a helper local variable for this
+			Player player = Main.player[projectile.owner];
+			// Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+			Vector2 ownerMountedCenter = player.RotatedRelativePoint(player.MountedCenter, true);
+			projectile.direction = player.direction;
+			player.heldProj = projectile.whoAmI;
+			player.itemTime = player.itemAnimation;
+			projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+			projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+			// As long as the player isn't frozen, the spear can move
+			if (!player.frozen) {
+				if (movementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+				{
+					movementFactor = 3f; // Make sure the spear moves forward when initially thrown out
+					projectile.netUpdate = true; // Make sure to netUpdate this spear
+				}
+				if (player.itemAnimation < player.itemAnimationMax / 3) // Somewhere along the item animation, make sure the spear moves back
+				{
+					movementFactor -= 2.4f;
+				}
+				else // Otherwise, increase the movement factor
+				{
+					movementFactor += 2.1f;
+				}
+			}
+			// Change the spear position based off of the velocity and the movementFactor
+			projectile.position += projectile.velocity * movementFactor;
+			// When we reach the end of the animation, we can kill the spear projectile
+			if (player.itemAnimation == 0) {
+				projectile.Kill();
+			}
+			// Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation.
+			projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+			// Offset by 90 degrees here
+			if (projectile.spriteDirection == -1) {
+				projectile.rotation -= MathHelper.ToRadians(90f);
+			}
+
+			// These dusts are added later, for the 'ExampleMod' effect
+			if (Main.rand.NextBool(3)) {
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.height, projectile.width, DustType<Sparkle>(),
+					projectile.velocity.X * .2f, projectile.velocity.Y * .2f, 200, Scale: 1.2f);
+				dust.velocity += projectile.velocity * 0.3f;
+				dust.velocity *= 0.2f;
+			}
+			if (Main.rand.NextBool(4)) {
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.height, projectile.width, DustType<Sparkle>(),
+					0, 0, 254, Scale: 0.3f);
+				dust.velocity += projectile.velocity * 0.5f;
+				dust.velocity *= 0.5f;
+			}
+		}
+	}
+}
