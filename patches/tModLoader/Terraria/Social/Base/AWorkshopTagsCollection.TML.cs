45bb777c1fd40101c7202ad8b3b487d786794def
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-06-12 20:09:47 -0600

    Fix a bad partialing

--- patches/tModLoader/Terraria/Social/Base/AWorkshopTagsCollection.TML.cs
+++ patches/tModLoader/Terraria/Social/Base/AWorkshopTagsCollection.TML.cs
@@ -4,15 +4,10 @@ namespace Terraria.Social.Base
 {
 	public abstract partial class AWorkshopTagsCollection
 	{
-		public readonly List<WorkshopTagOption> WorldTags = new List<WorkshopTagOption>();
-		public readonly List<WorkshopTagOption> ResourcePackTags = new List<WorkshopTagOption>();
+		public readonly List<WorkshopTagOption> ModTags = new List<WorkshopTagOption>();
 
-		protected void AddWorldTag(string tagNameKey, string tagInternalName) {
-			WorldTags.Add(new WorkshopTagOption(tagNameKey, tagInternalName));
-		}
-
-		protected void AddResourcePackTag(string tagNameKey, string tagInternalName) {
-			ResourcePackTags.Add(new WorkshopTagOption(tagNameKey, tagInternalName));
+		protected void AddModTag(string tagNameKey, string tagInternalName) {
+			ModTags.Add(new WorkshopTagOption(tagNameKey, tagInternalName));
 		}
 	}
 }
