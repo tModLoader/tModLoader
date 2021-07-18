4eb8f245c7e86102448cbd69b18a22c1e0922e1c
Mirsario <me@mirsar.io>
2021-07-18 19:04:58 +0300

    Updated Orian's set, heavily upgraded its effects.

--- patches/tModLoader/Terraria/ModLoader/Default/Patreon/Orian.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/Patreon/Orian.cs
@@ -1,20 +1,67 @@
+using Microsoft.Xna.Framework;
+using System.Linq;
+using Terraria.ID;
+
 namespace Terraria.ModLoader.Default.Patreon
 {
+	internal class OrianSetEffectPlayer : ModPlayer
+	{
+		public bool IsActive;
+
+		public override void ResetEffects() {
+			IsActive = false;
+		}
+
+		public override void PostUpdate() {
+			if (IsActive) {
+				var player = Player;
+				var playerCenter = player.Center;
+				bool closeToAnEnemy = Main.npc.Any(x => x != Main.npc[Main.maxNPCs] && x.active && !x.friendly && !NPCID.Sets.TownCritter[x.type] && x.type != NPCID.TargetDummy && x.WithinRange(player.position, 300));
+				float maxIntensity = 0f;
+
+				const float MaxDistance = 512f;
+				const float MaxDistanceSqr = MaxDistance * MaxDistance;
+
+				for (int i = 0; i < Main.maxNPCs; i++) {
+					var npc = Main.npc[i];
+
+					if (!npc.active || npc.damage <= 0 || npc.friendly || NPCID.Sets.TownCritter[npc.type] || npc.type == NPCID.TargetDummy) {
+						continue;
+					}
+
+					float distanceSquared = npc.DistanceSQ(playerCenter);
+					float intensity = 1f - System.MathF.Min(1f, distanceSquared / MaxDistanceSqr);
+
+					intensity *= intensity;
+
+					maxIntensity = System.MathF.Max(maxIntensity, intensity);
+				}
+
+				if (maxIntensity > 0f) {
+					float pulse = (System.MathF.Sin((float)Main.GameUpdateCount / 17f) * 0.25f) + 0.75f;
+
+					Lighting.AddLight(playerCenter, Color.DeepSkyBlue.ToVector3() * maxIntensity * pulse * 1.5f);
+				}
+			}
+		}
+	}
+
 	[AutoloadEquip(EquipType.Head)]
 	internal class Orian_Head : PatreonItem
 	{
 		public override bool IsVanitySet(int head, int body, int legs) {
 			return head == Mod.GetEquipSlot(nameof(Orian_Head), EquipType.Head)
-				   && body == Mod.GetEquipSlot(nameof(Orian_Body), EquipType.Body)
-				   && legs == Mod.GetEquipSlot(nameof(Orian_Legs), EquipType.Legs);
+				&& body == Mod.GetEquipSlot(nameof(Orian_Body), EquipType.Body)
+				&& legs == Mod.GetEquipSlot(nameof(Orian_Legs), EquipType.Legs);
 		}
 
 		public override void UpdateVanitySet(Player player) {
-			PatronModPlayer.Player(player).OrianSet = true;
+			player.GetModPlayer<OrianSetEffectPlayer>().IsActive = true;
 		}
 
 		public override void SetDefaults() {
 			base.SetDefaults();
+
 			Item.width = 24;
 			Item.height = 24;
 		}
@@ -25,6 +72,7 @@ namespace Terraria.ModLoader.Default.Patreon
 	{
 		public override void SetDefaults() {
 			base.SetDefaults();
+
 			Item.width = 30;
 			Item.height = 20;
 		}
@@ -35,13 +83,9 @@ namespace Terraria.ModLoader.Default.Patreon
 	{
 		public override void SetDefaults() {
 			base.SetDefaults();
+
 			Item.width = 22;
 			Item.height = 18;
-		}
-
-		public override void EquipFrameEffects(Player player, EquipType type)
-		{
-			player.shoe = 0;
 		}
 	}
 }
