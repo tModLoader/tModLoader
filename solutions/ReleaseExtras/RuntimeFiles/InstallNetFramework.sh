639d154cffc27f8b9e0d48786ed9b319fd402178
Mirsario <me@mirsar.io>
2021-07-30 15:26:00 +0300

    More fixes in unix scripts, by ru-rune

--- solutions/ReleaseExtras/RuntimeFiles/InstallNetFramework.sh
+++ solutions/ReleaseExtras/RuntimeFiles/InstallNetFramework.sh
@@ -3,8 +3,8 @@
 # Provided for use in tModLoader deployment. 
 
 #Get path of the script
-script_path=$(readlink -f "$0")
-script_dir=$(dirname "$script_path")
+script_path="$(readlink -f "$0")"
+script_dir="$(dirname "$script_path")"
 #CD to the script location.
 cd "$script_dir"
 
@@ -17,7 +17,7 @@ echo "This may take a few moments."
 if [[ "$(uname)" == Darwin ]]; then
   export DYLD_LIBRARY_PATH="$script_dir/Libraries/Native/OSX"
 else
-  library_dir=$script_dir/Libraries/Native/Linux
+  library_dir="$script_dir/Libraries/Native/Linux"
   export LD_LIBRARY_PATH="$library_dir"
   ln -sf "$library_dir/libSDL2-2.0.so.0" "$library_dir/libSDL2.so"
 fi
@@ -40,9 +40,9 @@ fi
 #If the dotnet dir exists, we need to do some cleanup
 if [ -d "$dotnet_dir" ]; then
   # Find all folders inside the dotnet dir that don't match our target version and nuke it
-  for folder in $(ls $script_dir/dotnet/); do
+  for folder in $(ls "$dotnet_dir"); do
     if [ ! $version = "$folder" ]; then
-      old_version="$script_dir/dotnet/$folder"
+      old_version="$dotnet_dir/$folder"
       echo "Cleaning $old_version"
       rm -rf "$old_version"
     fi
