e9dfd55282d7df53732fda1f723446a03960a89a
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2021-08-02 16:47:39 +0200

    Now mod icon scaling should work

--- patches/tModLoader/Terraria/ModLoader/UI/ModBrowser/UIModDownloadItem.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/ModBrowser/UIModDownloadItem.cs
@@ -294,8 +294,9 @@ namespace Terraria.ModLoader.UI.ModBrowser
 						_modIcon = new UIImage(iconTexture) {
 							Left = { Percent = 0f },
 							Top = { Percent = 0f },
-							Width = { Pixels = 80f },
-							Height = { Pixels = 80f }
+							MaxWidth = { Pixels = 80f, Percent = 0f },
+							MaxHeight = { Pixels = 80f, Percent = 0f },
+							ScaleToFit = true
 						};
 						_modIconStatus = ModIconStatus.READY;
 						success = true;
