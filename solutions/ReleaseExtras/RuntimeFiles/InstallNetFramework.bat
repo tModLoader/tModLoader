ece43e6439eafa8fa0a711122d6f843d29dc60c7
Mirsario <me@mirsar.io>
2021-08-03 19:54:56 +0300

    Fixed the install directory cleaning prompt being shown for every file.

--- solutions/ReleaseExtras/RuntimeFiles/InstallNetFramework.bat
+++ solutions/ReleaseExtras/RuntimeFiles/InstallNetFramework.bat
@@ -1,8 +1,11 @@
 ::Author: Solxanich
 :: Created for tModLoader deployment. 
 @echo off
+setlocal EnableDelayedExpansion
+
 cd /D "%~dp0"
 set LOGFILE=LaunchLogs\install.log
+
 if Not exist LaunchLogs (mkdir LaunchLogs)
 
 echo Verifying installation directory...
@@ -10,10 +13,11 @@ echo Verifying installation directory...
 set IllegalFiles=avcodec-58.dll avdevice-58.dll avfilter-7.dll avformat-58.dll avutil-56.dll FAudio.dll ffmpeg.exe ffplay.exe ffprobe.exe FNA.dll Ionic.Zip.CF.dll Ionic.Zip.Reduced.dll libjpeg-9.dll libpng16-16.dll libtheorafile.dll libtiff-5.dll libwebp-7.dll log4net.dll MojoShader.dll Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll MonoMod.RuntimeDetour.dll MonoMod.RuntimeDetour.xml MonoMod.Utils.dll MonoMod.Utils.xml MP3Sharp.dll Newtonsoft.Json.dll NVorbis.dll postproc-55.dll ReLogicFNA.dll SDL2.dll SDL2_image.dll soft_oal.dll Steamworks.NET.64Bit.dll steam_api64.dll swresample-3.dll swscale-5.dll System.Windows.Forms.Mono.dll zlib1.dll
 set ColorRed=[91m
 set ColorDefault=[0m
+set ShownFileDeletionPrompt=false
 
 for %%f in (%IllegalFiles%) do (
 	if exist %%f (
-		if "%ShownFileDeletionPrompt%"=="" (
+		if !ShownFileDeletionPrompt!==false (
 			echo.
 			echo %ColorRed%It has been detected that your installation directory contains third-party library files%ColorDefault%
 			echo %ColorRed%that could potentially break tModLoader. If you have previously installed other tModLoader editions%ColorDefault%
