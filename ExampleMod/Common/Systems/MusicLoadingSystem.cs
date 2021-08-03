23809fc9c2a2a3366fbd8c48dd85c60612152305
Stevie <xxlennygamerxx@gmail.com>
2021-08-03 13:38:40 -0700

    Update ExampleMod with newer music loading code.

--- ExampleMod/Common/Systems/MusicLoadingSystem.cs
+++ ExampleMod/Common/Systems/MusicLoadingSystem.cs
@@ -10,16 +10,14 @@ namespace ExampleMod.Common.Systems
 		public override void OnModLoad() {
 			base.OnModLoad();
 
-			// When registering music manually, you will
-			// have to provide an instance to your mod.
-			// You still have to enter the path including
-			// your mod's name, though. The extension is also
-			// required, but only when adding music.
-			MusicLoader.AddMusic(Mod, "ExampleMod/Assets/Sounds/Music/MarbleGallery.ogg");
+			// When registering music manually, you will have to provide an instance to your mod.
+			// Since you're providing an instance of your mod, you should not start the path with your mod's name.
+			// File extensions are optional. You can only provide a file extension when adding music, but they are automatically detected.
+			// Accepted music formats are: .mp3, .ogg, and .wav files.
+			MusicLoader.AddMusic(Mod, "Assets/Sounds/Music/MarbleGallery");
 
-			// When getting the music slow, you
-			// should not add the file extensions!
-			Mod.AddMusicBox(MusicLoader.GetMusicSlot("ExampleMod/Assets/Sounds/Music/MarbleGallery"), ModContent.ItemType<ExampleMusicBox>(), ModContent.TileType<ExampleMusicBoxTile>());
+			// When getting the music slot, you should not add the file extensions!
+			MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot("ExampleMod/Assets/Sounds/Music/MarbleGallery"), ModContent.ItemType<ExampleMusicBox>(), ModContent.TileType<ExampleMusicBoxTile>());
 		}
 	}
 }
