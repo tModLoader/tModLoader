7f2e669d02eb6e1270a0b9f4fccc9ad9f27e2300
JavidPack <javidpack@gmail.com>
2021-08-03 21:17:06 -0600

    Fix #1682

--- patches/tModLoader/Terraria/Utils/TML.cs
+++ patches/tModLoader/Terraria/Utils/TML.cs
@@ -35,9 +35,7 @@ namespace Terraria
 
 		public static DateTime UnixTimeStampToDateTime(long unixTimeStamp) {
 			// Unix timestamp is seconds past epoch
-			System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-			dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
-			return dtDateTime;
+			return DateTimeOffset.FromUnixTimeSeconds(unixTimeStamp).UtcDateTime;
 		}
 
 		//Struct extensions
