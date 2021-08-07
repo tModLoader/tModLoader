eabc6d4cc36d88cf20f40287d6afbec3d413c2f3
Pbone3 <pbone785@gmail.com>
2021-08-07 00:09:16 -0500

    ExampleWings IsLoadingEnabled example

--- ExampleMod/Content/Items/Accessories/ExampleWings.cs
+++ ExampleMod/Content/Items/Accessories/ExampleWings.cs
@@ -1,3 +1,4 @@
+using ExampleMod.Common.Configs;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
@@ -9,6 +10,9 @@ namespace ExampleMod.Content.Items.Accessories
 	[AutoloadEquip(EquipType.Wings)]
 	public class ExampleWings : ModItem
 	{
+		// To see how this config option was added, see ExampleModConfig.cs
+		public override bool IsLoadingEnabled(Mod mod) => ModContent.GetInstance<ExampleModConfig>().ExampleWingsToggle;
+
 		public override void SetStaticDefaults() {
 			Tooltip.SetDefault("This is a modded wing.");
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
