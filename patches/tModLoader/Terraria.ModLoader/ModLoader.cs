97f4c952504a70158ed816c6df7894a5e758d602
JavidPack <javidpack@gmail.com>
2021-08-24 14:22:35 -0600

    Ignore last digit in versions for version mismatch warning

--- patches/tModLoader/Terraria.ModLoader/ModLoader.cs
+++ patches/tModLoader/Terraria.ModLoader/ModLoader.cs
@@ -166,7 +166,7 @@ namespace Terraria.ModLoader
 				var msg = Language.GetTextValue("tModLoader.LoadError", string.Join(", ", responsibleMods));
 				if (responsibleMods.Count == 1) {
 					var mod = ModOrganizer.FindMods().FirstOrDefault(m => m.Name == responsibleMods[0]); //use First rather than Single, incase of "Two mods with the same name" error message from ModOrganizer (#639)
-					if (mod != null && mod.tModLoaderVersion != version)
+					if (mod != null && (mod.tModLoaderVersion.Major != version.Major || mod.tModLoaderVersion.Minor != version.Minor || mod.tModLoaderVersion.Build != version.Build))
 						msg += "\n" + Language.GetTextValue("tModLoader.LoadErrorVersionMessage", mod.tModLoaderVersion, versionedName);
 				}
 				if (responsibleMods.Count > 0)
