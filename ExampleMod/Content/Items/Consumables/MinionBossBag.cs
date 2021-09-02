3fbbf8ac6e924b8626aa01f1d529bc4de70b66ee
Mirsario <me@mirsar.io>
2021-09-02 22:15:21 +0300

    Fixing my own typos.

--- ExampleMod/Content/Items/Consumables/MinionBossBag.cs
+++ ExampleMod/Content/Items/Consumables/MinionBossBag.cs
@@ -20,8 +20,8 @@ namespace ExampleMod.Content.Items.Consumables
 			DisplayName.SetDefault("Treasure Bag");
 			Tooltip.SetDefault("{$CommonItemTooltip.RightClickToOpen}"); //References a language key that says "Right Click To Open" in the language of the game
 
-			ItemID.Sets.BossBag[Type] = true; // This set is one that every boss bag should have, it, for example, lets our boss bag drop boss armor..
-			ItemID.Sets.PreHardmodeLikeBossBag[Type] = true; // ..But this set ensures that it's only dropped on special world seeds, since that's the behavior of pre-hardmode boss bags.
+			ItemID.Sets.BossBag[Type] = true; // This set is one that every boss bag should have, it, for example, lets our boss bag drop dev armor..
+			ItemID.Sets.PreHardmodeLikeBossBag[Type] = true; // ..But this set ensures that dev armor will only be dropped on special world seeds, since that's the behavior of pre-hardmode boss bags.
 
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
 		}
