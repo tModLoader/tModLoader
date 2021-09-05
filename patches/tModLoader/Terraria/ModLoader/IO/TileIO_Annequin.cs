0830169517e9cc441f9db05675e3044af1c7f28f
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2021-09-05 20:39:20 +0200

    Cleaner now

--- patches/tModLoader/Terraria/ModLoader/IO/TileIO_Annequin.cs
+++ patches/tModLoader/Terraria/ModLoader/IO/TileIO_Annequin.cs
@@ -49,8 +49,6 @@ namespace Terraria.ModLoader.IO
 			ISet<int> headSlots = new HashSet<int>();
 			ISet<int> bodySlots = new HashSet<int>();
 			ISet<int> legSlots = new HashSet<int>();
-			IDictionary<int, (int itemFrameId, List<TagCompound> globalData)> itemFrames = 
-				new Dictionary<int, (int, List<TagCompound>)>();
 			for (int i = 0; i < Main.maxTilesX; i++) {
 				for (int j = 0; j < Main.maxTilesY; j++) {
 					Tile tile = Main.tile[i, j];
@@ -74,11 +72,15 @@ namespace Terraria.ModLoader.IO
 				}
 			}
 			int tileEntity = 0;
+			List<TagCompound> itemFrames = new List<TagCompound>();
 			foreach (KeyValuePair<int, TileEntity> entity in TileEntity.ByID) {
 				if (entity.Value is TEItemFrame itemFrame) {
 					var globalData = ItemIO.SaveGlobals(itemFrame.item);
 					if (globalData != null || ItemLoader.NeedsModSaving(itemFrame.item)) {
-						itemFrames.Add(itemFrame.ID, (tileEntity, globalData));
+						itemFrames.Add(new TagCompound {
+							["id"] = tileEntity,
+							["item"] = ItemIO.Save(itemFrame.item, globalData)
+						});
 						//flags[0] |= 2; legacy
 						numFlags = 1;
 					}
@@ -119,12 +121,7 @@ namespace Terraria.ModLoader.IO
 			tag.Set("data", ms.ToArray());
 
 			if (itemFrames.Count > 0) {
-				tag.Set("itemFrames", itemFrames.Select(entry =>
-					new TagCompound {
-						["id"] = entry.Value.itemFrameId,
-						["item"] = ItemIO.Save(((TEItemFrame)TileEntity.ByID[entry.Key]).item, entry.Value.globalData)
-					}
-				).ToList());
+				tag.Set("itemFrames", itemFrames);
 			}
 			return tag;
 		}
