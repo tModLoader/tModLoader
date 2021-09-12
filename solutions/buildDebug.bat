fe6db8cbc24a129ceef3018e10b4f688a734c231
Mirsario <me@mirsar.io>
2021-09-12 21:43:37 +0300

    Made the workflow upload msbuild.binlog

--- solutions/buildDebug.bat
+++ solutions/buildDebug.bat
@@ -5,5 +5,25 @@ echo Cleaning up output folders...
 dotnet clean tModLoader.sln -c Debug --nologo -v q
 
 echo Building Debug (1/1)
-dotnet build tModLoader.sln -c Debug --nologo -v q /clp:ErrorsOnly
-@IF %ERRORLEVEL% NEQ 0 (pause && EXIT /B %ERRORLEVEL%)
+
+@IF NOT "%UPLOAD_MSBUILD_LOGS%"=="" (
+	set BUILD_ARGS=-bl
+)
+
+@IF NOT "%BUILD_ARGS%"=="" (
+	echo Using additional build arguments: %BUILD_ARGS%
+)
+
+dotnet build tModLoader.sln %BUILD_ARGS% -c Debug --nologo -v q /clp:ErrorsOnly
+
+@IF NOT "%UPLOAD_MSBUILD_LOGS%"=="" (
+	echo Uploading msbuild log...
+	curl --upload-file ./msbuild.binlog http://transfer.sh/msbuild.binlog
+	echo.
+	echo Uploaded!
+)
+
+@IF %ERRORLEVEL% NEQ 0 (
+	pause
+	EXIT /B %ERRORLEVEL%
+)
