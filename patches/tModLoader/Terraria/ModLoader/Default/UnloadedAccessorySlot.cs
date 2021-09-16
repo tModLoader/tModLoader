e4b5e8f88561a3c4869a0261678387bc31a683e1
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-09-16 17:07:03 -0600

    Finish point-testing multiplayer and patch

--- patches/tModLoader/Terraria/ModLoader/Default/UnloadedAccessorySlot.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/UnloadedAccessorySlot.cs
@@ -13,5 +13,8 @@ namespace Terraria.ModLoader.Default
 		public override bool IsSlotValid() { 
 			return false;
 		}
+
+		// Only display unloaded slots if in singleplayer. Don't need people carrying items in to multiplayer they shouldn't have :(
+		public override bool IsSlotVisibleButNotValid() => !(Main.netMode == 1);
 	}
 }
