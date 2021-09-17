f11a562f80a77543df85cf8996d336d12aaebff7
Mirsario <me@mirsar.io>
2021-09-17 15:20:44 +0300

    StatModifier styling.

--- patches/tModLoader/Terraria/ModLoader/StatModifier.cs
+++ patches/tModLoader/Terraria/ModLoader/StatModifier.cs
@@ -28,37 +28,40 @@
 			return hashCode;
 		}
 
-		public static StatModifier operator +(StatModifier m, float add) =>
-			new StatModifier(m.Additive + add, m.Multiplicative);
+		public static StatModifier operator +(StatModifier m, float add)
+			=> new StatModifier(m.Additive + add, m.Multiplicative);
 
-		public static StatModifier operator -(StatModifier m, float sub) =>
-			new StatModifier(m.Additive - sub, m.Multiplicative);
+		public static StatModifier operator -(StatModifier m, float sub)
+			=> new StatModifier(m.Additive - sub, m.Multiplicative);
 
-		public static StatModifier operator *(StatModifier m, float mul) =>
-			new StatModifier(m.Additive, m.Multiplicative * mul);
+		public static StatModifier operator *(StatModifier m, float mul)
+			=> new StatModifier(m.Additive, m.Multiplicative * mul);
 
-		public static StatModifier operator /(StatModifier m, float div) =>
-			new StatModifier(m.Additive, m.Multiplicative / div);
+		public static StatModifier operator /(StatModifier m, float div)
+			=> new StatModifier(m.Additive, m.Multiplicative / div);
 
-		public static StatModifier operator +(float add, StatModifier m) => m + add;
-		public static StatModifier operator *(float mul, StatModifier m) => m * mul;
+		public static StatModifier operator +(float add, StatModifier m)
+			=> m + add;
 
-		public static bool operator ==(StatModifier m1, StatModifier m2) =>
-			m1.Additive == m2.Additive && m1.Multiplicative == m2.Multiplicative;
+		public static StatModifier operator *(float mul, StatModifier m)
+			=> m * mul;
 
-		public static bool operator !=(StatModifier m1, StatModifier m2) =>
-			m1.Additive != m2.Additive || m1.Multiplicative != m2.Multiplicative;
+		public static bool operator ==(StatModifier m1, StatModifier m2)
+			=> m1.Additive == m2.Additive && m1.Multiplicative == m2.Multiplicative;
 
-		public static implicit operator float(StatModifier m) =>
-			m.Additive * m.Multiplicative;
+		public static bool operator !=(StatModifier m1, StatModifier m2)
+			=> m1.Additive != m2.Additive || m1.Multiplicative != m2.Multiplicative;
 
-		public static implicit operator int(StatModifier m) =>
-			(int)(m.Additive * m.Multiplicative);
+		public static implicit operator float(StatModifier m)
+			=> m.Additive * m.Multiplicative;
 
-		public StatModifier CombineWith(StatModifier m) =>
-			new StatModifier(Additive + m.Additive - 1, Multiplicative * m.Multiplicative);
+		public static implicit operator int(StatModifier m)
+			=> (int)(float)m;
 
-		public StatModifier Scale(float scale) =>
-			new StatModifier(1 + (Additive - 1) * scale, 1 + (Multiplicative - 1) * scale);
+		public StatModifier CombineWith(StatModifier m)
+			=> new StatModifier(Additive + m.Additive - 1, Multiplicative * m.Multiplicative);
+
+		public StatModifier Scale(float scale)
+			=> new StatModifier(1 + (Additive - 1) * scale, 1 + (Multiplicative - 1) * scale);
 	}
 }
