a4aa86b6f97ac5cc4c29b89a48d1cc14c25fb8f1
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-09-18 16:43:23 -0600

    Many Misc Tiny Fixes
    1) Cleanup MonoModHooks file to have less extra stuff going on
    2) Improve logging surrounding MonoModHooks
    3) Fix Beta Welcome message conflicting with the FirstLaunch Message for first time users
    4) Add the COM_Plus variable in to tmodloader instead of start files, fixes #1718
    5) Cleanup the manual install .NET instructions a bit
    6) Add a "sysdotnet" launch param for start-tmod.bat files so can use a system dotnet in the event that portables just won't work (or w/e other reason).
    7) Make InstallNetFramework.bat able to be run independent to start scripts again.

--- solutions/ReleaseExtras/RuntimeFiles/start-tModLoaderServer.bat
+++ solutions/ReleaseExtras/RuntimeFiles/start-tModLoaderServer.bat
@@ -1,9 +1,6 @@
 @echo off
 cd /D "%~dp0"
 set Args=%* -server -config serverconfig.txt
-setlocal EnableDelayedExpansion
-
-call InstallNetFramework.bat
 
 set /P steam=Use Steam Server [y]/[n] steam:
 if NOT %steam%==y ( goto start )
@@ -14,8 +11,9 @@ if NOT %lobby%==p ( set Args=%Args% -lobby friends )
 if %lobby%==p ( set Args=%Args% -lobby private )
 
 :start
-if exist %INSTALLDIR%\dotnet.exe ( 
-	start dotnet\%VERSIONSEL%\dotnet.exe tModLoader.dll %Args% 
-) else (
+if "%~1"=="-sysdotnet"  ( 
 	dotnet tModLoader.dll %Args%
+) else (
+	call InstallNetFramework.bat
+	start dotnet\%VERSIONSEL%\dotnet.exe tModLoader.dll %Args%
 )
