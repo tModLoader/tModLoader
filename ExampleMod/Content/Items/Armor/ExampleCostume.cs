14a1cd24d0f6501550a106ae68cb03e511fab699
Exterminatorx99 <kristijan.kos99@gmail.com>
2021-09-18 23:44:08 +0200

    Better Instance

--- ExampleMod/Content/Items/Armor/ExampleCostume.cs
+++ ExampleMod/Content/Items/Armor/ExampleCostume.cs
@@ -10,8 +10,8 @@ namespace ExampleMod.Content.Items.Armor
 	// Typically Armor items are automatically paired with an EquipTexture, but we can manually use EquipTextures to achieve more unique effects.
 	// There is code for this effect in many places, look in the following files for the full implementation:
 	// NPCs.ExamplePerson drops this item when killed
-	// Items.Armor.ExampleCostume (below) is the accessory item that sets ExampleCostumePlayer values. Note that this item does not have EquipTypes set. This is a vital difference and key to our approach.
-	// Items.Armor.BlockyHead/Body/Legs (below) are EquipTexture classes. They simply disable the drawing of the player's head/body/legs respectively when they are set as the drawn EquipTexture. One spawns dust too.
+	// Content.Items.Armor.ExampleCostume (below) is the accessory item that sets ExampleCostumePlayer values. Note that this item does not have EquipTypes set. This is a vital difference and key to our approach.
+	// Content.Items.Armor.BlockyHead/Body/Legs (below) are EquipTexture classes. They simply disable the drawing of the player's head/body/legs respectively when they are set as the drawn EquipTexture. One spawns dust too.
 	// ExampleMod.Load() shows calling AddEquipTexture 3 times with appropriate parameters. This is how we register EquipTexture manually instead of the automatic pairing of ModItem and EquipTexture that other equipment uses.
 	// Buffs.Blocky is the Buff that is shown while in Blocky mode. The buff is responsible for the actual stat effects of the costume. It also needs to remove itself when not near town npcs.
 	// ExampleCostumePlayer has 5 bools. They manage the visibility and other things related to this effect.
@@ -23,11 +23,7 @@ namespace ExampleMod.Content.Items.Armor
 	// Remember that the visuals and the effects of Costumes must be kept separate. Follow this example for best results.
 	public class ExampleCostume : ModItem
 	{
-		public static ExampleCostume Instance { get; private set; }
-
-		public override void Load() {
-			Instance = this;
-		}
+		public static ExampleCostume Instance => ModContent.GetInstance<ExampleCostume>();
 
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Charm of Example");
