220ca92892f9c4e252ca2a2c723546b9fd04761c
nmoll <59670736+Solxanich@users.noreply.github.com>
2021-09-30 20:16:09 -0600

    Update PublishWorkshopItem.bat to be closer to desired

--- .github/workflows/PublishWorkshopItem.bat
+++ .github/workflows/PublishWorkshopItem.bat
@@ -2,26 +2,27 @@
 :: Configuration of all parameters
 ::Tmod Constants
 set appid=1281930
+set pass2=IUNDERSTANDTHATTHISALPHAISMOSTLYFORDEVELOPERS
 
 ::User Constants
-set publishid=%1
-set contentfolder=%~2
-
-::PublishData variables
-set changenote=%3
-set description=%4
-set modversion=%5
-set tmodversion=%6
+set root=%1
+set item=%2
 
 ::Steam Login Info variables
-set username=%7
-set password=%8
-set steamguard=%9
+set username=%4
+set password=%5
+set steamguard=%6
 
+:: Fetch tModLoader 1.4 from Steam
+steamcmd.exe +login %username% %password% %steamguard% +force_install_dir %root%/tMod +app_update %appid% -validate -beta public-1.4-alpha -betapassword %pass2% +quit
+
+dotnet %root%/tMod/tmodloader.dll -server -build %item% -tmlsavedirectory %root%/build -ciprep
+
+:: All the following should be done in tmodloader.dll build action when -ciprep is included as an argument [
 :: Verify workshop.json
 if Not exist %contentfolder%\workshop.json (echo "ERROR: no workshop.json found")
 
-:: Create the vdf file
+:: Create the vdf file 
 set descriptionFinal=[quote=GithubActions(Don't Modify)]Version %modversion% built for tModLoader v%tmodversion%[/quote]%description%
 (
 	echo "workshopitem"
@@ -33,7 +34,8 @@ set descriptionFinal=[quote=GithubActions(Don't Modify)]Version %modversion% bui
 	echo "description" "%descriptionFinal%"
 	echo }
 )>publish.vdf
+::] End comment
 
 :: Publish the item
 @echo on
-steamcmd.exe +login %username% %password% %steamguard% +workshop_build_item publish.vdf +quit
+steamcmd.exe +login %username% +workshop_build_item publish.vdf +quit
