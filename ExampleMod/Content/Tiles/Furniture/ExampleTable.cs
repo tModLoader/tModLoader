6430416ab2bbbf4a705fa032ebf8ab9b34c988d5
Mirsario <me@mirsar.io>
2021-10-06 19:48:07 +0300

    EM comments.

--- ExampleMod/Content/Tiles/Furniture/ExampleTable.cs
+++ ExampleMod/Content/Tiles/Furniture/ExampleTable.cs
@@ -16,7 +16,7 @@ namespace ExampleMod.Content.Tiles.Furniture
 			Main.tileLavaDeath[Type] = true;
 			Main.tileFrameImportant[Type] = true;
 			TileID.Sets.DisableSmartCursor[Type] = true;
-			TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
 
 			DustType = ModContent.DustType<Dusts.Sparkle>();
 			AdjTiles = new int[] { TileID.Tables };
