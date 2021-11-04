1b55ab5693f2e5aba118811f5d8fb8531c6fca96
Solxanich <59670736+Solxanich@users.noreply.github.com>
2021-11-04 06:02:21 -0600

    Refactor BiomeVisuals to be in ModSceneEffect (#1826)

--- patches/tModLoader/Terraria/ModLoader/ModSceneEffect.cs
+++ patches/tModLoader/Terraria/ModLoader/ModSceneEffect.cs
@@ -60,5 +60,10 @@ namespace Terraria.ModLoader
 		}
 
 		public virtual bool IsSceneEffectActive(Player player) => false;
+
+		/// <summary>
+		/// Allows you to create special visual effects in the area around the player. For example, the blood moon's red filter on the screen or the slime rain's falling slime in the background. You must create classes that override Terraria.Graphics.Shaders.ScreenShaderData or Terraria.Graphics.Effects.CustomSky, add them in your mod's Load hook, then call Player.ManageSpecialBiomeVisuals. See the ExampleMod if you do not have access to the source code.
+		/// </summary>
+		public virtual void SpecialVisuals(Player player) { }
 	}
 }
