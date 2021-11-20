a9b294e97fe9c4b0da654f839297a7131b1fde05
Pbone3 <pbone785@gmail.com>
2021-11-20 00:16:21 -0600

    Tracked BankID.cs

---
+++ patches/tModLoader/Terraria/ID/BankID.cs
@@ -0,0 +1,13 @@
+﻿namespace Terraria.ID
+{
+	/// <summary>
+	/// Values used to identify special banks with the <see cref="Player.chest"></see> field
+	/// </summary>
+	public static class BankID
+	{
+		public const int PiggyBank = -2;
+		public const int Safe = -3;
+		public const int DefendersForge = -4;
+		public const int VoidVault = 5;
+    }
+}
