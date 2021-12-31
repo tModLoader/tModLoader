5c8eb1e31e9c3d51cdb3331895a42830eeed9dda
Mirsario <me@mirsar.io>
2022-01-01 02:59:53 +0300

    EntitySource_TileUpdate

---
+++ patches/tModLoader/Terraria/DataStructures/EntitySource_TileUpdate.cs
@@ -0,0 +1,8 @@
+namespace Terraria.DataStructures
+{
+	// New class.
+	public class EntitySource_TileUpdate : EntitySource_Tile
+	{
+		public EntitySource_TileUpdate(int tileCoordsX, int tileCoordsY) : base(tileCoordsX, tileCoordsY) { }
+	}
+}
