6e1bdb8fb3a8ebe8725c70b76c6e6919d74adde4
Mirsario <Mirsario@users.noreply.github.com>
2022-01-20 16:55:11 +0000

    deploy: 9d644d48b36a9e3d41c0a56adcf1f70b1d6be57a

--- html_alpha/struct_terraria_1_1_n_p_c_spawn_params.js
+++ html_alpha/struct_terraria_1_1_n_p_c_spawn_params.js
@@ -1,7 +1,7 @@
 var struct_terraria_1_1_n_p_c_spawn_params =
 [
     [ "WithScale", "struct_terraria_1_1_n_p_c_spawn_params.html#ac34d2e5ac1e78218fbde4a02a1cf8965", null ],
-    [ "gameModeData", "struct_terraria_1_1_n_p_c_spawn_params.html#ac1c9d6f9dd9138761ca95c8248dd8ac6", null ],
+    [ "gameModeData", "struct_terraria_1_1_n_p_c_spawn_params.html#af6a39108e471a3b7cee11725416d8676", null ],
     [ "playerCountForMultiplayerDifficultyOverride", "struct_terraria_1_1_n_p_c_spawn_params.html#a5afa4f5ab8474dc391dbae100042e1cc", null ],
     [ "sizeScaleOverride", "struct_terraria_1_1_n_p_c_spawn_params.html#a05b7adab302685ca04674210e78bd12c", null ],
     [ "strengthMultiplierOverride", "struct_terraria_1_1_n_p_c_spawn_params.html#a4866b1f5a642bb88d20590b026b9a401", null ]
