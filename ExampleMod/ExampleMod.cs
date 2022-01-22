ade43686537dcbb0fd0af19a3ad0a91e23db68a0
Mirsario <me@mirsar.io>
2022-01-23 01:04:50 +0300

    EM: More on unloading.

--- ExampleMod/ExampleMod.cs
+++ ExampleMod/ExampleMod.cs
@@ -20,10 +20,13 @@ namespace ExampleMod
 		}
 
 		public override void Unload() {
-			// The unload method can be used for unloading/disposing/clearing special objects, unsubscribing from events, or for undoing some of your mod's actions.
+			// The Unload() methods can be used for unloading/disposing/clearing special objects, unsubscribing from events, or for undoing some of your mod's actions.
 			// Be sure to always write unloading code when there is a chance of some of your mod's objects being kept present inside the vanilla assembly.
-			// The most common reason for that to happen comes from using events, NOT counting On.* and IL.* TerrariaHooks namespaces. If you subscribe to one - be sure to eventually unsubscribe from it. 
-			// However, there is rarely a need to null values of static fields, since TML aims to completely dispose mod assemblies in-between mod reloads.
+			// The most common reason for that to happen comes from using events, NOT counting On.* and IL.* code-injection namespaces.
+			// If you subscribe to an event - be sure to eventually unsubscribe from it.
+
+			// NOTE: When writing unload code - be sure use 'defensive programming'. Or, in other words, you should always assume that everything in the mod you're unloading might've not even been initialized yet.
+			// NOTE: There is rarely a need to null-out values of static fields, since TML aims to completely dispose mod assemblies in-between mod reloads.
 		}
 	}
 }
