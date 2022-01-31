b829717399f05fe8a43ad7d561b5f32231ea282c
Mirsario <me@mirsar.io>
2022-01-31 15:15:49 +0300

    'A' meant 'Abstract'............

--- patches/tModLoader/Terraria/DataStructures/EntitySource_TileUpdate.cs
+++ patches/tModLoader/Terraria/DataStructures/EntitySource_TileUpdate.cs
@@ -1,7 +1,7 @@
 namespace Terraria.DataStructures
 {
 	// New class.
-	public class EntitySource_TileUpdate : EntitySource_Tile
+	public class EntitySource_TileUpdate : AEntitySource_Tile
 	{
 		public EntitySource_TileUpdate(int tileCoordsX, int tileCoordsY) : base(tileCoordsX, tileCoordsY) { }
 	}
