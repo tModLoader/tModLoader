3a90558b8320e69b169f70c26d5d4b1e6ea2d99f
Mirsario <me@mirsar.io>
2022-02-09 02:16:38 +0300

    Data-oriented tiles (#2047)
    * Experimental: Data-oriented struct tile & tile data.
    
    * AggressiveInlining for Tile.Get<T>()
    
    * Improved Tile.CopyFrom
    
    * Public replacement properties for internalized Tile fields.
    
    * Tile.GetHashCode
    
    * Fixed a bad typo.
    
    * Reorganized TileData methods
    
    * Unhardcoded Tile.ClearEverything.
    
    * Aggressive Inlining & Optimization, less casts.
    
    * Make all Tile null checks no-op
    
    * Improve performance of TileData with unsafe memory.
    
    * Throw less useful Tilemap[,] bounds exception. 2x perf benefit.
    
    * Add TilemapPerformance test
    
    * Revert to single tilemap.
    
    * Tile property cleanup pass
    
    * Fix ExampleMod Tile property name usages
    
    * Shuffle misc tile data around for better organisation and more liquid IDs!
    
    * Use Array.Clear to reset world data. 40% faster overall world load time.
    
    * Add on-demand load and unload of TileData<T> arrays
    
    * Fix server loading
    
    * Minor formatting
    
    * Swap rendering iteration order for faster accessing
    
    * Improve tile performance test system
    
    * Make tilemap width and height ushort. Add potential Tile.X and Tile.Y properties (behind compiler flag for now).
    
    * Add tile data array getter to Tilemap
    
    Co-authored-by: Chicken-Bones <mehvids@gmail.com>

---
+++ patches/tModLoader/Terraria/TileData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+
+namespace Terraria
+{
+	public interface ITileData { }
+
+	internal static class TileData
+	{
+		internal static Action OnClearEverything;
+		internal static Action<uint> OnSetLength;
+		internal static Action<uint> OnClearSingle;
+		internal static Action<uint, uint> OnCopySingle;
+
+		internal static uint Length { get; private set; }
+		internal static void SetLength(uint len) {
+			Length = len;
+			OnSetLength?.Invoke(len);
+		}
+
+		internal static void ClearEverything() => OnClearEverything?.Invoke();
+		internal static void ClearSingle(uint index) => OnClearSingle(index);
+		internal static void CopySingle(uint sourceIndex, uint destinationIndex) => OnCopySingle(sourceIndex, destinationIndex);
+	}
+
+	internal static unsafe class TileData<T> where T : unmanaged, ITileData
+	{
+		public static T[] data { get; private set; }
+		public static T* ptr { get; private set; }
+
+		private static GCHandle handle;
+
+		static TileData() {
+			TileData.OnSetLength += SetLength;
+			TileData.OnClearEverything += ClearEverything;
+			TileData.OnCopySingle += CopySingle;
+			TileData.OnClearSingle += ClearSingle;
+			AssemblyLoadContext.GetLoadContext(typeof(T).Assembly).Unloading += _ => Unload();
+
+			SetLength(TileData.Length);
+		}
+
+		private static void Unload() {
+			TileData.OnSetLength -= SetLength;
+			TileData.OnClearEverything -= ClearEverything;
+			TileData.OnCopySingle -= CopySingle;
+			TileData.OnClearSingle -= ClearSingle;
+			if (data != null) {
+				handle.Free();
+				data = null;
+			}
+		}
+
+		public static void ClearEverything() {
+			Array.Clear(data);
+		}
+
+		private static unsafe void SetLength(uint len) {
+			if (data != null)
+				handle.Free();
+
+			data = new T[len];
+			handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+			ptr = (T*)handle.AddrOfPinnedObject().ToPointer();
+		}
+
+		private static unsafe void ClearSingle(uint index) {
+			ptr[index] = default;
+		}
+
+		private static unsafe void CopySingle(uint sourceIndex, uint destinationIndex) {
+			ptr[destinationIndex] = ptr[sourceIndex];
+		}
+	}
+}
