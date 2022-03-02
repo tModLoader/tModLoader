427d380d1e14014bf796796e5a6efe8ce6f314d5
direwolf420 <amakejk1@web.de>
2022-03-02 18:10:29 +0100

    fix patches, EM

--- ExampleMod/Content/Tiles/Furniture/ExampleToilet.cs
+++ ExampleMod/Content/Tiles/Furniture/ExampleToilet.cs
@@ -56,7 +56,7 @@ namespace ExampleMod.Content.Tiles.Furniture
 		}
 
 		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-			Item.NewItem(i * 16, j * 16, 16, 32, ModContent.ItemType<Items.Placeable.Furniture.ExampleToilet>());
+			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 32, ModContent.ItemType<Items.Placeable.Furniture.ExampleToilet>());
 		}
 
 		public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) {
