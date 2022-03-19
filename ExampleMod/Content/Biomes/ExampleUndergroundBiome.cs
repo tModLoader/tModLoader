f8d60b020fe6c9f2c2d843ee08ef387ed47c6008
nmoll <59670736+Solxanich@users.noreply.github.com>
2022-03-19 15:37:34 -0600

    ExampleMod Publish Fix 2

--- ExampleMod/Content/Biomes/ExampleUndergroundBiome.cs
+++ ExampleMod/Content/Biomes/ExampleUndergroundBiome.cs
@@ -37,7 +37,7 @@ namespace ExampleMod.Content.Biomes
 				Math.Abs(player.position.ToTileCoordinates().X - Main.maxTilesX / 2) < Main.maxTilesX / 6;
 		}
 
-		// In the event that both our biome AND one or more modded SceneEffect layers are active with the same AVFXPriority, this can decide which one.
+		// In the event that both our biome AND one or more modded SceneEffect layers are active with the same SceneEffect Priority, this can decide which one.
 		// It's uncommon that need to assign a weight - you'd have to specifically believe that you don't need higher AVFXPriority, but do need to be the active AVFX within the priority you designated
 		// In this case, we don't need it, so this inclusion is purely to demonstrate this is available.
 		// See the GetWeight documentation for more information.
