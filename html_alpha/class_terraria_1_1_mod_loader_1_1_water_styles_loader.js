a46aedeea2bc1b65c39138339d16965888ce9bfc
Solxanich <Solxanich@users.noreply.github.com>
2022-04-01 19:05:51 +0000

    deploy: f029ccecb5fc4d938cc34fa4a24ed9e84d617335

--- html_alpha/class_terraria_1_1_mod_loader_1_1_water_styles_loader.js
+++ html_alpha/class_terraria_1_1_mod_loader_1_1_water_styles_loader.js
@@ -4,6 +4,6 @@ var class_terraria_1_1_mod_loader_1_1_water_styles_loader =
     [ "ChooseStyle", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#a63a5f63ae949c1c1d22b9b9e18daf4ca", null ],
     [ "DrawWaterfall", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#a3526a493dc76ba856cd7cc111d2fb1c8", null ],
     [ "DrawWatersToScreen", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#a4bd772f1b1c4e13075bfbcbcd0e7e88d", null ],
-    [ "LightColorMultiplier", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#afabaa5898c75292e1e9b044171dee519", null ],
+    [ "LightColorMultiplier", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#a15a55f4479ac007ecbefd1927c094fe4", null ],
     [ "UpdateLiquidAlphas", "class_terraria_1_1_mod_loader_1_1_water_styles_loader.html#aecbf6392d458a5f58a28869941dab11f", null ]
 ];
