a356f53b51785f0e3928fce06ae14acdd3f4a1a5
nmoll <59670736+Solxanich@users.noreply.github.com>
2022-04-04 19:19:53 -0600

    Fix '-Config' errors in launch Server

--- patches/tModLoader/Terraria/release_extras/start-tModLoaderServer.sh
+++ patches/tModLoader/Terraria/release_extras/start-tModLoaderServer.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-launch_args="-server -config serverconfig.txt"
+launch_args="$@ -server"
+if [[ ! "$launch_args" == *"-config"* ]]; then
+	launch_args="$launch_args -config serverconfig.txt"
+fi
 
 read -p "Use Steam Server (y)/(n) " steam
 
@@ -17,8 +20,6 @@ if [ $steam = "y" ]; then
 		launch_args="$launch_args -lobby private"
 	fi
 fi
-
-launch_args="$launch_args $@"
 
 chmod +x ./LaunchUtils/ScriptCaller.sh
 ./start-tModLoader.sh $launch_args
