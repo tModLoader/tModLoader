62f5a70e8daa6c20be410014640b53eb065bcd42
Steviegt6 <xxlennygamerxx@gmail.com>
2022-04-04 20:52:06 -0700

    More patchwork done to adjust for changed Achievement properties.

--- patches/tModLoader/Terraria/Achievements/Achievement.TML.cs
+++ patches/tModLoader/Terraria/Achievements/Achievement.TML.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Terraria.Achievements
@@ -11,6 +12,9 @@ namespace Terraria.Achievements
 	{
 		// Give vanilla achievements a normal name.
 		public override string Name => IsModded() ? base.Name : _name;
+
+		// Give vanilla achievements the normal massive atlas of textures.
+		public override string Texture => IsModded() ? base.Texture : "Images/UI/Achievements";
 
 		// Parameterless constructor for modded achievements.
 		// Vanilla ones using Achievement(string) which is located in the main class.
@@ -26,6 +30,9 @@ namespace Terraria.Achievements
 					$"An achievement ID mismatch occurred. Achievement \"{Name}\" was registered with an ID of {id}, but was instantiated with an ID of {Id}.");
 			}
 
+			FriendlyName = LocalizationLoader.GetOrCreateTranslation(Mod, $"AchievementName.{Name}");
+			Description = LocalizationLoader.GetOrCreateTranslation(Mod, $"AchievementDescription.{Name}");
+
 			Main.Achievements.Register(this);
 		}
 
@@ -35,6 +42,14 @@ namespace Terraria.Achievements
 		/// <returns></returns>
 		public bool IsModded() {
 			return Id > AchievementID.Count;
+		}
+
+		internal string GetFriendlyName() {
+			return FriendlyName.GetTranslation(LanguageManager.Instance.ActiveCulture);
+		}
+
+		internal string GetDescription() {
+			return Description.GetTranslation(LanguageManager.Instance.ActiveCulture);
 		}
 	}
 }
