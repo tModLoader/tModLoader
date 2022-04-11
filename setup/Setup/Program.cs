7d3ad6acb5e7d54c62bbed298b6823558d297e20
JavidPack <javidpack@gmail.com>
2022-04-11 03:45:53 -0600

    Account for postbuild modifying the file as well.

--- setup/Setup/Program.cs
+++ setup/Setup/Program.cs
@@ -220,6 +220,7 @@ namespace Terraria.ModLoader.Setup
 				string TMLVERSIONDefine = $"TML_{string.Join("_", TMLVERSION.Split('.').Take(2))}";
 				Console.WriteLine($"TMLVERSIONDefine: {TMLVERSIONDefine}");
 				tMLModTargetsContents = tMLModTargetsContents.Replace("<DefineConstants></DefineConstants>", $"<DefineConstants>{TMLVERSIONDefine}</DefineConstants>");
+				UpdateFileText("patches/tModLoader/Terraria/release_extras/tMLMod.targets", tMLModTargetsContents); // The patch file needs to be updated as well since it will be copied to src and the postbuild will copy it to the steam folder as well.
 			}
 			UpdateFileText(Path.Combine(TMLDevSteamDir, "tMLMod.targets"), tMLModTargetsContents);
 		}
