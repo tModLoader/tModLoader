96a911287dd89a37aa4f6b25b64c611af96c476c
ThomasThePencil <tailsguy24@gmail.com>
2022-04-14 16:16:42 -0400

    added all vanilla town NPCs to the town NPC profile system
    mod extensibility comin' soon

---
+++ patches/tModLoader/Terraria/GameContent/Profiles.TML.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+
+namespace Terraria.GameContent
+{
+	public partial class Profiles
+	{
+		public class AlternateLegacyNPCProfile : ITownNPCProfile
+		{
+			private string _rootFilePath;
+			private int _defaultVariationHeadIndex;
+			private Asset<Texture2D> _defaultNoAlt;
+
+			public LegacyNPCProfile(string npcFileTitleFilePath, int defaultHeadIndex) {
+				_rootFilePath = npcFileTitleFilePath;
+				_defaultVariationHeadIndex = defaultHeadIndex;
+				if (Main.dedServ)
+					return;
+
+				_defaultNoAlt = Main.Assets.Request<Texture2D>(npcFileTitleFilePath, AssetRequestMode.DoNotLoad);
+			}
+
+			public int RollVariation() => 0;
+			public string GetNameForVariant(NPC npc) => NPC.getNewNPCName(npc.type);
+
+			public Asset<Texture2D> GetTextureNPCShouldUse(NPC npc) => _defaultNoAlt;
+
+			public int GetHeadTextureIndex(NPC npc) => _defaultVariationHeadIndex;
+		}
+	}
+}
