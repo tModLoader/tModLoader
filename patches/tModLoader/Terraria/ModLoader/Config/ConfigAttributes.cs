41bbe644f12a0054cf1791f7a1efb98f2a2423e2
Lumelore <88530227+Lumelore@users.noreply.github.com>
2022-04-15 02:10:54 -0500

    ExampleMod: ExampleFoodItem (#2292)
    * ExampleMod: ExamplePie.cs + ExamplePie.png
    
    Adds example pie item. Code is commented and item uses the example palette
    
    * Formatting, documentation, DefaultToFood usage
    
    * Rename to ExampleFoodItem
    
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- patches/tModLoader/Terraria/ModLoader/Config/ConfigAttributes.cs
+++ patches/tModLoader/Terraria/ModLoader/Config/ConfigAttributes.cs
@@ -324,7 +324,7 @@ namespace Terraria.ModLoader.Config
 	}
 
 	/// <summary>
-	/// Affects whether this data will be presented as a slider of an input field. Add this attribute to use a slider. Currently only affects data of type int.
+	/// Affects whether this data will be presented as a slider or an input field. Add this attribute to use a slider. Currently only affects data of type int.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 	public class SliderAttribute : Attribute
