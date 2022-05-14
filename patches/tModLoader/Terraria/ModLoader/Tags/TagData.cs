5d3dcea68b2d0dd6bec02d9ce69975dc07acc21f
Mirsario <me@mirsar.io>
2022-05-14 03:15:53 +0300

    Workaround for new IOOB issues. Branch sucks.

--- patches/tModLoader/Terraria/ModLoader/Tags/TagData.cs
+++ patches/tModLoader/Terraria/ModLoader/Tags/TagData.cs
@@ -5,19 +5,19 @@ namespace Terraria.ModLoader.Tags
 	/// <summary> This class stores lists of content Ids associated with the tag that it represents. </summary>
 	public sealed class TagData
 	{
-		private readonly bool[] idToValue; //Accessed for quick HasTag checks.
+		//private readonly bool[] idToValue; //Accessed for quick HasTag checks.
 		private readonly List<int> entryList;
 		private readonly IReadOnlyList<int> readonlyEntryList; //Accessed for GetEntriesWithTag.
 
 		internal TagData(int maxEntries) {
-			idToValue = new bool[maxEntries];
+			//idToValue = new bool[maxEntries];
 			entryList = new List<int>();
 			readonlyEntryList = entryList.AsReadOnly();
 		}
 
 		/// <summary> Returns whether or not the content piece with the Id has this tag. </summary>
 		/// <param name="id"> The content id. </param>
-		public bool Has(int id) => idToValue[id];
+		public bool Has(int id) => entryList.Contains(id); //idToValue[id];
 
 		/// <summary> Returns a list of content Ids that have this tag. </summary>
 		public IReadOnlyList<int> GetEntries() => readonlyEntryList;
@@ -26,7 +26,7 @@ namespace Terraria.ModLoader.Tags
 		/// <param name="id"> The content id. </param>
 		/// <param name="value"> Whether or not the tag should be present for the provided content id. </param>
 		public void Set(int id, bool value) {
-			idToValue[id] = value;
+			//idToValue[id] = value;
 
 			if (!value) {
 				entryList.Remove(id);
