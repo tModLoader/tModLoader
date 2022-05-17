3ecd01fd33fd617c759107036ab4d6c691cb6a6f
nmoll <59670736+Solxanich@users.noreply.github.com>
2022-05-16 20:22:47 -0600

    First & hopefully only batch of code cleanups.

--- ExampleMod/Content/Tiles/Plants/ExamplePalmTree.cs
+++ ExampleMod/Content/Tiles/Plants/ExamplePalmTree.cs
@@ -37,11 +37,11 @@ namespace ExampleMod.Content.Tiles
 		// Regular Top Textures for the Ocean (we reused the same texture to save time, you don't have to reuse them): 
 		public override Asset<Texture2D> GetBranchTextures() => null;
 
-		// Palm Trees come in an Oasis variant. The Top Textures for it:
-		public override Asset<Texture2D> GetOasisTopTextures() => ModContent.Request<Texture2D>("ExampleMod/Content/Tiles/Plants/ExamplePalmTree_Tops");
+		// Palm Trees come in an Oasis variant. The Top Textures for it, if we had one that works:
+		public override Asset<Texture2D> GetOasisTopTextures() => null; //ModContent.Request<Texture2D>("ExampleMod/Content/Tiles/Plants/ExamplePalmTree_Tops");
 
 		// Regular Top Textures for the Ocean (we reused the same texture to save time, you don't have to reuse them): 
-		public override Asset<Texture2D> GetTopTextures() => ModContent.Request<Texture2D>("ExampleMod/Content/Tiles/Plants/ExamplePalmTree_Tops");
+		public override Asset<Texture2D> GetTopTextures() => null; //ModContent.Request<Texture2D>("ExampleMod/Content/Tiles/Plants/ExamplePalmTree_Tops");
 
 		//TODO: Is this the right item drop?
 		public override int DropWood() => ModContent.Find<ModItem>("ExampleMod/ExampleOre").Type; 
