b6bb670083f8b2ed21ef3688da2996ed1ff448b8
Mirsario <me@mirsar.io>
2022-05-20 19:41:41 +0300

    Reorganization shenanigans: Good, bad, and the ugly.

---
+++ patches/tModLoader/Terraria/Audio/SoundEngine.TML.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using ReLogic.Utilities;
+
+#nullable enable
+
+namespace Terraria.Audio
+{
+	partial class SoundEngine
+	{
+		public static SoundEffectInstance? PlaySound(ISoundStyle type, Vector2? position = null) {
+			if (type == null)
+				return null;
+
+			SlotId slot = PlayTrackedSound(type, position);
+
+			return slot.IsValid ? GetActiveSound(slot)?.Sound : null;
+		}
+
+		// Internal redirects
+
+		internal static SoundEffectInstance? PlaySound(LegacySoundStyle type, int x = -1, int y = -1)
+			=> PlaySound(type, XYToOptionalPosition(x, y));
+
+		internal static void PlaySound(int type, Vector2 position, int style = 1)
+			=> PlaySound(type, (int)position.X, (int)position.Y, style);
+
+		internal static SoundEffectInstance? PlaySound(int type, int x = -1, int y = -1, int Style = 1, float volumeScale = 1f, float pitchOffset = 0f) {
+			var soundStyle = new LegacySoundStyle(type, Style, SoundType.Sound) {
+				Volume = volumeScale,
+				Pitch = pitchOffset,
+			};
+
+			return PlaySound(soundStyle, XYToOptionalPosition(x, y));
+		}
+
+		// Utilities
+
+		private static Vector2? XYToOptionalPosition(int x, int y)
+			=> x != -1 || y != -1 ? new Vector2(x, y) : null;
+	}
+}
