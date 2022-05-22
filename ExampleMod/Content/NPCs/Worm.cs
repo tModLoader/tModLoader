97511bfaf4d6df95025c362b9a07a9059b74b5b4
Mirsario <me@mirsar.io>
2022-05-22 20:49:12 +0300

    EM fixes.

--- ExampleMod/Content/NPCs/Worm.cs
+++ ExampleMod/Content/NPCs/Worm.cs
@@ -366,7 +366,7 @@ namespace ExampleMod.NPCs
 							num195 = 20f;
 						}
 						NPC.soundDelay = (int)num195;
-						SoundEngine.PlaySound(SoundID.Roar, NPC.position, 1);
+						SoundEngine.PlaySound(SoundID.WormDig, NPC.position);
 					}
 					num193 = (float)System.Math.Sqrt((double)(num191 * num191 + num192 * num192));
 					float num196 = System.Math.Abs(num191);
