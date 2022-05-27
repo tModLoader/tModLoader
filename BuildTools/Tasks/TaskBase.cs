e5c6a9e84b737bfaae192ae30d379a4663921ff0
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-05-27 15:46:10 -0400

    Start work on build tasks

---
+++ BuildTools/Tasks/TaskBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+
+namespace tModLoader.BuildTools.Tasks;
+
+public abstract class TaskBase : Task
+{
+	public sealed override bool Execute()
+	{
+		try {
+			Run();
+		}
+		catch (Exception e) {
+			Log.LogErrorFromException(e, true);
+		}
+
+		return !Log.HasLoggedErrors;
+	}
+
+	protected abstract void Run();
+}
