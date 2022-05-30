0cfc569025f8bbb7283fc000bf36cc4798cdb503
direwolf420 <amakejk1@web.de>
2022-05-30 18:01:34 +0200

    font changes

---
+++ tModPorter/tModPorter.Tests/TestData/FontsTest.Expected.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+using ReLogic.Graphics;
+using Terraria.GameContent; //FontAssets
+
+public class FontsTest : Mod
+{
+	void Method() {
+		DynamicSpriteFont font = null;
+
+		font = FontAssets.DeathText.Value;
+		font = FontAssets.ItemStack.Value;
+		font = FontAssets.MouseText.Value;
+
+		int index = 0;
+		font = FontAssets.CombatText[index].Value;
+	}
+}
