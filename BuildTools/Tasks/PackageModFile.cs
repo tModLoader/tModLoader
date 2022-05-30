17000aaa0d9ec328e52a9530374aaac2731dfd97
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-05-30 15:12:51 -0400

    Found a way to get a mod's nuget references

--- BuildTools/Tasks/PackageModFile.cs
+++ BuildTools/Tasks/PackageModFile.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Build.Framework;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,10 @@ namespace tModLoader.BuildTools.Tasks;
 public class PackageModFile : TaskBase
 {
 	[Required]
-	public ITaskItem[] NugetReferences { get; set; }
+	public ITaskItem[] NugetReferences { get; set; } = Array.Empty<ITaskItem>();
+
+	[Required]
+	public ITaskItem[] ReferencePaths { get; set; } = Array.Empty<ITaskItem>();
 
 	[Required]
 	public string Test { get; set; } = string.Empty;
@@ -22,21 +26,25 @@ public class PackageModFile : TaskBase
 		Log.LogMessage(Test);
 		OutTest = Test;
 
+		Dictionary<string, ITaskItem> nugetLookup = NugetReferences.ToDictionary(x => x.ItemSpec);
+
 		foreach (ITaskItem nugetReference in NugetReferences) {
-			var identifier = nugetReference.GetMetadata("Identifier");
+			var identifier = nugetReference.GetMetadata("Identity");
 			var version = nugetReference.GetMetadata("Version");
 			var spec = nugetReference.ItemSpec;
-			Log.LogMessage($"{identifier} - v{version} - {spec}");
+			Log.LogMessage($"{spec} - v{version} - Metadatas: {string.Join(" | ", (IList<string>) nugetReference.MetadataNames)}");
+			Log.LogMessage($"\t{nugetReference.GetMetadata("FullPath")}"); // FullPath points to ModFolder/NugetPackageName
 		}
-		// Log.LogMessage(ProjectDepsFilePath);
-		// Log.LogMessage(LibrariesDir);
-		// Log.LogMessage(ProjectRuntimeConfigFilePath);
-		//
-		// // if (!AddProbingPaths()) {
-		// // 	Log.LogMessage(MessageImportance.High, $"'{Path.GetFileName(ProjectRuntimeConfigFilePath)}' is missing, skipping dependency reorganization.");
-		// // 	return;
-		// // }
-		//
-		// MoveFiles();
+
+		foreach (ITaskItem referencePath in ReferencePaths) {
+			var hintPath = referencePath.GetMetadata("HintPath");
+			var nugetPackageId = referencePath.GetMetadata("NuGetPackageId");
+			var nugetPackageVersion = referencePath.GetMetadata("NuGetPackageVersion");
+
+			if (string.IsNullOrEmpty(nugetPackageId)) continue;
+			if (!nugetLookup.ContainsKey(nugetPackageId)) continue;
+
+			Log.LogMessage($"{nugetPackageId} - v{nugetPackageVersion} - Found at: {hintPath}");
+		}
 	}
 }
