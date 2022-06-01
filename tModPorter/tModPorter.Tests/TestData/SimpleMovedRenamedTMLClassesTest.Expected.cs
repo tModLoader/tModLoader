a21d48e12dd2eb1100fd73ab1d578458eda6b6b7
direwolf420 <amakejk1@web.de>
2022-06-01 21:29:17 +0200

    SpawnCondition namespace move

--- tModPorter/tModPorter.Tests/TestData/SimpleMovedRenamedTMLClassesTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/SimpleMovedRenamedTMLClassesTest.Expected.cs
@@ -1,14 +1,15 @@
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
 
 // Misc moved classes that don't fit in any self-contained test
-// TODO NPCSpawnHelper (This mainly affects SpawnConditions): Terraria.ModLoader -> Terraria.ModLoader.Utilities
-// TODO RecipeGroupHelper: Terraria.ModLoader -> Terraria.ModLoader.Utilities
 // TODO PlayerDrawInfo -> PlayerDrawSet? Handled in ModPlayerTest
 public class SimpleMovedRenamedTMLClassesTest
 {
 	void Method() {
 		var player = new Player();
 		PlayerLoader.ResetEffects(player); // PlayerHooks -> PlayerLoader
+
+		SpawnCondition condition = SpawnCondition.TownWaterCritter; // SpawnCondition namespace: Terraria.ModLoader.Utilities
 	}
 }
--- tModPorter/tModPorter.Tests/TestData/SimpleMovedRenamedTMLClassesTest.cs
+++ tModPorter/tModPorter.Tests/TestData/SimpleMovedRenamedTMLClassesTest.cs
@@ -2,13 +2,13 @@ using Terraria;
 using Terraria.ModLoader;
 
 // Misc moved classes that don't fit in any self-contained test
-// TODO NPCSpawnHelper (This mainly affects SpawnConditions): Terraria.ModLoader -> Terraria.ModLoader.Utilities
-// TODO RecipeGroupHelper: Terraria.ModLoader -> Terraria.ModLoader.Utilities
 // TODO PlayerDrawInfo -> PlayerDrawSet? Handled in ModPlayerTest
 public class SimpleMovedRenamedTMLClassesTest
 {
 	void Method() {
 		var player = new Player();
 		PlayerHooks.ResetEffects(player); // PlayerHooks -> PlayerLoader
+
+		SpawnCondition condition = SpawnCondition.TownWaterCritter; // SpawnCondition namespace: Terraria.ModLoader.Utilities
 	}
 }
