4275538b20172ff7dca2d368752620324bf6e00e
direwolf420 <amakejk1@web.de>
2022-06-01 16:57:02 +0200

    attempt at testing method parameter changes

--- tModPorter/tModPorter.Tests/TestData/ModPlayerTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/ModPlayerTest.Expected.cs
@@ -3,6 +3,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
+using Microsoft.Xna.Framework;
 
 public class ModPlayerTest : ModPlayer
 {
@@ -23,4 +24,17 @@ public class ModPlayerTest : ModPlayer
 	}
 
 	public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright) { /* Empty */ }
+
+	public override void CatchFish(FishingAttempt attempt, ref int itemDrop, ref int npcSpawn, ref AdvancedPopupRequest sonar, ref Vector2 sonarPosition) {
+		// The following are 1.3 parameters written out using new 1.4 syntax
+		Item fishingRodLocal = attempt.playerFishingConditions.Pole;
+		Item baitLocal = attempt.playerFishingConditions.Bait;
+		int powerLocal = attempt.fishingLevel;
+		int liquidTypeLocal = attempt.inHoney ? 2 : attempt.inLava ? 1 : 0;
+		int poolSizeLocal = attempt.waterTilesCount;
+		int worldLayerLocal = attempt.heightLevel;
+		int questFishLocal = attempt.questFish;
+		ref int caughtTypeLocal = ref itemDrop;
+		// ref int junkLocal = ref junk; // Can't really be transformed, unless you check for fisher.rolledItemDrop = Main.rand.Next(2337, 2340);
+	}
 }
