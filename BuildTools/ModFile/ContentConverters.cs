5b17cadfe655b410996a44da1563726b41358bc0
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-06-01 15:29:21 -0400

    Fix issue with rawimg converter

--- BuildTools/ModFile/ContentConverters.cs
+++ BuildTools/ModFile/ContentConverters.cs
@@ -6,6 +6,8 @@ namespace tModLoader.BuildTools.ModFile;
 
 public static class ContentConverters
 {
+	private const int Version = 1;
+
 	public static bool Convert(ref string resourceName, FileStream src, MemoryStream dst) {
 		switch (Path.GetExtension(resourceName).ToLower()) {
 			case ".png":
@@ -23,6 +25,12 @@ public static class ContentConverters
 
 	public static void ToRaw(Stream src, Stream dst) {
 		using Image<Rgba32> image = Image.Load<Rgba32>(src);
+
+		using BinaryWriter writer = new(dst);
+		writer.Write(Version);
+		writer.Write(image.Width);
+		writer.Write(image.Height);
+
 		for (int y = 0; y < image.Height; y++) {
 			for (int x = 0; x < image.Width; x++) {
 				Rgba32 color = image[x, y];
