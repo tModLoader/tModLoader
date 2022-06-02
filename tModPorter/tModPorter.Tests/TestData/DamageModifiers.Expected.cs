62a75320f512de287a1135b2c316cf96d2756987
direwolf420 <amakejk1@web.de>
2022-06-02 18:54:31 +0200

    allDamage change

--- tModPorter/tModPorter.Tests/TestData/DamageModifiers.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/DamageModifiers.Expected.cs
@@ -6,6 +6,7 @@ public class DamageModifiers : ModPlayer
 	public override void UpdateEquips() {
 		Player.GetDamage(DamageClass.Magic) += 2;
 		Player.GetCritChance(DamageClass.Melee) *= 5;
+		Player.GetDamage(DamageClass.Generic) += 0.10f;
 #if COMPILE_ERROR
 		Player.GetDamage(DamageClass.Summon) = 8;
 #endif
--- tModPorter/tModPorter.Tests/TestData/DamageModifiers.cs
+++ tModPorter/tModPorter.Tests/TestData/DamageModifiers.cs
@@ -6,6 +6,7 @@ public class DamageModifiers : ModPlayer
 	public override void UpdateEquips() {
 		player.magicDamage += 2;
 		player.meleeCrit *= 5;
+		player.allDamage += 0.10f;
 #if COMPILE_ERROR
 		player.minionDamage = 8;
 #endif
