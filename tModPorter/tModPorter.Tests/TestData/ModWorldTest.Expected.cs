3525c08cc4d3257ec0d972d13c1e379d09a826b0
direwolf420 <amakejk1@web.de>
2022-06-02 20:30:57 +0200

    add more ModWorld changes

--- tModPorter/tModPorter.Tests/TestData/ModWorldTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/ModWorldTest.Expected.cs
@@ -4,4 +4,14 @@ using Terraria.ModLoader.IO;
 public class ModWorldTest : ModSystem
 {
 	public override void LoadWorldData(TagCompound tag) { /* Empty */ }
+
+#if COMPILE_ERROR
+	public override void SaveWorldData(TagCompound tag)/* Edit tag parameter rather than returning new TagCompound */ => new TagCompound();
+#endif
+
+	public override void OnWorldLoad() /* Also concider overriding OnWorldUnload */ { /* Empty */ }
+
+	public override void PreUpdateWorld() { /* Empty */ }
+
+	public override void PostUpdateWorld() { /* Empty */ }
 }
