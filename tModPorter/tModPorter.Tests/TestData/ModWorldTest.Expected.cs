ced83af37a3836e0a8cf167bc675e648fb6cfe91
direwolf420 <amakejk1@web.de>
2022-06-03 09:14:10 +0200

    remove modworld save, superceded

--- tModPorter/tModPorter.Tests/TestData/ModWorldTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/ModWorldTest.Expected.cs
@@ -5,10 +5,6 @@ public class ModWorldTest : ModSystem
 {
 	public override void LoadWorldData(TagCompound tag) { /* Empty */ }
 
-#if COMPILE_ERROR
-	public override void SaveWorldData(TagCompound tag)/* Edit tag parameter rather than returning new TagCompound */ => new TagCompound();
-#endif
-
 	public override void OnWorldLoad() /* Also concider overriding OnWorldUnload */ { /* Empty */ }
 
 	public override void PreUpdateWorld() { /* Empty */ }
--- tModPorter/tModPorter.Tests/TestData/ModWorldTest.cs
+++ tModPorter/tModPorter.Tests/TestData/ModWorldTest.cs
@@ -5,10 +5,6 @@ public class ModWorldTest : ModWorld
 {
 	public override void Load(TagCompound tag) { /* Empty */ }
 
-#if COMPILE_ERROR
-	public override TagCompound Save() => new TagCompound();
-#endif
-
 	public override void Initialize() { /* Empty */ }
 
 	public override void PreUpdate() { /* Empty */ }
