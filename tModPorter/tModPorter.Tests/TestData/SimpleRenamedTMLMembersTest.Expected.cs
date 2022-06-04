ebd969efa51d38b143978b1ad6efe265e3f6b609
direwolf420 <amakejk1@web.de>
2022-06-04 13:47:37 +0200

    move NPCSpawnInfo into own test

--- tModPorter/tModPorter.Tests/TestData/SimpleRenamedTMLMembersTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/SimpleRenamedTMLMembersTest.Expected.cs
@@ -9,25 +9,5 @@ public class SimpleRenamedTMLMembersTest
 		var mod = new Mod();
 		textureExists = mod.HasAsset("1");
 		textureExists = mod.HasAsset("1" + "2");
-
-		NPCSpawnInfo info = default;
-		var a = info.DesertCave;
-		var b = info.Granite;
-		var c = info.Invasion;
-		var d = info.Lihzahrd;
-		var e = info.Marble;
-		var f = info.PlanteraDefeated;
-		var g = info.Player;
-		var h = info.PlayerFloorX;
-		var i = info.PlayerFloorY;
-		var j = info.PlayerInTown;
-		var k = info.PlayerSafe;
-		var l = info.SafeRangeX;
-		var m = info.Sky;
-		var n = info.SpawnTileType;
-		var o = info.SpawnTileX;
-		var p = info.SpawnTileY;
-		var q = info.SpiderCave;
-		var r = info.Water;
 	}
 }
--- tModPorter/tModPorter.Tests/TestData/SimpleRenamedTMLMembersTest.cs
+++ tModPorter/tModPorter.Tests/TestData/SimpleRenamedTMLMembersTest.cs
@@ -9,25 +9,5 @@ public class SimpleRenamedTMLMembersTest
 		var mod = new Mod();
 		textureExists = mod.TextureExists("1");
 		textureExists = mod.TextureExists("1" + "2");
-
-		NPCSpawnInfo info = default;
-		var a = info.desertCave;
-		var b = info.granite;
-		var c = info.invasion;
-		var d = info.lihzahrd;
-		var e = info.marble;
-		var f = info.planteraDefeated;
-		var g = info.player;
-		var h = info.playerFloorX;
-		var i = info.playerFloorY;
-		var j = info.playerInTown;
-		var k = info.playerSafe;
-		var l = info.safeRangeX;
-		var m = info.sky;
-		var n = info.spawnTileType;
-		var o = info.spawnTileX;
-		var p = info.spawnTileY;
-		var q = info.spiderCave;
-		var r = info.water;
 	}
 }
