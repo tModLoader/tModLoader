2dbc3e357d07cd4d723fd9325ba42bf7eaa56ed5
JavidPack <javidpack@gmail.com>
2022-06-06 02:47:31 -0600

    SeenFirstLaunchModderWelcomeMessage, to tell people to use VS 2022

--- patches/tModLoader/Terraria/ModLoader/UI/Interface.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/Interface.cs
@@ -161,6 +161,17 @@ namespace Terraria.ModLoader.UI
 						infoMessage.Show(Language.GetTextValue("tModLoader.ShowNewUpdatedModsInfoMessage") + info, Main.menuMode);
 				}
 			}
+			if (Main.MenuUI.CurrentState == modSources) {
+				if (!ModLoader.SeenFirstLaunchModderWelcomeMessage) {
+					ModLoader.SeenFirstLaunchModderWelcomeMessage = true;
+					infoMessage.Show(Language.GetTextValue("tModLoader.MSFirstLaunchModderWelcomeMessage"), modSourcesID, null, Language.GetTextValue("tModLoader.ViewOnGitHub"),
+						() => {
+							SoundEngine.PlaySound(SoundID.MenuOpen);
+							Utils.OpenToURL($"https://github.com/tModLoader/tModLoader/wiki/Update-Migration-Guide");
+						});
+					Main.SaveSettings();
+				}
+			}
 			if (Main.menuMode == modsMenuID) {
 				Main.MenuUI.SetState(modsMenu);
 				Main.menuMode = 888;
