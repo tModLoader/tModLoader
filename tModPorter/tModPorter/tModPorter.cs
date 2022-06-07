00d08f93018ac24531cba4935fce6479f4e420ed
Chicken-Bones <mehvids@gmail.com>
2022-06-07 16:27:39 +1000

    Improve tModPorter speed 10x on large projects.

--- tModPorter/tModPorter/tModPorter.cs
+++ tModPorter/tModPorter/tModPorter.cs
@@ -15,9 +15,9 @@ namespace tModPorter;
 
 public class tModPorter
 {
-	private int documentCount;
-	private int pass = 1;
-	private int docsCompletedThisPass = 0;
+	private int documentsThisPass;
+	private int pass;
+	private int docsCompletedThisPass;
 
 	public bool DryRun { get; }
 	public bool? MakeBackups { get; private set; }
@@ -54,41 +54,62 @@ public class tModPorter
 		var project = await workspace.OpenProjectAsync(projectPath, new ProjectLoadProgressAdapter(updateProgress));
 		var updatedProject = await Process(project, updateProgress);
 		var changedDocs = updatedProject.GetChanges(project).GetChangedDocuments().Count();
-		updateProgress(new Complete(pass, changedDocs, documentCount, DateTime.Now - start));
+		updateProgress(new Complete(pass, changedDocs, project.Documents.Count(), DateTime.Now - start));
 	}
 
 	public async Task<Project> Process(Project project, Action<ProgressUpdate> updateProgress) {
-		documentCount = project.Documents.Count();
+		pass = 0;
+		bool finishingPass = false;
 
+		// The most expensive operation is updating the semantic model, so we run a system where all docs in a pass come from the same project
+		// As soon as a document syntax root gets changed, and its semantic model would need to be re-generated, we bail and re-queue it
+		// When no more docus change, we check the remaining docs to see if there are any cross-document dependencies.
+		var docs = project.Documents.ToArray();
 		while (true) {
+			pass++;
 			docsCompletedThisPass = 0;
-			updateProgress(new Progress(pass, docsCompletedThisPass, documentCount));
+			documentsThisPass = docs.Length;
+			updateProgress(new Progress(pass, docsCompletedThisPass, documentsThisPass));
 
-			var tasks = project.Documents.Select(doc => Task.Run(async () => (doc.Id, root: await Process(doc, updateProgress)))).ToArray();
+			var tasks = docs.Select(doc => Task.Run(async () => (doc.Id, root: await Process(doc, updateProgress)))).ToArray();
 			var changed = (await Task.WhenAll(tasks)).Where(e => e.root != null).ToArray();
-			if (!changed.Any())
-				break;
+			if (!changed.Any()) {
+				if (finishingPass) {
+					break;
+				}
+
+				finishingPass = true;
+				docs = project.Documents.Except(docs).ToArray(); // rerun on all the other documents
+				if (docs.Length == 0)
+					break;
+
+				continue;
+			}
 
 			var sln = project.Solution;
 			foreach (var (docId, root) in changed) {
 				sln = sln.WithDocumentSyntaxRoot(docId, root!, PreservationMode.PreserveIdentity);
 			}
 			project = sln.GetProject(project.Id)!;
-			pass++;
+
+			// rerun on just the documents which changed this pass
+			var changedIds = changed.Select(e => e.Id).ToHashSet();
+			docs = project.Documents.Where(doc => changedIds.Contains(doc.Id)).ToArray();
+			finishingPass = false;
 		}
 
 		return project;
 	}
 
 	private async Task<SyntaxNode?> Process(Document doc, Action<ProgressUpdate> updateProgress) {
-		var newDoc = await Rewrite(doc);
+		var newDoc = await RewriteOnce(doc);
 		if (newDoc != doc) {
 			await Update(newDoc, updateProgress);
 			updateProgress(new FileUpdated(doc.Name));
 		}
 
 		Interlocked.Increment(ref docsCompletedThisPass);
-		updateProgress(new Progress(pass, docsCompletedThisPass, documentCount));
+		updateProgress(new Progress(pass, docsCompletedThisPass, documentsThisPass));
 
 		return newDoc == doc ? null : await newDoc.GetSyntaxRootAsync();
 	}
@@ -99,6 +120,7 @@ public class tModPorter
 
 		var path = doc.FilePath ?? throw new NullReferenceException("No path? " + doc?.Name);
 
+		/*
 		Encoding encoding;
 		using (Stream fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
 			DetectionResult detectionResult = CharsetDetector.DetectFromStream(fs);
@@ -106,6 +128,7 @@ public class tModPorter
 			if (detectionResult.Detected.Confidence < .95f)
 				updateProgress(new Warning($"Less than 95% confidence about the file encoding of: {doc.FilePath}"));
 		}
+		*/
 
 		if (MakeBackups!.Value) {
 			int i = 2;
@@ -116,23 +139,15 @@ public class tModPorter
 			File.Move(path, backupPath);
 		}
 
-		await File.WriteAllTextAsync(path, (await doc.GetTextAsync()).ToString(), encoding);
+		await File.WriteAllTextAsync(path, (await doc.GetTextAsync()).ToString(), Encoding.UTF8);
 	}
 
-	public static async Task<Document> Rewrite(Document doc) {
-		Document prevDoc;
-		int i = 0;
-		do {
-			prevDoc = doc;
-			foreach (var rewriter in Config.CreateRewriters()) {
-				doc = await rewriter.Rewrite(doc);
-			}
-		} while (doc != prevDoc && ++i < 1000);
-
-#if DEBUG
-		if (i == 1000)
-			throw new Exception("Infinite recursion");
-#endif
+	public static async Task<Document> RewriteOnce(Document doc) {
+		var prevDoc = doc;
+		foreach (var rewriter in Config.CreateRewriters()) {
+			doc = await rewriter.Rewrite(doc);
+			if (doc != prevDoc) return doc;
+		}
 
 		return doc;
 	}
