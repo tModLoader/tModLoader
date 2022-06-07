59cb7b9a63edd48acab0e29d6bc331b76c92e74a
Kirtle <xkirtle26@gmail.com>
2022-06-07 12:03:07 +0100

    Added some use cases to example mod

--- ExampleMod/Content/ExampleBuilderToggle.cs
+++ ExampleMod/Content/ExampleBuilderToggle.cs
@@ -1,62 +1,57 @@
-﻿using Terraria.ModLoader;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace ExampleMod.Content
 {
 	public class ExampleBuilderToggle : BuilderToggle
 	{
-		public override string Texture => "ExampleMod/Content/ExampleInfoDisplay";
+		public override bool Active() => Main.LocalPlayer.HeldItem.IsAir;
+
 		public override int NumberOfStates => 4;
+		public override string DisplayValue() {
+			string text = "Color: ";
+			string[] textMessages = new[] {"Red", "Blue", "Green", "Yellow"};
 
-		public override void SetDefaults() {
-
+			return text + textMessages[CurrentState];
 		}
 
-		public override bool Active() => true;
+		public override Color DisplayColorTexture() {
+			Color[] colors = new[] {Color.Red, Color.Blue, Color.Green, Color.Yellow};
 
-		public override string DisplayValue() {
-			string text = "";
-
-			switch (CurrentState) {
-				case 0:
-					text = "Some random text";
-					break;
-				case 1:
-					text = "Yup, still no ideas";
-					break;
-				case 2:
-					text = "Hold on, how many states are there??";
-					break;
-				case 3:
-					text = "Ahah, we better stop now";
-					break;
-			}
-
-			return text;
+			return colors[CurrentState];
 		}
 	}
 
-	public class ExampleBuilderToggle2 : ExampleBuilderToggle { }
-	public class ExampleBuilderToggle3 : ExampleBuilderToggle { }
-	public class ExampleBuilderToggle4 : ExampleBuilderToggle { }
-	public class ExampleBuilderToggle5 : ExampleBuilderToggle { }
-	public class ExampleBuilderToggle6 : ExampleBuilderToggle { }
+	public class ExampleBuilderToggleDimmedLight : BuilderToggle
+	{
+		public override string Texture => "ExampleMod/Content/ExampleBuilderToggle";
+		public override bool Active() => true;
 
-	// public class GlobalToggle : GlobalBuilderToggle
-	// {
-	// 	public override void ModifyNumberOfStates(BuilderToggle builderToggle, ref int numberOfStates) {
-	// 		if (builderToggle == BuilderToggle.RulerGrid) {
-	// 			numberOfStates = 3;
-	// 		}
-	// 	}
-	//
-	// 	public override void ModifyDisplayValue(BuilderToggle builderToggle, ref string displayValue) {
-	// 		if (builderToggle == BuilderToggle.RulerGrid) {
-	// 			if (builderToggle.CurrentState == 2) {
-	// 				displayValue = "This is a third new state!";
-	// 			}
-	// 			else
-	// 				displayValue = $"Yeah okay {builderToggle.CurrentState}";
-	// 		}
-	// 	}
-	// }
+		public override int NumberOfStates => 2;
+		public override string DisplayValue() {
+			return CurrentState == 0 ? "Example On" : "Example Off";
+		}
+
+		public override Color DisplayColorTexture() {
+			return CurrentState == 0 ? Color.White : new Color(127, 127, 127);
+		}
+	}
+
+	public class GlobalToggle : GlobalBuilderToggle
+	{
+		public override void ModifyNumberOfStates(BuilderToggle builderToggle, ref int numberOfStates) {
+			if (builderToggle == BuilderToggle.RulerGrid) {
+				numberOfStates = 3;
+			}
+		}
+
+		public override void ModifyDisplayValue(BuilderToggle builderToggle, ref string displayValue) {
+			if (builderToggle == BuilderToggle.RulerGrid) {
+				if (builderToggle.CurrentState == 2) {
+					displayValue = "This text was modified by Example Mod!";
+				}
+			}
+		}
+	}
 }
