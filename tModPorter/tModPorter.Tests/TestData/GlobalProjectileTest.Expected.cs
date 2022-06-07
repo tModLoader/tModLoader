6671efd0eb8f43925a855ac6fb3dd3d62b926b41
direwolf420 <amakejk1@web.de>
2022-06-07 23:00:54 +0200

    move a bracket

--- tModPorter/tModPorter.Tests/TestData/GlobalProjectileTest.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/GlobalProjectileTest.Expected.cs
@@ -16,8 +16,7 @@ public class GlobalProjectileTest : GlobalProjectile
 
 	public override void PostDraw(Projectile projectile, Color lightColor) { /* Empty */ }
 
-	public override void DrawBehind(Projectile projectile, int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
-	{
+	public override void DrawBehind(Projectile projectile, int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI) {
 		behindNPCsAndTiles.Add(index);
 		behindNPCs.Add(index);
 		behindProjectiles.Add(index);
