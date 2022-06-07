0467c9859aa514764d6f9fbfd22cdf06292f6c34
direwolf420 <amakejk1@web.de>
2022-06-07 12:49:50 +0200

    add BackgroundTextureLoader methods with Mod param

--- ExampleMod/Content/Biomes/ExampleUndergroundBackgroundStyle.cs
+++ ExampleMod/Content/Biomes/ExampleUndergroundBackgroundStyle.cs
@@ -5,10 +5,10 @@ namespace ExampleMod.Backgrounds
 	public class ExampleUndergroundBackgroundStyle : ModUndergroundBackgroundStyle
 	{
 		public override void FillTextureArray(int[] textureSlots) {
-			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground0");
-			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground1");
-			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground2");
-			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("ExampleMod/Assets/Textures/Backgrounds/ExampleBiomeUnderground3");
+			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Textures/Backgrounds/ExampleBiomeUnderground0");
+			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Textures/Backgrounds/ExampleBiomeUnderground1");
+			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Textures/Backgrounds/ExampleBiomeUnderground2");
+			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Textures/Backgrounds/ExampleBiomeUnderground3");
 		}
 	}
 }
