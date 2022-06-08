5a2890868b638d82fff418d508428e69d422d336
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2022-06-08 06:58:12 +0500

    Update ExampleCrystal.cs

--- ExampleMod/Content/Tiles/ExampleCrystal.cs
+++ ExampleMod/Content/Tiles/ExampleCrystal.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using ExampleMod.Content.Dusts;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
@@ -11,9 +12,9 @@ namespace ExampleMod.Content.Tiles
         {
             TileObjectData.newTile.Width = 1;
             TileObjectData.newTile.Height = 1;
-            TileObjectData.newTile.CoordinateWidth = 16;
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16 };
-            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.CoordinateWidth = 16; // Width of tile on spritesheet
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16 }; // Sets an offset
+            TileObjectData.newTile.CoordinatePadding = 2; // Sets an "empty space" that should be between each 16x16 sprite in spritesheet
             TileObjectData.newTile.StyleHorizontal = true;
             TileObjectData.addTile(Type);
             Main.tileFrameImportant[Type] = true;
@@ -23,19 +24,17 @@ namespace ExampleMod.Content.Tiles
             Main.tileSpelunker[Type] = true;
         }
 
-        public override ushort GetMapOption(int i, int j) => (ushort)(Main.tile[i, j].TileFrameX / 18);
+        public override ushort GetMapOption(int i, int j) => (ushort)(Main.tile[i, j].TileFrameX / 18); // this override is redundant, but in case if you want more map names option ig, then you can leave it alone?
 
         public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
         {
             int toDrop = 0;
-            switch (Main.tile[i, j].TileFrameX / 18)
-            {
-                case 0:
-                    toDrop = Mod.Find<ModItem>("ExampleCrystalItem").Type;
-                    DustType = Mod.Find<ModDust>("Sparkle").Type;
-                    break;
-            }
-            if (toDrop > 0) Item.NewItem(Entity.GetSource_None(), i * 16, j * 16, 16, 16, toDrop);
+			if (!fail)
+			{
+				toDrop = ModContent.ItemType<ExampleCrystalItem>();
+				DustType = ModContent.DustType<Sparkle>();
+			}
+			if (toDrop > 0) Item.NewItem(Entity.GetSource_None(), i * 16, j * 16, 16, 16, toDrop);
         }
 
         public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
@@ -54,26 +53,26 @@ namespace ExampleMod.Content.Tiles
             if (Main.hardMode && type == TileID.Stone && j > Main.rockLayer && WorldGen.genRand.NextBool(110))
                 // this code line above means, that your crystal will grow only if: it's Hardmode, on Stone, in rock layer, and with same chance as vanilla Crystal Shards.
             {
-                int num = WorldGen.genRand.Next(4);
-                int num2 = 0;
-                int num3 = 0;
-                if (num == 0)
+                int coordinateChance = WorldGen.genRand.Next(4);
+                int additionalX = 0;
+                int additionalY = 0;
+                if (coordinateChance == 0)
                 {
-                    num2 = -1;
+                    additionalX = -1;
                 }
-                else if (num == 1)
+                else if (coordinateChance == 1)
                 {
-                    num2 = 1;
+                    additionalX = 1;
                 }
-                else if (num == 0)
+                else if (coordinateChance == 0)
                 {
-                    num3 = -1;
+                    additionalY = -1;
                 }
                 else
                 {
-                    num3 = 1;
+                    additionalY = 1;
                 }
-                if (!Main.tile[i + num2, j + num3].HasTile)
+                if (!Main.tile[i + additionalX, j + additionalY].HasTile)
                 {
                     int num4 = 0;
                     int num5 = 6;
@@ -91,12 +90,12 @@ namespace ExampleMod.Content.Tiles
                     if (num4 < 2)
                     {
                         int style = (short)WorldGen.genRand.Next(1); // chooses style(s) that you want
-                        if (WorldGen.PlaceTile(i + num2, j + num3, TileID.Crystals, true, style: style)) // Places crystal shards first, because hardcoded!
+                        if (WorldGen.PlaceTile(i + additionalX, j + additionalY, TileID.Crystals, true, style: style)) // Places crystal shards first, because hardcoded!
                         {
-                            Main.tile[i + num2, j + num3].TileType = Mod.Find<ModTile>("ExampleCrystal").Type; // ... if succesfully placed, then replace with our crystal
-                            Main.Map.Update(i + num2, j + num3, Main.Map[i + num2, j + num3].Light); // also don't forget to update it on map, so it won't appear as crystal shards
+                            Main.tile[i + additionalX, j + additionalY].TileType = ModContent.TileType<ExampleCrystal>(); // ... if succesfully placed, then replace with our crystal
+                            Main.Map.Update(i + additionalX, j + additionalY, Main.Map[i + additionalX, j + additionalY].Light); // also don't forget to update it on map, so it won't appear as crystal shards
                         }
-                        NetMessage.SendTileSquare(-1, i + num2, j + num3, TileChangeType.None);
+                        NetMessage.SendTileSquare(-1, i + additionalX, j + additionalY, TileChangeType.None);
                     }
                 }
             }
