164336357566e8ddffcb0ae4bf9e6f2b4ad2ebc0
ThomasThePencil <tailsguy24@gmail.com>
2022-06-08 07:03:38 -0400

    fix lack of Generic inheritance

--- patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
+++ patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
@@ -37,8 +37,12 @@ namespace Terraria.ModLoader
 	{
 		protected override string LangKey => "LegacyTooltip.2";
 
-		public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) =>
-			damageClass == Melee ? StatInheritanceData.Full with { attackSpeedInheritance = 0 } : StatInheritanceData.None;
+		public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) {
+			if (damageClass == Generic || damageClass == Melee)
+				return StatInheritanceData.Full with { attackSpeedInheritance = 0 };
+
+			return StatInheritanceData.None;
+		}
 
 		public override bool GetEffectInheritance(DamageClass damageClass) => damageClass == Melee;
 	}
