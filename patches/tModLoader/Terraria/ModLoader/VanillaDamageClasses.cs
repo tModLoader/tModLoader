d18c70e28bf2428c57ac4beccf3b94a22d4e10b5
ThomasThePencil <tailsguy24@gmail.com>
2022-06-08 06:52:39 -0400

    fixed all damage bonuses applyin' to certain melee weapons

--- patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
+++ patches/tModLoader/Terraria/ModLoader/VanillaDamageClasses.cs
@@ -37,13 +37,8 @@ namespace Terraria.ModLoader
 	{
 		protected override string LangKey => "LegacyTooltip.2";
 
-		public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) => new StatInheritanceData(
-			damageInheritance: 1f,
-			critChanceInheritance: 1f,
-			attackSpeedInheritance: 0f,
-			armorPenInheritance: 1f,
-			knockbackInheritance: 1f
-		);
+		public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) =>
+			damageClass == Melee ? StatInheritanceData.Full with { attackSpeedInheritance = 0 } : StatInheritanceData.None;
 
 		public override bool GetEffectInheritance(DamageClass damageClass) => damageClass == Melee;
 	}
