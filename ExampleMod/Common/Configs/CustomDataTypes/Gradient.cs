75e2a87175e4e4346bf3c4f7c6596504917781f7
NothyariS0 <105995054+NothyariS0@users.noreply.github.com>
2022-06-09 23:26:40 +0200

    [ExampleMod] Reorganize ModConfigShowcases (#2527)
    * [ExampleMod] Reorganize ModConfigShowcases
    
    * Split ModConfigShowcases file into separate files (one file per each config section)
    
    * Move custom data types from ModConfigShowcases to CustomDataTypes namespace (except Corner data type that is present in CornetElement class)
    
    * Move custom UI (ConfigElements) from ModConfigShowcases to CustomDataTypes namespace
    
    * Move ModConfigShowcases to separate folder

---
+++ ExampleMod/Common/Configs/CustomDataTypes/Gradient.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+// This file defines custom data type that represents Gradient data type that can be used in ModConfig classes.
+namespace ExampleMod.Common.Configs.CustomDataTypes
+{
+	public class Gradient
+	{
+		[Tooltip("The color the gradient starts at")]
+		[DefaultValue(typeof(Color), "0, 0, 255, 255")]
+		public Color start = Color.Blue; // For sub-objects, you'll want to make sure to set defaults in constructor or field initializer.
+		[Tooltip("The color the gradient ends at")]
+		[DefaultValue(typeof(Color), "255, 0, 0, 255")]
+		public Color end = Color.Red;
+
+		public override bool Equals(object obj) {
+			if (obj is Gradient other)
+				return start == other.start && end == other.end;
+			return base.Equals(obj);
+		}
+
+		public override int GetHashCode() {
+			return new { start, end }.GetHashCode();
+		}
+	}
+}
