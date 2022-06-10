f0084d28e642b76296f4384418ef4b28356ba5da
direwolf420 <amakejk1@web.de>
2022-06-10 13:42:20 +0200

    Mod.Properties change

---
+++ tModPorter/tModPorter.Tests/TestData/ModTest.Expected.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+public class ModTest : Mod
+{
+	public ModTest()
+	{
+#if COMPILE_ERROR
+		Properties/* tModPorter Note: Removed. Instead, assign the properties directly (ContentAutoloadingEnabled, GoreAutoloadingEnabled, MusicAutoloadingEnabled, and BackgroundAutoloadingEnabled) */ = new ModProperties()
+		{
+			Autoload = true,
+			AutoloadBackgrounds = true,
+			AutoloadGores = true,
+			AutoloadSounds = true
+		};
+#endif
+	}
+}
---
+++ tModPorter/tModPorter.Tests/TestData/ModTest.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+public class ModTest : Mod
+{
+	public ModTest()
+	{
+#if COMPILE_ERROR
+		Properties = new ModProperties()
+		{
+			Autoload = true,
+			AutoloadBackgrounds = true,
+			AutoloadGores = true,
+			AutoloadSounds = true
+		};
+#endif
+	}
+}
