2f32a4be3ebf1010f482e263dd51eef801b09daa
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-06-15 23:21:02 -0700

    Remove unnecessary usings

--- patches/tModLoader/Terraria/ModLoader/ModPylon.cs
+++ patches/tModLoader/Terraria/ModLoader/ModPylon.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
 using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.GameContent.Tile_Entities;
