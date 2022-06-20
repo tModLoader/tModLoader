b831de5f87384471169016e365c75a796304ae95
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-06-20 21:40:15 +0200

    Add port and test for `GrowthFXGore` -> `TreeLeaf`

--- tModPorter/tModPorter/Config.ModLoader.cs
+++ tModPorter/tModPorter/Config.ModLoader.cs
@@ -100,6 +100,8 @@ public static partial class Config
 		RenameMethod("Terraria.ModLoader.ModPlayer",	from: "SetupStartInventory",to: "AddStartingItems");
 		RenameMethod("Terraria.ModLoader.ModPrefix",	from: "AutoDefaults",		to: "AutoStaticDefaults");
 		RenameMethod("Terraria.ModLoader.ModType",		from: "Autoload",			to: "IsLoadingEnabled").FollowBy(AddCommentToOverride("Suggestion: If you return false for the purposes of manual loading, use the [Autoload(false)] attribute on your class instead"));
+		RenameMethod("Terraria.ModLoader.ModTree",		from: "GrowthFXGore",		to: "TreeLeaf");
+		RenameMethod("Terraria.ModLoader.ModPalmTree",	from: "GrowthFXGore",		to: "TreeLeaf");
 
 		ChangeHookSignature("Terraria.ModLoader.ModItem",			"HoldStyle");
 		ChangeHookSignature("Terraria.ModLoader.GlobalItem",		"HoldStyle");
