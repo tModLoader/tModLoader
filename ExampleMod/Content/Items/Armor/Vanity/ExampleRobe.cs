dbaeca27693aa9859c2d6bea010064221a859ab1
JayDevelopsShit <70743392+JayDevelopsShit@users.noreply.github.com>
2022-06-21 20:38:55 -0700

    Add ExampleRobe

---
+++ ExampleMod/Content/Items/Armor/Vanity/ExampleRobe.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ExampleMod.Content.Items.Armor.Vanity
+{
+	[AutoloadEquip(EquipType.Body)]
+	internal class ExampleRobe : ModItem
+	{
+		public override void Load() {
+			// The code below runs only if we're not loading on a server
+			if (Main.netMode == NetmodeID.Server)
+				return;
+
+			EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Legs}", EquipType.Legs, this);
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18;
+			Item.height = 14;
+			Item.rare = ItemRarityID.Blue;
+			Item.vanity = true;
+		}
+
+		public override void SetMatch(bool male, ref int equipSlot, ref bool robes) {
+			robes = true;
+			equipSlot = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Legs);
+		}
+	}
+}
