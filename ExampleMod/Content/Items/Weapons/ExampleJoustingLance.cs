38334b71ee8ffc38d2057044f8d7848b82eca7e6
Rijam <jacobrmoreno@hotmail.com>
2022-06-24 21:41:05 -0600

    Example Jousting Lance

---
+++ ExampleMod/Content/Items/Weapons/ExampleJoustingLance.cs
@@ -0,0 +1,94 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.Enums;
+using Terraria.Localization;
+using Terraria.Utilities;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	public class ExampleJoustingLance : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// The (English) text shown below your weapon's name. "ItemTooltip.HallowJoustingLance" will automatically be translated to "Build momentum to increase attack power".
+			Tooltip.SetDefault(Language.GetTextValue("ItemTooltip.HallowJoustingLance") + "\nThis is a modded jousting lance.");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1; // The number of sacrifices that is required to research the item in Journey Mode.
+		}
+
+		public override void SetDefaults() {
+
+			Item.DefaultToSpear(ModContent.ProjectileType<Projectiles.ExampleJoustingLanceProjectile>(), 3.5f, 24); // A special method that sets a variety of item parameters that make the item act like a spear weapon.
+
+			// The above Item.DefaultToSpear() does the following. Uncomment these if you don't want to use the above method or want to change something about it.
+			// Item.useStyle = ItemUseStyleID.Shoot;
+			// Item.useAnimation = 31;
+			// Item.useTime = 31;
+			// Item.shootSpeed = 3.5f;
+			// Item.width = 32;
+			// Item.height = 32;
+			// Item.UseSound = SoundID.Item1;
+			// Item.shoot = ModContent.ProjectileType<Projectiles.ExampleJoustingLance>();
+			// Item.noMelee = true;
+			// Item.noUseGraphic = true;
+			// Item.DamageType = DamageClass.Melee;
+			// Item.useAnimation = 24
+			// Item.useTime = 24;
+
+			Item.SetWeaponValues(56, 12f, 2); // A special method that sets the damage, knockback, and bonus critical strike chance.
+
+			// The above Item.SetWeaponValues() does the following. Uncomment these if you don't want to use the above method.
+			// Item.damage = 56;
+			// Item.knockBack = 12f;
+			// Item.crit = 2; // Even though this says 2, this is more like "bonus critical strike chance". All weapons have a base critical strike chance of 4.
+
+			Item.SetShopValues(ItemRarityColor.LightRed4, Item.buyPrice(0, 6)); // A special method that sets the rarity and value.
+
+			// The above Item.SetShopValues() does the following. Uncomment these if you don't want to use the above method.
+			// Item.rare = ItemRarityID.LightRed;
+			// Item.value = Item.buyPrice(0, 6); // The value of the item. In this case, 6 gold. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
+
+			Item.channel = true; // Channel is important for our projectile.
+		}
+
+		// These are all of the modifiers that Jousting Lances can receive in vanilla
+		private static readonly int[] wantedPrefixes = new int[] { PrefixID.Large, PrefixID.Massive, PrefixID.Dangerous, PrefixID.Savage,
+			PrefixID.Sharp, PrefixID.Pointy, PrefixID.Tiny, PrefixID.Terrible, PrefixID.Small, PrefixID.Dull, PrefixID.Unhappy, PrefixID.Bulky,
+			PrefixID.Shameful, PrefixID.Heavy, PrefixID.Light, PrefixID.Keen, PrefixID.Superior, PrefixID.Forceful, PrefixID.Hurtful, PrefixID.Strong,
+			PrefixID.Unpleasant, PrefixID.Broken, PrefixID.Damaged, PrefixID.Weak, PrefixID.Shoddy, PrefixID.Ruthless, PrefixID.Quick,
+			PrefixID.Deadly2, PrefixID.Agile, PrefixID.Nimble, PrefixID.Murderous, PrefixID.Slow, PrefixID.Sluggish, PrefixID.Lazy, PrefixID.Annoying,
+			PrefixID.Nasty, PrefixID.Godly, PrefixID.Demonic, PrefixID.Zealous, PrefixID.Legendary};
+
+		// This will allow our Jousting Lance to receive the modifiers we specified above
+		public override int ChoosePrefix(UnifiedRandom rand) {
+			return rand.Next(wantedPrefixes);
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>(5)
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+	}
+
+	// This will cause the Jousting Lance to become inactive if the player is hit with it out. Make sure to change the itemType to your item.
+	public class ExampleJoustingLancePlayer : ModPlayer
+	{
+		public override void Hurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit) {
+
+			int itemType = ModContent.ItemType<ExampleJoustingLance>(); // Change this to your item
+			double damageTaken = Main.CalculateDamagePlayersTake((int)damage, Player.statDefense);
+
+			if (!Player.immune && damageTaken >= 1.0 && Player.inventory[Player.selectedItem].type == itemType) {
+				for (int j = 0; j < 1000; j++) {
+					if (Main.projectile[j].active && Main.projectile[j].owner == Player.whoAmI) {
+						Main.projectile[j].active = false;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleJoustingLanceProjectile.cs
@@ -0,0 +1,190 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	public class ExampleJoustingLanceProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Jousting Lance"); // The English name of the projectile
+			ProjectileID.Sets.DismountsPlayersOnHit[Type] = true;
+		}
+
+		public override void SetDefaults() {
+
+			Projectile.netImportant = true; // Sync this projectile if a player joins mid game.
+
+			// The width and height do not affect the collision of the Jousting Lance because we calculate that separately (see Colliding() below)
+			Projectile.width = 25;
+			Projectile.height = 25;
+
+			// aiStyle 19 is the AI for Spears. Jousting Lances use the Spear AI. If you set the aiStyle to 19, make sure to set the AIType so it actually behaves like a Jousting Lance.
+			// Since we are using custom AI below, we set the aiStyle to -1.
+			Projectile.aiStyle = -1;
+
+			Projectile.alpha = 255; // The transparency of the projectile, 255 for completely transparent. Our projectile will fades in (see the AI() below).
+			Projectile.friendly = true; // Player shot projectile. Does damage to enemies but not to friendly Town NPCs.
+			Projectile.penetrate = -1; // Infinite penetration. The projectile can hit an infinite number of enemies.
+			Projectile.tileCollide = false; // Don't kill the projectile if it hits a tile.
+			Projectile.scale = 1f; // The scale of the projectile. This only effects the drawing.
+			Projectile.hide = true; // We are drawing the projectile ourself. See PreDraw() below.
+			Projectile.ownerHitCheck = true; // Make sure the owner of the projectile has line of sight to the target (aka can't hit things through tile).
+			Projectile.DamageType = DamageClass.Melee; // Set the damage to melee damage.
+
+			// AIType = ProjectileID.JoustingLance; // Act like the normal Jousting Lance. Use this if you set the aiStyle to 19.
+		}
+
+		// This is the behavior of the Jousting Lances.
+		public override void AI() {
+			Player owner = Main.player[Projectile.owner]; // Get the owner of the projectile.
+			Vector2 center = owner.RotatedRelativePoint(owner.MountedCenter); // Get the center of the owner.
+			Projectile.direction = owner.direction; // Set the direction of the projectile to the same direction as the owner.
+			owner.heldProj = Projectile.whoAmI; // Set the owner's held projectile to this projectile.
+			owner.itemTime = owner.itemAnimation; // Set the time left on this item to the animation time.
+			Projectile.Center = center; // Set the center of the projectile to the center of the owner.
+
+			int itemAnimationMax = owner.itemAnimationMax;
+			int itemAnimation = owner.itemAnimation;
+
+			// Changing this denominator will affect how far the projectile can move when it is shot (based on the scaleFactor).
+			// It is recommend to keep it between 2 and 12. 13 and above will kill the projectile. 1 will make it not move forward at all. Vanilla uses 3.
+			int itemAnimationMaxDiv = itemAnimationMax / 3;
+
+			float smallerItemAnimation = MathHelper.Min(itemAnimation, itemAnimationMaxDiv); // Choose the smaller of the two values.
+			float minimumItemAnimation = itemAnimation - smallerItemAnimation;
+			float spawnDistance = 28f; // This changes how far out the projectile is spawned. Vanilla uses 28f
+			float flyOut = 0.4f; // This changes how far out the projectile flies when it spawns. Vanilla uses 0.4f
+			float flyBack = 0.4f; // This changes how far back the projectile flies when it is killed. Vanilla uses 0.4f
+
+			// Fade the projectile in when it first spawns
+			Projectile.alpha -= 40;
+			if (Projectile.alpha < 0) {
+				Projectile.alpha = 0;
+			}
+
+			float pushOutMulti = itemAnimationMax - itemAnimationMaxDiv - minimumItemAnimation; // This value will get larger the further out the projectile is from the player until it reaches it's maximum distance.
+			float pullInMulti = itemAnimationMaxDiv - smallerItemAnimation; // This value will get larger when the projectile is being pulled back in.
+			float scaleFactor = spawnDistance + flyOut * pushOutMulti - flyBack * pullInMulti;
+			Projectile.position += Projectile.velocity * scaleFactor; // Move the projectile based on the velocity and scaleFactor
+
+			if (owner.channel && owner.itemAnimation < itemAnimationMaxDiv) {
+				owner.SetDummyItemTime(itemAnimationMaxDiv); // This makes it so the projectile never dies while we are holding it (except when we take damage, see ExampleJoustingLancePlayer).
+			}
+
+			// If the Jousting Lance is no longer being used, kill the projectile.
+			if (owner.ItemAnimationEndingOrEnded) {
+				Projectile.Kill();
+			}
+
+			// Set the rotation of the projectile.
+			// For reference, 0 is the top left, 180 degrees or pi radians is the bottom right.
+			Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + (float)Math.PI / 2f + (float)Math.PI / 4f;
+
+			// The Hallowed and Shadow Jousting Lance spawn dusts when the player is moving at a certain speed.
+			float minimumPlayerVelocity = 6f;
+			float minimumSpeedX = 0.8f;
+			float speedX = Vector2.Dot(Projectile.velocity.SafeNormalize(Vector2.UnitX * owner.direction), owner.velocity.SafeNormalize(Vector2.UnitX * owner.direction));
+			float playerVelocity = owner.velocity.Length();
+			if (playerVelocity > minimumPlayerVelocity && speedX > minimumSpeedX) {
+
+				// The chance for the dust to spawn. The actual chance (see below) is 1/dustChance. We make the chance higher the faster the player is moving by making the denominator smaller.
+				int dustChance = 8;
+				if (playerVelocity > minimumPlayerVelocity + 1f) {
+					dustChance = 5;
+				}
+				if (playerVelocity > minimumPlayerVelocity + 2f) {
+					dustChance = 2;
+				}
+
+				// Set your dust types here.
+				int dustTypeCommon = ModContent.DustType<Dusts.Sparkle>();
+				int dustTypeRare = DustID.WhiteTorch;
+
+				int offset = 4; // This offset will affect how much the dust spreads out.
+
+				// Spawn the dusts based on the dustChance. The dusts are spawned at the tip of the Jousting Lance.
+				if (Main.rand.NextBool(dustChance)) {
+					int newDust = Dust.NewDust(Projectile.Center - new Vector2(offset, offset), offset * 2, offset * 2, dustTypeCommon, Projectile.velocity.X * 0.2f + (Projectile.direction * 3), Projectile.velocity.Y * 0.2f, 100, default, 1.2f);
+					Main.dust[newDust].noGravity = true;
+					Main.dust[newDust].velocity *= 0.25f;
+					newDust = Dust.NewDust(Projectile.Center - new Vector2(offset, offset), offset * 2, offset * 2, dustTypeCommon, 0f, 0f, 150, default, 1.4f);
+					Main.dust[newDust].velocity *= 0.25f;
+				}
+
+				if (Main.rand.NextBool(dustChance + 3)) {
+					Dust.NewDust(Projectile.Center - new Vector2(offset, offset), offset * 2, offset * 2, dustTypeRare, 0f, 0f, 150, default, 1.4f);
+				}
+			}
+		}
+
+		// This will increase or decrease the knockback of the Jousting Lance depending on how fast the player is moving.
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
+			if (damage > 0) {
+				knockback *= Main.player[Projectile.owner].velocity.Length() / 7f;
+			}
+		}
+
+		// This will increase or decrease the damage of the Jousting Lance depending on how fast the player is moving.
+		public override void ModifyDamageScaling(ref float damageScale) {
+			damageScale *= 0.1f + Main.player[Projectile.owner].velocity.Length() / 7f * 0.9f;
+		}
+
+		// This Rectangle is the width and height of the Jousting Lance's hitbox. Modify these numbers if you have a bigger or smaller Jousting Lance.
+		private static Rectangle lanceHitboxBounds = new Rectangle(0, 0, 300, 300);
+
+		// This is the custom collision that Jousting Lances uses. 
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+			float rotationFactor = Projectile.rotation - (float)Math.PI / 4f - (float)Math.PI / 2f - (float)Math.PI;
+			float collisionPoint = 0f;
+			float scaleFactor = 95f;
+			lanceHitboxBounds.X = (int)Projectile.position.X - lanceHitboxBounds.Width / 2;
+			lanceHitboxBounds.Y = (int)Projectile.position.Y - lanceHitboxBounds.Height / 2;
+			if (lanceHitboxBounds.Intersects(targetHitbox) && Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center, Projectile.Center + rotationFactor.ToRotationVector2() * scaleFactor, 23f * Projectile.scale, ref collisionPoint)) {
+				return true;
+			}
+			return false;
+		}
+
+		// We need to draw the projectile manually. If you don't include this, the Jousting Lance will not be aligned with the player.
+		public override bool PreDraw(ref Color lightColor) {
+
+			SpriteEffects spriteEffects = SpriteEffects.None;
+
+			// Get texture of projectile
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+			// Get the currently selected frame on the texture.
+			Rectangle sourceRectangle = texture.Frame(1, Main.projFrames[Type], frameY: Projectile.frame);
+
+			Vector2 origin = Vector2.Zero;
+
+			float rotation = Projectile.rotation;
+
+			// If the projectile is facing right, we need to rotate it by -90 degrees, move the origin, and flip the sprite horizontally.
+			// This will make it so the bottom of the sprite is correctly facing down with shot to the right.
+			if (Projectile.direction > 0) {
+				rotation -= (float)Math.PI / 2f;
+				origin.X += sourceRectangle.Width;
+				spriteEffects = SpriteEffects.FlipHorizontally;
+			}
+
+			// The position of the sprite. Not subtracting Main.player[Projectile.owner].gfxOffY will cause the sprite to bounce when walking up blocks.
+			Vector2 position = new(Projectile.Center.X, Projectile.Center.Y - Main.player[Projectile.owner].gfxOffY);
+
+			// Applying lighting and draw our projectile
+			Color drawColor = Projectile.GetAlpha(lightColor);
+
+			Main.EntitySpriteDraw(texture,
+				position - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
+				sourceRectangle, drawColor, rotation, origin, Projectile.scale, spriteEffects, 0);
+
+			// It's important to return false, otherwise we also draw the original texture.
+			return false;
+		}
+	}
+}
