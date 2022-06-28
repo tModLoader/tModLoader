87fc981c4c1882f236176fa8092bf6999c9221fb
Rijam <jacobrmoreno@hotmail.com>
2022-06-27 21:19:36 -0600

    Example Yoyo port to 1.4

---
+++ ExampleMod/Content/Items/Weapons/ExampleYoyo.cs
@@ -0,0 +1,75 @@
+﻿using ExampleMod.Content.Projectiles;
+using System;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	public class ExampleYoyo : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Shoots out an example yoyo"); // The (English) text shown below your weapon's name. 
+
+			// These are all related to gamepad controls and don't seem to affect anything else.
+			ItemID.Sets.Yoyo[Item.type] = true;
+			ItemID.Sets.GamepadExtraRange[Item.type] = 15;
+			ItemID.Sets.GamepadSmartQuickReach[Item.type] = true;
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1; // The number of sacrifices that is required to research the item in Journey Mode.
+		}
+
+		public override void SetDefaults() {
+			Item.useStyle = ItemUseStyleID.Shoot; // How the item behaves when used. Shoot is a very common useStyle.
+			Item.width = 24; // The width of the item's hitbox.
+			Item.height = 24; // The height of the item's hitbox.
+			Item.useAnimation = 25; // All vanilla yoyos have a useAnimation of 25.
+			Item.useTime = 25; // All vanilla yoyos have a useTime of 25.
+			Item.shootSpeed = 16f; // All vanilla yoyos have a shootSpeed of 16f
+			Item.knockBack = 2.5f; // How much knockback the weapon does.
+			Item.damage = 9; // How much damage the weapon does.
+			Item.rare = ItemRarityID.White; // The rarity of the item. White rarity will burn in lava.
+
+			Item.DamageType = DamageClass.MeleeNoSpeed; // The type of damage the weapon does. MeleeNoSpeed means the item will not scale with attack speed.
+			Item.channel = true; // This means the attack key can be held down. 
+			Item.noMelee = true; // This makes it so the item doesn't do damage to enemies (the projectile does that).
+			Item.noUseGraphic = true; // Makes the item invisible while using it (the projectile is the visible part).
+
+			Item.UseSound = SoundID.Item1; // What sound the item makes when you use it.
+			Item.value = Item.sellPrice(silver: 1); // The value of the item. In this case, 1 silver.
+			Item.shoot = ModContent.ProjectileType<ExampleYoyoProjectile>(); // Which projectile this item will shoot. We set this to our corresponding projectile.
+		}
+
+		// Here is an example of blacklisting certain modifiers. Remove this section for standard vanilla behavior.
+		// In this example, we are blacklisting the ones that reduce damage of a melee weapon.
+		// Make sure that your item can even receive these prefixes (check the vanilla wiki on prefixes).
+		private static readonly int[] unwantedPrefixes = new int[] { PrefixID.Terrible, PrefixID.Dull, PrefixID.Shameful, PrefixID.Annoying, PrefixID.Broken, PrefixID.Damaged, PrefixID.Shoddy};
+
+		public override bool AllowPrefix(int pre) {
+			// return false to make the game reroll the prefix.
+
+			// DON'T DO THIS BY ITSELF:
+			// return false;
+			// This will get the game stuck because it will try to reroll every time. Instead, make it have a chance to return true.
+
+			if (Array.IndexOf(unwantedPrefixes, pre) > -1) {
+				// IndexOf returns a positive index of the element you search for. If not found, it's less than 0.
+				// Here we check if the selected prefix is positive (it was found).
+				// If so, we found a prefix that we don't want. Reroll.
+				return false;
+			}
+
+			// Don't reroll
+			return true;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient(ModContent.ItemType<ExampleItem>(), 10)
+				.AddIngredient(ItemID.WoodYoyo)
+				.Register();
+		}
+	}
+}
---
+++ ExampleMod/Content/Projectiles/ExampleYoyoProjectile.cs
@@ -0,0 +1,50 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+	public class ExampleYoyoProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			// The following sets are only applicable to yoyo that use aiStyle 99.
+			// YoyosLifeTimeMultiplier is how long in seconds the yoyo will stay out before automatically returning to the player. 
+			// Vanilla values range from 3f (Wood) to 16f (Chik), and defaults to -1f. Leaving as -1 will make the time infinite.
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 3.5f;
+			// YoyosMaximumRange is the maximum distance the yoyo sleep away from the player. 
+			// Vanilla values range from 130f (Wood) to 400f (Terrarian), and defaults to 200f.
+			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 300f;
+			// YoyosTopSpeed is top speed of the yoyo Projectile.
+			// Vanilla values range from 9f (Wood) to 17.5f (Terrarian), and defaults to 10f.
+			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 13f;
+		}
+
+		public override void SetDefaults() {
+			Projectile.extraUpdates = 0;
+			Projectile.width = 16; // The width of the projectile.
+			Projectile.height = 16; // The height of th projectile.
+			Projectile.aiStyle = 99; // aiStyle 99 is used for all yoyos, and is Extremely suggested, as yoyos are extremely difficult without them.
+			Projectile.friendly = true; // Player shot projectile. Does damage to enemies but not to friendly Town NPCs.
+			Projectile.penetrate = -1; // All vanilla yoyos have infinite penetration. The number of enemies the yoyo can hit before being pulled back in is based on YoyosLifeTimeMultiplier.
+			Projectile.DamageType = DamageClass.MeleeNoSpeed; // Benefits from melee bonuses. MeleeNoSpeed means the item will not scale with attack speed.
+			Projectile.scale = 1f; // The scale of the projectile. Most yoyos are 1f, but a few are larger. The Kraken is the largest at 1.2f
+		}
+
+		// notes for aiStyle 99: 
+		// localAI[0] is used for timing up to YoyosLifeTimeMultiplier
+		// localAI[1] can be used freely by specific types
+		// ai[0] and ai[1] usually point towards the x and y world coordinate hover point
+		// ai[0] is -1f once YoyosLifeTimeMultiplier is reached, when the player is stoned/frozen, when the yoyo is too far away, or the player is no longer clicking the shoot button.
+		// ai[0] being negative makes the yoyo move back towards the player
+		// Any AI method can be used for dust, spawning projectiles, etc specific to your yoyo.
+
+		// Let's add some dust for special effect.
+		public override void PostAI() {
+			if (Main.rand.NextBool()) {
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<Dusts.Sparkle>());
+				dust.noGravity = true;
+				dust.scale = 1.6f;
+			}
+		}
+	}
+}
