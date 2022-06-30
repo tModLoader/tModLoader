d1eede5dd02fe80dcfe37e5b0fc4d47f2b8680c0
Isaac Richards <andro951@gmail.com>
2022-06-30 12:28:38 -0700

    ExampleMod examples added

---
+++ ExampleMod/Common/GlobalItems/ExampleExperienceItem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader.IO;
+
+namespace ExampleMod.Common.GlobalItems
+{
+	public class ExampleExperienceItem : GlobalItem {
+		public int experience;
+		public int level;
+		public static int experiencePerLevel = 100;
+
+		public override bool InstancePerEntity => true;
+
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+			//Apply this GlobalItem to all swords
+			return entity.DamageType == DamageClass.Melee && entity.noMelee == false && entity.useStyle == ItemUseStyleID.Swing;
+		}
+
+		public override GlobalItem Clone(Item item, Item itemClone) {
+			//Overriding clone is required for GlobalItems that set InstancePerEntity to true.
+			//It is expremely important to include complex fields and properties such as arrays, lists and dictionaries in the Clone method.
+			//If they are not included, they will act like static fields or properties.
+			ExampleExperienceItem clone = (ExampleExperienceItem)base.Clone(item, itemClone);
+			clone.experience = experience;
+			return clone;
+		}
+
+		public override void LoadData(Item item, TagCompound tag) {
+			experience = tag.Get<int>("experience");//Load experience tag
+			item.value += experience * 5;
+			UpdateLevel();
+		}
+		public override void SaveData(Item item, TagCompound tag) {
+			tag["experience"] = experience;//Save experience tag
+		}
+
+		public override void OnHitNPC(Item item, Player player, NPC target, int damage, float knockBack, bool crit) {
+			//The sword gains experience when damaging an npc.
+			int xp = damage;
+			experience += xp;
+			item.value += xp * 5;
+			UpdateLevel();
+		}
+
+		public void UpdateLevel() {
+			level = experience / experiencePerLevel;
+		}
+
+		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+			if (experience > 0) {
+				UpdateLevel();
+				tooltips.Add(new TooltipLine(Mod, "level", $"Level: {level}") { OverrideColor = Color.LightGreen });
+				string levelString = $" ({(level + 1) * experiencePerLevel - experience} to next level)";
+				tooltips.Add(new TooltipLine(Mod, "experience", $"Experience: {experience}{levelString}") { OverrideColor = Color.White });
+			}
+		}
+
+		public static void SpawnCoins(Item item) {
+			int coinValue = item.value / 5;//The unmodified sell price is 1/5 of an item's value.
+			int valuePerCoin = 1000000;//Starting with value of 1 platinum coin.
+			for(int i = 3; i >= 0; i--) {
+				int coins = coinValue / valuePerCoin;
+				coinValue %= valuePerCoin;
+				valuePerCoin /= 100;
+				if(coins > 0)
+					Main.LocalPlayer.QuickSpawnItem(Main.LocalPlayer.GetSource_GiftOrReward(), ItemID.CopperCoin + i, coins);
+			}
+		}
+	}
+}
--- ExampleMod/ExampleMod.cs
+++ ExampleMod/ExampleMod.cs
@@ -1,4 +1,8 @@
+using ExampleMod.Common.GlobalItems;
+using System.Collections.Generic;
+using Terraria;
 using Terraria.GameContent.UI;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ExampleMod
@@ -9,6 +13,8 @@ namespace ExampleMod
 		public const string AssetPath = $"{nameof(ExampleMod)}/Assets/";
 
 		public static int ExampleCustomCurrencyId;
+
+		public static List<Item> consumedItems = new();
 
 		public override void Load() {
 			// Registers a new custom currency
@@ -23,6 +29,59 @@ namespace ExampleMod
 
 			// NOTE: When writing unload code - be sure use 'defensive programming'. Or, in other words, you should always assume that everything in the mod you're unloading might've not even been initialized yet.
 			// NOTE: There is rarely a need to null-out values of static fields, since TML aims to completely dispose mod assemblies in-between mod reloads.
+		}
+
+		public override void PostAddRecipes() {
+			for (int i = 0; i < Main.recipe.Length; i++) {
+				foreach(Item ingredient in Main.recipe[i].requiredItem) {
+					//Checks if any ingredient meets the conditions for ExampleExperienceItem by trying to get global item
+					//If at least one does, add the OnConsueItemCallback to the recipe.
+					//The "conditions" reffering to ExampleExperienceItem's AppliesToEntity() method.
+					if (ingredient.TryGetGlobalItem(out ExampleExperienceItem ingredientGlobal)) {
+						//OnConsumeItemCallback gives access to the item that is about to be consumed while crafting.
+						Recipe.OnConsumeItemCallback consumeCallback = (Recipe recipe, Item item, ref int amountRemaining) => {
+							//Because recipies can have multiple ingredients, some that dont meet the conditions of ExampleExperienceItem,
+							//We need to try getting the global item again inside the OnConsumeItemCallback.
+							if (item.TryGetGlobalItem(out ExampleExperienceItem exampleExperienceItem)) {
+								//The return value determines if the vanilla code for consuming this specific item will be ran.
+								//If trying to prevent an item being consumed, either set amountRemaining = 0 or return false;
+								//Note: retruning false will prevent the item being consumed, however the craft has already begun and will not stop.
+								//If the ammountRemaining is > 0, it will continue looking for the same item type to consume.
+								//It would generally be best to reduce the amountRemaining by the item.stack
+								//	or setting amountRemaining to 0 when prevening an item from being consumed.
+								if (exampleExperienceItem.experience < 1000) {
+									ExampleExperienceItem.SpawnCoins(item);
+									consumedItems.Add(item.Clone());
+									return true;
+								}
+								else {
+									exampleExperienceItem.experience -= 1000;
+									amountRemaining -= item.stack;
+									return false;
+								}
+							}
+							//If the ingredient, item, does not meet the conditions of ExampleExperienceItem,
+							//Return true to allow the vanilla consume item code to run.
+							return true;
+						};
+						Main.recipe[i].AddOnConsumeItemCallback(consumeCallback);
+
+						Recipe.OnCraftCallback craftCallback = (Recipe recipe, Item item) => {
+							if(item.TryGetGlobalItem(out ExampleExperienceItem itemGlobal)) {
+								foreach (Item consumedItem in consumedItems) {
+									itemGlobal.experience += consumedItem.GetGlobalItem<ExampleExperienceItem>().experience;
+									itemGlobal.UpdateLevel();
+								}
+							}
+							consumedItems.Clear();
+						};
+						Main.recipe[i].AddOnCraftCallback(craftCallback);
+						//We only want to add the callback to a recipe once, so if we find one ingredient that meets the conditions
+						//of ExampleExperienceItem, stop checking the ingredients from that recipe with break.
+						break;
+					}
+				}
+			}
 		}
 	}
 }
