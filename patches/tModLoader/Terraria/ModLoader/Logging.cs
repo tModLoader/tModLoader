675bb38b7f863d1004ce08b1453f11193d2c5ce9
JavidPack <javidpack@gmail.com>
2022-07-02 22:02:31 -0600

    Log OS version, catch more ControlledFolderAccess errors

--- patches/tModLoader/Terraria/ModLoader/Logging.cs
+++ patches/tModLoader/Terraria/ModLoader/Logging.cs
@@ -55,7 +55,7 @@ namespace Terraria.ModLoader
 		internal static void LogStartup(bool dedServ) {
 			tML.InfoFormat("Starting tModLoader {0} {1} built {2}", dedServ ? "server" : "client", BuildInfo.BuildIdentifier, $"{BuildInfo.BuildDate:g}");
 			tML.InfoFormat("Log date: {0}", DateTime.Now.ToString("d"));
-			tML.InfoFormat("Running on {0} {1} {2} {3}", ReLogic.OS.Platform.Current.Type, System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture, FrameworkVersion.Framework, FrameworkVersion.Version);
+			tML.InfoFormat("Running on {0} (v{1}) {2} {3} {4}", ReLogic.OS.Platform.Current.Type, Environment.OSVersion.Version, System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture, FrameworkVersion.Framework, FrameworkVersion.Version);
 			tML.InfoFormat("Executable: {0}", Assembly.GetEntryAssembly().Location);
 			tML.InfoFormat("Working Directory: {0}", Path.GetFullPath(Directory.GetCurrentDirectory()));
 
