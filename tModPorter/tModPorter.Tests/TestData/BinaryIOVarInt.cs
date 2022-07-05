7be76be18f263bc7d4041328e196a517ed179f86
Exterminator <kristijan.kos99@gmail.com>
2022-07-05 14:42:53 +0200

    Remove `Read/WriteVarInt` extensions (#2678)
    * Add tests
    
    * Remove `ReadVarInt` and `WriteVarInt` extensions
    
    Use `Read7BitEncodedInt` and `Write7BitEncodedInt` instead
    
    * Obsolete the extensions instead

---
+++ tModPorter/tModPorter.Tests/TestData/BinaryIOVarInt.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Terraria.ModLoader;
+
+namespace tModPorter.Tests.TestData
+{
+	public class BinaryIOVarInt : ModSystem
+	{
+		public static int Value1 = 1;
+		public static int Value2 => 2;
+		public static int Value3() => 3;
+
+		public override void NetSend(BinaryWriter writer) {
+			writer.WriteVarInt(Value1);
+			writer.WriteVarInt(Value2);
+			writer.WriteVarInt(Value3());
+			writer.WriteVarInt(4);
+		}
+
+		public override void NetReceive(BinaryReader reader) {
+			int value1 = reader.ReadVarInt();
+			int value2 = reader.ReadVarInt();
+			int value3 = reader.ReadVarInt();
+			int value4 = reader.ReadVarInt();
+		}
+	}
+}
