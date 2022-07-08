d1716258bcba0a657b0b55dd1a12a0f2e64f77c1
Rijam <jacobrmoreno@hotmail.com>
2022-07-07 23:38:34 -0600

    Scuffed Implementation

--- patches/tModLoader/Terraria/GameContent/UI/States/UIWorkshopHub.TML.cs
+++ patches/tModLoader/Terraria/GameContent/UI/States/UIWorkshopHub.TML.cs
@@ -1,4 +1,8 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria.Audio;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
 using Terraria.ModLoader.UI;
 using Terraria.Social.Steam;
 using Terraria.UI;
@@ -13,7 +17,7 @@ namespace Terraria.GameContent.UI.States
 		private UIElement _buttonModPack;
 
 		private UIElement MakeButton_OpenModsMenu() {
-			UIElement uIElement = MakeFancyButton("Images/UI/Workshop/HubResourcepacks", "tModLoader.MenuManageMods");
+			UIElement uIElement = MakeFancyButtonMod($"Terraria.GameContent.UI.States.HubManageMods", "tModLoader.MenuManageMods");
 			uIElement.OnClick += Click_OpenModsMenu;
 			_buttonMods = uIElement;
 			return uIElement;
@@ -26,7 +30,7 @@ namespace Terraria.GameContent.UI.States
 		}
 
 		private UIElement MakeButton_OpenModSourcesMenu() {
-			UIElement uIElement = MakeFancyButton("Images/UI/Workshop/HubPublishResourcepacks", "tModLoader.MenuDevelopMods");
+			UIElement uIElement = MakeFancyButtonMod($"Terraria.GameContent.UI.States.HubDevelopMods", "tModLoader.MenuDevelopMods");
 			uIElement.OnClick += Click_OpenModSourcesMenu;
 			_buttonModSources = uIElement;
 			return uIElement;
@@ -39,7 +43,7 @@ namespace Terraria.GameContent.UI.States
 		}
 
 		private UIElement MakeButton_OpenModBrowserMenu() {
-			UIElement uIElement = MakeFancyButton("Images/UI/Workshop/HubWorlds", "tModLoader.MenuDownloadMods");
+			UIElement uIElement = MakeFancyButtonMod($"Terraria.GameContent.UI.States.HubDownloadMods", "tModLoader.MenuDownloadMods");
 			uIElement.OnClick += Click_OpenModBrowserMenu;
 			_buttonModBrowser = uIElement;
 			return uIElement;
@@ -52,7 +56,7 @@ namespace Terraria.GameContent.UI.States
 		}
 
 		private UIElement MakeButton_ModPackMenu() {
-			UIElement uIElement = MakeFancyButton("Images/UI/Workshop/HubPublishWorlds","tModLoader.ModsModPacks");
+			UIElement uIElement = MakeFancyButtonMod($"Terraria.GameContent.UI.States.HubModPacks", "tModLoader.ModsModPacks");
 			uIElement.OnClick += Click_OpenModPackMenu;
 			_buttonModPack = uIElement;
 			return uIElement;
@@ -62,6 +66,47 @@ namespace Terraria.GameContent.UI.States
 			SoundEngine.PlaySound(10);
 			Interface.modPacksMenu.PreviousUIState = this;
 			Main.MenuUI.SetState(Interface.modPacksMenu);
+		}
+
+		private UIElement MakeFancyButtonMod(string iconImagePath, string textKey) {
+			UIPanel uIPanel = new UIPanel();
+			int num = -3;
+			int num2 = -3;
+			uIPanel.Width = StyleDimension.FromPixelsAndPercent(num, 0.5f);
+			uIPanel.Height = StyleDimension.FromPixelsAndPercent(num2, 0.33f);
+			uIPanel.OnMouseOver += SetColorsToHovered;
+			uIPanel.OnMouseOut += SetColorsToNotHovered;
+			uIPanel.BackgroundColor = new Color(63, 82, 151) * 0.7f;
+			uIPanel.BorderColor = new Color(89, 116, 213) * 0.7f;
+			uIPanel.SetPadding(6f);
+			UIImage uIImage = new UIImage(ModLoader.ModLoader.ManifestAssets.Request<Texture2D>(iconImagePath)) {
+				IgnoresMouseInteraction = true,
+				VAlign = 0.5f
+			};
+
+			uIImage.Left.Set(2f, 0f);
+			uIPanel.Append(uIImage);
+			uIPanel.OnMouseOver += ShowOptionDescription;
+			uIPanel.OnMouseOut += ClearOptionDescription;
+			UIText uIText = new UIText(Language.GetText(textKey), 0.45f, large: true) {
+				HAlign = 0f,
+				VAlign = 0.5f,
+				Width = StyleDimension.FromPixelsAndPercent(-80f, 1f),
+				Height = StyleDimension.FromPixelsAndPercent(0f, 1f),
+				Top = StyleDimension.FromPixelsAndPercent(5f, 0f),
+				Left = StyleDimension.FromPixels(80f),
+				IgnoresMouseInteraction = true,
+				TextOriginX = 0f,
+				TextOriginY = 0f
+			};
+
+			uIText.PaddingLeft = 0f;
+			uIText.PaddingRight = 20f;
+			uIText.PaddingTop = 10f;
+			uIText.IsWrapped = true;
+			uIPanel.Append(uIText);
+			uIPanel.SetSnapPoint("Button", 0);
+			return uIPanel;
 		}
 	}
 }
