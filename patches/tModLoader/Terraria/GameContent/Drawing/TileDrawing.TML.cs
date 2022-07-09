015f5394749c70d0a80cda28ebc4a7eff7dd8030
ThomasThePencil <tailsguy24@gmail.com>
2022-07-09 12:20:16 -0400

    better wind grid solution maybe

--- patches/tModLoader/Terraria/GameContent/Drawing/TileDrawing.TML.cs
+++ patches/tModLoader/Terraria/GameContent/Drawing/TileDrawing.TML.cs
@@ -2,6 +2,7 @@ namespace Terraria.GameContent.Drawing
 {
 	public partial class TileDrawing
 	{
+		public WindGrid Wind => _windGrid;
 		/// <summary>
 		/// Checks if a tile at the given coordinates counts towards tile coloring from the Dangersense buff.
 		/// <br/>Vanilla only uses Main.LocalPlayer for <paramref name="player"/>
