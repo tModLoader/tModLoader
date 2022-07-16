b79dee73b07ee91f845c32661bda5a496662a6b5
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2022-07-16 15:01:16 +0500

    meow

---
+++ patches/tModLoader/Terraria/GameContent/ItemDropRules/FewFromOptionsDropRule.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Terraria.GameContent.ItemDropRules
+{
+	public class FewFromOptionsDropRule : IItemDropRule
+	{
+		public int[] dropIds;
+		public int chanceDenominator;
+		public int chanceNumerator;
+		public int amount;
+
+		public List<IItemDropRuleChainAttempt> ChainedRules {
+			get;
+			private set;
+		}
+
+		public FewFromOptionsDropRule(int amount, int chanceDenominator, int chanceNumerator, params int[] options) {
+			this.amount = amount;
+			this.chanceDenominator = chanceDenominator;
+			this.chanceNumerator = chanceNumerator;
+			dropIds = options;
+			ChainedRules = new List<IItemDropRuleChainAttempt>();
+		}
+
+		public bool CanDrop(DropAttemptInfo info) => true;
+
+		public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info) {
+			ItemDropAttemptResult result;
+			int count = 0;
+			if (info.player.RollLuck(chanceDenominator) < chanceNumerator) {
+				List<int> savedDropIds = dropIds.ToList();
+				int index = info.rng.Next(savedDropIds.Count);
+				CommonCode.DropItemFromNPC(info.npc, savedDropIds[index], 1, false, info.player, info.item);
+				savedDropIds.RemoveAt(index);
+
+				while (++count < amount) {
+					int index2 = info.rng.Next(savedDropIds.Count);
+					CommonCode.DropItemFromNPC(info.npc, savedDropIds[index2], 1, false, info.player, info.item);
+					savedDropIds.RemoveAt(index2);
+				}
+
+				result = default(ItemDropAttemptResult);
+				result.State = ItemDropAttemptResultState.Success;
+				return result;
+			}
+
+			result = default(ItemDropAttemptResult);
+			result.State = ItemDropAttemptResultState.FailedRandomRoll;
+			return result;
+		}
+
+		public void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo) {
+			float num = (float)chanceNumerator / (float)chanceDenominator;
+			float num2 = num * ratesInfo.parentDroprateChance;
+			float dropRate = 1f / (float)dropIds.Length * num2;
+			for (int i = 0; i < dropIds.Length; i++) {
+				drops.Add(new DropRateInfo(dropIds[i], 1, 1, dropRate, ratesInfo.conditions));
+			}
+
+			Chains.ReportDroprates(ChainedRules, num, drops, ratesInfo);
+		}
+	}
+}
---
+++ patches/tModLoader/Terraria/GameContent/ItemDropRules/FewFromRulesRule.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Terraria.GameContent.ItemDropRules
+{
+	public class FewFromRulesRule : IItemDropRule, INestedItemDropRule
+	{
+		public int amount;
+		public IItemDropRule[] options;
+		public int chanceDenominator;
+
+		public List<IItemDropRuleChainAttempt> ChainedRules {
+			get;
+			private set;
+		}
+
+		public FewFromRulesRule(int amount, int chanceNumerator, params IItemDropRule[] options) {
+			this.amount = amount;
+			chanceDenominator = chanceNumerator;
+			this.options = options;
+			ChainedRules = new List<IItemDropRuleChainAttempt>();
+		}
+
+		public bool CanDrop(DropAttemptInfo info) => true;
+
+		public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info) {
+			ItemDropAttemptResult result = default(ItemDropAttemptResult);
+			result.State = ItemDropAttemptResultState.DidNotRunCode;
+			return result;
+		}
+
+		public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info, ItemDropRuleResolveAction resolveAction) {
+			int num = -1;
+			ItemDropAttemptResult result;
+			int count = 0;
+			if (info.rng.Next(chanceDenominator) == 0) {
+				List<IItemDropRule> savedDropIds = options.ToList();
+				num = info.rng.Next(savedDropIds.Count);
+				resolveAction(savedDropIds[num], info);
+				savedDropIds.RemoveAt(num);
+
+				while (++count < amount) {
+					num = info.rng.Next(savedDropIds.Count);
+					resolveAction(savedDropIds[num], info);
+					savedDropIds.RemoveAt(num);
+				}
+
+				result = default(ItemDropAttemptResult);
+				result.State = ItemDropAttemptResultState.Success;
+				return result;
+			}
+
+			result = default(ItemDropAttemptResult);
+			result.State = ItemDropAttemptResultState.FailedRandomRoll;
+			return result;
+		}
+
+		public void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo) {
+			float num = 1f / (float)chanceDenominator;
+			float num2 = num * ratesInfo.parentDroprateChance;
+			float multiplier = 1f / (float)options.Length * num2;
+			for (int i = 0; i < options.Length; i++) {
+				options[i].ReportDroprates(drops, ratesInfo.With(multiplier));
+			}
+
+			Chains.ReportDroprates(ChainedRules, num, drops, ratesInfo);
+		}
+	}
+}
