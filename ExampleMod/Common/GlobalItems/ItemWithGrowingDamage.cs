525fb5102c85e908e115ba62145a7cfe0d3b8e12
andro951 <andro951@gmail.com>
2022-07-18 18:07:08 -0400

    removed unneeded using statements

--- ExampleMod/Common/GlobalItems/ItemWithGrowingDamage.cs
+++ ExampleMod/Common/GlobalItems/ItemWithGrowingDamage.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Microsoft.Xna.Framework;
 using Terraria.ModLoader.IO;
-using Terraria.Utilities;
 using System.IO;
 
 namespace ExampleMod.Common.GlobalItems
