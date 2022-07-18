62c18ae6277b0697f66b5623c67676500b70a33f
Chicken-Bones <mehvids@gmail.com>
2022-07-19 05:32:37 +1000

    Move ItemLoader.OnCreate after recipe OnCraft hooks and combine hook call-point a bit

--- patches/tModLoader/Terraria/ModLoader/RecipeLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/RecipeLoader.cs
@@ -150,13 +150,22 @@ namespace Terraria.ModLoader
 		}
 
 		/// <summary>
-		/// Allows you to make anything happen when a player uses this recipe.
+		/// recipe.OnCraftHooks followed by Calls ItemLoader.OnCreate with a RecipeCreationContext
 		/// </summary>
 		/// <param name="item">The item crafted.</param>
 		/// <param name="recipe">The recipe used to craft the item.</param>
 		/// <param name="consumedItems">Materials used to craft the item.</param>
 		public static void OnCraft(Item item, Recipe recipe, List<Item> consumedItems) {
 			recipe.OnCraftHooks?.Invoke(recipe, item, consumedItems);
+			ItemLoader.OnCreate(item, new RecipeCreationContext { recipe = recipe, ConsumedItems = consumedItems });
+		}
+
+		/// <summary>
+		/// Helper version of OnCraft, used in combination with Recipe.Create and the internal ConsumedItems list
+		/// </summary>
+		public static void OnCraft(Item item, Recipe recipe) {
+			OnCraft(item, recipe, ConsumedItems);
+			ConsumedItems.Clear();
 		}
 
 		/// <summary>
