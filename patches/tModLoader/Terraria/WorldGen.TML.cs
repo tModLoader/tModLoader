faccdac9430fc68b7d7b370526a3c6a048c4c01f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-07-18 17:53:51 -0700

    WorldGen local variable accessing

---
+++ patches/tModLoader/Terraria/WorldGen.TML.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terraria.ModLoader;
+
+namespace Terraria
+{
+	public partial class WorldGen
+	{
+		public static WorldGenLocals GenLocalValues;
+		internal static event Action UpdateGenLocals;
+
+		internal static void InvokeGenLocalUpdate() {
+			UpdateGenLocals?.Invoke();
+		}
+	}
+}
