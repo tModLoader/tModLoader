411d647026355f08c59e39b43364e6892090a97d
ThomasThePencil <tailsguy24@gmail.com>
2022-07-20 17:13:27 -0400

    Vanilla Documentation & Publicize some methods (#2670)
    * WorldGen.Convert
    
    * Player.Heal
    
    * Player.GetItemGrabRange
    
    * Projectile.FindTargetWithLineOfSight
    
    * Player.spikedBoots
    
    * Player.meleeEnchant
    
    * Player.meleeEnchant afterthought
    
    * various Player dash fields
    
    * several buff-related arrays. why are they in Main, you ask?
    
    I have no fuckin' idea, but they're documented now
    
    * Main.npcCatchable (is cringe)
    
    * MouseScreen and MouseWOrld
    
    * NPC.realLife
    
    * downed bools my beloved (why)
    
    * [NPC] damage things + NAMES
    
    * TileDrawing sometimes publication
    
    * slight GetWindGridPush update + partial GetWindGridPushComplex docu
    
    * better wind grid solution maybe
    
    * Wind docu
    
    * LOTS more for Item
    
    * forgot Item.DD2Summon l0l
    
    * concise WorldGen.Convert to-dos

--- patches/tModLoader/Terraria/GameContent/Drawing/TileDrawing.TML.cs
+++ patches/tModLoader/Terraria/GameContent/Drawing/TileDrawing.TML.cs
@@ -3,6 +3,11 @@ namespace Terraria.GameContent.Drawing
 	public partial class TileDrawing
 	{
 		/// <summary>
+		/// The wind grid used to exert wind effects on tiles.
+		/// </summary>
+		public WindGrid Wind => _windGrid;
+
+		/// <summary>
 		/// Checks if a tile at the given coordinates counts towards tile coloring from the Dangersense buff.
 		/// <br/>Vanilla only uses Main.LocalPlayer for <paramref name="player"/>
 		/// </summary>
