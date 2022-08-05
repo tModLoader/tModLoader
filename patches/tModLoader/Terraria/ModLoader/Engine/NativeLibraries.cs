a0d2bfd00c198c26fdc8a263e9144ed8e715e82a
Chicken-Bones <mehvids@gmail.com>
2022-08-05 20:33:36 +1000

    Use kernel32 Add/SetDllDirectory to fix bad native library overrides on windows

---
+++ patches/tModLoader/Terraria/ModLoader/Engine/NativeLibraries.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Terraria.ModLoader.Engine
+{
+	internal class NativeLibraries
+	{
+		internal static void CheckForMSVCRuntimeWindows() {
+			if (!OperatingSystem.IsWindows())
+				return;
+
+			try { // FAudio on windows needs vcruntime140.dll
+				NativeLibrary.Load("vcruntime140.dll", Assembly.GetExecutingAssembly(), DllImportSearchPath.System32);
+			}
+			catch (DllNotFoundException e) {
+				e.HelpLink = "https://www.microsoft.com/en-us/download/details.aspx?id=53587";
+				ErrorReporting.FatalExit("Microsoft Visual C++ 2015 Redistributable Update 3 is missing. You will need to download and install it from the Microsoft website.", e);
+			}
+		}
+
+		internal static void SetNativeLibraryPath(string nativesDir) {
+			if (!OperatingSystem.IsWindows())
+				return;
+
+			// Setting PATH in the launch scripts is insufficient to guard from bad libs in System32 and similar on windows
+			// Add/SetDllDirectory has the highest priority so will pick our shipped natives first
+			try {
+				SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+				AddDllDirectory(nativesDir);
+			}
+			catch {
+				// Pre-Windows 7, KB2533623 
+				SetDllDirectory(nativesDir);
+			}
+		}
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool SetDefaultDllDirectories(int directoryFlags);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		static extern void AddDllDirectory(string lpPathName);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool SetDllDirectory(string lpPathName);
+
+		const int LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000;
+	}
+}
