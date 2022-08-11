d12f7509fdb10c244ba254bb3eb91fd1b82770f7
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-08-11 20:00:10 +0200

    You see nothing

--- patches/tModLoader/Terraria/GameContent/UI/Elements/UIList.TML.cs
+++ patches/tModLoader/Terraria/GameContent/UI/Elements/UIList.TML.cs
@@ -13,9 +13,10 @@ namespace Terraria.GameContent.UI.Elements
 		}
 
 		public virtual void AddRange(IEnumerable<UIElement> items) {
-			foreach (var item in items)
+			foreach (var item in items) {
 				_items.Add(item);
 				_innerList.Append(item);
+			}
 
 			UpdateOrder();
 			_innerList.Recalculate();
