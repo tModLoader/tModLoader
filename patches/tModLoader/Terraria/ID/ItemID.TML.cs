69d3852365e649ebdda90d245cee722471557eef
Chicken-Bones <mehvids@gmail.com>
2022-08-19 04:22:26 +1000

    Fix deerclops boss-bag missing from ItemID.Sets.BossBag and PreHardmodeLikeBossBag

--- patches/tModLoader/Terraria/ID/ItemID.TML.cs
+++ patches/tModLoader/Terraria/ID/ItemID.TML.cs
@@ -14,7 +14,7 @@
 				KingSlimeBossBag, EyeOfCthulhuBossBag, EaterOfWorldsBossBag, BrainOfCthulhuBossBag, QueenBeeBossBag,
 				SkeletronBossBag, WallOfFleshBossBag, DestroyerBossBag, TwinsBossBag, SkeletronPrimeBossBag,
 				PlanteraBossBag, GolemBossBag, FishronBossBag, CultistBossBag, MoonLordBossBag,
-				BossBagBetsy, FairyQueenBossBag, QueenSlimeBossBag
+				BossBagBetsy, FairyQueenBossBag, QueenSlimeBossBag, DeerclopsBossBag
 			);
 
 			/// <summary>
@@ -23,7 +23,7 @@
 			/// </summary>
 			public static bool[] PreHardmodeLikeBossBag = Factory.CreateBoolSet(
 				KingSlimeBossBag, EyeOfCthulhuBossBag, EaterOfWorldsBossBag, BrainOfCthulhuBossBag, QueenBeeBossBag,
-				SkeletronBossBag, WallOfFleshBossBag, QueenSlimeBossBag
+				SkeletronBossBag, WallOfFleshBossBag, QueenSlimeBossBag, DeerclopsBossBag
 			);
 
 			/// <summary>
