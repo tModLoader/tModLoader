82830ab2fc64a364a679777d093b15dbf279b209
Crapsky233 <59193481@qq.com>
2022-09-17 17:14:03 +0800

    ExItemEmote that shows when ExZombieThief steals a ExItem

---
+++ ExampleMod/Content/EmoteBubbles/ExampleItemEmote.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.GameContent.UI;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.EmoteBubbles
+{
+	// This emote is displayed whenever ExampleZombieThief steals an Example Item
+	public class ExampleItemEmote : ModEmoteBubble
+	{
+		public override void SetStaticDefaults() {
+			EmoteName.SetDefault("exitem");
+			AddToCategory(EmoteID.Category.Items);
+		}
+	}
+}
