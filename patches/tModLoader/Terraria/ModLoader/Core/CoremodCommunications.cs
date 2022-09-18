ee76be4730a46336556ff32aa3d8c81e1d60496c
Tomat <xxlennygamerxx@gmail.com>
2022-09-17 19:34:04 -0700

    Launch tModLoader through a child ALC.

---
+++ patches/tModLoader/Terraria/ModLoader/Core/CoremodCommunications.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Terraria.ModLoader.Core
+{
+	/// <summary>
+	///		Handles communicating between the parent <see cref="AssemblyLoadContext"/> and child, coremodded <see cref="AssemblyLoadContext"/>.
+	/// </summary>
+	internal static class CoremodCommunications
+	{
+		internal static bool Initialized;
+
+		/// <summary>
+		///		Retrieve the <see cref="AssemblyLoadContext"/> instance being used. This is the child instance when coremods are enabled, and the disabled instance when not.
+		/// </summary>
+		public static Func<AssemblyLoadContext> GetLoadContext;
+
+		/// <summary>
+		///		Determines whether the <see cref="CoremodLauncher"/> permits the loading of coremods.
+		/// </summary>
+		public static Func<bool> CoremodsEnabled;
+
+		/// <summary>
+		///		Initializes communications when coremods are allowed to be loaded by the <see cref="CoremodLauncher"/> (when -coremods-loaded is passed).
+		/// </summary>
+		internal static void InitializeEnabled() {
+			GetLoadContext = () => CoremodLauncher.ChildLoadContext;
+			CoremodsEnabled = () => true;
+
+			Initialized = true;
+		}
+
+		/// <summary>
+		///		Initializes communications when coremodes are not allowed to be loaded by the <see cref="CoremodLauncher"/> (when -skip-coremods is passed).
+		/// </summary>
+		internal static void InitializeDisabled() {
+			GetLoadContext = () => AssemblyLoadContext.Default;
+			CoremodsEnabled = () => false;
+
+			Initialized = true;
+		}
+
+		/// <summary>
+		///		Sets the fields of this class to the values of the fields from the parent <see cref="AssemblyLoadContext"/>.
+		/// </summary>
+		/// <param name="parentAlcType">This class, but from the parent <see cref="AssemblyLoadContext"/>.</param>
+		internal static void InitializeFromParent(Type parentAlcType) {
+			foreach (FieldInfo parentField in parentAlcType.GetFields(BindingFlags.Public | BindingFlags.Static)) {
+				FieldInfo childField = typeof(CoremodCommunications).GetField(parentField.Name, BindingFlags.Public | BindingFlags.Static);
+
+				// Set the child ALC's type's value to the parent ALC's type's value. This allows us to share instances.
+				childField?.SetValue(null, parentField.GetValue(null));
+			}
+
+			Initialized = true;
+		}
+	}
+}
---
+++ patches/tModLoader/Terraria/ModLoader/Core/CoremodLauncher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Terraria.ModLoader.Core
+{
+	/// <summary>
+	///		Handles creating an <see cref="AssemblyLoadContext"/> with modified (coremodded) assemblies in which the game should be played.
+	/// </summary>
+	internal static class CoremodLauncher
+	{
+		/// <summary>
+		///		An <see cref="AssemblyLoadContext"/> which modifies resolved assemblies before using them.
+		/// </summary>
+		internal class CoremodLoadContext : AssemblyLoadContext
+		{
+			protected override Assembly Load(AssemblyName assemblyName) {
+				return Default.LoadFromAssemblyName(assemblyName);
+			}
+
+			protected override IntPtr LoadUnmanagedDll(string unmanagedDllName) {
+				Console.WriteLine("Loading unmanaged DLL: " + unmanagedDllName);
+				return base.LoadUnmanagedDll(unmanagedDllName);
+			}
+		}
+
+		public const string SkipCoremods = "-skip-coremods";
+		public const string AppliedCoremods = "-applied-coremods";
+		private const string CoremodLockFileName = "coremod.lock";
+
+		internal static CoremodLoadContext ChildLoadContext;
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <param name="args">The program arguments that should be passed.</param>
+		internal static void LaunchWithCoremods(string[] args) {
+			args = args.Concat(new[] {AppliedCoremods}).ToArray();
+
+			ChildLoadContext = new CoremodLoadContext();
+
+			Assembly tmlAsm = ChildLoadContext.LoadFromAssemblyPath(typeof(CoremodLauncher).Assembly.Location);
+
+			// TODO: Do we need to bother with null checking here and throwing if it's null?
+
+			// Initialize inter-ALC communications.
+			Type coremodCommunications = tmlAsm.GetType(typeof(CoremodCommunications).FullName!);
+			MethodInfo initializeFromparent = coremodCommunications?.GetMethod(nameof(CoremodCommunications.InitializeFromParent), BindingFlags.NonPublic | BindingFlags.Static);
+			initializeFromparent?.Invoke(null, new object[] {typeof(CoremodCommunications)});
+
+			// Launch the game.
+			Type monoLaunch = tmlAsm.GetType(nameof(MonoLaunch));
+			MethodInfo main = monoLaunch?.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static);
+			main?.Invoke(null, new object[] {args});
+		}
+
+		private static bool CanCoremodsLoad() {
+			return !File.Exists(CoremodLockFileName);
+		}
+	}
+}
