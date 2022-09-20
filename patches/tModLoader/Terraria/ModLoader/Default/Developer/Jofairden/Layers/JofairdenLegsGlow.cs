e4dade8937663c81c6c675f6a8e2a23081977525
Exterminator <kristijan.kos99@gmail.com>
2022-09-21 00:13:02 +0200

    [1.4] Fully unload Jofairden armor assets (#2944)
    * Fully unload Jofairden armor assets
    
    Fixes #2902
    
    * Remove the `static` modifier from the fields
    
    The layers are re-created each reload, so new assets will be procured correctly

--- patches/tModLoader/Terraria/ModLoader/Default/Developer/Jofairden/Layers/JofairdenLegsGlow.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/Developer/Jofairden/Layers/JofairdenLegsGlow.cs
@@ -6,7 +6,7 @@ namespace Terraria.ModLoader.Default.Developer.Jofairden
 {
 	internal class JofairdenLegsGlow : JofairdenArmorGlowLayer
 	{
-		private static Asset<Texture2D> _glowTexture;
+		private Asset<Texture2D> _glowTexture;
 
 		public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
 			=> drawInfo.drawPlayer.legs == ModContent.GetInstance<Jofairden_Legs>().Item.legSlot && base.GetDefaultVisibility(drawInfo);
