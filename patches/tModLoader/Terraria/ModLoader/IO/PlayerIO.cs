bf6c4e2dbdff7a449e73272853214d77a8bced00
Chicken-Bones <mehvids@gmail.com>
2022-09-22 14:04:53 +1000

    Comment out vanilla code instead of removing

--- patches/tModLoader/Terraria/ModLoader/IO/PlayerIO.cs
+++ patches/tModLoader/Terraria/ModLoader/IO/PlayerIO.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader.Default;
