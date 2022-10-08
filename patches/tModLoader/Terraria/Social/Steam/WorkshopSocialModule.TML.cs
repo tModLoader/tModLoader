a98cf75c268c64e8d2a6a578f06277e3d370db2c
nmoll <59670736+Solxanich@users.noreply.github.com>
2022-10-07 21:33:20 -0600

    Fixes https://discord.com/channels/103110554649894912/1023345393716707488

--- patches/tModLoader/Terraria/Social/Steam/WorkshopSocialModule.TML.cs
+++ patches/tModLoader/Terraria/Social/Steam/WorkshopSocialModule.TML.cs
@@ -106,6 +106,13 @@ namespace Terraria.Social.Steam
 
 			if (MakeTemporaryFolder(contentFolderPath)) {
 				string modPath = Path.Combine(contentFolderPath, modFile.Name + ".tmod");
+
+				// Solxan: File.Copy sometimes fails to delete the file that it needs to replace.
+				// This mitigates that issue by forcing the deletion before copying.
+				//TODO: But why though? Needs deeper look later.
+				if (File.Exists(modPath))
+					File.Delete(modPath);
+
 				File.Copy(modFile.path, modPath, true);
 
 				// Cleanup Old Folders
@@ -133,7 +140,8 @@ namespace Terraria.Social.Steam
 					if (mod.properties.version >= new Version(buildData["version"]))
 						return false;
 
-				buildData["versionsummary"] += $";{mod.tModLoaderVersion.MajorMinor()}:{mod.properties.version}";
+				if (mod.tModLoaderVersion.MajorMinor() != BuildInfo.tMLVersion.MajorMinor())
+					buildData["versionsummary"] += $";{mod.tModLoaderVersion.MajorMinor()}:{mod.properties.version}";
 			}
 
 			return true;
