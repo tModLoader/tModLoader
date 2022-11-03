d1a7dc22143b752461f2c5775fd1100e6667bad6
JavidPack <javidpack@gmail.com>
2022-11-03 17:29:33 -0600

    Merged in various #2642 comments, AllowPrefix example, remove from Old folder
    Co-Authored-By: Rijam <11262234+Rijam@users.noreply.github.com>

--- ExampleMod/Content/Items/Weapons/ExampleYoyo.cs
+++ ExampleMod/Content/Items/Weapons/ExampleYoyo.cs
@@ -5,13 +5,14 @@ using Terraria;
 using Terraria.ID;
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
+using System;
 
 namespace ExampleMod.Content.Items.Weapons
 {
 	public class ExampleYoyo : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("This is a modded yoyo.");
+			Tooltip.SetDefault("This is a modded yoyo with normal behavior. It won't receive bad prefixes.");
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1; // The amount of this item that needs to be researched to unlock it in the journey mode duplication menu.
 
 			// These are all related to gamepad controls and don't seem to affect anything else
@@ -25,22 +26,45 @@ namespace ExampleMod.Content.Items.Weapons
 			Item.height = 24; // The height of the item's hitbox.
 
 			Item.useStyle = ItemUseStyleID.Shoot; // The way the item is used (e.g. swinging, throwing, etc.)
-			Item.useTime = 25; // The item's use time in ticks (1 second == 60 ticks)
-			Item.useAnimation = 25; // The item's use animation length in ticks (1 second == 60 ticks)
-			Item.noMelee = true; // The item's animation won't deal damage if this is set to true.
-			Item.noUseGraphic = true; // The item's animation will be invisible if this is set to true.
+			Item.useTime = 25; // All vanilla yoyos have a useTime of 25.
+			Item.useAnimation = 25; // All vanilla yoyos have a useAnimation of 25.
+			Item.noMelee = true; // This makes it so the item doesn't do damage to enemies (the projectile does that).
+			Item.noUseGraphic = true; // Makes the item invisible while using it (the projectile is the visible part).
 			Item.UseSound = SoundID.Item1; // The sound that will play when the item is used.
 
 			Item.damage = 40; // The amount of damage the item does to an enemy or player.
-			Item.DamageType = DamageClass.Melee; // Sets the item's damage type to melee.
+			Item.DamageType = DamageClass.MeleeNoSpeed; // The type of damage the weapon does. MeleeNoSpeed means the item will not scale with attack speed.
 			Item.knockBack = 2.5f; // The amount of knockback the item inflicts.
 			Item.crit = 8; // The percent chance for the weapon to deal a critical strike. Defaults to 4.
 			Item.channel = true; // Set to true for items that require the attack button to be held out (e.g. yoyos and magic missile weapons)
 			Item.rare = ModContent.RarityType<ExampleModRarity>(); // The item's rarity. This changes the color of the item's name.
 			Item.value = Item.buyPrice(gold: 1); // The amount of money that the item is can be bought for.
 
-			Item.shoot = ModContent.ProjectileType<ExampleYoyoProjectile>(); // The projectile that the item shoots.
+			Item.shoot = ModContent.ProjectileType<ExampleYoyoProjectile>(); // Which projectile this item will shoot. We set this to our corresponding projectile.
 			Item.shootSpeed = 16f; // The velocity of the shot projectile.			
+		}
+
+		// Here is an example of blacklisting certain modifiers. Remove this section for standard vanilla behavior.
+		// In this example, we are blacklisting the ones that reduce damage of a melee weapon.
+		// Make sure that your item can even receive these prefixes (check the vanilla wiki on prefixes).
+		private static readonly int[] unwantedPrefixes = new int[] { PrefixID.Terrible, PrefixID.Dull, PrefixID.Shameful, PrefixID.Annoying, PrefixID.Broken, PrefixID.Damaged, PrefixID.Shoddy };
+
+		public override bool AllowPrefix(int pre) {
+			// return false to make the game reroll the prefix.
+
+			// DON'T DO THIS BY ITSELF:
+			// return false;
+			// This will get the game stuck because it will try to reroll every time. Instead, make it have a chance to return true.
+
+			if (Array.IndexOf(unwantedPrefixes, pre) > -1) {
+				// IndexOf returns a positive index of the element you search for. If not found, it's less than 0.
+				// Here we check if the selected prefix is positive (it was found).
+				// If so, we found a prefix that we don't want. Reroll.
+				return false;
+			}
+
+			// Don't reroll
+			return true;
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
--- ExampleMod/Content/Projectiles/ExampleYoyoProjectile.cs
+++ ExampleMod/Content/Projectiles/ExampleYoyoProjectile.cs
@@ -8,26 +8,30 @@ namespace ExampleMod.Content.Projectiles
 	public class ExampleYoyoProjectile : ModProjectile
 	{
 		public override void SetStaticDefaults() {
-			// The following sets are only applicable to yoyos that use aiStyle 99.
-			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 18f; // The amount of time that the yoyo can be held out, in seconds.
-		    // Vanilla values range from 3f(Wood) to 16f(Chik), and defaults to -1f. Leaving it as -1 will make the time infinite.
-																			  
-			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 300f; // The maximum distance the yoyo sleep away from the player.
-			// Vanilla values range from 130f(Wood) to 400f(Terrarian), and defaults to 200f.
-																		
-			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 17f; // The top speed of the yoyo.
-			// Vanilla values range from 9f(Wood) to 17.5f(Terrarian), and defaults to 10f.
+			// The following sets are only applicable to yoyo that use aiStyle 99.
+
+			// YoyosLifeTimeMultiplier is how long in seconds the yoyo will stay out before automatically returning to the player. 
+			// Vanilla values range from 3f (Wood) to 16f (Chik), and defaults to -1f. Leaving as -1 will make the time infinite.
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 3.5f;
+
+			// YoyosMaximumRange is the maximum distance the yoyo sleep away from the player. 
+			// Vanilla values range from 130f (Wood) to 400f (Terrarian), and defaults to 200f.
+			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 300f;
+
+			// YoyosTopSpeed is top speed of the yoyo Projectile.
+			// Vanilla values range from 9f (Wood) to 17.5f (Terrarian), and defaults to 10f.
+			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 13f;
 		}
 
 		public override void SetDefaults() {
 			Projectile.width = 16; // The width of the projectile's hitbox.
 			Projectile.height = 16; // The height of the projectile's hitbox.
 
-			Projectile.aiStyle = 99; // The projectile's ai style. Yoyos use aiStyle 99.
+			Projectile.aiStyle = ProjAIStyleID.Yoyo; // The projectile's ai style. Yoyos use aiStyle 99 (ProjAIStyleID.Yoyo). A lot of yoyo code checks for this aiStyle to work properly.
 
-			Projectile.friendly = true; // Makes the projectile deal damage to enemies.
-			Projectile.DamageType = DamageClass.Melee; // Sets the item's damage type to melee.
-			Projectile.penetrate = -1; // The amount of enemies the projectile can hit before disappearing. Setting it to -1 makes it infinite.
+			Projectile.friendly = true; // Player shot projectile. Does damage to enemies but not to friendly Town NPCs.
+			Projectile.DamageType = DamageClass.MeleeNoSpeed; // Benefits from melee bonuses. MeleeNoSpeed means the item will not scale with attack speed.
+			Projectile.penetrate = -1; // All vanilla yoyos have infinite penetration. The number of enemies the yoyo can hit before being pulled back in is based on YoyosLifeTimeMultiplier.
 		}
 
 		// notes for aiStyle 99: 
