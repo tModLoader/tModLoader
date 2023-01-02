df6f22328cab0a43f88effdfdceaf2f91ae70782
direwolf420 <amakejk1@web.de>
2023-01-02 11:54:47 +0100

    doc fix

--- patches/tModLoader/Terraria/ID/TileID.TML.cs
+++ patches/tModLoader/Terraria/ID/TileID.TML.cs
@@ -51,7 +51,7 @@ partial class TileID
 		/// <summary> Whether or not saplings count this tile as empty when trying to grow. </summary>
 		public static bool[] IgnoredByGrowingSaplings = Factory.CreateBoolSet(3, 24, 32, 61, 62, 69, 71, 73, 74, 82, 83, 84, 110, 113, 201, 233, 352, 485, 529, 530, 637, 655);
 
-		/// <summary> Whether or not this solid tile prevents a meteor from landing near it.</summary>
+		/// <summary> Whether or not this tile prevents a meteor from landing near it.</summary>
 		/// <remarks> Note: Chests and Dungeon tiles are not in this set, but also prevent landing (handled through <see cref="BasicChest"/> and <see cref="Main.tileDungeon"/>)</remarks>
 		public static bool[] AvoidedByMeteorGeneration = Factory.CreateBoolSet(226, 470, 475, 448, 597);
 
