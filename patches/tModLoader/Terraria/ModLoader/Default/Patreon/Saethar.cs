91a0277f3d28119feb71dc79acccaf5faca6b83f
Mirsario <me@mirsar.io>
2023-02-01 17:04:56 +0300

    Modernized Saethar.cs

--- patches/tModLoader/Terraria/ModLoader/Default/Patreon/Saethar.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/Patreon/Saethar.cs
@@ -2,50 +2,60 @@ using Microsoft.Xna.Framework;
 using Terraria.DataStructures;
 using Terraria.ID;
 
-namespace Terraria.ModLoader.Default.Patreon
+#nullable enable
+
+namespace Terraria.ModLoader.Default.Patreon;
+
+[AutoloadEquip(EquipType.Head)]
+internal class Saethar_Head : PatreonItem
 {
-	[AutoloadEquip(EquipType.Head)]
-	internal class Saethar_Head : PatreonItem
+	public override void SetDefaults()
 	{
-		public override void SetDefaults() {
-			base.SetDefaults();
-			Item.Size = new Vector2(34);
-		}
+		base.SetDefaults();
+
+		Item.Size = new Vector2(34);
+	}
+}
+
+[AutoloadEquip(EquipType.Body)]
+internal class Saethar_Body : PatreonItem
+{
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.Size = new Vector2(30, 18);
+	}
+}
+
+[AutoloadEquip(EquipType.Legs)]
+internal class Saethar_Legs : PatreonItem
+{
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.Size = new Vector2(22, 18);
+	}
+}
+
+[AutoloadEquip(EquipType.Wings)]
+internal class Saethar_Wings : PatreonItem
+{
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(150, 7f);
 	}
 
-	[AutoloadEquip(EquipType.Body)]
-	internal class Saethar_Body : PatreonItem
+	public override void SetDefaults()
 	{
-		public override void SetDefaults() {
-			base.SetDefaults();
-			Item.Size = new Vector2(30, 18);
-		}
-	}
+		base.SetDefaults();
 
-	[AutoloadEquip(EquipType.Legs)]
-	internal class Saethar_Legs : PatreonItem
-	{
-		public override void SetDefaults() {
-			base.SetDefaults();
-			Item.Size = new Vector2(22, 18);
-		}
-	}
-
-	[AutoloadEquip(EquipType.Wings)]
-	internal class Saethar_Wings : PatreonItem
-	{
-		public override void SetStaticDefaults() {
-			base.SetStaticDefaults();
-
-			ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(150, 7f);
-		}
-
-		public override void SetDefaults() {
-			base.SetDefaults();
-			Item.vanity = false;
-			Item.width = 24;
-			Item.height = 8;
-			Item.accessory = true;
-		}
+		Item.vanity = false;
+		Item.width = 24;
+		Item.height = 8;
+		Item.accessory = true;
 	}
 }
