88d08efc3984e8ca214060edd4e651c805a776e7
Mirsario <me@mirsar.io>
2023-02-01 17:29:48 +0300

    Eye glow rendering.

--- patches/tModLoader/Terraria/ModLoader/Default/Patreon/Saethar.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/Patreon/Saethar.cs
@@ -1,4 +1,6 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria.DataStructures;
 using Terraria.ID;
 
@@ -6,9 +8,77 @@ using Terraria.ID;
 
 namespace Terraria.ModLoader.Default.Patreon;
 
+internal class SaetharSetEffectPlayer : ModPlayer
+{
+	public bool IsActive;
+
+	public override void ResetEffects()
+	{
+		IsActive = false;
+	}
+}
+
+internal class SaetharEyeGlowDrawLayer : PlayerDrawLayer
+{
+	private Asset<Texture2D>? textureAsset;
+
+	public override Position GetDefaultPosition()
+		=> new AfterParent(PlayerDrawLayers.Head);
+
+	public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
+		=> drawInfo.drawPlayer.TryGetModPlayer(out SaetharSetEffectPlayer modPlayer) && modPlayer.IsActive;
+
+	protected override void Draw(ref PlayerDrawSet drawInfo)
+	{
+		// Don't render as after-image
+		if (drawInfo.shadow != 0f) {
+			return;
+		}
+
+		textureAsset ??= ModContent.Request<Texture2D>("ModLoader/Patreon.Saethar_Head_Glow");
+
+		if (textureAsset is not { IsLoaded: true, Value: Texture2D texture }) {
+			return;
+		}
+
+		var headOrigin = drawInfo.headVect;
+		var player = drawInfo.drawPlayer;
+
+		//TODO: Mitigate this boilerplate.
+		var position = drawInfo.Position - Main.screenPosition;
+		position.X -= player.bodyFrame.Width / 2;
+		position.X += player.width / 2;
+		position.Y += player.height - player.bodyFrame.Height + 4f;
+		position += player.headPosition + headOrigin;
+		position = position.ToPoint().ToVector2();
+
+		drawInfo.DrawDataCache.Add(new DrawData(texture, position, player.bodyFrame, Color.White, player.headRotation, headOrigin, 1f, drawInfo.playerEffect, 0));
+	}
+}
+
 [AutoloadEquip(EquipType.Head)]
 internal class Saethar_Head : PatreonItem
 {
+	private static int[]? equipSlots;
+
+	public override bool IsVanitySet(int head, int body, int legs)
+	{
+		equipSlots ??= new int[] {
+			EquipLoader.GetEquipSlot(Mod, $"{InternalSetName}_{EquipType.Head}", EquipType.Head),
+			EquipLoader.GetEquipSlot(Mod, $"{InternalSetName}_{EquipType.Body}", EquipType.Body),
+			EquipLoader.GetEquipSlot(Mod, $"{InternalSetName}_{EquipType.Legs}", EquipType.Legs),
+		};
+
+		return head == equipSlots[0] && body == equipSlots[1] && legs == equipSlots[2];
+	}
+
+	public override void UpdateVanitySet(Player player)
+	{
+		if (player.TryGetModPlayer(out SaetharSetEffectPlayer modPlayer)) {
+			modPlayer.IsActive = true;
+		}
+	}
+
 	public override void SetDefaults()
 	{
 		base.SetDefaults();
