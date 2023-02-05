1cecf5b25b6c4bb5bac4d46a893ffbddb8a4cbdc
Destructor_Ben <ben.honeyfield@gmail.com>
2023-02-05 16:02:04 +1300

    IL Editing GenPasses

---
+++ ExampleMod/Common/Systems/ExampleWorldGenHookingSystem.cs
@@ -0,0 +1,54 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Terraria;
+using Terraria.GameContent.Generation;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+
+namespace ExampleMod.Common.Systems
+{
+	// This ModSystem will demonstrate how to IL Edit and Detour the methods used in world generation
+	// TODO: elaborate + explain why they can't be done the normal way. Also, aren't detours be as simple as removing the genpass? maybe they have a usecase outside of method swaps
+	// TODO: also make a wiki page and remove the old part about how to il edit worldgen
+	public class ExampleWorldGenHookingSystem : ModSystem
+	{
+		// TODO this is where world generation tasks are modified
+		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight) {
+			var task = tasks.Find(t => t.Name == "Living Trees"); // Find the task we want to modify, in this case the "Living Trees" pass
+			WorldGen.ModifyTask(task, ModifyLivingTrees); // Then we call WorldGen.ModifyTask, which IL edits the pass
+
+			var task2 = tasks.Find(t => t.Name == "Living Trees");
+			//WorldGen.DetourTask(task2, DetourShinies);
+		}
+
+		// The IL editing shouldn't be different
+		private void ModifyLivingTrees(ILContext il) {
+			var c = new ILCursor(il); // Create an ILCursor
+
+			// Find where to apply the patch
+			if (!c.TryGotoNext(i => i.MatchStloc(2))) {
+				// Can't apply the patch
+				Mod.Logger.Debug("Unable to apply patch for generation pass \"Living Trees\"");
+				return;
+			}
+
+			// Applying the patch
+			// This pops the original number of living trees off the stack, and then we add 1 in case it is 0 then multiply it by 5
+			c.EmitDelegate(delegate (int originalNumberOfLivingTrees) {
+				return (originalNumberOfLivingTrees + 1) * 5;
+			});
+		}
+
+		private void DetourShinies(WorldGenLegacyMethod orig, GenerationProgress progress, GameConfiguration configuration) {
+			progress.Message = "Skipped Shinies";
+			Thread.Sleep(5000);
+			//orig(progress, configuration);
+		}
+	}
+}
--- patches/tModLoader/Terraria/WorldGen.TML.cs
+++ patches/tModLoader/Terraria/WorldGen.TML.cs
@@ -1,4 +1,16 @@
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour.HookGen;
+using MonoMod.RuntimeDetour;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using Terraria.GameContent.Generation;
+using Terraria.ModLoader;
 using Terraria.WorldBuilding;
+using Steamworks;
+using System.Threading.Tasks;
+using Terraria.Audio;
+using Terraria.IO;
 
 namespace Terraria;
 
@@ -7,4 +19,80 @@ public partial class WorldGen
 	private static double _timePass = 0.0; // Used to account for more precise time rates.
 
 	internal static void ClearGenerationPasses() => _generator?._passes.Clear();
+
+	internal static List<Tuple<GenPass, ILContext.Manipulator>> genPassILEdits;
+	internal static List<Tuple<GenPass, GenPassDetour>> genPassDetours;
+
+	public delegate void GenPassDetour(WorldGenLegacyMethod orig, GenerationProgress progress, GameConfiguration configuration);
+
+	/// <summary>
+	/// IL edits a PassLegacy during world generation. Use in ModifyWorldGenerationTasks or ModifyHardmodeTasks.
+	/// </summary>
+	/// <param name="task">The task to be IL edited</param>
+	/// <param name="callback">The method that IL edits the pass</param>
+	// TODO: Add support for hardmode tasks
+	public static void ModifyTask(GenPass task, ILContext.Manipulator callback)
+	{
+		HookEndpointManager.Modify(GetGenPassMethod(task), callback);
+		genPassILEdits.Add(Tuple.Create(task, callback));
+	}
+
+	/// <summary>
+	/// Detours a PassLegacy during world generation. Use in ModifyWorldGenerationTasks or ModifyHardmodeTasks.
+	/// </summary>
+	/// <param name="task">The task to be detoured</param>
+	/// <param name="hookDelegate">The detour method</param>
+	// TODO: make detours work
+	public static void DetourTask(GenPass task, GenPassDetour hookDelegate)
+	{
+		HookEndpointManager.Add(GetGenPassMethod(task), hookDelegate);
+		genPassDetours.Add(Tuple.Create(task, hookDelegate));
+	}
+
+	internal static void RemoveGenPassILEditsAndDetours()
+	{
+		foreach (var ilEdit in genPassILEdits) {
+			HookEndpointManager.Unmodify(GetGenPassMethod(ilEdit.Item1), ilEdit.Item2);
+		}
+
+		foreach (var detour in genPassDetours) {
+			HookEndpointManager.Remove(GetGenPassMethod(detour.Item1), detour.Item2);
+		}
+	}
+
+	// TODO: tidy up
+	internal static MethodInfo GetGenPassMethod(GenPass task)
+	{
+		if (task is PassLegacy pass) {
+			var methodField = typeof(PassLegacy).GetField("_method", BindingFlags.NonPublic | BindingFlags.Instance);
+			object method = methodField.GetValue(pass);
+			if (method is WorldGenLegacyMethod legacyMethod) {
+				return legacyMethod.Method;
+			}
+			else {
+				throw new Exception("Method is not a WorldGenLegacyMethod");
+				return null;
+			}
+		}
+		else {
+			throw new Exception("Task is not a PassLegacy");
+			return null;
+		}
+	}
+
+	// TODO: Remove after testing and create DumpHooks for detours
+	internal static void DumpILHooks()
+	{
+		var ilHookList = typeof(HookEndpointManager).GetField("ILHooks", BindingFlags.NonPublic | BindingFlags.Static);
+		var list = ilHookList.GetValue(null);
+		if (list is Dictionary<(MethodBase, Delegate), ILHook> dict) {
+			Logging.tML.Debug("ILHooks Dump:");
+			foreach (var item in dict) {
+				Logging.tML.Debug(item.Key + ": " + item.Value);
+			}
+		}
+		else {
+			throw new Exception($"Failed to get HookEndpointManager.ILHooks: Type is {list.GetType()}");
+		}
+	}
 }
