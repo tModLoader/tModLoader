ca3dbb59b13749ea1198b855d2b5be7d0d29f177
direwolf420 <amakejk1@web.de>
2023-02-13 14:06:52 +0100

    example comments

--- ExampleMod/Content/Items/Tools/ExampleHook.cs
+++ ExampleMod/Content/Items/Tools/ExampleHook.cs
@@ -118,7 +118,7 @@ namespace ExampleMod.Content.Items.Tools
 
 		// Can customize what tiles this hook can latch onto, or force/prevent latching alltogether, like Squirrel Hook also latching to trees
 		public override bool? GrappleCanLatchOnTo(Player player, int x, int y) {
-			// By default, the hook returns the vanilla conditions for the given tile position to be concidered "latchable" (this tile position could be air or an actuated tile!)
+			// By default, the hook returns null to apply the vanilla conditions for the given tile position (this tile position could be air or an actuated tile!)
 			// If you want to return true here, make sure to check for Main.tile[x, y].HasUnactuatedTile (and Main.tileSolid[Main.tile[x, y].TileType] and/or Main.tile[x, y].HasTile if needed)
 
 			// We make this hook latch onto trees just like Squirrel Hook
@@ -129,7 +129,8 @@ namespace ExampleMod.Content.Items.Tools
 				return true;
 			}
 
-			return base.GrappleCanLatchOnTo(player, x, y);
+			// In any other case, behave like a normal hook
+			return null;
 		}
 
 		// Draws the grappling hook's chain.
