2a18ad0a9332579e899650233689c48bbbbfa0c8
Destructor_Ben <ben.honeyfield@gmail.com>
2023-02-19 11:53:45 +1300

    Fixes the EM example and made sure the hooks automatically unloaded

--- ExampleMod/Common/Systems/ExampleWorldGenHookingSystem.cs
+++ ExampleMod/Common/Systems/ExampleWorldGenHookingSystem.cs
@@ -25,14 +25,14 @@ namespace ExampleMod.Common.Systems
 			// IL editing the pyramids pass
 			GenPass pyramidsPass = WorldGen.VanillaGenPasses.First(t => t.Name == "Pyramids");// Locate the generation pass
 			if (pyramidsPass is PassLegacy pass1)// Check if the pass is a PassLegacy, since PassLegacys are the generation passes that can't normally be edited
-				WorldGen.ModifyTask(pass1, Modify_Pyramids);// IL edit the pass
+				WorldGen.ModifyPass(pass1, Modify_Pyramids);// IL edit the pass
 			else
 				Mod.Logger.Warn("Unable to modify pyramids pass");// Log an error if the pass can't be located
 
 			// Detouring the shinies pass (generates ore)
 			GenPass shiniesPass = WorldGen.VanillaGenPasses.First(t => t.Name == "Shinies");// Locate the generation pass
 			if (shiniesPass is PassLegacy pass2)// Check if the pass is a PassLegacy, since PassLegacys are the generation passes that can't normally be edited
-				WorldGen.DetourTask(pass2, Detour_Shinies);// Detour the pass
+				WorldGen.DetourPass(pass2, Detour_Shinies);// Detour the pass
 			else
 				Mod.Logger.Warn("Unable to detour shinies pass");// Log an error if the pass can't be located
 		}
@@ -42,12 +42,13 @@ namespace ExampleMod.Common.Systems
 			var c = new ILCursor(il);
 
 			c.EmitDelegate(delegate () {
-				if (WorldGen.genRand.NextBool())
-					WorldGen.Pyramid(Main.spawnTileX, Main.spawnTileY);
+				WorldGen.Pyramid(Main.maxTilesX / 2, Main.maxTilesY / 2);
 			});
 		}
 
-		// Detouring should be the same, this is just an example so you can check this is actually working
+		// Detouring should be the same (except for one thing mentioned below), this is just an example so you can check this is actually working
+		// One thing to note is that for techincal reasons, the self parameter is an object type
+		// You will never need to actually cast it to type WorldGen though, since it contains no instance fields or methods
 		void Detour_Shinies(WorldGen.orig_GenPassDetour orig, object self, GenerationProgress progress, GameConfiguration configuration) {
 			// orig(self, progress, configuration); This stops underground ore generating by not calling the original method
 		}
--- patches/tModLoader/Terraria/WorldGen.TML.cs
+++ patches/tModLoader/Terraria/WorldGen.TML.cs
@@ -27,24 +27,19 @@ public partial class WorldGen
 	internal static List<GenPass> vanillaGenPasses_internal = new();
 	public static ReadOnlyCollection<GenPass> VanillaGenPasses = vanillaGenPasses_internal.AsReadOnly();
 
-	internal static List<Tuple<PassLegacy, ILContext.Manipulator>> genPassILEdits = new();
-	internal static List<Tuple<PassLegacy, GenPassDetour>> genPassDetours = new();
-
-	public static void ModifyTask(PassLegacy pass, ILContext.Manipulator callback)
+	public static void ModifyPass(PassLegacy pass, ILContext.Manipulator callback)
 	{
 		HookEndpointManager.Modify(GetGenPassMethod(pass), callback);
-		genPassILEdits.Add(Tuple.Create(pass, callback));
 	}
 
 	// TODO: Self cannot be WorldGen, since the one used in the actual method is WorldGen+<>c, which is compiler generated or something like that
-	// the self parameter should be removed, since world gen is an instanced class, but has no instance fields or methods (it could be made static in another pr)
+	// The self parameter should be removed, since world gen is an instanced class, but has no instance fields or methods (it could be made static in another pr)
 	public delegate void GenPassDetour(orig_GenPassDetour orig, object self, GenerationProgress progress, GameConfiguration configuration);
 	public delegate void orig_GenPassDetour(object self, GenerationProgress progress, GameConfiguration configuration);
 
-	public static void DetourTask(PassLegacy pass, GenPassDetour hookDelegate)
+	public static void DetourPass(PassLegacy pass, GenPassDetour hookDelegate)
 	{
 		HookEndpointManager.Add(GetGenPassMethod(pass), hookDelegate);
-		genPassDetours.Add(Tuple.Create(pass, hookDelegate));
 	}
 
 	internal static MethodInfo GetGenPassMethod(PassLegacy pass)
@@ -57,20 +52,5 @@ public partial class WorldGen
 		else {
 			throw new Exception("Method is not a WorldGenLegacyMethod");
 		}
-	}
-
-	// TODO: Unload it in MonoModHooks.cs?
-	internal static void RemoveGenPassILEditsAndDetours()
-	{
-		foreach (var ilEdit in genPassILEdits) {
-			HookEndpointManager.Unmodify(GetGenPassMethod(ilEdit.Item1), ilEdit.Item2);
-		}
-
-		foreach (var detour in genPassDetours) {
-			HookEndpointManager.Remove(GetGenPassMethod(detour.Item1), detour.Item2);
-		}
-
-		genPassILEdits.Clear();
-		genPassDetours.Clear();
 	}
 }
