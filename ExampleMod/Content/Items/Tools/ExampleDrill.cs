c4274dfef43e6bd2f53d659adae0891541ce2f35
Blockaroz <lbpk.123fan@gmail.com>
2023-02-26 14:12:54 -0500

    elaboration and accuracy

--- ExampleMod/Content/Items/Tools/ExampleDrill.cs
+++ ExampleMod/Content/Items/Tools/ExampleDrill.cs
@@ -11,6 +11,12 @@ namespace ExampleMod.Content.Items.Tools
 	{
 		public override void SetStaticDefaults() {
 			ItemID.Sets.IsDrill[Type] = true;
+			//ItemID.Sets.IsChainsaw[Type] = true;
+			//IsDrill and IsChainsaw normally automatically reduce useTime and useAnimation to 60% of what is set in SetDefaults and decrease tileBoost by 1.
+			//They will not decrease tileBoost by 2 or decrease use time to 36% if both are true.
+
+			//Because modded item defaults are set after this takes place, you need to set useTime/useAnimation manually.
+			//tileBoost will be set, but it will be replaced if you set it again in SetDefaults.
 		}
 
 		public override void SetDefaults() {
@@ -18,18 +24,24 @@ namespace ExampleMod.Content.Items.Tools
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 20;
 			Item.height = 12;
-			Item.useTime = 10;
-			Item.useAnimation = 10;
+			//IsDrill/IsChainsaw effects must be applied manually, so 60% or 0.6 times the time of a pickaxe. In this case, 60% of 10, or 6.
+			//If you decide to copy values from vanilla drills or chainsaws, you should multiply each one by 0.6 to get actual time.
+			Item.useTime = 6;
+			Item.useAnimation = 6;
 			Item.useStyle = ItemUseStyleID.Shoot;
 			Item.knockBack = 0.6f;
 			Item.value = Item.buyPrice(gold: 1);
 			Item.rare = ItemRarityID.Green;
 			Item.UseSound = SoundID.Item23;
-			Item.shoot = ModContent.ProjectileType<ExampleDrillProjectile>();
+			Item.shoot = ModContent.ProjectileType<ExampleDrillProjectile>(); //Create the drill projectile
 			Item.shootSpeed = 32f;
 			Item.noMelee = true; //Turns off damage from the item itself, as we have a projectile
 			Item.noUseGraphic = true; //Stops the item from drawing in your hands, for the aforementioned reason
 			Item.channel = true; //Important as the projectile checks if the player channels
+
+			//tileBoost changes the range of tiles that the item can reach. Since IsDrill/IsChainsaw (above) is true, we can leave it alone...
+			//Or we can give the drill 10 blocks of of extra range
+			Item.tileBoost = 10;
 
 			Item.pick = 220; // How strong the drill is, see https://terraria.wiki.gg/wiki/Pickaxe_power for a list of common values
 		}
--- ExampleMod/Content/Projectiles/ExampleDrillProjectile.cs
+++ ExampleMod/Content/Projectiles/ExampleDrillProjectile.cs
@@ -1,5 +1,6 @@
 ﻿using ExampleMod.Content.Dusts;
 using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
@@ -18,48 +19,66 @@ namespace ExampleMod.Content.Projectiles
 			Projectile.DamageType = DamageClass.Melee;
 			Projectile.ownerHitCheck = true;
 			Projectile.aiStyle = -1; //Replace with 20 if you do not want custom code
-			Projectile.hide = true; //Hides the projectile, so it will draw in the player's hand when
+			Projectile.hide = true; //Hides the projectile, so it will draw in the player's hand when we set the player's heldProj to this one.
 		}
 
-		//This code is adapted from aiStyle 20, to use a different dust. If you want to use aiStyle 20, you do not need to override AI.
+		//This code is adapted from aiStyle 20 to use a different dust and more noises. If you want to use aiStyle 20, you do not need to do any of this.
+		//It should be noted that this projectile has no effect on mining and is mostly visual.
 		public override void AI() {
 
 			Player player = Main.player[Projectile.owner];
 
+			Projectile.timeLeft = 60;
+
+			//Plays a sound every 20 ticks. In aiStyle 20, soundDelay is set to 30 ticks.
 			if (Projectile.soundDelay <= 0) {
 				SoundEngine.PlaySound(SoundID.Item22, Projectile.Center);
-				Projectile.soundDelay = 30;
+				Projectile.soundDelay = 20;
 			}
 
 			Vector2 myPosition = player.RotatedRelativePoint(player.MountedCenter);
 			if (player.channel) {
-				float velocity = player.HeldItem.shootSpeed * Projectile.scale;
-				Projectile.velocity = Projectile.DirectionTo(Main.MouseWorld) * velocity;
+				float num178 = player.inventory[player.selectedItem].shootSpeed * Projectile.scale;
+				float newVelocityX = Main.mouseX + Main.screenPosition.X - myPosition.X;
+				float newVelocityY = Main.mouseY + Main.screenPosition.Y - myPosition.Y;
+				if (player.gravDir == -1f)
+					newVelocityY = Main.screenHeight - Main.mouseY + Main.screenPosition.Y - myPosition.Y;
+
+				float num181 = (float)Math.Sqrt(newVelocityX * newVelocityX + newVelocityY * newVelocityY);
+				num181 = num178 / num181;
+				newVelocityX *= num181;
+				newVelocityY *= num181;
+				if (newVelocityX != Projectile.velocity.X || newVelocityY != Projectile.velocity.Y)
+					Projectile.netUpdate = true;
+
+				Projectile.velocity.X = newVelocityX;
+				Projectile.velocity.Y = newVelocityY;
 			}
 			else
 				Projectile.Kill();
 
 			if (Projectile.velocity.X > 0f)
-				Projectile.direction = 1;
-			if (Projectile.velocity.X < 0f)
-				Projectile.direction = -1;
+				player.ChangeDir(1);
+			else if (Projectile.velocity.X < 0f)
+				player.ChangeDir(-1);
 
 			Projectile.spriteDirection = Projectile.direction;
+			player.ChangeDir(Projectile.direction); //Change the player's direction based on the projectile's own
+			player.heldProj = Projectile.whoAmI; //We tell the player that the drill is the held projectile, so it will draw in their hand
+			player.SetDummyItemTime(2); //Make sure the player's item time does not change while the projectile is out
+			Projectile.Center = myPosition;
 			Projectile.rotation = (Projectile.velocity).ToRotation() + MathHelper.PiOver2;
-			player.ChangeDir(Projectile.direction);
-			player.heldProj = Projectile.whoAmI;
-			player.SetDummyItemTime(2);
 			player.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
 
-			Projectile.Center = myPosition;
 			Projectile.velocity.X *= 1f + Main.rand.Next(-3, 4) * 0.01f;
 
 			//Spawning dust
-			if (Main.rand.NextBool(15)) {
-				Dust dust = Dust.NewDustDirect(Projectile.position + Projectile.velocity * Main.rand.Next(6, 10) * 0.1f, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), 0f, 0f, 80, Color.White, 1.4f);
+			if (Main.rand.NextBool(10)) {
+				Dust dust = Dust.NewDustDirect(Projectile.position + Projectile.velocity * Main.rand.Next(6, 10) * 0.15f, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), 0f, 0f, 80, Color.White, 1f);
 				dust.position.X -= 4f;
 				dust.noGravity = true;
-				dust.velocity += Projectile.velocity * 0.03f;
+				dust.velocity.X *= 0.5f;
+				dust.velocity.Y = -Main.rand.Next(3, 8) * 0.1f;
 			}
 		}
 	}
