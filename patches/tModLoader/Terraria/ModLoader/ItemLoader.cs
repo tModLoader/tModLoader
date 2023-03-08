c6881cfc7cca5f9ded812e531b3e8307d4bb843c
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2023-03-09 02:09:52 +0500

    Not so minimal patches

--- patches/tModLoader/Terraria/ModLoader/ItemLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/ItemLoader.cs
@@ -144,11 +144,10 @@ public static class ItemLoader
 			ContentSamples.ItemsByType[item.Type].RebuildTooltip();
 		}
 
-		ValidateGeodeDropsSet();
-		ValidateSlimeOreDropsSet();
+		ValidateDropsSet();
 	}
 
-	internal static void ValidateGeodeDropsSet()
+	internal static void ValidateDropsSet()
 	{
 		foreach (var pair in ItemID.Sets.GeodeDrops) {
 			string exceptionCommon = $"{Lang.GetItemNameValue(pair.Key)} registered in 'ItemID.Sets.{nameof(ItemID.Sets.GeodeDrops)}'";
@@ -158,10 +157,7 @@ public static class ItemLoader
 			if (pair.Value.maxStack <= pair.Value.minStack)
 				throw new Exception($"{exceptionCommon} must have maxStack bigger than minStack");
 		}
-	}
 
-	internal static void ValidateSlimeOreDropsSet()
-	{
 		foreach (var pair in ItemID.Sets.OreDropsFromSlime) {
 			string exceptionCommon = $"{Lang.GetItemNameValue(pair.Key)} registered in 'ItemID.Sets.{nameof(ItemID.Sets.OreDropsFromSlime)}'";
 			if (pair.Value.minStack < 1)
