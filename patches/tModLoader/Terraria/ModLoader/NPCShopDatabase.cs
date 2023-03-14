df8acc8b3cdc7c065987c0e61900b38f6991aed5
Chicken-Bones <mehvids@gmail.com>
2023-03-14 12:27:49 +1000

    Improve code formatting of Bartender shop

--- patches/tModLoader/Terraria/ModLoader/NPCShopDatabase.cs
+++ patches/tModLoader/Terraria/ModLoader/NPCShopDatabase.cs
@@ -818,61 +818,64 @@ public static class NPCShopDatabase
 
 	private static void RegisterBartender()
 	{
-		// Woah! Whole nothing here!
-		// Look in ModLoader.Default.BartenderShopNPC for actual implementation.
 		var shop = new NPCShop(NPCID.DD2Bartender);
 
-		static Item MakeItem(int id, int currencyPrice) =>
-			new(id) { shopCustomPrice = currencyPrice, shopSpecialCurrency = CustomCurrencyID.DefenderMedals };
+		void AddEntry(int id, int price, params Condition[] conditions) =>
+			shop.Add(
+				new Entry(
+					new Item(id) { shopCustomPrice = price, shopSpecialCurrency = CustomCurrencyID.DefenderMedals },
+					conditions
+				).ReserveSlot());
 
 		// 1st row
 		shop.Add(ItemID.Ale);
-		shop.Add(ItemID.DD2ElderCrystal);                                                                                   // Eternia Crystal
-		shop.Add(ItemID.DD2ElderCrystalStand);                                                                              // Eternia Crystal Stand
-		shop.Add(MakeItem(ItemID.DefendersForge, 50));
+		// See BartenderShopNPC for the price modification of elder crystal based on difficulty
+		shop.Add(ItemID.DD2ElderCrystal);                                                // Eternia Crystal
+		shop.Add(ItemID.DD2ElderCrystalStand);                                           // Eternia Crystal Stand
+		AddEntry(ItemID.DefendersForge, 50);
 
-		shop.Add(new Entry(MakeItem(ItemID.SquireGreatHelm, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.SquirePlating, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.SquireGreaves, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.SquireAltHead, 50), Condition.DownedGolem).ReserveSlot());                       // Valhalla Knight's Helm
-		shop.Add(new Entry(MakeItem(ItemID.SquireAltShirt, 50), Condition.DownedGolem).ReserveSlot());                      // Valhalla Knight's Breastplate
-		shop.Add(new Entry(MakeItem(ItemID.SquireAltPants, 50), Condition.DownedGolem).ReserveSlot());                      // Valhalla Knight's Greaves
+		AddEntry(ItemID.SquireGreatHelm,			15,	Condition.DownedMechBossAny);
+		AddEntry(ItemID.SquirePlating,				15,	Condition.DownedMechBossAny);
+		AddEntry(ItemID.SquireGreaves,				15,	Condition.DownedMechBossAny);
+		AddEntry(ItemID.SquireAltHead,				50,	Condition.DownedGolem);              // Valhalla Knight's Helm
+		AddEntry(ItemID.SquireAltShirt,				50,	Condition.DownedGolem);              // Valhalla Knight's Breastplate
+		AddEntry(ItemID.SquireAltPants,				50,	Condition.DownedGolem);              // Valhalla Knight's Greaves
 
 		// 2nd row
-		shop.Add(new Entry(MakeItem(ItemID.DD2FlameburstTowerT1Popper, 5)).ReserveSlot());                                  // Flameburst Rod
-		shop.Add(new Entry(MakeItem(ItemID.DD2BallistraTowerT1Popper, 5)).ReserveSlot());                                   // Ballista Rod
-		shop.Add(new Entry(MakeItem(ItemID.DD2ExplosiveTrapT1Popper, 5)).ReserveSlot());                                    // Explosive Trap Rod
-		shop.Add(new Entry(MakeItem(ItemID.DD2LightningAuraT1Popper, 5)).ReserveSlot());                                    // Lightning Aura Rod
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeHat, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeRobe, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeTrousers, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeAltHead, 50), Condition.DownedGolem).ReserveSlot());                   // Dark Atrist's Hat
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeAltShirt, 50), Condition.DownedGolem).ReserveSlot());                  // Dark Atrist's Robes
-		shop.Add(new Entry(MakeItem(ItemID.ApprenticeAltPants, 50), Condition.DownedGolem).ReserveSlot());                  // Dark Atrist's Leggings
+		AddEntry(ItemID.DD2FlameburstTowerT1Popper,	5);                                  // Flameburst Rod
+		AddEntry(ItemID.DD2BallistraTowerT1Popper,	5);                                  // Ballista Rod
+		AddEntry(ItemID.DD2ExplosiveTrapT1Popper,	5);                                  // Explosive Trap Rod
+		AddEntry(ItemID.DD2LightningAuraT1Popper,	5);                                  // Lightning Aura Rod
+		AddEntry(ItemID.ApprenticeHat,				15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.ApprenticeRobe,				15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.ApprenticeTrousers,			15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.ApprenticeAltHead,			50, Condition.DownedGolem);          // Dark Atrist's Hat
+		AddEntry(ItemID.ApprenticeAltShirt,			50, Condition.DownedGolem);          // Dark Atrist's Robes
+		AddEntry(ItemID.ApprenticeAltPants,			50, Condition.DownedGolem);          // Dark Atrist's Leggings
 
 		// 3rd row
-		shop.Add(new Entry(MakeItem(ItemID.DD2FlameburstTowerT2Popper, 15), Condition.DownedMechBossAny).ReserveSlot());    // Flameburst Cane
-		shop.Add(new Entry(MakeItem(ItemID.DD2BallistraTowerT2Popper, 15), Condition.DownedMechBossAny).ReserveSlot());    // Ballista Cane
-		shop.Add(new Entry(MakeItem(ItemID.DD2ExplosiveTrapT2Popper, 15), Condition.DownedMechBossAny).ReserveSlot());    // Explosive Trap Cane
-		shop.Add(new Entry(MakeItem(ItemID.DD2LightningAuraT2Popper, 15), Condition.DownedMechBossAny).ReserveSlot());    // Lightning Aura Cane
-		shop.Add(new Entry(MakeItem(ItemID.HuntressWig, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.HuntressJerkin, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.HuntressPants, 15), Condition.DownedMechBossAny).ReserveSlot());
-		shop.Add(new Entry(MakeItem(ItemID.HuntressAltHead, 50), Condition.DownedGolem).ReserveSlot());                     // Red Riding Hood
-		shop.Add(new Entry(MakeItem(ItemID.HuntressAltShirt, 50), Condition.DownedGolem).ReserveSlot());                    // Red Riding Dress
-		shop.Add(new Entry(MakeItem(ItemID.HuntressAltPants, 50), Condition.DownedGolem).ReserveSlot());                    // Red Riding Leggings
+		AddEntry(ItemID.DD2FlameburstTowerT2Popper, 15, Condition.DownedMechBossAny);    // Flameburst Cane
+		AddEntry(ItemID.DD2BallistraTowerT2Popper,	15, Condition.DownedMechBossAny);    // Ballista Cane
+		AddEntry(ItemID.DD2ExplosiveTrapT2Popper,	15, Condition.DownedMechBossAny);    // Explosive Trap Cane
+		AddEntry(ItemID.DD2LightningAuraT2Popper,	15, Condition.DownedMechBossAny);    // Lightning Aura Cane
+		AddEntry(ItemID.HuntressWig,				15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.HuntressJerkin,				15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.HuntressPants,				15, Condition.DownedMechBossAny);
+		AddEntry(ItemID.HuntressAltHead,			50, Condition.DownedGolem);          // Red Riding Hood
+		AddEntry(ItemID.HuntressAltShirt,			50, Condition.DownedGolem);          // Red Riding Dress
+		AddEntry(ItemID.HuntressAltPants,			50, Condition.DownedGolem);          // Red Riding Leggings
 
 		// 4th row
-		shop.Add(new Entry(MakeItem(ItemID.DD2FlameburstTowerT3Popper, 60), Condition.DownedGolem).ReserveSlot());          // Flameburst Staff
-		shop.Add(new Entry(MakeItem(ItemID.DD2BallistraTowerT3Popper, 60), Condition.DownedGolem).ReserveSlot());           // Ballista Staff
-		shop.Add(new Entry(MakeItem(ItemID.DD2ExplosiveTrapT3Popper, 60), Condition.DownedGolem).ReserveSlot());          // Explosive Trap Staff
-		shop.Add(new Entry(MakeItem(ItemID.DD2LightningAuraT3Popper, 60), Condition.DownedGolem).ReserveSlot());          // Lightning Aura Staff
-		shop.Add(new Entry(MakeItem(ItemID.MonkBrows, 15), Condition.DownedMechBossAny).ReserveSlot());                // Monk's Bushy Brow Bald Cap
-		shop.Add(new Entry(MakeItem(ItemID.MonkShirt, 15), Condition.DownedMechBossAny).ReserveSlot());             // Monk's Shirt
-		shop.Add(new Entry(MakeItem(ItemID.MonkPants, 15), Condition.DownedMechBossAny).ReserveSlot());             // Monk's Pants
-		shop.Add(new Entry(MakeItem(ItemID.MonkAltHead, 50), Condition.DownedGolem).ReserveSlot());                         // Shinobi Infiltrator's Helmet
-		shop.Add(new Entry(MakeItem(ItemID.MonkAltShirt, 50), Condition.DownedGolem).ReserveSlot());                        // Shinobi Infiltrator's Torso
-		shop.Add(new Entry(MakeItem(ItemID.MonkAltPants, 50), Condition.DownedGolem).ReserveSlot());                        // Shinobi Infiltrator's Pants
+		AddEntry(ItemID.DD2FlameburstTowerT3Popper, 60, Condition.DownedGolem);          // Flameburst Staff
+		AddEntry(ItemID.DD2BallistraTowerT3Popper,	60, Condition.DownedGolem);          // Ballista Staff
+		AddEntry(ItemID.DD2ExplosiveTrapT3Popper,	60, Condition.DownedGolem);          // Explosive Trap Staff
+		AddEntry(ItemID.DD2LightningAuraT3Popper,	60, Condition.DownedGolem);          // Lightning Aura Staff
+		AddEntry(ItemID.MonkBrows,					15, Condition.DownedMechBossAny);    // Monk's Bushy Brow Bald Cap
+		AddEntry(ItemID.MonkShirt,					15, Condition.DownedMechBossAny);    // Monk's Shirt
+		AddEntry(ItemID.MonkPants,					15, Condition.DownedMechBossAny);    // Monk's Pants
+		AddEntry(ItemID.MonkAltHead,				50, Condition.DownedGolem);          // Shinobi Infiltrator's Helmet
+		AddEntry(ItemID.MonkAltShirt,				50, Condition.DownedGolem);          // Shinobi Infiltrator's Torso
+		AddEntry(ItemID.MonkAltPants,				50, Condition.DownedGolem);          // Shinobi Infiltrator's Pants
 
 		shop.Register();
 	}
