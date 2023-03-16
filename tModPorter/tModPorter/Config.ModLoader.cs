6bb248ec4ba99140701e372395d22ec00a282af3
stormytuna <82264356+stormytuna@users.noreply.github.com>
2023-03-16 23:21:12 +0000

    Changes ModifyBuffTip hook into ModifyBuffText hook (#3307) (#3309)
    * ModifyBuffTip -> ModifyBuffText
    
    * Example mod changes
    
    * Add tModPorter tests
    
    * Remove comments
    
    * Flip tip and rare back around
    
    * Fix tests
    
    * Add a buffName editing example
    
    ---------
    
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- tModPorter/tModPorter/Config.ModLoader.cs
+++ tModPorter/tModPorter/Config.ModLoader.cs
@@ -497,5 +497,10 @@ public static partial class Config
 		RefactorInstanceMember("Terraria.ModLoader.ModTile", "ContainerName", Removed("Override DefaultContainerName instead"));
 		RenameMethod("Terraria.ModLoader.TileLoader", "ContainerName", "DefaultContainerName");
 		RefactorStaticMethodCall("Terraria.ModLoader.TileLoader", "DefaultContainerName", Comment("Note: new method takes in FrameX and FrameY"));
+    
+    RenameMethod("Terraria.ModLoader.ModBuff", from: "ModifyBuffTip", to: "ModifyBuffText");
+		RenameMethod("Terraria.ModLoader.GlobalBuff", from: "ModifyBuffTip", to: "ModifyBuffText");
+		ChangeHookSignature("Terraria.ModLoader.ModBuff", "ModifyBuffText");
+		ChangeHookSignature("Terraria.ModLoader.GlobalBuff", "ModifyBuffText");
 	}
 }
