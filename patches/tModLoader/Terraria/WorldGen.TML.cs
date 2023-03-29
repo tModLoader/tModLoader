b7f45cacba76c040a752dfaa91c5ce763c1212fb
Chicken-Bones <mehvids@gmail.com>
2023-03-30 06:10:43 +1000

    Handle MonoMod update (made HookEndpointManager inaccessible)

--- patches/tModLoader/Terraria/WorldGen.TML.cs
+++ patches/tModLoader/Terraria/WorldGen.TML.cs
@@ -1,22 +1,30 @@
 using MonoMod.Cil;
-using MonoMod.RuntimeDetour.HookGen;
 using System.Collections.Generic;
 using Terraria.GameContent.Generation;
 using Terraria.WorldBuilding;
 using Terraria.IO;
+using MonoMod.RuntimeDetour;
+using System.Collections.Concurrent;
 
 namespace Terraria;
 
 public partial class WorldGen
 {
-	internal static void ClearGenerationPasses() => _generator?._passes.Clear();
+	internal static void ClearGenerationPasses()
+	{
+		_generator?._passes.Clear();
+		_hookRefs.Clear();
+	}
 
 	internal static Dictionary<string, GenPass> _vanillaGenPasses = new();
 	public static IReadOnlyDictionary<string, GenPass> VanillaGenPasses => _vanillaGenPasses;
 
+	// To prevent hooks being unloaded early due to garbage collection
+	private static ConcurrentBag<object> _hookRefs = new();
+
 	public static void ModifyPass(PassLegacy pass, ILContext.Manipulator callback)
 	{
-		HookEndpointManager.Modify(pass._method.Method, callback);
+		_hookRefs.Add(new ILHook(pass._method.Method, callback));
 	}
 
 	// TODO: Self cannot be WorldGen, since the one used in the actual method is WorldGen+<>c, which is compiler generated or something like that
@@ -26,6 +34,6 @@ public partial class WorldGen
 
 	public static void DetourPass(PassLegacy pass, GenPassDetour hookDelegate)
 	{
-		HookEndpointManager.Add(pass._method.Method, hookDelegate);
+		_hookRefs.Add(new Hook(pass._method.Method, hookDelegate));
 	}
 }
