ba432b06c56d47c1cbc67eae74e06501911bf3d6
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2023-04-23 23:28:31 +0500

    Overloads for ConvertX in Conversion

--- patches/tModLoader/Terraria/ModLoader/ConversionHandler.cs
+++ patches/tModLoader/Terraria/ModLoader/ConversionHandler.cs
@@ -39,9 +39,9 @@ public sealed class Conversion
 		}
 	}
 
-	private readonly List<BlockConversion> blockConversions = new();
+	private readonly LinkedList<BlockConversion> blockConversions = new();
 
-	public IReadOnlyList<BlockConversion> BlockConversions => blockConversions;
+	public IReadOnlyCollection<BlockConversion> BlockConversions => blockConversions;
 
 	public ushort Index { get; private set; }
 	public string Name { get; }
@@ -53,26 +53,32 @@ public sealed class Conversion
 
 	public BlockConversion ConvertTile(int from, int to)
 	{
-		blockConversions.Add(new BlockConversion(from, to, true));
-		return blockConversions[^1];
+		var blockConversion = new BlockConversion(from, to, true);
+		blockConversions.AddLast(blockConversion);
+		return blockConversion;
 	}
+	public BlockConversion ConvertTile<TFrom>(int to) where TFrom : ModTile => ConvertTile(ModContent.TileType<TFrom>(), to);
+	public BlockConversion ConvertTile<TFrom, TTo>() where TFrom : ModTile where TTo : ModTile => ConvertTile(ModContent.TileType<TFrom>(), ModContent.TileType<TTo>());
 
 	public BlockConversion ConvertWall(int from, int to)
 	{
-		blockConversions.Add(new BlockConversion(from, to, false));
-		return blockConversions[^1];
+		var blockConversion = new BlockConversion(from, to, false);
+		blockConversions.AddLast(blockConversion);
+		return blockConversion;
 	}
+	public BlockConversion ConvertWall<TFrom>(int to) where TFrom : ModWall => ConvertWall(ModContent.WallType<TFrom>(), to);
+	public BlockConversion ConvertWall<TFrom, TTo>() where TFrom : ModWall where TTo : ModWall => ConvertWall(ModContent.WallType<TFrom>(), ModContent.WallType<TTo>());
 
 	internal void Fill(BlockConversion[] data)
 	{
-		blockConversions.ForEach(conversion => {
+		foreach (var conversion in blockConversions) {
 			if (conversion.IsTile) {
 				data[ConversionHandler.TileIndex(Index, conversion.From)] = conversion;
 			}
 			else {
 				data[ConversionHandler.WallIndex(Index, conversion.From)] = conversion;
 			}
-		});
+		}
 	}
 
 	public void Register()
