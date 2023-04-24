5db53db5bd6c9e93af82a7bdbffa1de532867d2a
Tomat <xxlennygamerxx@gmail.com>
2023-04-23 22:40:49 -0700

    Working hair API, still needs premade conditions.

--- patches/tModLoader/Terraria/ModLoader/ModContent.cs
+++ patches/tModLoader/Terraria/ModLoader/ModContent.cs
@@ -463,6 +463,7 @@ public static class ModContent
 		InfoDisplayLoader.Unload();
 		GoreLoader.Unload();
 		PlantLoader.UnloadPlants();
+		HairLoader.Unload();
 
 		ResourceOverlayLoader.Unload();
 		ResourceDisplaySetLoader.Unload();
@@ -526,6 +527,7 @@ public static class ModContent
 		BuffLoader.ResizeArrays();
 		PlayerLoader.ResizeArrays();
 		PlayerDrawLayerLoader.ResizeArrays();
+		HairLoader.ResizeArrays();
 		SystemLoader.ResizeArrays();
 
 		if (!Main.dedServ) {
---
+++ patches/tModLoader/Terraria/ModLoader/ModHair.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace Terraria.ModLoader;
+
+/// <summary>
+/// This class allows you to define custom hair styles for a player, controlling the associated gender and unlock conditions.
+/// </summary>
+public abstract class ModHair : ModTexturedType
+{
+	public int Type { get; internal set; }
+
+	/// <summary>
+	/// The path to the alternative texture used for when the hair is covered by a hat.
+	/// </summary>
+	public virtual string AltTexture => Texture + "Alt";
+
+	/// <summary>
+	/// Determines whether this hairstyle is considered male, which influences what gender is picked when randomizing a character.
+	/// </summary>
+	public virtual bool IsMale => true;
+
+	protected sealed override void Register()
+	{
+		ModTypeLookup<ModHair>.Register(this);
+		Type = HairLoader.Register(this);
+	}
+
+	public sealed override void SetupContent()
+	{
+		AutoStaticDefaults();
+		SetStaticDefaults();
+	}
+
+	/// <summary>
+	/// Automatically sets certain static defaults. Override this if you do not want the properties to be set for you.
+	/// </summary>
+	public virtual void AutoStaticDefaults()
+	{
+		TextureAssets.PlayerHair[Type] = ModContent.Request<Texture2D>(Texture);
+		TextureAssets.PlayerHairAlt[Type] = ModContent.Request<Texture2D>(AltTexture);
+	}
+
+	/// <summary>
+	/// Gets the unlock conditions for this hairstyle. No conditions by default.
+	/// </summary>
+	public virtual IEnumerable<Condition> GetUnlockConditions()
+	{
+		return Enumerable.Empty<Condition>();
+	}
+}
