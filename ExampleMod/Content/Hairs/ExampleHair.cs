080f4eb35dbe0f6d6e7a484fa96f67ccc2f5ad18
JavidPack <javidpack@gmail.com>
2023-04-24 19:44:45 -0600

    Added ExampleHair

---
+++ ExampleMod/Content/Hairs/ExampleHair.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Hairs
+{
+	// Based on Player_Hair_88 and Player_Hair_98
+	// Note that internal hair ids are 1 less than the texture filename
+	public class ExampleHair : ModHair
+	{
+		public override bool IsMale => false;
+		
+		public override void SetStaticDefaults() {
+			HairID.Sets.DrawBackHair[Type] = true;
+		}
+
+		public override IEnumerable<Condition> GetUnlockConditions() {
+			return Enumerable.Empty<Condition>();
+			// TODO: throws exception on player creation menu, biome flags not available on player instance?
+			//yield return ExampleConditions.InExampleBiome;
+		}
+	}
+}
