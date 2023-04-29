768b533c97db0ac53d557ae05e00f222905cc722
Tomat <xxlennygamerxx@gmail.com>
2023-04-28 22:03:16 -0700

    EM comment.

--- ExampleMod/Content/Hairs/ExampleHair.cs
+++ ExampleMod/Content/Hairs/ExampleHair.cs
@@ -11,6 +11,10 @@ namespace ExampleMod.Content.Hairs
 	// Note that internal hair ids are 1 less than the texture filename
 	public class ExampleHair : ModHair
 	{
+		// This determines what gender the character will be when randomizing during character creation.
+		// Keep in mind that hairstyles can be used with any gender; this property just allows for defining behavior during randomization.
+		// If not set by us like here, Gender.Unspecified will be used instead, which randomizes the gender independent of the hairstyle.
+		// This may be particularly useful if your hairstyle doesn't lean either way.
 		public override Gender RandomizedCharacterCreationGender => Gender.Female;
 		
 		public override void SetStaticDefaults() {
