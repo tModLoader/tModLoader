ae502e476f3f0b0f86360f6452f5e3c635c71a66
Tomat <xxlennygamerxx@gmail.com>
2023-04-28 22:19:16 -0700

    Set hair visibility, add comments.

--- ExampleMod/Content/Hairs/ExampleHair.cs
+++ ExampleMod/Content/Hairs/ExampleHair.cs
@@ -16,11 +16,17 @@ namespace ExampleMod.Content.Hairs
 		// If not set by us like here, Gender.Unspecified will be used instead, which randomizes the gender independent of the hairstyle.
 		// This may be particularly useful if your hairstyle doesn't lean either way.
 		public override Gender RandomizedCharacterCreationGender => Gender.Female;
-		
+
+		// By default, Visibility is set to Visibility.All, which allows this hairstyle appear in all UIs.
+		// We want our hairstyle to specifically appear in just the Stylist's UI, not both the Stylist UI and the Character Creation UI.
+		// This is because all modded hairstyles which may appear in the Character Creation UI entirely ignore GetUnlockConditions.
+		public override HairVisibility Visibility => HairVisibility.Stylist;
+
 		public override void SetStaticDefaults() {
 			HairID.Sets.DrawBackHair[Type] = true;
 		}
 
+		// See the comment about Visibility for additional information.
 		public override IEnumerable<Condition> GetUnlockConditions() {
 			yield return ExampleConditions.InExampleBiome;
 		}
