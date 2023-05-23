b40606d2fb0f9df895aff4d961b96806dacca8cd
Chicken-Bones <mehvids@gmail.com>
2023-05-23 15:49:19 +1000

    Better Player.GetSource_Buff patch

--- patches/tModLoader/Terraria/Player.TML.Sources.cs
+++ patches/tModLoader/Terraria/Player.TML.Sources.cs
@@ -2,12 +2,8 @@ using Terraria.DataStructures;
 
 namespace Terraria;
 
+#nullable enable
+
 public partial class Player
 {
-	public IEntitySource GetSource_Buff(int buffIndex)
-	{
-		int buffTypeId = buffType[buffIndex];
-
-		return new EntitySource_Buff(this, buffTypeId, buffIndex);
-	}
 }
