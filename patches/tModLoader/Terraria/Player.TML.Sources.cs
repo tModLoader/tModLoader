d43390293de6586c90443a7c719137ed4bf0d212
Chicken-Bones <mehvids@gmail.com>
2023-05-25 16:13:42 +1000

    Better organized and documented entity sources.

--- patches/tModLoader/Terraria/Player.TML.Sources.cs
+++ patches/tModLoader/Terraria/Player.TML.Sources.cs
@@ -6,4 +6,17 @@ namespace Terraria;
 
 public partial class Player
 {
+	// Item Use / Equipment Effects
+
+	public IEntitySource GetSource_Accessory(Item item, string? context = null)
+		=> new EntitySource_ItemUse(this, item, context);
+
+	public IEntitySource GetSource_OpenItem(int itemType, string? context = null)
+		=> new EntitySource_ItemOpen(this, itemType, context);
+
+	public IEntitySource GetSource_ItemUse(Item item, string? context = null)
+		=> new EntitySource_ItemUse(this, item, context);
+
+	public IEntitySource GetSource_ItemUse_WithPotentialAmmo(Item item, int ammoItemId, string? context = null)
+		=> new EntitySource_ItemUse_WithAmmo(this, item, ammoItemId, context);
 }
