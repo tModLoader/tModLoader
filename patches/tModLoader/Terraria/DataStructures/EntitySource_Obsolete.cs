432055fb8fa7a6947c851fd3afe5a8e85dea332c
Chicken-Bones <mehvids@gmail.com>
2023-06-01 14:01:21 +1000

    Add tModPorter and restore some obsolete  types with porting instructions

---
+++ patches/tModLoader/Terraria/DataStructures/EntitySource_Obsolete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Terraria.DataStructures;
+
+[Obsolete("Check Context == \"TorchGod\" instead.", error: true)]
+public class EntitySource_TorchGod { }
+
+[Obsolete("Renamed to EntitySource_Caught, note that Entity has become Catcher, and CaughtEntity has become Entity", error: true)]
+public class EntitySource_CatchEntity { }
+
+[Obsolete("Provides no utility over EntitySource_Parent, use that instead.")]
+public class EntitySource_HitEffect : EntitySource_Parent
+{
+	public EntitySource_HitEffect(Entity entity, string context = null) : base(entity, context) { }
+}
