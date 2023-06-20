da38645a224956c25e388d82d8d1035a8f664313
JavidPack <javidpack@gmail.com>
2023-06-20 15:53:20 -0600

    SteamWorkshopLocalization folder and scripts for updating/uploading tag localization files

---
+++ solutions/SteamWorkshopLocalization/UpdateWorkshopTagFiles.py
@@ -0,0 +1,95 @@
+
+# Run this script after updating en-US\tModLoader.json with new keys. python 3.
+# Also make sure the file encodings are UTF-8 not UTF-8-BOM.
+
+import json
+import json5
+import os.path
+
+tModLoaderLocalizationFilename = '../../src/tModLoader/Terraria/Localization/Content/{0}/tModLoader.json'
+
+# Map steams identifiers to our keys.
+steamToLocalizationKey = {
+  #"language":"english",
+  #"appid":1281930,
+  # TODO: Localize the Categories.
+  #"category_0":"Mod Features",
+  #"category_1":"Mod Side",
+  #"category_2":"tModLoader Version",
+  "tag_197618":"TagsQoL", #"quality of life",
+  "tag_197621":"TagsContent", #"new content",
+  "tag_275098":"TagsLibrary", #"library",
+  "tag_307061":"TagsUtility", #"Utilities",
+  "tag_307062":"TagsGameplay", #"Gameplay Tweaks",
+  "tag_307063":"TagsAudio", #"Audio Tweaks",
+  "tag_307064":"TagsVisual", #"Visual Tweaks",
+  "tag_307066":"TagsGen", #"Custom World Gen",
+  #"tag_307067":"Both",
+  #"tag_307172":"Client",
+  #"tag_307173":"Server",
+  #"tag_307174":"NoSync",
+  #"tag_348888":"1.4.4",
+  #"tag_349568":"1.4.3"
+  # TODO: Add entries for each language once they are in.
+}
+
+# English will be updated directly, if ever needed.
+languages = ['de-DE', 'it-IT', 'fr-FR', 'es-ES', 'ru-RU', 'zh-Hans', 'pt-BR', 'pl-PL']
+steamLanguages = ['german', 'italian', 'french', 'spanish', 'russian', 'schinese', 'brazilian', 'polish']
+# TODO: what to do about latam-spanishlatinamerica, sc_schinese-steamsimplifiedchinese, tchinese-traditionalchinese, portuguese-portugalportuguese?
+# should we copy the results from the similar language? Not sure why there are 3 chinese either, steam website only has 2 options.
+missings = []
+anyFileNeedsUploading = False
+
+# For each language, update the steamtagfile:
+# open the template, open the tmod file
+# for each entry in the mapping, update the json if exists
+# write out steam tag json
+
+for language, steamLanguage in zip(languages, steamLanguages):
+    print("Updating:", language)
+    missing = 0
+
+    with open("workshop_tags_1281930_{0}.json".format(steamLanguage), 'r', encoding='utf-8') as tagFile:
+        steamTagData = json5.load(tagFile)
+    with open(tModLoaderLocalizationFilename.format(language), 'r', encoding='utf-8') as english:
+        tModLoaderJsonData = json5.load(english) # use json5 to load to support comments
+
+    for key, value in steamToLocalizationKey.items():
+        # key is steam
+        if value in tModLoaderJsonData["tModLoader"]:
+            temp = tModLoaderJsonData["tModLoader"][value]
+            steamTagData[key] = temp
+        else:
+            missing += 1
+
+    if missing > 0:
+        missings.append( (language, missing) )
+        print("Missing:", missing)
+
+    outputFilename = "workshop_tags_1281930_{0}_Output.json".format(steamLanguage)
+    outputString = json.dumps(steamTagData, indent=4, ensure_ascii=False) # json.dump instead of json5.dump for quoted keys
+
+    originalString = ""
+    if os.path.isfile(outputFilename):
+        with open(outputFilename, 'r', encoding='utf-8') as tagFileExisting:
+            originalString = tagFileExisting.read()
+
+    if outputString != originalString:
+        print("Updated File:", outputFilename)
+        anyFileNeedsUploading = True
+        
+        with open(outputFilename, "w", encoding='utf-8') as tagFileNew:
+            tagFileNew.write(outputString)
+
+with open('./TranslationsNeeded.txt', 'w', encoding='utf-8') as output:
+    if len(missings) == 0:
+        output.write('All Translations up-to-date!')
+    else:
+        for entry in missings: 
+            output.write(str(entry[0]) + " " + str(entry[1]) + "\n")
+if anyFileNeedsUploading:
+    print("Make sure to upload the Output files to steam. (Any file in the output mentioned as updated file)")
+else:
+    print("No files changed, no need to upload any files.")
+input("Press Enter to continue...")
