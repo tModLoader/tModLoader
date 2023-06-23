f7f17a4716dfce5d29dd9476448394e23959e6f0
Cerbrus <michiel1900@gmail.com>
2023-06-23 15:55:44 +0200

    Add a tall helmet in ExampleMod

---
+++ ExampleMod/Content/Items/Armor/ExampleTallHelmet.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Armor
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Head)]
+	public class ExampleTallHelmet : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// If your head equipment should draw hair while drawn, use one of the following:
+			// ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false; // Don't draw the head at all. Used by Space Creature Mask
+			// ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true; // Draw hair as if a hat was covering the top. Used by Wizards Hat
+			// ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true; // Draw all hair as normal. Used by Mime Mask, Sunglasses
+			// ArmorIDs.Head.Sets.DrawsBackHairWithoutHeadgear[Item.headSlot] = true;
+			ArmorIDs.Head.Sets.IsTallHat[Item.headSlot] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18; // Width of the item
+			Item.height = 24; // Height of the item
+			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.rare = ItemRarityID.Green; // The rarity of the item
+			Item.defense = 5; // The amount of defense the item will give when equipped
+		}
+
+		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ModContent.ItemType<ExampleBreastplate>() && legs.type == ModContent.ItemType<ExampleLeggings>();
+		}
+
+		// UpdateArmorSet allows you to give set bonuses to the armor.
+		public override void UpdateArmorSet(Player player) {
+			player.setBonus = "Increases dealt damage by 20%"; // This is the setbonus tooltip
+			player.GetDamage(DamageClass.Generic) += 0.2f; // Increase dealt damage for all weapon classes by 20%
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+	}
+}
