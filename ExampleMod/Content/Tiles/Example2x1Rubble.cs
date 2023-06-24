5f696a9928d1420584bbd9696ed03bea416db5a2
JavidPack <javidpack@gmail.com>
2023-06-24 15:23:59 -0600

    Rename files to match

--- ExampleMod/Content/Tiles/Example2x1Rubble.cs
+++ ExampleMod/Content/Tiles/Example2x1Rubble.cs
@@ -12,11 +12,11 @@ namespace ExampleMod.Content.Tiles
 	// These tiles go by many names, such as ambient tiles, background tiles, piles, detritus, and rubble. We will use the term rubble because of the recently added Rubblemaker item. 
 	// The Rubblemaker (https://terraria.wiki.gg/wiki/Rubblemaker) is a special item that can place these decorative tiles. The tile placed by the Rubblemaker looks the same as the original rubble tile but behaves slightly differently.
 
-	// Example3x2RubbleBase is an abstract class, it is not an actual tile, but the other 2 classes in this file will reuse the Texture and SetStaticDefaults code shown here because they inherit from it. 
-	public abstract class Example3x2RubbleBase : ModTile
+	// Example2x1RubbleBase is an abstract class, it is not an actual tile, but the other 2 classes in this file will reuse the Texture and SetStaticDefaults code shown here because they inherit from it. 
+	public abstract class Example2x1RubbleBase : ModTile
 	{
 		// We want both tiles to use the same texture
-		public override string Texture => "ExampleMod/Content/Tiles/ExampleChunkLarge";
+		public override string Texture => "ExampleMod/Content/Tiles/Example2x1Rubble";
 
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
@@ -25,8 +25,9 @@ namespace ExampleMod.Content.Tiles
 
 			DustType = DustID.Stone;
 
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.DrawYOffset = 2;
+
 			TileObjectData.addTile(Type);
 
 			AddMapEntry(new Color(152, 171, 198));
@@ -34,14 +35,14 @@ namespace ExampleMod.Content.Tiles
 	}
 
 	// This is the fake tile that will be placed by the Rubblemaker.
-	public class Example3x2RubbleFake : Example3x2RubbleBase
+	public class Example2x1RubbleFake : Example2x1RubbleBase
 	{
 		public override void SetStaticDefaults() {
 			// Call to base SetStaticDefaults. Must inherit static defaults from base type 
 			base.SetStaticDefaults();
 
 			// Add rubble variant, all existing styles, to Rubblemaker, allowing to place this tile by consuming ExampleBlock
-			FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<Items.Placeable.ExampleBlock>(), Type, 0, 1, 2, 3, 4, 5);
+			FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Items.Placeable.ExampleBlock>(), Type, 0, 1, 2, 3, 4, 5);
 
 			// Tiles placed by Rubblemaker drop the item used to place them.
 			RegisterItemDrop(ModContent.ItemType<Items.Placeable.ExampleBlock>());
@@ -49,16 +50,15 @@ namespace ExampleMod.Content.Tiles
 	}
 
 	// This is the natural tile, this version is placed during world generation in the RubbleWorldGen class.
-	public class Example3x2RubbleNatural : Example3x2RubbleBase
+	public class Example2x1RubbleNatural : Example2x1RubbleBase
 	{
 		public override void SetStaticDefaults() {
 			base.SetStaticDefaults();
 
-			// By default, the TileObjectData.Style3x2 tile we copied in Example3x2RubbleBase has LavaDeath = true. Natural rubble tiles don't have this behavior, so we want to be immune to lava.
+			// By default, the TileObjectData.Style2x1 tile we copied in Example2x1RubbleBase has LavaDeath = true. Natural rubble tiles don't have this behavior, so we want to be immune to lava.
 			TileObjectData.GetTileData(Type, 0).LavaDeath = false;
 		}
 
-		// Natural rubble tiles typically drop critters, but no item.
 		public override void DropCritterChance(int i, int j, ref int wormChance, ref int grassHopperChance, ref int jungleGrubChance) {
 			wormChance = 6;
 		}
--- ExampleMod/Content/Tiles/Example3x2Rubble.cs
+++ ExampleMod/Content/Tiles/Example3x2Rubble.cs
@@ -12,12 +12,11 @@ namespace ExampleMod.Content.Tiles
 	// These tiles go by many names, such as ambient tiles, background tiles, piles, detritus, and rubble. We will use the term rubble because of the recently added Rubblemaker item. 
 	// The Rubblemaker (https://terraria.wiki.gg/wiki/Rubblemaker) is a special item that can place these decorative tiles. The tile placed by the Rubblemaker looks the same as the original rubble tile but behaves slightly differently.
 
-	// Example1x1RubbleBase is an abstract class, it is not an actual tile, but the other 2 classes in this file will reuse the Texture and SetStaticDefaults code shown here because they inherit from it. 
-
-	public abstract class Example1x1RubbleBase : ModTile
+	// Example3x2RubbleBase is an abstract class, it is not an actual tile, but the other 2 classes in this file will reuse the Texture and SetStaticDefaults code shown here because they inherit from it. 
+	public abstract class Example3x2RubbleBase : ModTile
 	{
 		// We want both tiles to use the same texture
-		public override string Texture => "ExampleMod/Content/Tiles/ExampleChunkSmall";
+		public override string Texture => "ExampleMod/Content/Tiles/Example3x2Rubble";
 
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
@@ -26,8 +25,7 @@ namespace ExampleMod.Content.Tiles
 
 			DustType = DustID.Stone;
 
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
-			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
 			TileObjectData.newTile.DrawYOffset = 2;
 			TileObjectData.addTile(Type);
 
@@ -36,14 +34,14 @@ namespace ExampleMod.Content.Tiles
 	}
 
 	// This is the fake tile that will be placed by the Rubblemaker.
-	public class Example1x1RubbleFake : Example1x1RubbleBase
+	public class Example3x2RubbleFake : Example3x2RubbleBase
 	{
 		public override void SetStaticDefaults() {
 			// Call to base SetStaticDefaults. Must inherit static defaults from base type 
 			base.SetStaticDefaults();
 
 			// Add rubble variant, all existing styles, to Rubblemaker, allowing to place this tile by consuming ExampleBlock
-			FlexibleTileWand.RubblePlacementSmall.AddVariations(ModContent.ItemType<Items.Placeable.ExampleBlock>(), Type, 0, 1, 2, 3, 4, 5);
+			FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<Items.Placeable.ExampleBlock>(), Type, 0, 1, 2, 3, 4, 5);
 
 			// Tiles placed by Rubblemaker drop the item used to place them.
 			RegisterItemDrop(ModContent.ItemType<Items.Placeable.ExampleBlock>());
@@ -51,17 +49,18 @@ namespace ExampleMod.Content.Tiles
 	}
 
 	// This is the natural tile, this version is placed during world generation in the RubbleWorldGen class.
-	public class Example1x1RubbleNatural : Example1x1RubbleBase
+	public class Example3x2RubbleNatural : Example3x2RubbleBase
 	{
 		public override void SetStaticDefaults() {
 			base.SetStaticDefaults();
 
-			// By default, the TileObjectData.Style1x1 tile we copied in Example1x1RubbleBase has LavaDeath = true. Natural rubble tiles don't have this behavior, so we want to be immune to lava.
+			// By default, the TileObjectData.Style3x2 tile we copied in Example3x2RubbleBase has LavaDeath = true. Natural rubble tiles don't have this behavior, so we want to be immune to lava.
 			TileObjectData.GetTileData(Type, 0).LavaDeath = false;
 		}
 
+		// Natural rubble tiles typically drop critters, but no item.
 		public override void DropCritterChance(int i, int j, ref int wormChance, ref int grassHopperChance, ref int jungleGrubChance) {
-			wormChance = 8;
+			wormChance = 6;
 		}
 	}
 }
