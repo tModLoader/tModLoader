82a94332c795511351f91d3b6526091d1a1337ae
nmoll <59670736+Solxanich@users.noreply.github.com>
2023-07-04 11:03:11 -0600

    Review feedback - cleanup leftover const, Path.GetDirectoryName, relativePath

--- patches/tModLoader/Terraria/Social/Steam/CoreSocialModule.TML.cs
+++ patches/tModLoader/Terraria/Social/Steam/CoreSocialModule.TML.cs
@@ -4,10 +4,6 @@ namespace Terraria.Social.Steam
 {
 	public partial class CoreSocialModule : ISocialModule
 	{
-		public const string WindowsSteamCloudPath = "C:\\Program Files (x86)\\Steam\\userdata";
-		public const string LinuxSteamCloudPath = "~/.local/share/Steam/userdata";
-		public const string MacSteamCloudPath = "~/Library/Application Support/Steam/userdata";
-
 		private static void PortFilesToCurrentStructure() {
 			// Note that if we want Steam to actually acknowledge the changes, we have to use the API calls for IREmoteStorage (the isCloud flag)
 			Program.PortFilesMaster(GetCloudSaveLocation(), isCloud: true);
@@ -19,7 +15,7 @@ namespace Terraria.Social.Steam
 
 			// current steamCloudFolder looks like: C:\\Program Files (x86)\\Steam\\userdata\\SteamID OLD\\1281930\\local
 			// So we have to do some path manipulation to get to C:\\Program Files (x86)\\Steam\\userdata\\SteamID OLD\\\\105600\\local
-			return Path.Combine(Directory.GetParent(Directory.GetParent(steamCloudFolder).ToString()).ToString(), "105600", "remote");
+			return Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(steamCloudFolder)), "105600", "remote");
 		}
 	}
 }
