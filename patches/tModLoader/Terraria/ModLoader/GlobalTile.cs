7096b36e5e316a30ab7743ba769b0ca20c39c0a7
Cerbrus <michiel1900@gmail.com>
2023-07-04 10:19:10 +0200

    Added missing comment

--- patches/tModLoader/Terraria/ModLoader/GlobalTile.cs
+++ patches/tModLoader/Terraria/ModLoader/GlobalTile.cs
@@ -118,6 +118,7 @@ public abstract class GlobalTile : GlobalBlockType
 	/// <param name="i">The x position in tile coordinates.</param>
 	/// <param name="j">The y position in tile coordinates.</param>
 	/// <param name="type">The tile type</param>
+	/// <param name="sightColor">The color this tile should glow with.</param>
 	public virtual bool? IsTileBiomeSightable(int i, int j, int type, ref Color sightColor)
 	{
 		return null;
