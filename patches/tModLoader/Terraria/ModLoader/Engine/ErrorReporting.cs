309b063650d6c2ca75cf3b5e88ac70318de7b160
JavidPack <javidpack@gmail.com>
2023-07-08 19:20:32 -0600

    Fallback to manual OneDriveHint if error happens before language loaded, open FAQ URL for error

--- patches/tModLoader/Terraria/ModLoader/Engine/ErrorReporting.cs
+++ patches/tModLoader/Terraria/ModLoader/Engine/ErrorReporting.cs
@@ -40,23 +40,28 @@ namespace Terraria.ModLoader.Engine
 		}
 
 		public static void FatalExit(string message, Exception e) {
-			if (e.HelpLink != null) {
-				try {
-					Utils.OpenToURL(e.HelpLink);
-				} catch { }
-			}
-
 			string tip = null;
 			if (e is OutOfMemoryException)
 				tip = Language.GetTextValue("tModLoader.OutOfMemoryHint");
 			else if (e is InvalidOperationException || e is NullReferenceException || e is IndexOutOfRangeException || e is ArgumentNullException)
 				tip = Language.GetTextValue("tModLoader.ModExceptionHint");
-			else if (e is IOException && e.Message.Contains("cloud file provider"))
+			else if (e is IOException && e.Message.Contains("cloud file provider")) {
+				e.HelpLink = "https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Usage-FAQ#save-data-file-issues";
 				tip = Language.GetTextValue("tModLoader.OneDriveHint");
+				if (Language.ActiveCulture == null) // This error typically happens before localization is loaded, so fallback to english text.
+					tip = "Tip: Try installing/enabling OneDrive. Right click your Documents folder and enable \"Always save on this device\"";
+			}
 			else if (e is SynchronizationLockException)
 				tip = Language.GetTextValue("tModLoader.AntivirusHint");
 			else if (e is TypeInitializationException)
 				tip = Language.GetTextValue("tModLoader.TypeInitializationHint");
+
+			if (e.HelpLink != null) {
+				try {
+					Utils.OpenToURL(e.HelpLink);
+				}
+				catch { }
+			}
 
 			if (tip != null)
 				message += "\n\n" + tip;
