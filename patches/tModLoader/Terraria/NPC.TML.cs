272d7ab5881450b2e8ccfac280a9ebc6e7fc09e9
Kogsey <KadenKogsey@gmail.com>
2023-07-20 00:06:13 +0800

    Minor cleanup

--- patches/tModLoader/Terraria/NPC.TML.cs
+++ patches/tModLoader/Terraria/NPC.TML.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using Terraria.DataStructures;
 using Terraria.GameContent.UI.BigProgressBar;
 using Terraria.ID;
