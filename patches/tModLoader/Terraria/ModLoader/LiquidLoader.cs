e500b89868f77158079d7f9e2b2531eaf70e5718
mysteroman <mysteroman.minecraft@gmail.com>
2023-08-11 17:29:18 -0400

    modliquid start

---
+++ patches/tModLoader/Terraria/ModLoader/LiquidLoader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.Liquid;
+using Terraria.ID;
+
+namespace Terraria.ModLoader;
+
+public static class LiquidLoader
+{
+	private static int nextLiquid = LiquidID.Count;
+	private static bool loaded = false;
+
+	internal static readonly IList<ModLiquid> liquids = new List<ModLiquid>();
+
+	public static int LiquidCount => nextLiquid;
+
+	public static ModLiquid GetLiquid(int type)
+	{
+		return type >= LiquidID.Count && type < LiquidCount ? liquids[type - LiquidID.Count] : null;
+	}
+
+	internal static int ReserveLiquidID()
+	{
+		return nextLiquid++;
+	}
+
+	internal static void ResizeArrays(bool unloading = false)
+	{
+		Array.Resize(ref LiquidRenderer.WATERFALL_LENGTH, nextLiquid);
+		Array.Resize(ref LiquidRenderer.DEFAULT_OPACITY, nextLiquid);
+		Array.Resize(ref LiquidRenderer.WAVE_MASK_STRENGTH, nextLiquid + 1);
+		Array.Resize(ref LiquidRenderer.VISCOSITY_MASK, nextLiquid + 1);
+
+		if (!unloading) {
+			loaded = true;
+		}
+	}
+
+	internal static void Unload()
+	{
+		loaded = false;
+		nextLiquid = LiquidID.Count;
+
+		liquids.Clear();
+	}
+}
---
+++ patches/tModLoader/Terraria/ModLoader/ModLiquid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terraria.ModLoader;
+
+public abstract class ModLiquid : ModBlockType
+{
+	private int waterfallLength = 10;
+	private float defaultOpacity = 0.6f;
+
+	public int WaterfallLength {
+		get => waterfallLength;
+		set {
+			if (value < 0)
+				throw new ArgumentOutOfRangeException(nameof(value), "Waterfall Length must not be inferior to 0.");
+			waterfallLength = value;
+		}
+	}
+
+	public float DefaultOpacity {
+		get => defaultOpacity;
+		set {
+			if (value < 0 || value > 1)
+				throw new ArgumentOutOfRangeException(nameof(value), "Default opacity must be between 0 and 1.");
+			defaultOpacity = value;
+		}
+	}
+
+	public byte WaveMaskStrength { get; set; } = 0;
+
+	public byte ViscosityMask { get; set; } = 0;
+
+	public override string LocalizationCategory => "Liquids";
+
+	protected sealed override void Register()
+	{
+		ModTypeLookup<ModLiquid>.Register(this);
+		Type = (ushort)LiquidLoader.ReserveLiquidID();
+		LiquidLoader.liquids.Add(this);
+	}
+}
