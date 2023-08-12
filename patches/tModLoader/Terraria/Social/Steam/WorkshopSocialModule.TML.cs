2dc48fffd5e5ba93d8e7996a7e665d92463e1939
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2023-08-12 12:01:44 +1200

    Prevent publishing dev-built mods on stable (#3711)
    * Fix
    
    * Review changes

--- patches/tModLoader/Terraria/Social/Steam/WorkshopSocialModule.TML.cs
+++ patches/tModLoader/Terraria/Social/Steam/WorkshopSocialModule.TML.cs
@@ -2,10 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
+using System.Net;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Core;
-using Terraria.ModLoader.UI.ModBrowser;
 using Terraria.Social.Base;
 using Terraria.Utilities;
 
@@ -40,7 +40,12 @@ public partial class WorkshopSocialModule
 	public override bool PublishMod(TmodFile modFile, NameValueCollection buildData, WorkshopItemPublishSettings settings)
 	{
 		if (!SteamedWraps.SteamClient) {
-			base.IssueReporter.ReportInstantUploadProblem("tModLoader.SteamPublishingLimit");
+			IssueReporter.ReportInstantUploadProblem("tModLoader.SteamPublishingLimit");
+			return false;
+		}
+		
+		if (modFile.TModLoaderVersion.MajorMinor() != BuildInfo.tMLVersion.MajorMinor()) {
+			IssueReporter.ReportInstantUploadProblem("tModLoader.WrongVersionCantPublishError");
 			return false;
 		}
 
