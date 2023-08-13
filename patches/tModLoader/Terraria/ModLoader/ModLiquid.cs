5b419df5765795ec2fd556723a40be5890149d76
mysteroman <mysteroman.minecraft@gmail.com>
2023-08-13 13:49:24 -0400

    updated ModLiquid & LiquidID
    They are now more in line with what ModTile & TileID are like.

--- patches/tModLoader/Terraria/ModLoader/ModLiquid.cs
+++ patches/tModLoader/Terraria/ModLoader/ModLiquid.cs
@@ -1,31 +1,38 @@
-﻿namespace Terraria.ModLoader;
+﻿using System;
+using Terraria.GameContent.Liquid;
+using Terraria.ID;
+
+namespace Terraria.ModLoader;
 
 public abstract class ModLiquid : ModBlockType
 {
-	private int waterfallLength = 10;
-	private float defaultOpacity = 0.6f;
-
 	public int WaterfallLength {
-		get => waterfallLength;
+		get => LiquidRenderer.WATERFALL_LENGTH[Type];
 		set {
 			if (value < 0)
 				throw new ArgumentOutOfRangeException(nameof(value), "Waterfall Length must not be inferior to 0.");
-			waterfallLength = value;
+			LiquidRenderer.WATERFALL_LENGTH[Type] = value;
 		}
 	}
 
 	public float DefaultOpacity {
-		get => defaultOpacity;
+		get => LiquidRenderer.DEFAULT_OPACITY[Type];
 		set {
 			if (value < 0 || value > 1)
 				throw new ArgumentOutOfRangeException(nameof(value), "Default opacity must be between 0 and 1.");
-			defaultOpacity = value;
+			LiquidRenderer.DEFAULT_OPACITY[Type] = value;
 		}
 	}
 
-	public byte WaveMaskStrength { get; set; } = 0;
+	public byte WaveMaskStrength {
+		get => LiquidRenderer.WAVE_MASK_STRENGTH[Type + 1];
+		set => LiquidRenderer.WAVE_MASK_STRENGTH[Type + 1] = value;
+	}
 	
-	public byte ViscosityMask { get; set; } = 0;
+	public byte ViscosityMask {
+		get => LiquidRenderer.VISCOSITY_MASK[Type + 1];
+		set => LiquidRenderer.VISCOSITY_MASK[Type + 1] = value;
+	}
 
 	public override string LocalizationCategory => "Liquids";
 
@@ -34,5 +41,18 @@ public abstract class ModLiquid : ModBlockType
 		ModTypeLookup<ModLiquid>.Register(this);
 		Type = (ushort)LiquidLoader.ReserveLiquidID();
 		LiquidLoader.liquids.Add(this);
+	}
+
+	public sealed override void SetupContent()
+	{
+		SetStaticDefaults();
+
+		LiquidID.Search.Add(FullName, Type);
+	}
+
+	public override void SetStaticDefaults()
+	{
+		WaterfallLength = 10;
+		DefaultOpacity = 0.6f;
 	}
 }
