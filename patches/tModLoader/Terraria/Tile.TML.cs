f0e52d8849d01482240cfdf6c36607b2147a1f98
Destructor_Ben <ben.honeyfield@gmail.com>
2023-08-15 19:59:23 +1200

    Change 'cell' to 'position'

--- patches/tModLoader/Terraria/Tile.TML.cs
+++ patches/tModLoader/Terraria/Tile.TML.cs
@@ -7,7 +7,7 @@ using Terraria.ID;
 namespace Terraria;
 
 /// <summary>
-/// A data structure used for accessing information about a single cell in the world.<br/>
+/// A data structure used for accessing information about tiles, walls, wires, and liquids at a single position in the world.<br/>
 /// Vanilla tile code and a mods tile code will be quite different, since tModLoader reworked how tiles function to improve performance. This means that copying vanilla code will leave you with many errors.<br/>
 /// For your sanity, all of the changes are well documented to make it easier to port vanilla code.
 /// </summary>
@@ -25,27 +25,27 @@ public readonly partial struct Tile
 	// General state
 
 	/// <summary>
-	/// The <see cref="TileID"/> of the cell.<br/>
+	/// The <see cref="TileID"/> of the tile at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="type"/>.
 	/// </summary>
 	public ref ushort TileType => ref Get<TileTypeData>().Type;
 	/// <summary>
-	/// The <see cref="WallID"/> of the cell.<br/>
+	/// The <see cref="WallID"/> of the wall at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wall"/>.
 	/// </summary>
 	public ref ushort WallType => ref Get<WallTypeData>().Type;
 
 	/// <summary>
-	/// Whether a cell contains a tile. Check this whenever you are accessing data from a tile to avoid getting data from an empty tile.<br/>
+	/// Whether there is a tile at this position. Check this whenever you are accessing data from a tile to avoid getting data from an empty tile.<br/>
 	/// Legacy/vanilla equivalent is <see cref="active()"/> or <see cref="active(bool)"/>.
 	/// </summary>
 	/// <remarks>
 	/// Actuated tiles are not solid, so use <see cref="HasUnactuatedTile"/> instead of <see cref="HasTile"/> for collision checks.<br/>
-	/// This only corresponds to a tile in a cell, which means a wall can exist without a tile. To check if a wall exists, use <c>tile.WallType != WallID.None</c>.
+	/// This only corresponds to whether a tile exists, however, a wall can exist without a tile. To check if a wall exists, use <c>tile.WallType != WallID.None</c>.
 	/// </remarks>
 	public bool HasTile { get => Get<TileWallWireStateData>().HasTile; set => Get<TileWallWireStateData>().HasTile = value; }
 	/// <summary>
-	/// Whether a cell is actuated by an actuator.<br/>
+	/// Whether the tile at this position is actuated by an actuator.<br/>
 	/// Legacy/vanilla equivalent is <see cref="inActive()"/> or <see cref="inActive(bool)"/>.
 	/// </summary>
 	/// <remarks>
@@ -53,12 +53,12 @@ public readonly partial struct Tile
 	/// </remarks>
 	public bool IsActuated { get => Get<TileWallWireStateData>().IsActuated; set => Get<TileWallWireStateData>().IsActuated = value; }
 	/// <summary>
-	/// Whether a cell contains an actuator.<br/>
+	/// Whether there is an actuator at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="actuator()"/> or <see cref="actuator(bool)"/>.
 	/// </summary>
 	public bool HasActuator { get => Get<TileWallWireStateData>().HasActuator; set => Get<TileWallWireStateData>().HasActuator = value; }
 	/// <summary>
-	/// Whether a cell contains a tile that isn't actuated.<br/>
+	/// Whether there is a tile at this position that isn't actuated.<br/>
 	/// Legacy/vanilla equivalent is <see cref="nactive"/>.
 	/// </summary>
 	/// <remarks>
@@ -111,33 +111,33 @@ public readonly partial struct Tile
 	// Framing
 
 	/// <summary>
-	/// The X coordinate of the top left of this cell's tile in it's spritesheet.<br/>
+	/// The X coordinate of the top left of the tile at this position in it's spritesheet.<br/>
 	/// Legacy/vanilla equivalent is <see cref="frameX"/>.
 	/// </summary>
 	public ref short TileFrameX => ref Get<TileWallWireStateData>().TileFrameX;
 	/// <summary>
-	/// The Y coordinate of the top left of this cell's tile in it's spritesheet.<br/>
+	/// The Y coordinate of the top left of the tile at this position in it's spritesheet.<br/>
 	/// Legacy/vanilla equivalent is <see cref="frameY"/>.
 	/// </summary>
 	public ref short TileFrameY => ref Get<TileWallWireStateData>().TileFrameY;
 
 	/// <summary>
-	/// The X coordinate of the top left of this cell's wall in it's spritesheet.<br/>
+	/// The X coordinate of the top left of the wall at this position in it's spritesheet.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wallFrameX()"/> or <see cref="wallFrameX(int)"/>.
 	/// </summary>
 	public int WallFrameX { get => Get<TileWallWireStateData>().WallFrameX; set => Get<TileWallWireStateData>().WallFrameX = value; }
 	/// <summary>
-	/// The Y coordinate of the top left of this cell's wall in it's spritesheet.<br/>
+	/// The Y coordinate of the top left of the wall at this position in it's spritesheet.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wallFrameY()"/> or <see cref="wallFrameY(int)"/>.
 	/// </summary>
 	public int WallFrameY { get => Get<TileWallWireStateData>().WallFrameY; set => Get<TileWallWireStateData>().WallFrameY = value; }
 	/// <summary>
-	/// The random style number this cell's tile has, which is random number between 0 and 2 (inclusive).<br/>
+	/// The random style number the tile at this position has, which is random number between 0 and 2 (inclusive).<br/>
 	/// Legacy/vanilla equivalent is <see cref="frameNumber()"/> or <see cref="frameNumber(byte)"/>.
 	/// </summary>
 	public int TileFrameNumber { get => Get<TileWallWireStateData>().TileFrameNumber; set => Get<TileWallWireStateData>().TileFrameNumber = value; }
 	/// <summary>
-	/// The random style number this cell's wall has, which is a random number between 0 and 2 (inclusive).<br/>
+	/// The random style number the wall at this position has, which is a random number between 0 and 2 (inclusive).<br/>
 	/// Legacy/vanilla equivalent is <see cref="wallFrameNumber()"/> or <see cref="wallFrameNumber(byte)"/>.
 	/// </summary>
 	public int WallFrameNumber { get => Get<TileWallWireStateData>().WallFrameNumber; set => Get<TileWallWireStateData>().WallFrameNumber = value; }
@@ -145,12 +145,12 @@ public readonly partial struct Tile
 	// Color
 
 	/// <summary>
-	/// The <see cref="PaintID"/> this tile is painted with. Is <see cref="PaintID.None"/> if not painted.<br/>
+	/// The <see cref="PaintID"/> the tile at this position is painted with. Is <see cref="PaintID.None"/> if not painted.<br/>
 	/// Legacy/vanilla equivalent is <see cref="color()"/> or <see cref="color(byte)"/>.
 	/// </summary>
 	public byte TileColor { get => Get<TileWallWireStateData>().TileColor; set => Get<TileWallWireStateData>().TileColor = value; }
 	/// <summary>
-	/// The <see cref="PaintID"/> this wall is painted with. Is <see cref="PaintID.None"/> if not painted.<br/>
+	/// The <see cref="PaintID"/> the wall at this position is painted with. Is <see cref="PaintID.None"/> if not painted.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wallColor()"/> or <see cref="wallColor(byte)"/>.
 	/// </summary>
 	public byte WallColor { get => Get<TileWallWireStateData>().WallColor; set => Get<TileWallWireStateData>().WallColor = value; }
@@ -158,23 +158,23 @@ public readonly partial struct Tile
 	// Liquids
 
 	/// <summary>
-	/// The amount of liquid in the cell.<br/>
+	/// The amount of liquid at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="liquid"/>.
 	/// </summary>
 	public ref byte LiquidAmount => ref Get<LiquidData>().Amount;
 
 	/// <summary>
-	/// The <see cref="LiquidID"/> of the liquid in the cell.<br/>
+	/// The <see cref="LiquidID"/> of the liquid at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="liquidType()"/> or <see cref="liquidType(int)"/>.
 	/// </summary>
 	public int LiquidType { get => Get<LiquidData>().LiquidType; set => Get<LiquidData>().LiquidType = value; }
 	/// <summary>
-	/// Whether the liquid in this cell should skip updating for 1 tick.<br/>
+	/// Whether the liquid at this position should skip updating for 1 tick.<br/>
 	/// Legacy/vanilla equivalent is <see cref="skipLiquid()"/> or <see cref="skipLiquid(bool)"/>.
 	/// </summary>
 	public bool SkipLiquid { get => Get<LiquidData>().SkipLiquid; set => Get<LiquidData>().SkipLiquid = value; }
 	/// <summary>
-	/// Whether this cell contains liquid.<br/>
+	/// Whether there is liquid at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="checkingLiquid()"/> or <see cref="checkingLiquid(bool)"/>.
 	/// </summary>
 	public bool CheckingLiquid { get => Get<LiquidData>().CheckingLiquid; set => Get<LiquidData>().CheckingLiquid = value; }
@@ -182,22 +182,22 @@ public readonly partial struct Tile
 	// Wires
 
 	/// <summary>
-	/// Whether a cell contains red wire.<br/>
+	/// Whether there is red wire at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wire()"/> or <see cref="wire(bool)"/>.
 	/// </summary>
 	public bool RedWire { get => Get<TileWallWireStateData>().RedWire; set => Get<TileWallWireStateData>().RedWire = value; }
 	/// <summary>
-	/// Whether a cell contains green wire.<br/>
+	/// Whether there is green wire at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wire3()"/> or <see cref="wire3(bool)"/>.
 	/// </summary>
 	public bool GreenWire { get => Get<TileWallWireStateData>().GreenWire; set => Get<TileWallWireStateData>().GreenWire = value; }
 	/// <summary>
-	/// Whether a cell contains blue wire.<br/>
+	/// Whether there is blue wire at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wire2()"/> or <see cref="wire2(bool)"/>.
 	/// </summary>
 	public bool BlueWire { get => Get<TileWallWireStateData>().BlueWire; set => Get<TileWallWireStateData>().BlueWire = value; }
 	/// <summary>
-	/// Whether a cell contains yellow wire.<br/>
+	/// Whether there is yellow wire at this position.<br/>
 	/// Legacy/vanilla equivalent is <see cref="wire4()"/> or <see cref="wire4(bool)"/>.
 	/// </summary>
 	public bool YellowWire { get => Get<TileWallWireStateData>().YellowWire; set => Get<TileWallWireStateData>().YellowWire = value; }
@@ -205,7 +205,7 @@ public readonly partial struct Tile
 	// Invisibility
 
 	/// <summary>
-	/// Whether a tile is invisible. Used by <see cref="ItemID.EchoCoating"/>.<br/>
+	/// Whether the tile at this position is invisible. Used by <see cref="ItemID.EchoCoating"/>.<br/>
 	/// Legacy/vanilla equivalent is <see cref="invisibleBlock()"/> or <see cref="invisibleBlock(bool)"/>.
 	/// </summary>
 	public bool IsTileInvisible {
@@ -213,7 +213,7 @@ public readonly partial struct Tile
 		set => Get<TileWallBrightnessInvisibilityData>().IsTileInvisible = value;
 	}
 	/// <summary>
-	/// Whether a wall is invisible. Used by <see cref="ItemID.EchoCoating"/>.<br/>
+	/// Whether the wall at this position is invisible. Used by <see cref="ItemID.EchoCoating"/>.<br/>
 	/// Legacy/vanilla equivalent is <see cref="invisibleWall()"/> or <see cref="invisibleWall(bool)"/>.
 	/// </summary>
 	public bool IsWallInvisible {
@@ -224,7 +224,7 @@ public readonly partial struct Tile
 	// Fullbright
 
 	/// <summary>
-	/// Whether a tile is fully illuminated. Used by <see cref="ItemID.GlowPaint"/>.<br/>
+	/// Whether the tile at this position is fully illuminated. Used by <see cref="ItemID.GlowPaint"/>.<br/>
 	/// Legacy/vanilla equivalent is <see cref="fullbrightBlock()"/> or <see cref="fullbrightBlock(bool)"/>.
 	/// </summary>
 	public bool IsTileFullbright {
@@ -232,7 +232,7 @@ public readonly partial struct Tile
 		set => Get<TileWallBrightnessInvisibilityData>().IsTileFullbright = value;
 	}
 	/// <summary>
-	/// Whether a wall is fully illuminated. Used by <see cref="ItemID.GlowPaint"/>.<br/>
+	/// Whether the wall at this position is fully illuminated. Used by <see cref="ItemID.GlowPaint"/>.<br/>
 	/// Legacy/vanilla equivalent is <see cref="fullbrightWall()"/> or <see cref="fullbrightWall(bool)"/>.
 	/// </summary>
 	public bool IsWallFullbright {
@@ -257,10 +257,10 @@ public readonly partial struct Tile
 #endif
 
 	/// <summary>
-	/// Used to get a reference to a cell's <see cref="ITileData"/> .
+	/// Used to get a reference to the <see cref="ITileData"/> at this position.
 	/// </summary>
 	/// <typeparam name="T">The <see cref="ITileData"/> to get.</typeparam>
-	/// <returns>The <see cref="ITileData"/> of type <typeparamref name="T"/> that this cell contains.</returns>
+	/// <returns>The <see cref="ITileData"/> of type <typeparamref name="T"/> at this position.</returns>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public unsafe ref T Get<T>() where T : unmanaged, ITileData
 		=> ref TileData<T>.ptr[TileId];
