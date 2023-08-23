1ff43edc308a6357b580ff3341538495fee81e40
Kogsey <kad4419@gmail.com>
2023-08-23 16:15:36 +0800

    Updated ExampleCritter stuff
    NPC changed to be a frog because "Lava Snail" is too similar to "Magma Snail" added in 1.4, lava snail sprites left

---
+++ ExampleMod/Content/NPCs/ExampleCritter.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Xna.Framework;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.NPCs;
+
+/// <summary>
+/// This file shows off a critter npc. The unique thing about critters is how you can catch them with a bug net.
+/// The important bits are: Main.npcCatchable, npc.catchItem, and item.makeNPC
+/// We will also show off adding an item to an existing RecipeGroup (see ExampleMod.AddRecipeGroups)
+/// </summary>
+internal class ExampleCritterNPC : ModNPC
+{
+	private const int ClonedNPCID = NPCID.Frog;
+
+	public override bool IsLoadingEnabled(Mod mod) {
+		IL_Wiring.HitWireSingle += HookFrogStatue;
+		return true;
+	}
+
+	/// <summary>
+	/// Change the following code sequence in Wiring.HitWireSingle
+	/// <code>
+	///		case 61:
+	///			num115 = 361;
+	/// </code>
+	/// to
+	/// <code>
+	///		case 61:
+	///			num115 = Utils.SelectRandom(Main.rand, new int[2] {
+	///				361,
+	///				(our npc type)
+	///			});
+	/// </code>
+	/// </summary>
+	/// <param name="ilContext"></param>
+	private void HookFrogStatue(ILContext ilContext) {
+		try {
+			// obtain a cursor positioned before the first instruction of the method
+			// the cursor is used for navigating and modifying the il
+			ILCursor ilCursor = new ILCursor(ilContext);
+
+			// the exact location for this hook is very complex to search for due to the hook instructions not being unique, and buried deep in control flow
+			// switch statements are sometimes compiled to if-else chains, and debug builds litter the code with no-ops and redundant locals
+
+			// in general you want to search using structure and function rather than numerical constants which may change across different versions or compile settings
+			// using local variable indices is almost always a bad idea
+
+			// we can search for
+			// switch (*)
+			//   case 61:
+
+			// in general you'd want to look for a specific switch variable, or perhaps the containing switch (type) { case 105:
+			// but the generated IL is really variable and hard to match in this case
+
+			// we'll just use the fact that there are no other switch statements with case 61
+
+			ILLabel[] targets = null;
+			while (ilCursor.TryGotoNext(i => i.MatchSwitch(out targets))) {
+				// some optimising compilers generate a sub so that all the switch cases start at 0
+				// ldc.i4.s 51
+				// sub
+				// switch
+				int offset = 0;
+				if (ilCursor.Prev.MatchSub() && ilCursor.Prev.Previous.MatchLdcI4(out offset)) {
+					;
+				}
+
+				// get the label for case 61: if it exists
+				int case61Index = 61 - offset;
+				if (case61Index < 0 || case61Index >= targets.Length || targets[case61Index] is not ILLabel target) {
+					continue;
+				}
+
+				// move the cursor to case 61:
+				ilCursor.GotoLabel(target);
+				ilCursor.Index++;
+				// there's lots of extra checks we could add here to make sure we're at the right spot, such as not encountering any branching instructions
+				//ilCursor.GotoNext(i => i.MatchCall(typeof(Utils), nameof(Utils.SelectRandom)));
+
+				// goto next positions us before the instruction we searched for, so we can insert our array modifying code right here
+				ilCursor.EmitDelegate((int originalAssign) => Main.rand.NextBool() ? originalAssign : NPC.type);
+
+				// hook applied successfully
+				return;
+			}
+
+			// couldn't find the right place to insert
+			throw new Exception("Hook location not found, switch(*) { case 61: ...");
+		}
+		catch {
+			MonoModHooks.DumpIL(ModContent.GetInstance<ExampleMod>(), ilContext);
+			throw;
+		}
+	}
+
+	public override void SetDefaults() {
+		NPC.CloneDefaults(ClonedNPCID);
+		NPC.catchItem = ItemType<ExampleCritterItem>();
+		NPC.lavaImmune = true;
+		NPC.friendly = true;
+		AIType = ClonedNPCID;
+		NPC.aiStyle = NPCAIStyleID.Passive;
+		AnimationType = ClonedNPCID;
+	}
+
+	public override void SetStaticDefaults() {
+		Main.npcFrameCount[Type] = Main.npcFrameCount[ClonedNPCID];
+		NPCID.Sets.CountsAsCritter[NPC.type] = true;
+		NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[NPC.type] = true;
+		NPCID.Sets.TownCritter[NPC.type] = true;
+		NPC.buffImmune[BuffID.Confused] = true;
+	}
+
+	public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+		return SpawnCondition.Underworld.Chance * 0.1f;
+	}
+
+	public override void HitEffect(NPC.HitInfo hit) {
+		if (NPC.life <= 0) {
+			for (int i = 0; i < 6; i++) {
+				Dust dust = Dust.NewDustDirect(NPC.position, NPC.width, NPC.height, DustID.Worm, 2 * hit.HitDirection, -2f);
+				if (Main.rand.NextBool(2)) {
+					dust.noGravity = true;
+					dust.scale = 1.2f * NPC.scale;
+				}
+				else {
+					dust.scale = 0.7f * NPC.scale;
+				}
+			}
+			//Gore.NewGore(NPC.position, NPC.velocity, Mod.GetGoreSlot("Gores/LavaSnailHead"), NPC.scale);
+			//Gore.NewGore(NPC.position, NPC.velocity, Mod.GetGoreSlot("Gores/LavaSnailShell"), NPC.scale);
+		}
+	}
+
+	public override Color? GetAlpha(Color drawColor) {
+		// GetAlpha gives our Lava Snail a red glow.
+		// both these do the same in this situation, using these methods is useful.
+		return drawColor with {
+			R = 255,
+			G = Utils.Clamp<byte>(drawColor.G, 175, 255),
+			B = Math.Min(drawColor.B, (byte)75),
+			A = 255
+		};
+	}
+
+	public override bool PreAI() {//TODO: Make the frog swim in lava instead of water
+								  // Usually we can use npc.wet, but aiStyle 67 prevents wet from being set.
+		if (Collision.WetCollision(NPC.position, NPC.width, NPC.height)) //if (npc.wet)
+		{
+			// These 3 lines instantly kill the npc without showing damage numbers, dropping loot, or playing DeathSound. Use this for instant deaths
+			NPC.life = 0;
+			NPC.HitEffect();
+			NPC.active = false;
+			SoundEngine.PlaySound(SoundID.NPCDeath16, NPC.position); // plays a fizzle sound
+		}
+		return base.PreAI();
+	}
+
+	public override void OnCaughtBy(Player player, Item item, bool failed) {
+		//try {
+		Point npcTile = NPC.Center.ToTileCoordinates();
+
+		if (!WorldGen.SolidTile(npcTile.X, npcTile.Y)) { // Check if the tile the npc resides the most in is non solid
+			Main.tile[npcTile].LiquidAmount = Main.tile[npcTile].Get<LiquidData>().LiquidType == LiquidID.Lava ? // Check if the tile has lava in it
+				Math.Max((byte)Main.rand.Next(50, 150), Main.tile[npcTile].LiquidAmount) // If it does, then top up the amount
+				: (byte)Main.rand.Next(50, 150); // If it doesn't, then overwrite the amount
+			Main.tile[npcTile].Get<LiquidData>().LiquidType = LiquidID.Lava; // Set the liquid type to lava
+			WorldGen.SquareTileFrame(npcTile.X, npcTile.Y, true); // Update the surrounding area in the tilemap
+		}
+		//}
+		//catch {
+		//	return;
+		//} Is this still crash code in 1.4.4?
+	}
+}
+
+internal class ExampleCritterItem : ModItem
+{
+	private const int ClonedItemID = ItemID.Frog;
+
+	public override void SetDefaults() {
+		//useStyle = 1;
+		//autoReuse = true;
+		//useTurn = true;
+		//useAnimation = 15;
+		//useTime = 10;
+		//maxStack = CommonMaxStack;
+		//consumable = true;
+		//width = 12;
+		//height = 12;
+		//makeNPC = 361;
+		//noUseGraphic = true;
+
+		// Cloning ItemID.Frog sets the preceeding values
+		Item.CloneDefaults(ClonedItemID);
+		Item.makeNPC = NPCType<ExampleCritterNPC>();
+		Item.value += Item.buyPrice(0, 0, 30, 0); // Make this critter worth slightly more than the frog
+		Item.rare = ItemRarityID.Blue;
+	}
+}
---
+++ ExampleMod/Content/Tiles/ExampleCritterCage.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExampleMod.Tiles;
+
+public class ExampleCritterCage : ModTile
+{
+	public override void SetStaticDefaults() {
+		//Here we just copy a bunch of values from the frog cage
+		TileID.Sets.CritterCageLidStyle[Type] = TileID.Sets.CritterCageLidStyle[TileID.FrogCage]; // This is how vanilla draws the roof of the cage
+		Main.tileFrameImportant[Type] = Main.tileFrameImportant[TileID.FrogCage];
+		Main.tileLavaDeath[Type] = Main.tileLavaDeath[TileID.FrogCage];
+		Main.tileSolidTop[Type] = Main.tileSolidTop[TileID.FrogCage];
+		Main.tileTable[Type] = Main.tileTable[TileID.FrogCage];
+		AdjTiles = new int[] { TileID.FrogCage, TileID.GoldFrogCage }; // Just in case another mod uses the frog cage to craft
+
+		// These two lines are functionally identical, here we use the first to keep the style of copy from the frog, use the second if you're making an entirely new tile
+		TileObjectData.newTile.CopyFrom(TileObjectData.GetTileData(TileID.FrogCage, 0));
+		//TileObjectData.newTile.CopyFrom(TileObjectData.StyleSmallCage);
+
+		TileObjectData.addTile(Type);
+		AddMapEntry(new Color(122, 217, 232), CreateMapEntryName());
+	}
+
+	private static int tileCageFrameHolder;
+
+	public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
+		offsetY = 2; //From vanilla
+		Main.critterCage = true; // Vanilla doesn't run the animation code for critters unless this is checked
+		tileCageFrameHolder = TileDrawing.GetSmallAnimalCageFrame(i, j, tileFrameX, tileFrameY);
+	}
+
+	public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset) {
+		frameYOffset = Main.frogCageFrame[tileCageFrameHolder] * 36;
+	}
+
+	// The below code should still work, but the above code is less verbose and a little friendlier due to using vanilla code 
+	//public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset) {
+	//	// This code utilizes some math to stagger each individual tile. First the top left tile is found, then those coordinates are passed into some math to stagger an index into Main.snail2CageFrame
+	//	// Main.snail2CageFrame is used since we want the same animation, but if we wanted a different frame count or a different animation timing, we could write our own by adapting vanilla code and placing the code in AnimateTile
+	//	Tile tile = Main.tile[i, j];
+	//	Main.critterCage = true;
+	//	int left = i - frameXOffset / 18;
+	//	int top = j - frameYOffset / 18;
+	//	int offset = left / 3 * (top / 3);
+	//	offset %= Main.cageFrames;
+	//	frameYOffset = Main.snail2CageFrame[offset] * AnimationFrameHeight;
+	//}
+}
+
+public class ExampleCritterCageItem : ModItem
+{
+	public override void SetDefaults() {
+		Item.CloneDefaults(TileID.FrogCage);
+		Item.createTile = TileType<ExampleCritterCage>();
+	}
+
+	public override void AddRecipes() {
+		CreateRecipe()
+			.AddIngredient(ItemID.Terrarium)
+			.AddIngredient(ItemType<NPCs.ExampleCritterItem>())
+			.Register();
+	}
+}
