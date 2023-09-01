6fbafaa62fcf92abbae85bfa233123a135b7dc93
VaemaVoid <78240180+Vaema@users.noreply.github.com>
2023-09-01 17:53:01 -0400

    Ported the SparklingBall projectile to 1.4.4.
    Made for the ExampleStaff port.

--- ExampleMod/Content/Projectiles/SparklingBall.cs
+++ ExampleMod/Content/Projectiles/SparklingBall.cs
@@ -1,15 +1,17 @@
-using ExampleMod.Content.Dusts;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
+using ExampleMod.Content.Dusts;
+using static Terraria.ModLoader.ModContent;
 
 namespace ExampleMod.Content.Projectiles
 {
 	public class SparklingBall : ModProjectile
 	{
-		public override void SetDefaults() {
+		public override void SetDefaults()
+		{
 			Projectile.width = 16;
 			Projectile.height = 16;
 			Projectile.friendly = true;
@@ -18,42 +20,44 @@ namespace ExampleMod.Content.Projectiles
 			Projectile.timeLeft = 600;
 		}
 
-		public override void AI() {
+		public override void AI()
+		{
 			Projectile.velocity.Y += Projectile.ai[0];
-			if (Main.rand.NextBool(3)) {
-				Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
-			}
+			if (Main.rand.NextBool(3))
+				Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustType<Sparkle>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
 		}
 
-		public override bool OnTileCollide(Vector2 oldVelocity) {
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
 			Projectile.penetrate--;
-			if (Projectile.penetrate <= 0) {
+			if (Projectile.penetrate <= 0)
 				Projectile.Kill();
-			}
-			else {
-				Projectile.ai[0] += 0.1f;
-				if (Projectile.velocity.X != oldVelocity.X) {
-					Projectile.velocity.X = -oldVelocity.X;
-				}
-				if (Projectile.velocity.Y != oldVelocity.Y) {
-					Projectile.velocity.Y = -oldVelocity.Y;
-				}
-				Projectile.velocity *= 0.75f;
+			else
+			{
 				SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+				Projectile.ai[0] += 0.1f;
+
+				Projectile.velocity *= 0.75f;
+				if (Projectile.velocity.X != oldVelocity.X)
+					Projectile.velocity.X = -oldVelocity.X;
+				if (Projectile.velocity.Y != oldVelocity.Y)
+					Projectile.velocity.Y = -oldVelocity.Y;
 			}
 			return false;
 		}
 
-		public override void OnKill(int timeLeft) {
-			for (int k = 0; k < 5; k++) {
-				Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
-			}
+		public override void Kill(int timeLeft)
+		{
+			for (int k = 0; k < 5; k++)
+				Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustType<Sparkle>(), Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
+
 			SoundEngine.PlaySound(SoundID.Item25, Projectile.position);
 		}
 
-		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
 			Projectile.ai[0] += 0.1f;
 			Projectile.velocity *= 0.75f;
 		}
-	}
+    }
 }
