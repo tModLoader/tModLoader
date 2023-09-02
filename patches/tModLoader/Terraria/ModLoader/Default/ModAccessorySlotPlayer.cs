4ead0ba5da36370f4b418ab959f819d9fc54e682
JavidPack <javidpack@gmail.com>
2023-09-02 12:30:21 -0600

    Seal ModAccessorySlotPlayer to prevent modder mistakes

--- patches/tModLoader/Terraria/ModLoader/Default/ModAccessorySlotPlayer.cs
+++ patches/tModLoader/Terraria/ModLoader/Default/ModAccessorySlotPlayer.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader.IO;
 namespace Terraria.ModLoader.Default;
 
 // Test in Multiplayer, suspect there is some issue with synchronization of unloaded slots
-public class ModAccessorySlotPlayer : ModPlayer
+public sealed class ModAccessorySlotPlayer : ModPlayer
 {
 	internal static AccessorySlotLoader Loader => LoaderManager.Get<AccessorySlotLoader>();
 
