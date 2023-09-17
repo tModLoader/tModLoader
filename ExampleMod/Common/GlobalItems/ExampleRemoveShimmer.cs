c26128a642540cc87cf15ba84a501f4c4477c17d
Kogsey <KadenKogsey@gmail.com>
2023-09-17 21:06:59 +0800

    Added examplemod example

---
+++ ExampleMod/Common/GlobalItems/ExampleRemoveShimmer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Common.GlobalItems
+{
+	public class ExampleRemoveShimmer : GlobalItem
+	{
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+			return entity.type == ItemID.RodofDiscord;
+		}
+
+		public override bool CanShimmer(Item item) {
+			return false; // The rod of discord can no longer shimmer.
+		}
+	}
+}
