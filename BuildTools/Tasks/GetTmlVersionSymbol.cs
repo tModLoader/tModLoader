e4ae7d5d1c7f29d875215bfc79a582a15374f475
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-09-18 22:47:54 +0100

    add TML_Major_Minor preprocessor symbol

---
+++ BuildTools/Tasks/GetTmlVersionSymbol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+
+namespace tModLoader.BuildTools.Tasks;
+
+public class GetTmlVersionSymbol : TaskBase
+{
+	/// <summary>
+	/// The path to tModLoader's assembly file.
+	/// </summary>
+	[Required]
+	public string TmlDllPath { get; set; } = string.Empty;
+
+	[Output]
+	public string TmlVersionSymbol { get; set; } = string.Empty;
+
+	protected override void Run()
+	{
+		if (!File.Exists(TmlDllPath)) {
+			Log.LogError("tModLoader assembly file does not exist. (Path: {0})", TmlDllPath);
+			return;
+		}
+
+		Version version = SavePathLocator.GetTmlVersion(TmlDllPath);
+		TmlVersionSymbol = $"TML_{version.Major}_{version.Minor:D2}";
+	}
+}
