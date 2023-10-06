71a78e51bfedd4c0f391d242ed303cc55357cd09
Tomat <xxlennygamerxx@gmail.com>
2023-10-05 19:38:36 -0700

    Remove commented out code.

--- patches/tModLoader/Terraria/UI/InGameNotificationsTracker.TML.cs
+++ patches/tModLoader/Terraria/UI/InGameNotificationsTracker.TML.cs
@@ -2,14 +2,6 @@
 
 partial class InGameNotificationsTracker
 {
-	// /// <summary>
-	// ///	The list of all in-game notifications being tracked.
-	// /// </summary>
-	// /// <remarks>
-	// /// You can clear this collection with either <see cref="Clear"/> or <see cref="List{T}.Clear"/>, this property has no setter and thus does not allow you to directly change the collection instance.
-	// /// </remarks>
-	// public static List<IInGameNotification> Notifications => _notifications;
-
 	/// <summary>
 	/// Adds an in-game notification to the tracker.
 	/// </summary>
