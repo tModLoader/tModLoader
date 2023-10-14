6470e273b631bccf1eca5f47c4ca3ff175fcebc0
David F <contact@davidfdev.com>
2023-10-15 10:05:32 +1100

    TravelingMerchantSystem saves and syncs shop items

--- ExampleMod/Common/Systems/TravelingMerchantSystem.cs
+++ ExampleMod/Common/Systems/TravelingMerchantSystem.cs
@@ -12,14 +12,37 @@ public class TravelingMerchantSystem : ModSystem
 	}
 
 	public override void SaveWorldData(TagCompound tag) {
+ 		tag["shopItems"] = ExampleTravelingMerchant.shopItems;
 		if (ExampleTravelingMerchant.spawnTime != double.MaxValue) {
 			tag["ExampleTravelingMerchantSpawnTime"] = ExampleTravelingMerchant.spawnTime;
 		}
 	}
 
 	public override void LoadWorldData(TagCompound tag) {
-		if (!tag.TryGet("ExampleTravelingMerchantSpawnTime", out ExampleTravelingMerchant.spawnTime)) {
+        ExampleTravelingMerchant.shopItems.Clear();
+        ExampleTravelingMerchant.shopItems.AddRange(tag.Get<List<Item>>("shopItems"));
+  		if (!tag.TryGet("ExampleTravelingMerchantSpawnTime", out ExampleTravelingMerchant.spawnTime)) {
 			ExampleTravelingMerchant.spawnTime = double.MaxValue;
 		}
 	}
+
+    public override void ClearWorld() {
+        ExampleTravelingMerchant.shopItems.Clear();
+        ExampleTravelingMerchant.spawnTime = double.MaxValue;
+    }
+ 
+    public override void NetSend(BinaryWriter writer) {
+        writer.Write(ExampleTravelingMerchant.shopItems.Count);
+        foreach (Item item in ExampleTravelingMerchant.shopItems) {
+            ItemIO.Send(item, writer, writeStack: true);
+        }
+    }
+
+    public override void NetReceive(BinaryReader reader) {
+        ExampleTravelingMerchant.shopItems.Clear();
+        int count = reader.ReadInt32();
+        for (int i = 0; i < count; i++) {
+            ExampleTravelingMerchant.shopItems.Add(ItemIO.Receive(reader, readStack: true));
+        }
+    }
 }
