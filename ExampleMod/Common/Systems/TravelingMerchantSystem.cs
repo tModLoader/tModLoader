99fd8ea3126a03bfb94daac7f192aaee3f591624
David F <contact@davidfdev.com>
2023-10-20 18:08:25 +1100

    Fixed ExampleTravelingMerchant Shop Not Functioning On Servers (#3876)
    * Fixed ExampleTravelingMerchant Shop Not Functioning On Servers
    
    * Fixed Net Spam Caused By Syncing Shop Items
    
    Shop items are synced when the NPC spawns (OnSpawn) and when the active player count changes (AI).
    
    * Fixed Code-Styling To Match Example Mod
    
    * ExampleTravelingMerchant no longer responsible for saving and syncing its items
    
    Removed Save/LoadData and Send/ReceiveExtraAI. This will be handled by TravelingMerchantSystem instead.
    
    * TravelingMerchantSystem saves and syncs shop items
    
    * Add some warnings about bandwidth usage
    
    * Bad submodule merge, reset to 1.4.4 branch
    
    ---------
    
    Co-authored-by: Chicken-Bones <mehvids@gmail.com>

--- ExampleMod/Common/Systems/TravelingMerchantSystem.cs
+++ ExampleMod/Common/Systems/TravelingMerchantSystem.cs
@@ -1,4 +1,7 @@
 using ExampleMod.Content.NPCs;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
@@ -11,14 +14,42 @@ namespace ExampleMod.Common.Systems
 		}
 
 		public override void SaveWorldData(TagCompound tag) {
+			tag["shopItems"] = ExampleTravelingMerchant.shopItems;
 			if (ExampleTravelingMerchant.spawnTime != double.MaxValue) {
-				tag["ExampleTravelingMerchantSpawnTime"] = ExampleTravelingMerchant.spawnTime;
+				tag["spawnTime"] = ExampleTravelingMerchant.spawnTime;
 			}
 		}
 
 		public override void LoadWorldData(TagCompound tag) {
-			if (!tag.TryGet("ExampleTravelingMerchantSpawnTime", out ExampleTravelingMerchant.spawnTime)) {
+			ExampleTravelingMerchant.shopItems.Clear();
+			ExampleTravelingMerchant.shopItems.AddRange(tag.Get<List<Item>>("shopItems"));
+			if (!tag.TryGet("spawnTime", out ExampleTravelingMerchant.spawnTime)) {
 				ExampleTravelingMerchant.spawnTime = double.MaxValue;
+			}
+		}
+
+		public override void ClearWorld() {
+			ExampleTravelingMerchant.shopItems.Clear();
+			ExampleTravelingMerchant.spawnTime = double.MaxValue;
+		}
+ 
+		public override void NetSend(BinaryWriter writer) {
+			// Note that NetSend is called whenever WorldData packet is sent.
+			// We use this so that shop items can easily be synced to joining players
+			// We recommend modders avoid sending WorldData too often, or filling it with too much data, lest too much bandwidth be consumed sending redundant data repeatedly
+			// Consider sending a custom packet instead of WorldData if you have a significant amount of data to synchronise
+
+			writer.Write(ExampleTravelingMerchant.shopItems.Count);
+			foreach (Item item in ExampleTravelingMerchant.shopItems) {
+				ItemIO.Send(item, writer, writeStack: true);
+			}
+		}
+
+		public override void NetReceive(BinaryReader reader) {
+			ExampleTravelingMerchant.shopItems.Clear();
+			int count = reader.ReadInt32();
+			for (int i = 0; i < count; i++) {
+				ExampleTravelingMerchant.shopItems.Add(ItemIO.Receive(reader, readStack: true));
 			}
 		}
 	}
