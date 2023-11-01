f72b3b77a7a8ea5d8217afd104cc833790368e7a
kurti256 <Kurtis.t.l.gibson@gmail.com>
2023-11-01 21:58:31 +0000

    Creates IDs for dashes from Player.dashType
    In similar vain to ItemUseStyleID
    no changes to examplemod

---
+++ patches/tModLoader/Terraria/ID/DashID.cs
@@ -0,0 +1,12 @@
+namespace Terraria.ID;
+
+public static class DashID
+{
+	public const int None = 0;
+	public const int Tabi = 1;
+	public const int MasterNinjaGear = 1;
+	public const int SheildOfCthulhu = 2;
+	public const int SolarFlare = 3;
+	public const int Unused = 4; //included for parity, code is in vanilla but not used.
+	public const int CrystalAssasin = 5;
+}
