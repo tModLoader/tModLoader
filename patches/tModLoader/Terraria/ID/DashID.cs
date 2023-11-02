e53d5fbff88f76205847e290536678c037548bcd
kurti256 <Kurtis.t.l.gibson@gmail.com>
2023-11-02 22:20:31 +0000

    updated DashID
    on the suggestion of jopojelly:
    1) changed 2 variables  that share a value into one
    
    2)  Added summary concerning what I'm making IDs for

--- patches/tModLoader/Terraria/ID/DashID.cs
+++ patches/tModLoader/Terraria/ID/DashID.cs
@@ -1,10 +1,13 @@
 namespace Terraria.ID;
 
+/// <summary>
+/// Corresponds to values assigned to <see cref="Player.dashType"/>.
+/// </summary>
+
 public static class DashID
 {
 	public const int None = 0;
-	public const int Tabi = 1;
-	public const int MasterNinjaGear = 1;
+	public const int TabiOrMasterNinjaGear = 1;
 	public const int SheildOfCthulhu = 2;
 	public const int SolarFlare = 3;
 	public const int Unused = 4; //included for parity, code is in vanilla but not used.
