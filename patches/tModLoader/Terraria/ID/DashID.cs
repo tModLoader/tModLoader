69dacd192f1e504535cc96332a5b975d80b0db92
JavidPack <javidpack@gmail.com>
2023-11-06 11:56:46 -0700

    Fix some typos

--- patches/tModLoader/Terraria/ID/DashID.cs
+++ patches/tModLoader/Terraria/ID/DashID.cs
@@ -3,13 +3,12 @@ namespace Terraria.ID;
 /// <summary>
 /// Corresponds to values assigned to <see cref="Player.dashType"/>.
 /// </summary>
-
 public static class DashID
 {
 	public const int None = 0;
-	public const int TabiOrMasterNinjaGear = 1;
-	public const int SheildOfCthulhu = 2;
+	public const int TabiAndMasterNinjaGear = 1;
+	public const int ShieldOfCthulhu = 2;
 	public const int SolarFlare = 3;
-	public const int Unused = 4; //included for parity, code is in vanilla but not used.
-	public const int CrystalAssasin = 5;
+	public const int Unused4 = 4; // included for parity, code is in vanilla but not used.
+	public const int CrystalAssassin = 5;
 }
