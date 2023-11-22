a9b3c39ffa45748476adf1d6eb4d1a0508fc75b7
JavidPack <javidpack@gmail.com>
2023-11-22 14:45:35 -0700

    Ensure that tmodporter launches in a terminal

--- patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
+++ patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+if [ ! -t 0 ]; then
+	cd "$(dirname "$0")"
+	echo "Not in a terminal, attempting to open terminal"
+	. ../LaunchUtils/BashUtils.sh
+	if machine_has "konsole"; then
+		konsole -e "$0 $@"
+	elif machine_has "gnome-terminal"; then
+		gnome-terminal -- "$0 $@"
+	else
+		xterm -e "$0 $@"
+	fi
+	echo "Done"
+	exit
+fi
+
 cd "$(dirname "$0")"/..
 if [[ ! -z "$DOTNET_ROOT" ]]; then
 	echo "DOTNET_ROOT is $DOTNET_ROOT"
@@ -6,4 +21,4 @@ if [[ ! -z "$DOTNET_ROOT" ]]; then
 	export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 fi
 export DOTNET_ROLL_FORWARD=Minor
-dotnet tModLoader.dll -tModPorter $@ &> ./tModLoader-Logs/tModPorter.log
+dotnet tModLoader.dll -tModPorter $@ 2>&1 | tee ./tModLoader-Logs/tModPorter.log
