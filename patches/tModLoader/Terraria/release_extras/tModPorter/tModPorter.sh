ca56cdb7802cc69d81f1f36169fea7e4b52e5987
JavidPack <javidpack@gmail.com>
2023-11-21 20:26:48 -0700

    Revert to using dotnet for tmodporter.sh, infinite loop for some reason

--- patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
+++ patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"/..
-./start-tModLoader.sh -tModPorter $@
+if [[ ! -z "$DOTNET_ROOT" ]]; then
+	echo "DOTNET_ROOT is $DOTNET_ROOT"
+	export DOTNET_ROOT=$HOME/.dotnet
+    export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+fi
+export DOTNET_ROLL_FORWARD=Minor
+dotnet tModLoader.dll -tModPorter $@ &> ./tModLoader-Logs/tModPorter.log
