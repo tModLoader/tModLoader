3b23798b8f6922a3340e5cdc3a59d6a697ab0604
JavidPack <javidpack@gmail.com>
2023-11-21 20:29:45 -0700

    Update tModPorter.sh

--- patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
+++ patches/tModLoader/Terraria/release_extras/tModPorter/tModPorter.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"/..
 if [[ ! -z "$DOTNET_ROOT" ]]; then
 	echo "DOTNET_ROOT is $DOTNET_ROOT"
 	export DOTNET_ROOT=$HOME/.dotnet
-    export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+	export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 fi
 export DOTNET_ROLL_FORWARD=Minor
 dotnet tModLoader.dll -tModPorter $@ &> ./tModLoader-Logs/tModPorter.log
