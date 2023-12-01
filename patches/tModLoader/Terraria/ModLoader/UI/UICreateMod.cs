9d4f1d3f123d2b9d76448080de4085adf5893c82
-Cyril- <59193481@qq.com>
2023-12-02 01:00:05 +0800

    Call it `$(DotNetName)` and put it in `tMLMod.targets`
    undefined

--- patches/tModLoader/Terraria/ModLoader/UI/UICreateMod.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/UICreateMod.cs
@@ -262,9 +262,6 @@ $@"<?xml version=""1.0"" encoding=""utf-8""?>
     <TargetFramework>net6.0</TargetFramework>
     <PlatformTarget>AnyCPU</PlatformTarget>
     <LangVersion>latest</LangVersion>
-    <DotNetPath Condition="" '$(OS)' == 'Windows_NT' "">dotnet.exe</DotNetPath>
-    <DotNetPath Condition="" '$(OS)' == 'Unix' "">dotnet</DotNetPath>
-    <DotNetPath Condition="" '$(DotNetPath)' == '' "">dotnet</DotNetPath>
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include=""tModLoader.CodeAssist"" Version=""0.1.*"" />
@@ -289,13 +286,13 @@ $@"{{
   ""profiles"": {{
     ""Terraria"": {{
       ""commandName"": ""Executable"",
-      ""executablePath"": ""$(DotNetPath)"",
+      ""executablePath"": ""$(DotNetName)"",
       ""commandLineArgs"": ""$(tMLPath)"",
       ""workingDirectory"": ""$(tMLSteamPath)""
     }},
     ""TerrariaServer"": {{
       ""commandName"": ""Executable"",
-      ""executablePath"": ""$(DotNetPath)"",
+      ""executablePath"": ""$(DotNetName)"",
       ""commandLineArgs"": ""$(tMLServerPath)"",
       ""workingDirectory"": ""$(tMLSteamPath)""
     }}
