2c62e64678097a9aafbc03b419b1e2811f36b055
Johannes Grimm <111083448+Metratrj@users.noreply.github.com>
2024-02-11 13:12:05 +0100

    Add commenting for CalculateQuerySort

--- patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
+++ patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
@@ -223,6 +223,7 @@ public static class SteamedWraps
 
 	public static EUGCQuery CalculateQuerySort(QueryParameters qParams)
 	{
+		// Only let steam rank by text when we want sorting for popularity, otherwise the results are not sorted when filtered by search term. 
 		if ((!string.IsNullOrEmpty(qParams.searchGeneric) || !string.IsNullOrEmpty(qParams.searchAuthor)) && qParams.sortingParamater == ModBrowserSortMode.Hot)
 			return EUGCQuery.k_EUGCQuery_RankedByTextSearch;
 
