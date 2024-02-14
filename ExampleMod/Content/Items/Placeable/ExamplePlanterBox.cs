3963455e3049ac4eb36ede5fa24112134549883a
Lion8cake <69915693+Lion8cake@users.noreply.github.com>
2024-02-14 17:18:42 +1100

    Update ExampleMod/Content/Items/Placeable/ExamplePlanterBox.cs
    Co-authored-by: BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>

--- ExampleMod/Content/Items/Placeable/ExamplePlanterBox.cs
+++ ExampleMod/Content/Items/Placeable/ExamplePlanterBox.cs
@@ -12,17 +12,10 @@ namespace ExampleMod.Content.Items.Placeable
 		}
 
 		public override void SetDefaults() {
+			Item.DefaultToPlaceableTile(ModContent.TileType<Content.Tiles.ExamplePlanterBox>());
 			Item.width = 12;
 			Item.height = 12;
-			Item.maxStack = Item.CommonMaxStack;
-			Item.useTurn = true;
-			Item.autoReuse = true;
-			Item.useAnimation = 15;
-			Item.useTime = 10;
 			Item.value = Item.buyPrice(silver: 1);
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.consumable = true;
-			Item.createTile = ModContent.TileType<Content.Tiles.ExamplePlanterBox>();
 		}
 	}
 }
