43e58972317db248c8654999df6d1ee0cffdd814
Solxan <59670736+Solxanich@users.noreply.github.com>
2024-02-27 20:41:50 -0700

    Workaround for unknown behaviour in #4085- argument disableugcplaytime

--- patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
+++ patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using ReLogic.OS;
 using Steamworks;
 using System;
@@ -318,6 +319,10 @@ public static class SteamedWraps
 
 	public static void StopPlaytimeTracking()
 	{
+		// Check for https://github.com/tModLoader/tModLoader/issues/4085
+		if (Program.LaunchParameters.ContainsKey("-disableugcplaytime"))
+			return;
+
 		// Call the appropriate variant
 		if (SteamClient)
 			SteamUGC.StopPlaytimeTrackingForAllItems();
@@ -329,7 +334,8 @@ public static class SteamedWraps
 
 	public static void BeginPlaytimeTracking()
 	{
-		if (!SteamAvailable)
+		// Second check is for https://github.com/tModLoader/tModLoader/issues/4085
+		if (!SteamAvailable || Program.LaunchParameters.ContainsKey("-disableugcplaytime"))
 			return;
 
 		List<PublishedFileId_t> list = new List<PublishedFileId_t>();
