cad273b5ce08992867213873f8aafc79a0441d36
tomat <xxlennygamerxx@gmail.com>
2024-03-16 14:13:08 -0700

    Define ModuleTransformer.

---
+++ patches/tModLoader/Terraria/ModLoader/ModuleTransformer.cs
@@ -0,0 +1,11 @@
+﻿using Mono.Cecil;
+
+namespace Terraria.ModLoader;
+
+/// <summary>
+///		SUMMARY TODO HI MOM
+/// </summary>
+public abstract class ModuleTransformer
+{
+	public abstract bool Transform(ModuleDefinition module);
+}
