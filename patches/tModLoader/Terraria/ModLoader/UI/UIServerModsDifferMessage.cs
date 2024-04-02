d864fa5cb1ed10946746843e46790f5d3ddff85c
JavidPack <javidpack@gmail.com>
2024-04-02 11:48:30 -0600

    Fix #4145

--- patches/tModLoader/Terraria/ModLoader/UI/UIServerModsDifferMessage.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/UIServerModsDifferMessage.cs
@@ -188,6 +188,7 @@ internal class UIServerModsDifferMessage : UIState, IHaveBackButtonCommand
 		this.reloadRequiredExplanationEntries = reloadRequiredExplanationEntries?.OrderBy(x => x.typeOrder).ThenBy(x => x.mod).ToList();
 		Main.menuMode = Interface.serverModsDifferMessageID;
 		Main.MenuUI.SetState(null); // New SetState code ignores setting to current state, so this is necessary to ensure OnActivate is called.
+		Main.alreadyGrabbingSunOrMoon = false; // Prevents cursor from being invisible in rare situations because netmode is technically 1 at this menu so it won't reset correctly.
 	}
 
 	private void BackClick(UIMouseEvent evt, UIElement listeningElement)
