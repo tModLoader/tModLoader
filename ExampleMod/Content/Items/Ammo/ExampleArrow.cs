5eaa8c484a8ecb7c6bd1ad92fa9f3a741887e3ae
Blu3 <124432839+harlowo@users.noreply.github.com>
2024-05-05 14:28:04 +0100

    Add files via upload

---
+++ ExampleMod/Content/Items/Ammo/ExampleArrow.cs
@@ -0,0 +1,39 @@
+﻿using ExampleMod.Content.Tiles.Furniture;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Ammo
+{
+	public class ExampleArrow : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 99;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 14;
+			Item.height = 36;
+
+			Item.damage = 6; // Keep in mind that the arrows damage is additive on the base weapon damage.
+			Item.DamageType = DamageClass.Ranged;
+
+			Item.maxStack = Item.CommonMaxStack;
+			Item.consumable = true; // Makes the item consumable, relatively self explanitory.
+			Item.knockBack = 1.5f;
+			Item.value = Item.sellPrice(copper: 16);
+			Item.shoot = ModContent.ProjectileType<Projectiles.ExampleArrowProjectile>(); // The projectile that weapons fire when using this item as ammunition.
+			Item.shootSpeed = 3f; // The speed of the projectile.
+			Item.ammo = AmmoID.Arrow; // The ammo class this ammo belongs to.
+		}
+
+		// For a more detailed explanation of recipe creation, please go to Content/ExampleRecipes.cs.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<ExampleItem>()
+				.AddTile<ExampleWorkbench>()
+				.Register();
+		}
+	}
+}
