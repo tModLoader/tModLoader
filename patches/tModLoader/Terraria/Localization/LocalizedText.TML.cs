8570a8aab4044f1811394c238e042600f5dab3e3
JavidPack <javidpack@gmail.com>
2024-05-09 16:36:11 -0600

    Update LocalizedText.TML.cs

--- patches/tModLoader/Terraria/Localization/LocalizedText.TML.cs
+++ patches/tModLoader/Terraria/Localization/LocalizedText.TML.cs
@@ -86,7 +86,7 @@ public partial class LocalizedText
 
 	/// <summary>
 	/// Creates a string from this LocalizedText populated with data from the provided <paramref name="args"/> arguments. Formats the string in the same manner as <see href="https://learn.microsoft.com/en-us/dotnet/api/system.string.format?view=net-6.0">string.Format</see>. Placeholders such as "{0}", "{1}", etc will be replaced with the string representation of the corresponding objects provided.<br/>
-	/// Additionally, pluralization is supported as well. The <see href="https://github.com/tModLoader/tModLoader/wiki/Contributing-Localization#placeholders">Contributing Localization Guide</see> teaches more about placeholders and plural support.
+	/// Additionally, pluralization is supported as well. The <see href="https://github.com/tModLoader/tModLoader/wiki/Localization#string-formatting">Localization Guide</see> teaches more about placeholders and plural support.
 	/// 
 	/// </summary>
 	/// <param name="args"></param>
