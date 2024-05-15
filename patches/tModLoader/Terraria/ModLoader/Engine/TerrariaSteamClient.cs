e351b35db2f8ffc61e774ef027a512e19f23434b
Solxan <59670736+Solxanich@users.noreply.github.com>
2024-05-14 19:58:23 -0600

    Production Testing - Is PLaytime 4 Terraria still working?

--- patches/tModLoader/Terraria/ModLoader/Engine/TerrariaSteamClient.cs
+++ patches/tModLoader/Terraria/ModLoader/Engine/TerrariaSteamClient.cs
@@ -45,7 +45,7 @@ internal class TerrariaSteamClient
 		// To Disable Playtime tracking without breaking the family share workaround,
 		// we continue if SteamAppId not set (which should not yet be set for Family Shared) && SteamClientLaunch is set
 		// Inverting the output for when abort, we get !(SCL && !SA) -> !SCL || SA
-		if (Environment.GetEnvironmentVariable("SteamClientLaunch") != "1" /*|| Environment.GetEnvironmentVariable("STEAMAPPID") != null*/) {
+		if (Environment.GetEnvironmentVariable("SteamClientLaunch") != "1" || Environment.GetEnvironmentVariable("STEAMAPPID") != null) {
 			Logger.Debug("Disabled. Launched outside steam client.");
 			return LaunchResult.Ok;
 		}
