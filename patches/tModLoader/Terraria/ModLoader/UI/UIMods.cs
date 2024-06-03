tModLoader/tModLoader

commit a9a10cb7e73946fa8f5207b115a2eb92dc352261

Unload dependents of a mod which failed to load (Redo), Also improve UIMods behavior (#4112)
* Unload dependents when a hard dependency fails to load

* Include disabled dependent mods in message

Mods that were disabled because a hard dependency encountered a load error will also appear in the error message as they are being unloaded.

* Use functional-style LINQ query

This commit changes the LINQ query for finding mods which depend on another mod to adhere to tModLoader code style by using functions rather than the query syntax.

Relevant discussion: https://github.com/tModLoader/tModLoader/pull/3829#discussion_r1337892779

* Use RefNames to find mod references

Changes the dependent mods query to use BuildProperties.RefNames rather than looking through BuildProperties.modReferences.

Relevant discussion: https://github.com/tModLoader/tModLoader/pull/3829#discussion_r1337893174

* Apply feedback, notes for future improvements

* UI more readable, address feedback

* More text fixes, RecheckVersionsToLoad instead of FindMods

* Fix typo

* Simplify tooltip, new icon image, change enable/disable tooltip

---------

Co-authored-by: Jomar Milan <jomarm@jomarm.tech>

patches/tModLoader/Terraria/ModLoader/UI/UIMods.cs
@@ -13,6 +13,7 @@ using Terraria.ModLoader.Config;
 using Terraria.ModLoader.UI.ModBrowser;
 using Terraria.ModLoader.Core;
 using Terraria.Audio;
+using Terraria.ID;
 
 namespace Terraria.ModLoader.UI;
 
@@ -305,7 +306,7 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 
 	private void EnableAll(UIMouseEvent evt, UIElement listeningElement)
 	{
-		SoundEngine.PlaySound(12, -1, -1, 1);
+		SoundEngine.PlaySound(SoundID.MenuTick);
 		foreach (UIModItem modItem in items) {
 			if (modItem.tMLUpdateRequired != null)
 				continue;
@@ -315,7 +316,7 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 
 	private void DisableAll(UIMouseEvent evt, UIElement listeningElement)
 	{
-		SoundEngine.PlaySound(12, -1, -1, 1);
+		SoundEngine.PlaySound(SoundID.MenuTick);
 		foreach (UIModItem modItem in items) {
 			modItem.Disable();
 		}
@@ -339,7 +340,8 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 			needToRemoveLoading = false;
 			uIPanel.RemoveChild(uiLoader);
 		}
-		if (!updateNeeded) return;
+		if (!updateNeeded)
+			return;
 		updateNeeded = false;
 		filter = filterTextBox.Text;
 		modList.Clear();
@@ -395,12 +397,12 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 						text = "None";
 						break;
 				}
-				UICommon.DrawHoverStringInBounds(spriteBatch, text);
+				UICommon.TooltipMouseText(text);
 				return;
 			}
 		}
-		if(buttonOMF.IsMouseHovering)
-			UICommon.DrawHoverStringInBounds(spriteBatch, Language.GetTextValue("tModLoader.ModsOpenModsFoldersTooltip"));
+		if (buttonOMF.IsMouseHovering)
+			UICommon.TooltipMouseText(Language.GetTextValue("tModLoader.ModsOpenModsFoldersTooltip"));
 	}
 
 	public override void OnActivate()
@@ -430,8 +432,10 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 			var mods = ModOrganizer.FindMods(logDuplicates: true);
 			foreach (var mod in mods) {
 				UIModItem modItem = new UIModItem(mod);
-				modItem.Activate();
 				items.Add(modItem);
+			}
+			foreach (var modItem in items) {
+				modItem.Activate();
 			}
 			needToRemoveLoading = true;
 			updateNeeded = true;

patches/tModLoader/Terraria/Localization/Language.cs.patch
@@ -17,7 +17,7 @@
  
 +	/// <summary>
 +	/// Retrieves a LocalizedText object for a specified localization key. The actual text value can be retrieved from LocalizedText by accessing the <see cref="LocalizedText.Value"/> property or by using the <see cref="GetTextValue(string)"/> method directly.<br/><br/>
-+	/// Using LocalizedText instead of string is prefered when the value is stored. If the user switches languages or if resource packs or mods change text, the LocalizedText will automatically receive the new value. In turn, mods using those LocalizedText will also start displaying the updated values.<br/><br/>
++	/// Using LocalizedText instead of string is preferred when the value is stored. If the user switches languages or if resource packs or mods change text, the LocalizedText will automatically receive the new value. In turn, mods using those LocalizedText will also start displaying the updated values.<br/><br/>
 +	/// 
 +	/// </summary>
 +	/// <param name="key"></param>

patches/tModLoader/Terraria/ModLoader/Core/ModCompile.cs
@@ -224,6 +224,7 @@ $@"<Project ToolsVersion=""14.0"" xmlns=""http://schemas.microsoft.com/developer
 
 			mod.modFile.Save();
 			ModLoader.EnableMod(mod.Name);
+			// TODO: This should probably enable dependencies recursively as well. They will load properly, but right now the UI does not show them as loaded.
 			LocalizationLoader.HandleModBuilt(mod.Name);
 		}
 		catch (Exception e) {

patches/tModLoader/Terraria/ModLoader/ModLoader.cs
@@ -166,7 +166,7 @@ public static class ModLoader
 				else if (mod != null)
 					// if the mod exists, and the MajorMinorBuild() is identical, then assume it is an error in the Steam install/deployment - Solxan 
 					SteamedWraps.QueueForceValidateSteamInstall();
-					
+
 				if (e is Exceptions.JITException)
 					msg += "\n" + $"The mod will need to be updated to match the current tModLoader version, or may be incompatible with the version of some of your other mods. Click the '{Language.GetTextValue("tModLoader.OpenWebHelp")}' button to learn more.";
 			}
@@ -181,10 +181,23 @@ public static class ModLoader
 			if (e.Data.Contains("contentType") && e.Data["contentType"] is Type contentType)
 				msg += "\n" + Language.GetTextValue("tModLoader.LoadErrorContentType", contentType.FullName);
 
-			Logging.tML.Error(msg, e);
-
-			foreach (var mod in responsibleMods)
+			foreach (var mod in responsibleMods) {
+				DisableModAndDependents(mod);
+			}
+			void DisableModAndDependents(string mod)
+			{
 				DisableMod(mod);
+
+				var dependents = availableMods
+					.Where(m => IsEnabled(m.Name) && m.properties.RefNames(includeWeak: false).Any(refName => refName.Equals(mod)))
+					.Select(m => m.Name);
+
+				foreach (var dependent in dependents) {
+					DisableModAndDependents(dependent);
+				}
+			}
+			
+			Logging.tML.Error(msg, e);
 
 			isLoading = false; // disable loading flag, because server will just instantly retry reload
 			DisplayLoadError(msg, e, e.Data.Contains("fatal"), responsibleMods.Count == 0);

patches/tModLoader/Terraria/ModLoader/UI/UICommon.cs
@@ -119,6 +119,7 @@ public static class UICommon
 	public static Asset<Texture2D> ButtonExpandedTexture { get; internal set; }
 	public static Asset<Texture2D> ModBrowserIconsTexture { get; internal set; }
 	public static Asset<Texture2D> ButtonExclamationTexture { get; internal set; }
+	public static Asset<Texture2D> ButtonDepsTexture { get; internal set; }
 	public static Asset<Texture2D> ButtonTranslationModTexture { get; internal set; }
 	public static Asset<Texture2D> LoaderTexture { get; internal set; }
 	public static Asset<Texture2D> LoaderBgTexture { get; internal set; }
@@ -146,6 +147,7 @@ public static class UICommon
 		ButtonExpandedTexture = LoadEmbeddedTexture("Config.UI.ButtonExpanded");
 		ModBrowserIconsTexture = LoadEmbeddedTexture("UI.UIModBrowserIcons");
 		ButtonExclamationTexture = LoadEmbeddedTexture("UI.ButtonExclamation");
+		ButtonDepsTexture = LoadEmbeddedTexture("UI.ButtonDeps");
 		ButtonTranslationModTexture = LoadEmbeddedTexture("UI.ButtonTranslationMod");
 		LoaderTexture = LoadEmbeddedTexture("UI.Loader");
 		LoaderBgTexture = LoadEmbeddedTexture("UI.LoaderBG");

patches/tModLoader/Terraria/ModLoader/UI/UIModItem.cs
@@ -10,12 +10,9 @@ using Terraria.Localization;
 using Terraria.ModLoader.Config;
 using Terraria.ModLoader.Core;
 using Terraria.ModLoader.UI.ModBrowser;
-using Terraria.UI.Chat;
 using Terraria.Audio;
 using Terraria.GameContent;
 using ReLogic.Content;
-using System.IO;
-using ReLogic.Utilities;
 using Terraria.Social.Base;
 
 namespace Terraria.ModLoader.UI;
@@ -50,9 +47,25 @@ internal class UIModItem : UIPanel
 	private int _modIconAdjust;
 	private string _tooltip;
 	private string[] _modReferences;
+	private string[] _modDependents; // Note: Recursive
+	private string[] _modDependencies; // Note: Recursive
+	private string _modRequiresTooltip;
 	public readonly string DisplayNameClean; // No chat tags: for search and sort functionality.
 
-	private string ToggleModStateText => _mod.Enabled ? Language.GetTextValue("tModLoader.ModsDisable") : Language.GetTextValue("tModLoader.ModsEnable");
+	private string ToggleModStateText {
+		get {
+			if (_mod.Enabled) {
+				if(_modDependents.Any())
+					return Language.GetTextValue("tModLoader.ModsDisableAndDependents", _mod.DisplayName, _modDependents.Length);
+				return Language.GetTextValue("tModLoader.ModsDisable");
+			}
+			else {
+				if (_modDependencies.Any())
+					return Language.GetTextValue("tModLoader.ModsEnableAndDependencies", _mod.DisplayName, _modDependencies.Length);
+				return Language.GetTextValue("tModLoader.ModsEnable");
+			}
+		}
+	}
 
 	public string ModName => _mod.Name;
 	public bool NeedsReload => _mod.properties.side != ModSide.Server && (_mod.Enabled != _loaded || _configChangesRequireReload);
@@ -120,7 +133,7 @@ internal class UIModItem : UIPanel
 		// Detect if it's for a preview version ahead of our time
 		if (BuildInfo.tMLVersion.MajorMinorBuild() < _mod.tModLoaderVersion.MajorMinorBuild()) {
 			updateVersion = $"v{_mod.tModLoaderVersion}";
-			
+
 			if (_mod.tModLoaderVersion.MajorMinor() > BuildInfo.stableVersion)
 				updateVersion = $"Preview {updateVersion}";
 		}
@@ -174,8 +187,51 @@ internal class UIModItem : UIPanel
 
 		_modReferences = _mod.properties.modReferences.Select(x => x.mod).ToArray();
 
-		if (_modReferences.Length > 0 && !_mod.Enabled) {
-			var icon = UICommon.ButtonExclamationTexture;
+		var availableMods = ModOrganizer.RecheckVersionsToLoad();
+		_modRequiresTooltip = "";
+		HashSet<string> allDependencies = new();
+		GetDependencies(_mod.Name, allDependencies);
+		_modDependencies = allDependencies.ToArray();
+		if (_modDependencies.Any()) {
+			string refs = string.Join("\n", _modDependencies.Select(x => "- " + (Interface.modsMenu.FindUIModItem(x)?._mod.DisplayName ?? x + Language.GetTextValue("tModLoader.ModPackMissing"))));
+			_modRequiresTooltip += Language.GetTextValue("tModLoader.ModDependencyTooltip", refs);
+		}
+		void GetDependencies(string modName, HashSet<string> allDependencies)
+		{
+			var modItem = Interface.modsMenu.FindUIModItem(modName);
+			if (modItem == null)
+				return; // Mod isn't downloaded, can't determine further recursive dependencies
+
+			var dependencies = modItem._mod.properties.modReferences.Select(x => x.mod).ToArray();
+			foreach (var dependency in dependencies) {
+				if (allDependencies.Add(dependency)) {
+					GetDependencies(dependency, allDependencies);
+				}
+			}
+		}
+
+		HashSet<string> allDependents = new();
+		GetDependents(_mod.Name, allDependents);
+		_modDependents = allDependents.ToArray();
+		if (_modDependents.Any()) {
+			if (!string.IsNullOrWhiteSpace(_modRequiresTooltip))
+				_modRequiresTooltip += "\n\n";
+			string refs = string.Join("\n", _modDependents.Select(x => "- " + (Interface.modsMenu.FindUIModItem(x)?._mod.DisplayName ?? x + Language.GetTextValue("tModLoader.ModPackMissing"))));
+			_modRequiresTooltip += Language.GetTextValue("tModLoader.ModDependentsTooltip", refs);
+		}
+		void GetDependents(string modName, HashSet<string> allDependents)
+		{
+			var dependents = availableMods
+				.Where(m => m.properties.RefNames(includeWeak: false).Any(refName => refName.Equals(modName)))
+				.Select(m => m.Name).ToArray();
+			foreach (var dependent in dependents) {
+				if (allDependents.Add(dependent))
+					GetDependents(dependent, allDependents);
+			}
+		}
+
+		if (!string.IsNullOrWhiteSpace(_modRequiresTooltip)) {
+			var icon = UICommon.ButtonDepsTexture;
 			_modReferenceIcon = new UIImage(icon) {
 				Left = new StyleDimension(_uiModStateText.Left.Pixels + _uiModStateText.Width.Pixels + PADDING + left2ndLine, 0f),
 				Top = { Pixels = 42.5f }
@@ -330,9 +386,9 @@ internal class UIModItem : UIPanel
 		_tooltip = null;
 		base.Draw(spriteBatch);
 		if (!string.IsNullOrEmpty(_tooltip)) {
-			var bounds = GetOuterDimensions().ToRectangle();
-			bounds.Height += 16;
-			UICommon.DrawHoverStringInBounds(spriteBatch, _tooltip, bounds);
+			//var bounds = GetOuterDimensions().ToRectangle();
+			//bounds.Height += 16;
+			UICommon.TooltipMouseText(_tooltip);
 		}
 	}
 
@@ -381,8 +437,7 @@ internal class UIModItem : UIPanel
 			_tooltip = Language.GetTextValue("tModLoader.SwitchVersionInfoButton");
 		}
 		else if (_modReferenceIcon?.IsMouseHovering == true) {
-			string refs = string.Join(", ", _mod.properties.modReferences);
-			_tooltip = Language.GetTextValue("tModLoader.ModDependencyTooltip", refs);
+			_tooltip = _modRequiresTooltip;
 		}
 		else if (_translationModIcon?.IsMouseHovering == true) {
 			string refs = string.Join(", ", _mod.properties.RefNames(true)); // Translation mods can be strong or weak references.
@@ -416,26 +471,36 @@ internal class UIModItem : UIPanel
 		SoundEngine.PlaySound(SoundID.MenuTick);
 		_mod.Enabled = !_mod.Enabled;
 
-		if (!_mod.Enabled)
+		UpdateUIForEnabledChange();
+
+		if (!_mod.Enabled) {
+			DisableDependents();
 			return;
+		}
 
 		EnableDependencies();
 	}
 
 	internal void Enable()
 	{
-		if(_mod.Enabled){return;}
-		SoundEngine.PlaySound(SoundID.MenuTick);
+		if (_mod.Enabled) { return; }
 		_mod.Enabled = true;
-		_uiModStateText.SetEnabled();
+		UpdateUIForEnabledChange();
 	}
 
 	internal void Disable()
 	{
-		if(!_mod.Enabled){return;}
-		SoundEngine.PlaySound(SoundID.MenuTick);
+		if (!_mod.Enabled) { return; }
 		_mod.Enabled = false;
-		_uiModStateText.SetDisabled();
+		UpdateUIForEnabledChange();
+	}
+
+	private void UpdateUIForEnabledChange()
+	{
+		if (_mod.Enabled)
+			_uiModStateText.SetEnabled();
+		else
+			_uiModStateText.SetDisabled();
 	}
 
 	internal void EnableDependencies()
@@ -458,6 +523,23 @@ internal class UIModItem : UIPanel
 			}
 			dep.EnableDepsRecursive(missingRefs);
 			dep.Enable();
+		}
+	}
+
+	private void DisableDependents()
+	{
+		DisableDependentsRecursive();
+	}
+
+	private void DisableDependentsRecursive()
+	{
+		foreach (var name in _modDependents) {
+			var dep = Interface.modsMenu.FindUIModItem(name);
+			if (dep == null) {
+				continue;
+			}
+			dep.DisableDependentsRecursive();
+			dep.Disable();
 		}
 	}
 

patches/tModLoader/Terraria/ModLoader/UI/UIModStateText.cs
@@ -25,14 +25,6 @@ internal class UIModStateText : UIElement
 		PaddingBottom = PaddingTop = 10f;
 	}
 
-	public override void OnInitialize()
-	{
-		OnLeftClick += (evt, el) => {
-			if (_enabled) SetDisabled();
-			else SetEnabled();
-		};
-	}
-
 	public void SetEnabled()
 	{
 		_enabled = true;

commit 5532e0bb17724393550a3fe05efb429d6677908f

Fix #4242 , regression from #4112

patches/tModLoader/Terraria/ModLoader/UI/UIMods.cs
@@ -14,6 +14,7 @@ using Terraria.ModLoader.UI.ModBrowser;
 using Terraria.ModLoader.Core;
 using Terraria.Audio;
 using Terraria.ID;
+using System;
 
 namespace Terraria.ModLoader.UI;
 
@@ -27,6 +28,7 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 	private UIList modList;
 	private float modListViewPosition;
 	private readonly List<UIModItem> items = new List<UIModItem>();
+	private Task<List<UIModItem>> modItemsTask;
 	private bool updateNeeded;
 	public bool loading;
 	private UIInputTextField filterTextBox;
@@ -336,6 +338,17 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 	public override void Update(GameTime gameTime)
 	{
 		base.Update(gameTime);
+		if (modItemsTask is { IsCompleted: true }) {
+			var result = modItemsTask.Result;
+			items.AddRange(result);
+			foreach (var item in items) {
+				item.Activate(); // Activate must happen after all UIModItem are in `items`
+			}
+			needToRemoveLoading = true;
+			updateNeeded = true;
+			loading = false;
+			modItemsTask = null;
+		}
 		if (needToRemoveLoading) {
 			needToRemoveLoading = false;
 			uIPanel.RemoveChild(uiLoader);
@@ -428,19 +441,15 @@ internal class UIMods : UIState, IHaveBackButtonCommand
 
 	internal void Populate()
 	{
-		Task.Run(() => {
+		modItemsTask = Task.Run(() => {
 			var mods = ModOrganizer.FindMods(logDuplicates: true);
+			var pendingUIModItems = new List<UIModItem>();
 			foreach (var mod in mods) {
 				UIModItem modItem = new UIModItem(mod);
-				items.Add(modItem);
+				pendingUIModItems.Add(modItem);
 			}
-			foreach (var modItem in items) {
-				modItem.Activate();
-			}
-			needToRemoveLoading = true;
-			updateNeeded = true;
-			loading = false;
-		});
+			return pendingUIModItems;
+		}, _cts.Token);
 	}
 }
 