cc0f762a43833b5f524b01e01beee76c89f75cbe
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2024-06-24 10:10:40 +0500

    Update Tile.TML.cs

--- patches/tModLoader/Terraria/Tile.TML.cs
+++ patches/tModLoader/Terraria/Tile.TML.cs
@@ -22,6 +22,12 @@ public readonly partial struct Tile
 	public readonly ushort Y;
 #endif
 
+#if !TILE_X_Y
+	// TileId = y + x * tilemap.Height
+	public readonly int X() => TileId / Main.tile.Height;
+	public readonly int Y() => TileId % Main.tile.Height;
+#endif
+
 	// General state
 
 	/// <summary>
