cd89f9b38baa193024263ba458d2ef3c909cb9fb
JavidPack <javidpack@gmail.com>
2024-07-23 13:19:29 -0600

    Fix #4299, Version upgrade hints now check for preview build value directly to better support users on old versions

--- patches/tModLoader/Terraria/ModLoader/UI/ModBrowser/UIModDownloadItem.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/ModBrowser/UIModDownloadItem.cs
@@ -84,8 +84,8 @@ internal class UIModDownloadItem : UIPanel
 		tMLNeedUpdate = !BuildInfo.IsDev && BuildInfo.tMLVersion.MajorMinorBuild() < modBuildVersion.MajorMinorBuild();
 		if (tMLNeedUpdate) {
 			string updateVersion = $"v{modBuildVersion}";
-			bool lastMonth = BuildInfo.tMLVersion.Minor == 12;
-			if (BuildInfo.IsStable && new Version(modBuildVersion.Major, modBuildVersion.Minor) == new Version(BuildInfo.tMLVersion.Major + (lastMonth ? 1 : 0), lastMonth ? 1 : BuildInfo.tMLVersion.Minor + 1))
+
+			if (modBuildVersion.Build == 2)
 				updateVersion = $"Preview {updateVersion}";
 
 			tMLUpdateRequired = new UIAutoScaleTextTextPanel<string>(Language.GetTextValue("tModLoader.MBRequiresTMLUpdate", updateVersion)).WithFadedMouseOver(Color.Orange, Color.Orange * 0.7f);
