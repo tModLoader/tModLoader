tModLoader/tModLoader

commit 3039d9df5ac0f72b43242be2a3929d8d4efe1ca0

Example Rockets and explosive projectile fixes and features
* Redone Commits

* Maybe fix localization merge conflicts

* Just undo localization

* Redid Localization

* Basic ExampleRocketLauncher & ProjectileID.Sets.Explosive

Still WIP. Need to apply the set to the rest of the EM explosive projectiles and remove the code that the set now handles.

* New Projectile.HurtPlayer

* Cleanup & Player.PickAmmo Change

* MP For the Worthy Testing

* PickAmmo update

* Fixed space

* Merge patch fixes

* Added AmmoID.Sets.SpecificLauncherAmmoProjectileFallback

* Revert commenting out old logic, some mods might depend on it.

Also, Celebration plus RocketIV was shooting using starfury logic

* Move rocket projectiles to ExampleMod.Content.Projectiles.Rockets

* Missed changes from last commit

* PrepareBombToBlow hook, docs, cleanup, remove unneeded code from examples

* More cleanup

---------

Co-authored-by: JavidPack <javidpack@gmail.com>

ExampleMod/Content/Items/Ammo/ExampleRocket.cs
@@ -1,0 +1,59 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ExampleMod.Content.Projectiles.Rockets;
+
+namespace ExampleMod.Content.Items.Ammo
+{
+	public class ExampleRocket : ModItem
+	{
+		// Rocket Ammo is a little weird and does not work the same as bullets or arrows.
+		// Rockets I through IV have four versions: normal Rocket, Grenade, Proximity Mine, and Snowman Rocket.
+		// This example is a clone of Rocket I.
+
+		public override void SetStaticDefaults() {
+			AmmoID.Sets.IsSpecialist[Type] = true; // This item will benefit from the Shroomite Helmet.
+
+			// This is where we tell the game which projectile to spawn when using this rocket as ammo with certain launchers.
+			// This specific rocket ammo is like Rocket I's.
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.RocketLauncher].Add(Type, ModContent.ProjectileType<ExampleRocketProjectile>());
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.GrenadeLauncher].Add(Type, ModContent.ProjectileType<ExampleGrenadeProjectile>());
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.ProximityMineLauncher].Add(Type, ModContent.ProjectileType<ExampleProximityMineProjectile>());
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.SnowmanCannon].Add(Type, ModContent.ProjectileType<ExampleSnowmanRocketProjectile>());
+			// We also need to say which type of Celebration Mk2 rockets to use.
+			// The Celebration Mk 2 only has four types of rockets. Change the projectile to match your ammo type.
+			// Rocket I like   == ProjectileID.Celeb2Rocket
+			// Rocket II like  == ProjectileID.Celeb2RocketExplosive
+			// Rocket III like == ProjectileID.Celeb2RocketLarge
+			// Rocket IV like  == ProjectileID.Celeb2RocketExplosiveLarge
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.Celeb2].Add(Type, ProjectileID.Celeb2Rocket);
+			// The Celebration and Electrosphere Launcher will always use their own projectiles no matter which rocket you use as ammo.
+		}
+
+		public override void SetDefaults() {
+			Item.width = 20;
+			Item.height = 14;
+			Item.damage = 40;
+			Item.knockBack = 4f;
+			Item.consumable = true;
+			Item.DamageType = DamageClass.Ranged;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.value = Item.buyPrice(copper: 50);
+			Item.ammo = AmmoID.Rocket; // The ammo type is Rocket Ammo
+
+			// The projectile that is set in Item.shoot will be the "default" projectile.
+			// i.e. if a launcher doesn't have this ammo defined in SpecificLauncherAmmoProjectileMatches, it'll use this projectile instead.
+			Item.shoot = ModContent.ProjectileType<ExampleRocketProjectile>();
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe(100)
+				.AddIngredient(ItemID.RocketI, 100)
+				.AddIngredient<ExampleItem>()
+				.AddTile(TileID.Anvils)
+				.AddCondition(Condition.NpcIsPresent(NPCID.Cyborg))
+				.Register();
+		}
+	}
+}

patches/tModLoader/Terraria/Player.cs.patch
@@ -7494,7 +7494,17 @@
  	}
  
  	private bool PickAmmo_TryFindingSpecificMatches(int launcher, int ammo, out int pickedProjectileId)
-@@ -40945,15 +_,68 @@
+@@ -40942,18 +_,78 @@
+ 		if (AmmoID.Sets.SpecificLauncherAmmoProjectileMatches.TryGetValue(launcher, out var value) && value.TryGetValue(ammo, out pickedProjectileId))
+ 			return true;
+ 
++		// Added fallback functionality for mod compatibility
++		launcher = AmmoID.Sets.SpecificLauncherAmmoProjectileFallback[launcher];
++		if (launcher != -1) {
++			if (AmmoID.Sets.SpecificLauncherAmmoProjectileMatches.TryGetValue(launcher, out var fallbackValue) && fallbackValue.TryGetValue(ammo, out pickedProjectileId))
++				return true;
++		}
++
  		return false;
  	}
  

ExampleMod/Common/GlobalNPCs/ExampleNPCShop.cs
@@ -1,5 +1,7 @@
 ﻿using ExampleMod.Content.Items;
+using ExampleMod.Content.Items.Ammo;
 using ExampleMod.Content.Items.Mounts;
+using ExampleMod.Content.NPCs;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -34,6 +36,9 @@ namespace ExampleMod.Common.GlobalNPCs
 			}
 			else if (shop.NpcType == NPCID.Stylist) {
 				shop.Add<ExampleHairDye>();
+			}
+			else if (shop.NpcType == NPCID.Cyborg) {
+				shop.Add<ExampleRocket>(Condition.NpcIsPresent(ModContent.NPCType<ExamplePerson>()));
 			}
 
 			// Example of adding new items with complex conditions in the Merchant shop.

ExampleMod/Content/Items/Weapons/ExampleRocketLauncher.cs
@@ -1,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Weapons
+{
+	// Rocket launchers are special because they typically have ammo-specific variant projectiles.
+	// ExampleRocketLauncher will inherit the variants specified by the Rocket Launcher weapon
+	public class ExampleRocketLauncher : ModItem {
+		public override void SetStaticDefaults() {
+			// This line lets ExampleRocketLauncher act like a normal RocketLauncher in regard to any variant projectiles
+			// corresponding to ammo that aren't specifically populated in SpecificLauncherAmmoProjectileMatches below.
+			AmmoID.Sets.SpecificLauncherAmmoProjectileFallback[Type] = ItemID.RocketLauncher;
+
+			// SpecificLauncherAmmoProjectileMatches can be used to provide specific projectiles for specific ammo items.
+			// This example dictates that when RocketIII ammo is used, this weapon will fire the Meowmere projectile.
+			// This is purely to show off this capability, typically SpecificLauncherAmmoProjectileFallback is all
+			// that is needed for an "upgrade". A completely custom rocket launcher would instead specify new and
+			// unique projectiles for all possible rocket ammo.
+			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches.Add(Type, new Dictionary<int, int> {
+				{ ItemID.RocketIII, ProjectileID.Meowmere }, 
+			});
+
+			// Note that some rocket launchers, like Celebration and Electrosphere Launcher, will always
+			// use their own projectiles no matter which rocket is used as ammo.
+			// This type of behavior can be implemented in ModifyShootStats
+		}
+
+		public override void SetDefaults() {
+			Item.DefaultToRangedWeapon(ProjectileID.RocketI, AmmoID.Rocket, singleShotTime: 30, shotVelocity: 5f, hasAutoReuse: true);
+			Item.width = 50;
+			Item.height = 20;
+			Item.damage = 55;
+			Item.knockBack = 4f;
+			Item.UseSound = SoundID.Item11;
+			Item.value = Item.buyPrice(gold: 40);
+			Item.rare = ItemRarityID.Yellow;
+		}
+
+		public override Vector2? HoldoutOffset() {
+			return new Vector2(-8f, 2f); // Moves the position of the weapon in the player's hand.
+		}
+	}
+}

ExampleMod/Content/Projectiles/ExampleExplosive.cs
@@ -8,11 +8,24 @@ using Terraria.ModLoader;
 namespace ExampleMod.Content.Projectiles
 {
 	// This projectile demonstrates exploding tiles (like a bomb or dynamite), spawning child projectiles, and explosive visual effects.
-	// TODO: This projectile does not currently damage the owner, or damage other players on the For the worthy secret seed.
 	public class ExampleExplosive : ModProjectile
 	{
 		private const int DefaultWidthHeight = 15;
 		private const int ExplosionWidthHeight = 250;
+
+		private bool IsChild {
+			get => Projectile.localAI[0] == 1;
+			set => Projectile.localAI[0] = value.ToInt();
+		}
+
+		public override void SetStaticDefaults() {
+			ProjectileID.Sets.PlayerHurtDamageIgnoresDifficultyScaling[Type] = true; // Damage dealt to players does not scale with difficulty in vanilla.
+
+			// This set handles some things for us already:
+			// Sets the timeLeft to 3 and the projectile direction when colliding with an NPC or player in PVP (so the explosive can detonate).
+			// Explosives also bounce off the top of Shimmer, detonate with no blast damage when touching the bottom or sides of Shimmer, and damage other players in For the Worthy worlds.
+			ProjectileID.Sets.Explosive[Type] = true;
+		}
 
 		public override void SetDefaults() {
 			// While the sprite is actually bigger than 15x15, we use 15x15 since it lets the projectile clip into tiles as it bounces. It looks better.
@@ -40,8 +53,11 @@ namespace ExampleMod.Content.Projectiles
 
 		// The projectile is very bouncy, but the spawned children projectiles shouldn't bounce at all.
 		public override bool OnTileCollide(Vector2 oldVelocity) {
-			// Die immediately if ai[1] isn't 0 (We set this to 1 for the 5 extra explosives we spawn in Kill)
-			if (Projectile.ai[1] != 0) {
+			// Die immediately if IsChild is true (We set this to true for the 5 extra explosives we spawn in OnKill)
+			if (IsChild) {
+				// These two are so the bomb will damage the player correctly.
+				Projectile.timeLeft = 0;
+				Projectile.PrepareBombToBlow();
 				return true;
 			}
 			// OnTileCollide can trigger quite frequently, so using soundDelay helps prevent the sound from overlapping too much.
@@ -68,15 +84,7 @@ namespace ExampleMod.Content.Projectiles
 		public override void AI() {
 			// The projectile is in the midst of exploding during the last 3 updates.
 			if (Projectile.owner == Main.myPlayer && Projectile.timeLeft <= 3) {
-				Projectile.tileCollide = false;
-				// Set to transparent. This projectile technically lives as transparent for about 3 frames
-				Projectile.alpha = 255;
-
-				// change the hitbox size, centered about the original projectile center. This makes the projectile damage enemies during the explosion.
-				Projectile.Resize(ExplosionWidthHeight, ExplosionWidthHeight);
-
-				Projectile.damage = 250;
-				Projectile.knockBack = 10f;
+				Projectile.PrepareBombToBlow(); // Get ready to explode.
 			}
 			else {
 				// Smoke and fuse dust spawn. The position is calculated to spawn the dust directly on the fuse.
@@ -112,14 +120,27 @@ namespace ExampleMod.Content.Projectiles
 			Projectile.rotation += Projectile.velocity.X * 0.1f;
 		}
 
+		public override void PrepareBombToBlow() {
+			Projectile.tileCollide = false; // This is important or the explosion will be in the wrong place if the bomb explodes on slopes.
+			Projectile.alpha = 255; // Set to transparent. This projectile technically lives as transparent for about 3 frames
+
+			// Change the hitbox size, centered about the original projectile center. This makes the projectile damage enemies during the explosion.
+			Projectile.Resize(ExplosionWidthHeight, ExplosionWidthHeight);
+
+			Projectile.damage = 250; // Bomb: 100, Dynamite: 250
+			Projectile.knockBack = 10f; // Bomb: 8f, Dynamite: 10f
+		}
+
 		public override void OnKill(int timeLeft) {
 			// If we are the original projectile running on the owner, spawn the 5 child projectiles.
-			if (Projectile.owner == Main.myPlayer && Projectile.ai[1] == 0) {
+			if (Projectile.owner == Main.myPlayer && !IsChild) {
 				for (int i = 0; i < 5; i++) {
 					// Random upward vector.
 					Vector2 launchVelocity = new Vector2(Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-10, -8));
-					// Importantly, ai1 is set to 1 here. This is checked in OnTileCollide to prevent bouncing and here in Kill to prevent an infinite chain of splitting projectiles.
-					Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, launchVelocity, Projectile.type, Projectile.damage, Projectile.knockBack, Main.myPlayer, 0, 1);
+					// Importantly, IsChild is set to true here. This is checked in OnTileCollide to prevent bouncing and here in OnKill to prevent an infinite chain of splitting projectiles.
+					Projectile child = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, launchVelocity, Projectile.type, Projectile.damage, Projectile.knockBack, Main.myPlayer, 0, 1);
+					(child.ModProjectile as ExampleExplosive).IsChild = true;
+					// Usually editing a projectile after NewProjectile would require sending MessageID.SyncProjectile, but IsChild only affects logic running for the owner so it is not necessary here.
 				}
 			}
 
@@ -165,7 +186,7 @@ namespace ExampleMod.Content.Projectiles
 
 			// Finally, actually explode the tiles and walls. Run this code only for the owner
 			if (Projectile.owner == Main.myPlayer) {
-				int explosionRadius = 7;
+				int explosionRadius = 7; // Bomb: 4, Dynamite: 7, Explosives & TNT Barrel: 10
 				int minTileX = (int)(Projectile.Center.X / 16f - explosionRadius);
 				int maxTileX = (int)(Projectile.Center.X / 16f + explosionRadius);
 				int minTileY = (int)(Projectile.Center.Y / 16f - explosionRadius);

commit 8d140dd9ff7dddf418ba904812a24fb70efd833f

Fix for Celebration regression from #3501

ExampleMod/Content/Items/Ammo/ExampleRocket.cs
@@ -40,10 +40,8 @@ namespace ExampleMod.Content.Items.Ammo
 			Item.maxStack = Item.CommonMaxStack;
 			Item.value = Item.buyPrice(copper: 50);
 			Item.ammo = AmmoID.Rocket; // The ammo type is Rocket Ammo
-
-			// The projectile that is set in Item.shoot will be the "default" projectile.
-			// i.e. if a launcher doesn't have this ammo defined in SpecificLauncherAmmoProjectileMatches, it'll use this projectile instead.
-			Item.shoot = ModContent.ProjectileType<ExampleRocketProjectile>();
+			// Unlike other ammo, we don't set Item.shoot to the projectile for rocket ammo due to the logic involved.
+			// AmmoID.Sets.SpecificLauncherAmmoProjectileMatches is used to determine the projectile spawned based on the weapon.
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.

patches/tModLoader/Terraria/Player.cs.patch
@@ -7101,6 +7101,16 @@
  		KnockBack = GetWeaponKnockback(sItem, KnockBack);
  		IEntitySource projectileSource_Item_WithPotentialAmmo = GetProjectileSource_Item_WithPotentialAmmo(sItem, usedAmmoItemId);
  		if (projToShoot == 228)
+@@ -36695,6 +_,9 @@
+ 		if (sItem.type == 2796)
+ 			projToShoot = 442;
+ 
++		if (sItem.type == ItemID.FireworksLauncher)
++			projToShoot = ProjectileID.RocketFireworkRed; // Make sure Celebration has correct projectile set for logic.
++
+ 		if (sItem.type == 2223)
+ 			projToShoot = 357;
+ 
 @@ -36708,7 +_,8 @@
  		Vector2 pointPoisition = RotatedRelativePoint(MountedCenter);
  		bool flag = true;
@@ -7648,7 +7658,7 @@
  					item = inventory[k];
  					canShoot = true;
  					break;
-@@ -40981,10 +_,43 @@
+@@ -40981,17 +_,52 @@
  			}
  		}
  
@@ -7692,6 +7702,15 @@
  		int pickedProjectileId = -1;
  		if (PickAmmo_TryFindingSpecificMatches(sItem.type, item.type, out pickedProjectileId))
  			projToShoot = pickedProjectileId;
++		/* These 2 are unused and will never be right for modded rocket ammo. Following check is kept in to preserve existing behavior if being relied on.
+ 		else if (sItem.type == 1946)
+ 			projToShoot = 338 + item.type - 771;
+ 		else if (sItem.type == 3930)
+ 			projToShoot = 715 + item.type - AmmoID.Rocket;
++		*/
+ 		else if (sItem.useAmmo == AmmoID.Rocket)
+ 			projToShoot += item.shoot;
+ 		else if (sItem.useAmmo == AmmoID.Solution)
 @@ -41015,6 +_,7 @@
  			projToShoot = 117;
  