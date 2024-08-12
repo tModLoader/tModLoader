fc9bc9ea08f8a1bf043716f7a86ba338e0bb8d58
Adrian Lück <github@sl.alueck.de>
2024-08-12 22:35:37 +0200

    Revert NPCShop.Entry changes

--- patches/tModLoader/Terraria/ModLoader/NPCShop.Entry.cs
+++ patches/tModLoader/Terraria/ModLoader/NPCShop.Entry.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Terraria.GameContent.Items;
+using System;
 
 namespace Terraria.ModLoader;
 
