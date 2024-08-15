2849d0efdec9c56e7f8e4636d16263c15d9ffeea
JavidPack <javidpack@gmail.com>
2024-08-15 12:28:26 -0600

    Adjust terminal config color for getters,

--- patches/tModLoader/Terraria/ModLoader/UI/Interface.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/Interface.cs
@@ -551,7 +551,7 @@ internal static class Interface
 			int size = text.Length;
 			text = (variable.CanWrite ? key : "-") + "\t" + text + new string('\t', Math.Max((55 - size) / 8, 1));
 			if (!variable.CanWrite)
-				Console.ForegroundColor = ConsoleColor.DarkGray;
+				Console.ForegroundColor = (Console.BackgroundColor == ConsoleColor.DarkGray || Console.BackgroundColor == ConsoleColor.Gray) ? ConsoleColor.Blue : ConsoleColor.DarkGray;
 			text += JsonConvert.SerializeObject(variable.GetValue(config));
 			MethodInfo methodInfo = variable.Type.GetMethod("ToString", Array.Empty<Type>());
 			bool hasToString = methodInfo != null && methodInfo.DeclaringType != typeof(object);
