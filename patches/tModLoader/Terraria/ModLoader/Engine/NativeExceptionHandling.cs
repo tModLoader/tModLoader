a7c997bf9d4541476e41a7c33ced50470262a32d
JavidPack <javidpack@gmail.com>
2024-08-21 16:17:09 -0600

    Initial commit

---
+++ patches/tModLoader/Terraria/ModLoader/Engine/NativeExceptionHandling.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Terraria.ModLoader.Engine;
+
+internal static class NativeExceptionHandling
+{
+	internal static void Init(){
+		if (!OperatingSystem.IsWindows())
+			return;
+
+		// Note: Only called when not being debugged!
+		SetUnhandledExceptionFilter(OurUnhandledExceptionFilter);
+	}
+
+	// Unhandled exception filter callback
+	static IntPtr OurUnhandledExceptionFilter(IntPtr exceptionInfo)
+	{
+		Logging.tML.Fatal("Native exception has occurred, attempting to determine erroring module...");
+
+		// System.Diagnostics.Debugger.Launch(); // To debug this code, uncomment this line and start tModLoader without debugging.
+
+		// Cast the pointer to EXCEPTION_POINTERS
+		var exPointers = Marshal.PtrToStructure<EXCEPTION_POINTERS>(exceptionInfo);
+
+		// Get the exception address
+		var ExceptionRecord = Marshal.PtrToStructure<EXCEPTION_RECORD>(exPointers.ExceptionRecord);
+		IntPtr exceptionAddress = ExceptionRecord.ExceptionAddress;
+
+		// Try to get the module handle
+		IntPtr moduleHandle;
+		bool success = GetModuleHandleEx(
+			GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+			exceptionAddress, out moduleHandle);
+
+		if (success && moduleHandle != IntPtr.Zero) {
+			// Get the module file name
+			StringBuilder moduleName = new StringBuilder(260);
+			GetModuleFileName(moduleHandle, moduleName, moduleName.Capacity);
+
+			// Log the module file path
+			Logging.tML.Fatal("Exception occurred in module: " + moduleName.ToString());
+			// These end up in the console or also natives.log if launched through launch script
+		}
+		else {
+			Logging.tML.Fatal("Failed to retrieve module information.");
+		}
+
+		// Return EXCEPTION_EXECUTE_HANDLER to let the OS handle the exception
+		return (IntPtr)1;
+	}
+
+	// P/Invoke declarations
+	[DllImport("kernel32.dll")]
+	static extern IntPtr SetUnhandledExceptionFilter(UnhandledExceptionFilter filter);
+
+	[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+	static extern bool GetModuleHandleEx(uint dwFlags, IntPtr lpModuleName, out IntPtr phModule);
+
+	[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+	static extern uint GetModuleFileName(IntPtr hModule, StringBuilder lpFilename, int nSize);
+	
+
+	// Constants for GetModuleHandleEx
+	const uint GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS = 0x00000004;
+	const uint GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT = 0x00000002;
+
+	// Structure definitions
+	[StructLayout(LayoutKind.Sequential)]
+	struct EXCEPTION_RECORD
+	{
+		public uint ExceptionCode;
+		public uint ExceptionFlags;
+		public IntPtr ExceptionRecord;
+		public IntPtr ExceptionAddress;
+		public uint NumberParameters;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
+		public IntPtr[] ExceptionInformation;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	struct EXCEPTION_POINTERS
+	{
+		public IntPtr ExceptionRecord;
+		public IntPtr ContextRecord;
+	}
+
+	// Delegate for the unhandled exception filter
+	delegate IntPtr UnhandledExceptionFilter(IntPtr exceptionInfo);
+}
