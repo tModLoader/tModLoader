09bf4a82ff6d0dc93863469bdb1c106dd9325fc8
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2024-10-23 21:26:12 -0700

    Re-add Assembly Location change lost in merge

--- patches/tModLoader/Terraria/ModLoader/UI/UIModSourceItem.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/UIModSourceItem.cs
@@ -36,7 +36,7 @@ internal class UIModSourceItem : UIPanel
 	private Task<string[]> langFileTask;
 	private Task<bool> sourceUpgradeTask;
 	private CancellationToken _modSourcesToken;
-	
+
 	public UIModSourceItem(string mod, LocalMod builtMod, CancellationToken modSourcesToken)
 	{
 		_mod = mod;
@@ -420,7 +420,7 @@ internal class UIModSourceItem : UIPanel
 			string csprojFile = Path.Combine(_mod, $"{modFolderName}.csproj");
 
 			string args = $"\"{csprojFile}\"";
-			var tMLPath = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+			var tMLPath = Path.GetFileName(Program.tMLAssemblyLocation);
 			var porterPath = Path.Combine(Path.GetDirectoryName(tMLPath), "tModPorter", (Platform.IsWindows ? "tModPorter.bat" : "tModPorter.sh"));
 
 			var porterInfo = new ProcessStartInfo() {
