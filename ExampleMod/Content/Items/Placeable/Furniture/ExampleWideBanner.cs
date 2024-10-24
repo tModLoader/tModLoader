a926eb795873ca71e6afb6fca283b51c6c617f03
JavidPack <javidpack@gmail.com>
2024-10-24 13:27:45 -0600

    Fix formatting

--- ExampleMod/Content/Items/Placeable/Furniture/ExampleWideBanner.cs
+++ ExampleMod/Content/Items/Placeable/Furniture/ExampleWideBanner.cs
@@ -6,17 +6,18 @@ namespace ExampleMod.Content.Items.Placeable.Furniture;
 /// <summary>
 /// Item that places <see cref="ExampleWideBannerTile"/>
 /// </summary>
-public class ExampleWideBanner : ModItem {
-   public override void SetDefaults() {
-      Item.DefaultToPlaceableTile(ModContent.TileType<ExampleWideBannerTile>());
-      Item.value = Terraria.Item.buyPrice(copper: 10);
-   }
-   
-   // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
-   	public override void AddRecipes() {
-   		CreateRecipe()
-   			.AddIngredient<ExampleItem>()
-   			.AddTile<Tiles.Furniture.ExampleWorkbench>()
-   			.Register();
-   	}
+public class ExampleWideBanner : ModItem
+{
+	public override void SetDefaults() {
+		Item.DefaultToPlaceableTile(ModContent.TileType<ExampleWideBannerTile>());
+		Item.value = Terraria.Item.buyPrice(copper: 10);
+	}
+
+	// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+	public override void AddRecipes() {
+		CreateRecipe()
+			.AddIngredient<ExampleItem>()
+			.AddTile<Tiles.Furniture.ExampleWorkbench>()
+			.Register();
+	}
 }
