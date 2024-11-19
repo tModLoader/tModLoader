b85e5aac19a916c33462adb21053f76d897220cf
Tyfyter <tyfyter2616@gmail.com>
2024-11-19 16:22:59 -0700

    More declarative chest loot progress

---
+++ patches/tModLoader/Terraria/GameContent/ItemDropRules/DropFromItemPoolRule.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.Utilities;
+
+namespace Terraria.GameContent.ItemDropRules;
+internal class DropFromItemPoolRule : IItemDropRule
+{
+	public string poolName;
+	public int chanceDenominator;
+	public int amountDroppedMinimum;
+	public int amountDroppedMaximum;
+	public int chanceNumerator;
+	public List<IItemDropRuleChainAttempt> ChainedRules { get; private set; }
+	public DropFromItemPoolRule(string poolName, int chanceDenominator, int amountDroppedMinimum = 1, int amountDroppedMaximum = 1, int chanceNumerator = 1)
+	{
+		if (amountDroppedMinimum > amountDroppedMaximum) {
+			throw new ArgumentOutOfRangeException(nameof(amountDroppedMinimum), $"{nameof(amountDroppedMinimum)} must be lesser or equal to {nameof(amountDroppedMaximum)}.");
+		}
+
+		this.poolName = poolName;
+		this.chanceDenominator = chanceDenominator;
+		this.amountDroppedMinimum = amountDroppedMinimum;
+		this.amountDroppedMaximum = amountDroppedMaximum;
+		this.chanceNumerator = chanceNumerator;
+		ChainedRules = [];
+	}
+
+	public bool CanDrop(DropAttemptInfo info) => true;
+
+	public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info)
+	{
+		ItemDropAttemptResult result;
+		if (info.player.RollLuck(chanceDenominator) < chanceNumerator) {
+			Tuple<(int type, List<IItemDropRule> chainedRules), double>[] options = GetDropableEntries(info).ToArray();
+			if (options.Length == 0) {
+				result = default;
+				result.State = ItemDropAttemptResultState.DoesntFillConditions;
+				return result;
+			}
+			(int itemId, List<IItemDropRule> chainedRules) = new WeightedRandom<(int, List<IItemDropRule>)>(info.rng, options).Get();
+			CommonCode.DropItem(info, itemId, info.rng.Next(amountDroppedMinimum, amountDroppedMaximum + 1));
+			for (int i = 0; i < chainedRules.Count; i++) {
+				ItemDropResolver.ResolveRule(chainedRules[i], info);
+			}
+			result = default;
+			result.State = ItemDropAttemptResultState.Success;
+			return result;
+		}
+
+		result = default;
+		result.State = ItemDropAttemptResultState.FailedRandomRoll;
+		return result;
+	}
+
+	public void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo)
+	{
+		float num = (float)chanceNumerator / (float)chanceDenominator;
+		float dropRate = num * ratesInfo.parentDroprateChance;
+		DropRateInfoChainFeed thisRatesInfo = ratesInfo;
+		thisRatesInfo.parentDroprateChance = dropRate;
+
+		foreach (IGrouping<HashSet<IItemDropRuleCondition>, ItemPoolEntry> group in ChestLootLoader.GetItemPool(poolName).GroupBy(e => e.Conditions.ToHashSet(), HashSet<IItemDropRuleCondition>.CreateSetComparer())) {
+			DropRateInfoChainFeed groupRatesInfo = thisRatesInfo;
+			groupRatesInfo.conditions = groupRatesInfo.conditions.Concat(group.Key).ToList();
+			float totalWeight = 0;
+			foreach (ItemPoolEntry item in group)
+				totalWeight += item.Weight;
+			foreach (ItemPoolEntry item in group) {
+				DropRateInfoChainFeed itemRatesInfo = groupRatesInfo;
+				itemRatesInfo.parentDroprateChance *= item.Weight / totalWeight;
+				drops.Add(new DropRateInfo(item.Type, amountDroppedMinimum, amountDroppedMaximum, dropRate, itemRatesInfo.conditions));
+				foreach (IItemDropRule ChainedRule in item.ChainedRules) {
+					ChainedRule.ReportDroprates(drops, itemRatesInfo);
+				}
+			}
+		}
+		Chains.ReportDroprates(ChainedRules, num, drops, ratesInfo);
+	}
+
+	private IEnumerable<Tuple<(int type, List<IItemDropRule> chainedRules), double>> GetDropableEntries(DropAttemptInfo info)
+	{
+		List<ItemPoolEntry> entries = ChestLootLoader.GetItemPool(poolName);
+		for (int i = 0; i < entries.Count; i++) {
+			ItemPoolEntry entry = entries[i];
+			bool shouldBreak = false;
+			for (int j = 0; j < entry.Conditions.Count && !shouldBreak; j++) {
+				if (!entry.Conditions[j].CanDrop(info))
+					shouldBreak = true;
+			}
+			if (shouldBreak)
+				break;
+			yield return new((entry.Type, entry.ChainedRules), entry.Weight);
+		}
+	}
+}
--- patches/tModLoader/Terraria/ModLoader/ChestLootLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/ChestLootLoader.cs
@@ -1,46 +1,116 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using Microsoft.Xna.Framework;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
+using Terraria.Localization;
+using Terraria.WorldBuilding;
 using static Terraria.GameContent.ItemDropRules.ItemDropRule;
 
 namespace Terraria.ModLoader;
 
 public static class ChestLootLoader
 {
-	static Dictionary<string, List<IItemDropRule>> lootPools = [];
+	private static readonly Dictionary<string, List<IItemDropRule>> lootPools = [];
+	private static readonly Dictionary<string, List<ItemPoolEntry>> itemPools = [];
 	static ChestLootLoader()
 	{
 		RegisterDefaultLootPools();
 	}
 
-	public static void Add(string name, List<IItemDropRule> lootPool)
+	public static void AddLootPool(string name, List<IItemDropRule> lootPool)
 	{
 		lootPools.Add(name, lootPool);
+	}
+	public static void AddItemPool(string name, List<ItemPoolEntry> itemPool)
+	{
+		itemPools.Add(name, itemPool);
 	}
 
 	internal static void Unload()
 	{
 		RegisterDefaultLootPools();
 	}
+	public static class ItemPoolNames
+	{
+		public const string CopperBar = nameof(CopperBar);
+		public const string IronBar = nameof(IronBar);
+		public const string SilverBar = nameof(SilverBar);
+		public const string GoldBar = nameof(GoldBar);
+		public const string SurfaceWoodenPrimary = nameof(SurfaceWoodenPrimary);
+		public const string DesertHighPrimary = nameof(DesertHighPrimary);
+		public const string DesertLowPrimary = nameof(DesertLowPrimary);
+	}
 
+	private static readonly Dictionary<(string name, int type), SimpleItemDropRuleCondition> genVarConditions = [];
+	public static SimpleItemDropRuleCondition GenVarCondition(string name, int type)
+	{
+		if (genVarConditions.TryGetValue((name, type), out SimpleItemDropRuleCondition condition))
+			return condition;
+		FieldInfo field = typeof(GenVars).GetField(name) ?? throw new ArgumentException($"No such static field {nameof(GenVars)}.{name} exists", nameof(name));
+		if (field.FieldType != typeof(int))
+			throw new ArgumentException($"Field type must be {typeof(int)}", nameof(name));
+		DynamicMethod getterMethod = new($"GenVars.{name}_Equals_{type}", typeof(bool), [], true);
+		ILGenerator gen = getterMethod.GetILGenerator();
+
+		gen.Emit(OpCodes.Ldsfld, field);
+		gen.Emit(OpCodes.Ldc_I4, type);
+		gen.Emit(OpCodes.Ceq);
+		gen.Emit(OpCodes.Ret);
+
+		genVarConditions[(name, type)] = condition = new Condition(LocalizedText.Empty, getterMethod.CreateDelegate<Func<bool>>()).ToDropCondition(ShowItemDropInUI.Always);
+		return condition;
+	}
+	public static ItemPoolEntry DropItemFromGenVar(string name, int type) => new(type, [GenVarCondition(name, type)]);
 	internal static void RegisterDefaultLootPools()
 	{
+		genVarConditions.Clear();
 		lootPools.Clear();
+		itemPools.Clear();
+		AddItemPool(ItemPoolNames.SurfaceWoodenPrimary, [
+			new(ItemID.Spear),
+			new(ItemID.Blowpipe),
+			new(ItemID.WoodenBoomerang),
+			new(ItemID.Aglet),
+			new(ItemID.ClimbingClaws),
+			new(ItemID.Umbrella),
+			new(ItemID.CordageGuide),
+			new(ItemID.WandofSparking),
+			new(ItemID.Radar),
+			new(ItemID.PortableStool)
+		]);
+		AddItemPool(ItemPoolNames.DesertHighPrimary, [
+			new(ItemID.AncientChisel),
+			new(ItemID.SandBoots),
+			new(ItemID.MysticCoilSnake),
+			new(ItemID.MagicConch)
+		]);
+		AddItemPool(ItemPoolNames.DesertLowPrimary, [
+			new(ItemID.ThunderSpear),
+			new(ItemID.ThunderStaff),
+			new(ItemID.CatBast)
+		]);
+		AddItemPool(ItemPoolNames.CopperBar, [
+			DropItemFromGenVar(nameof(GenVars.copperBar), ItemID.CopperBar),
+			DropItemFromGenVar(nameof(GenVars.copperBar), ItemID.TinBar)
+		]);
+		AddItemPool(ItemPoolNames.IronBar, [
+			DropItemFromGenVar(nameof(GenVars.ironBar), ItemID.IronBar),
+			DropItemFromGenVar(nameof(GenVars.ironBar), ItemID.LeadBar)
+		]);
+		AddItemPool(ItemPoolNames.SilverBar, [
+			DropItemFromGenVar(nameof(GenVars.silverBar), ItemID.SilverBar),
+			DropItemFromGenVar(nameof(GenVars.silverBar), ItemID.TungstenBar)
+		]);
+		AddItemPool(ItemPoolNames.GoldBar, [
+			DropItemFromGenVar(nameof(GenVars.goldBar), ItemID.GoldBar),
+			DropItemFromGenVar(nameof(GenVars.goldBar), ItemID.PlatinumBar)
+		]);
 		lootPools["SurfaceWooden"] = [
-			new OneFromRulesRule(1,
-				Common(ItemID.Spear),
-				Common(ItemID.Blowpipe),
-				Common(ItemID.WoodenBoomerang),
-				Common(ItemID.Aglet),
-				Common(ItemID.ClimbingClaws),
-				Common(ItemID.Umbrella),
-				Common(ItemID.CordageGuide),
-				Common(ItemID.WandofSparking),
-				Common(ItemID.Radar),
-				Common(ItemID.PortableStool)
-			),
+			new DropFromItemPoolRule(ItemPoolNames.SurfaceWoodenPrimary, 1),
 			Common(ItemID.Glowstick, chanceDenominator: 6, minimumDropped: 40, maximumDropped: 75),
 			Common(ItemID.ThrowingKnife, chanceDenominator: 6, minimumDropped: 150, maximumDropped: 300),
 			Common(ItemID.HerbBag, chanceDenominator: 6, minimumDropped: 1, maximumDropped: 4),
@@ -81,4 +151,10 @@ public static class ChestLootLoader
 	}
 
 	public static List<IItemDropRule> GetLootPool(string name) => lootPools.TryGetValue(name, out var pool) ? pool : null;
+	public static List<ItemPoolEntry> GetItemPool(string name) => itemPools.TryGetValue(name, out var pool) ? pool : null;
+}
+public record class ItemPoolEntry(int Type, List<IItemDropRuleCondition> Conditions = null, List<IItemDropRule> ChainedRules = null, float Weight = 1f)
+{
+	public List<IItemDropRuleCondition> Conditions { get; } = Conditions ?? [];
+	public List<IItemDropRule> ChainedRules { get; } = ChainedRules ?? [];
 }
