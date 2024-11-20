96de8c12c1b3bb15dd352911abd9b9cd4a2fe6d9
JavidPack <javidpack@gmail.com>
2024-11-20 13:57:07 -0700

    Rename custom set method overloads to CreateNamedXSet

--- patches/tModLoader/Terraria/ID/SetFactory.TML.cs
+++ patches/tModLoader/Terraria/ID/SetFactory.TML.cs
@@ -22,74 +22,74 @@ public partial class SetFactory
 
 	// Copies of existing methods with an additional key parameter.
 	// TODO: CreateNamedCustomSet?
-	public T[] CreateCustomSet<T>(string key, T defaultState, params object[] inputs)
+	public T[] CreateNamedCustomSet<T>(string key, T defaultState, params object[] inputs)
 	{
 		var set = CreateCustomSet(defaultState, inputs);
-		RegisterCustomSet(key, defaultState, ref set);
+		RegisterNamedCustomSet(key, defaultState, ref set);
 		return set;
 	}
 
-	public float[] CreateFloatSet(string key, float defaultState, params float[] inputs)
+	public float[] CreateNamedFloatSet(string key, float defaultState, params float[] inputs)
 	{
 		var set = CreateFloatSet(defaultState, inputs);
-		RegisterCustomSet(key, defaultState, ref set);
+		RegisterNamedCustomSet(key, defaultState, ref set);
 		return set;
 	}
-	public ushort[] CreateUshortSet(string key, ushort defaultState, params ushort[] inputs)
+	public ushort[] CreateNamedUshortSet(string key, ushort defaultState, params ushort[] inputs)
 	{
 		var set = CreateUshortSet(defaultState, inputs);
-		RegisterCustomSet(key, defaultState, ref set);
+		RegisterNamedCustomSet(key, defaultState, ref set);
 		return set;
 	}
-	public int[] CreateIntSet(string key, int defaultState, params int[] inputs)
+	public int[] CreateNamedIntSet(string key, int defaultState, params int[] inputs)
 	{
 		var set = CreateIntSet(defaultState, inputs);
-		RegisterCustomSet(key, defaultState, ref set);
+		RegisterNamedCustomSet(key, defaultState, ref set);
 		return set;
 	}
-	public int[] CreateIntSet(string key, params int[] types)
+	public int[] CreateNamedIntSet(string key, params int[] types)
 	{
 		var set = CreateIntSet(types);
-		RegisterCustomSet(key, -1, ref set);
+		RegisterNamedCustomSet(key, -1, ref set);
 		return set;
 	}
-	public bool[] CreateBoolSet(string key, params int[] types)
+	public bool[] CreateNamedBoolSet(string key, params int[] types)
 	{
 		var set = CreateBoolSet(false, types);
-		RegisterCustomSet(key, false, ref set);
+		RegisterNamedCustomSet(key, false, ref set);
 		return set;
 	}
-	public bool[] CreateBoolSet(string key, bool defaultState, params int[] types)
+	public bool[] CreateNamedBoolSet(string key, bool defaultState, params int[] types)
 	{
 		var set = CreateBoolSet(defaultState, types);
-		RegisterCustomSet(key, defaultState, ref set);
+		RegisterNamedCustomSet(key, defaultState, ref set);
 		return set;
 	}
 
 	// modName + key overloads
-	public T[] CreateCustomSet<T>(string modName, string key, T defaultState, params object[] inputs) => CreateCustomSet<T>($"{modName}/{key}", defaultState, inputs);
-	public float[] CreateFloatSet(string modName, string key, float defaultState, params float[] inputs) => CreateFloatSet($"{modName}/{key}", defaultState, inputs);
-	public ushort[] CreateUshortSet(string modName, string key, ushort defaultState, params ushort[] inputs) => CreateUshortSet($"{modName}/{key}", defaultState, inputs);
-	public int[] CreateIntSet(string modName, string key, int defaultState, params int[] inputs) => CreateIntSet($"{modName}/{key}", defaultState, inputs);
-	public int[] CreateIntSet(string modName, string key, params int[] types) => CreateIntSet($"{modName}/{key}", -1, types);
-	public bool[] CreateBoolSet(string modName, string key, params int[] types) => CreateBoolSet($"{modName}/{key}", false, types);
-	public bool[] CreateBoolSet(string modName, string key, bool defaultState, params int[] types) => CreateBoolSet($"{modName}/{key}", defaultState, types);
+	public T[] CreateNamedCustomSet<T>(string modName, string key, T defaultState, params object[] inputs) => CreateNamedCustomSet<T>($"{modName}/{key}", defaultState, inputs);
+	public float[] CreateNamedFloatSet(string modName, string key, float defaultState, params float[] inputs) => CreateNamedFloatSet($"{modName}/{key}", defaultState, inputs);
+	public ushort[] CreateNamedUshortSet(string modName, string key, ushort defaultState, params ushort[] inputs) => CreateNamedUshortSet($"{modName}/{key}", defaultState, inputs);
+	public int[] CreateNamedIntSet(string modName, string key, int defaultState, params int[] inputs) => CreateNamedIntSet($"{modName}/{key}", defaultState, inputs);
+	public int[] CreateNamedIntSet(string modName, string key, params int[] types) => CreateNamedIntSet($"{modName}/{key}", -1, types);
+	public bool[] CreateNamedBoolSet(string modName, string key, params int[] types) => CreateNamedBoolSet($"{modName}/{key}", false, types);
+	public bool[] CreateNamedBoolSet(string modName, string key, bool defaultState, params int[] types) => CreateNamedBoolSet($"{modName}/{key}", defaultState, types);
 
 	// Mod + key overloads
-	public T[] CreateCustomSet<T>(Mod mod, string key, T defaultState, params object[] inputs) => CreateCustomSet<T>($"{mod.Name}/{key}", defaultState, inputs);
-	public float[] CreateFloatSet(Mod mod, string key, float defaultState, params float[] inputs) => CreateFloatSet($"{mod.Name}/{key}", defaultState, inputs);
-	public ushort[] CreateUshortSet(Mod mod, string key, ushort defaultState, params ushort[] inputs) => CreateUshortSet($"{mod.Name}/{key}", defaultState, inputs);
-	public int[] CreateIntSet(Mod mod, string key, int defaultState, params int[] inputs) => CreateIntSet($"{mod.Name}/{key}", defaultState, inputs);
-	public int[] CreateIntSet(Mod mod, string key, params int[] types) => CreateIntSet($"{mod.Name}/{key}", -1, types);
-	public bool[] CreateBoolSet(Mod mod, string key, params int[] types) => CreateBoolSet($"{mod.Name}/{key}", false, types);
-	public bool[] CreateBoolSet(Mod mod, string key, bool defaultState, params int[] types) => CreateBoolSet($"{mod.Name}/{key}", defaultState, types);
+	public T[] CreateNamedCustomSet<T>(Mod mod, string key, T defaultState, params object[] inputs) => CreateNamedCustomSet<T>($"{mod.Name}/{key}", defaultState, inputs);
+	public float[] CreateNamedFloatSet(Mod mod, string key, float defaultState, params float[] inputs) => CreateNamedFloatSet($"{mod.Name}/{key}", defaultState, inputs);
+	public ushort[] CreateNamedUshortSet(Mod mod, string key, ushort defaultState, params ushort[] inputs) => CreateNamedUshortSet($"{mod.Name}/{key}", defaultState, inputs);
+	public int[] CreateNamedIntSet(Mod mod, string key, int defaultState, params int[] inputs) => CreateNamedIntSet($"{mod.Name}/{key}", defaultState, inputs);
+	public int[] CreateNamedIntSet(Mod mod, string key, params int[] types) => CreateNamedIntSet($"{mod.Name}/{key}", -1, types);
+	public bool[] CreateNamedBoolSet(Mod mod, string key, params int[] types) => CreateNamedBoolSet($"{mod.Name}/{key}", false, types);
+	public bool[] CreateNamedBoolSet(Mod mod, string key, bool defaultState, params int[] types) => CreateNamedBoolSet($"{mod.Name}/{key}", defaultState, types);
 
 	/// <summary>
 	/// Registers a custom "set", meaning an array of values of length equal to the count of the content the set corresponds to. This is typically done through the Terraria.ID.XID.Sets.Factory.CreateXSet method.
 	/// <para/> The set reference passed in may change as a result of this method. This method will merge sets together regardless of mod load order, allowing for ad-hoc collaboration. Note that this merge behavior is dependent on mods agreeing on key and default value. It is important that set names are unique, so it is good practice to include the entity name in the set name to avoid mods accidentally using the same name for different things. For example, a set named "Acidic" might be used by 1 mod to describe projectiles and another mod to describe items. Sets representing mod-specific ideas should prepend the key with the mod name to ensure a unique key that will not be used by any other mod: "ExampleMod/Jiggly"
 	/// <para/> Throws an exception if the Type, data length, or default value does not match the data registered using the same key by any mod loaded before this mod.
 	/// </summary>
-	public void RegisterCustomSet<T>(string key, T defaultValue, ref T[] input)
+	public void RegisterNamedCustomSet<T>(string key, T defaultValue, ref T[] input)
 	{
 		// Could make a ModLoader.loadStage enum or another bool, but this behaves exactly how we want anyway.
 		if (!ContentCache.contentLoadingFinished) 
@@ -152,18 +152,17 @@ public partial class SetFactory
 		input = value;
 	}
 
-
 	/// <summary>
-	/// <inheritdoc cref="RegisterCustomSet{T}(string, T, ref T[])"/>
+	/// <inheritdoc cref="RegisterNamedCustomSet{T}(string, T, ref T[])"/>
 	/// <para/> This particular overload will result in a final key constructed from the provided <paramref name="modName"/> and <paramref name="key"/>: "{modName}/{key}".
 	/// </summary>
-	public void RegisterCustomSet<T>(string modName, string key, T defaultValue, ref T[] input) => RegisterCustomSet($"{modName}/{key}", defaultValue, ref input);
+	public void RegisterNamedCustomSet<T>(string modName, string key, T defaultValue, ref T[] input) => RegisterNamedCustomSet($"{modName}/{key}", defaultValue, ref input);
 
 	/// <summary>
-	/// <inheritdoc cref="RegisterCustomSet{T}(string, T, ref T[])"/>
+	/// <inheritdoc cref="RegisterNamedCustomSet{T}(string, T, ref T[])"/>
 	/// <para/> This particular overload will result in a final key constructed from the provided <paramref name="mod"/> and <paramref name="key"/>: "{mod.Name}/{key}".
 	/// </summary>
-	public void RegisterCustomSet<T>(Mod mod, string key, T defaultValue, ref T[] input) => RegisterCustomSet($"{mod.Name}/{key}", defaultValue, ref input);
+	public void RegisterNamedCustomSet<T>(Mod mod, string key, T defaultValue, ref T[] input) => RegisterNamedCustomSet($"{mod.Name}/{key}", defaultValue, ref input);
 
 	/* Error prone method overload? No need.
 	public T[] RegisterCustomSet<T>(Mod mod, string key, T defaultValue, T[] input) => RegisterCustomSet($"{mod.Name}/{key}", defaultValue, input);
