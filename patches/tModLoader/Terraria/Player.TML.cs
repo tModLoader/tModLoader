7665a0480ea97001edcac347cb31ab26acbae23c
JavidPack <javidpack@gmail.com>
2025-02-05 14:39:47 -0700

    Update docs

--- patches/tModLoader/Terraria/Player.TML.cs
+++ patches/tModLoader/Terraria/Player.TML.cs
@@ -85,6 +85,10 @@ public partial class Player : IEntityWithInstances<ModPlayer>
 	/// </summary>
 	public const int ManaCrystalMax = 9;
 
+	/// <summary>
+	/// How effectively the player can hold their breath underwater. Controls how long it takes for <see cref="breath"/> to decrease. Breathing Rod doubles this value and Diving Gear multiplies it by 6.
+	/// <para/> Modded effects should add to this instead of multiplying to avoid values getting unreasonable large. Adding 1.5f for example will increase breath time by 150%.
+	/// </summary>
 	public StatModifier breathEffectiveness = StatModifier.Default;
 
 	public RefReadOnlyArray<ModPlayer> ModPlayers => modPlayers;
