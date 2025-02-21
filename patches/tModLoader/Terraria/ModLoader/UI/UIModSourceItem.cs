b1d8072c5250838bc2bc231e5fd0093a461a3145
Stephen T <xxlennygamerxx@gmail.com>
2025-02-20 21:48:11 -0600

    Can skip reading here.

--- patches/tModLoader/Terraria/ModLoader/UI/UIModSourceItem.cs
+++ patches/tModLoader/Terraria/ModLoader/UI/UIModSourceItem.cs
@@ -323,10 +323,9 @@ internal class UIModSourceItem : UIPanel
 			using (modFile.Open()) // savehere, -tmlsavedirectory, normal (test linux too)
 				localMod = new LocalMod(ModLocation.Local, modFile);
 
-			var properties = BuildProperties.ReadModFile(modFile);
-			string icon = Path.Combine(properties.modSource, "icon_workshop.png");
+			string icon = Path.Combine(localMod.properties.modSource, "icon_workshop.png");
 			if (!File.Exists(icon))
-				icon = Path.Combine(properties.modSource, "icon.png");
+				icon = Path.Combine(localMod.properties.modSource, "icon.png");
 
 			WorkshopHelper.PublishMod(localMod, icon);
 		}
