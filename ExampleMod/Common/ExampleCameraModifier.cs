f1ed4b366210b5b656f618911260bb7242b8617e
Ebon1 <73665110+Ebon1@users.noreply.github.com>
2025-04-02 19:34:22 +0200

    ExampleCameraModifier and CameraModifierShowcase

---
+++ ExampleMod/Common/ExampleCameraModifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Terraria.Graphics.CameraModifiers;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace ExampleMod.Common
+{
+	// This example shows a Camera Modifier that pans the camera to a chosen point.
+	// CameraModifierShowcase.cs shows how this can be used
+	public class ExampleCameraModifier : ICameraModifier
+	{
+		private int _framesToLast;
+		private int _framesLasted;
+		public Vector2 _position;
+
+		// This makes sure that other modifiers of the same identity don't run at the same time
+		public string UniqueIdentity { get; private set; }
+		public bool Finished { get; private set; }
+		public ExampleCameraModifier(Vector2 position, int frames, string uniqueIdentity = null) {
+			_position = position - new Vector2(Main.screenWidth / 2, Main.screenHeight / 2);
+			_framesToLast = frames;
+			UniqueIdentity = uniqueIdentity;
+		}
+		public void Update(ref CameraInfo cameraInfo) {
+			float lerpT = MathHelper.Clamp(MathF.Sin(MathHelper.Pi * Utils.GetLerpValue(0, _framesToLast, _framesLasted)) * 2, 0, 1);
+
+			// Smoothly pans the camera from the start position to the desired position, and back
+			cameraInfo.CameraPosition = Vector2.Lerp(cameraInfo.CameraPosition, _position, lerpT);
+
+			// Pauses the effect if the game is tabbed out or paused
+			if (!Main.gameInactive && !Main.gamePaused)
+				_framesLasted++;
+
+			if (_framesLasted >= _framesToLast)
+				Finished = true;
+		}
+	}
+}
---
+++ ExampleMod/Content/Items/CameraModifierShowcase.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ExampleMod.Common;
+
+namespace ExampleMod.Content.Items
+{
+	// Showcases a usage of ExampleCameraModifier.cs ingame.
+	public class CameraModifierShowcase : ModItem
+	{
+		public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.JimsDrone}";
+		public override void SetDefaults() {
+			Item.width = 20;
+			Item.height = 20;
+			Item.maxStack = 1;
+			Item.rare = ItemRarityID.Blue;
+			Item.useAnimation = 30;
+			Item.useTime = 30;
+			Item.noUseGraphic = true;
+			Item.useStyle = ItemUseStyleID.HoldUp;
+			Item.consumable = false;
+			Item.useTurn = false;
+		}
+		public override bool? UseItem(Player player) {
+			// Pans the camera to the mouse, lasting 300 frames
+			Main.instance.CameraModifiers.Add(new ExampleCameraModifier(Main.MouseWorld, 300, FullName));
+			return true;
+		}
+	}
+}
