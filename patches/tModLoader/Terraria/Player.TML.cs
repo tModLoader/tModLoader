dfd5e119fd111be8ef9f6ef37f2cc45027be4eae
JavidPack <javidpack@gmail.com>
2025-04-30 11:26:01 -0600

    Update Player.TML.cs

--- patches/tModLoader/Terraria/Player.TML.cs
+++ patches/tModLoader/Terraria/Player.TML.cs
@@ -86,7 +86,7 @@ public partial class Player : IEntityWithInstances<ModPlayer>
 	public const int ManaCrystalMax = 9;
 
 	/// <summary>
-	/// How effectively the player can hold their breath underwater. Controls how long it takes for <see cref="breath"/> to decrease. Breathing Rod doubles this value and Diving Gear multiplies it by 6.
+	/// How effectively the player can hold their breath underwater. Controls how long it takes for <see cref="breath"/> to decrease. Breathing Reed adds 1 (100%) to this value and Diving Gear multiplies it by 6.
 	/// <para/> Modded effects should add to this instead of multiplying to avoid values getting unreasonable large. Adding 1.5f for example will increase breath time by 150%.
 	/// <para/> Applied in the calculation of <see cref="breathCDMax"/>.
 	/// </summary>
