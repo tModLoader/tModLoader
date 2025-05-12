0cd2ce8952db839ee5b58449b005b50813d7de0d
JavidPack <javidpack@gmail.com>
2025-05-12 17:35:37 -0600

    Update ExampleBar.cs

--- ExampleMod/Content/Tiles/ExampleBar.cs
+++ ExampleMod/Content/Tiles/ExampleBar.cs
@@ -23,9 +23,12 @@ namespace ExampleMod.Content.Tiles
 		}
 
 		public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak) {
-			// This check will destroy this tile if the tile below has become slopped such that it doesn't have a solid top side. 
-			WorldGen.Check1x1(i, j, Type);
-			return false;
+			// This check will destroy this tile if the tile below has become slopped such that it doesn't have a solid top side.
+			// This is necessary in this case because Bar tiles can be placed ontop of each other but can also be hammered to be half bricks despite being tileSolidTop.
+			if (!WorldGen.SolidTileAllowBottomSlope(i, j + 1)) {
+				WorldGen.KillTile(i, j);
+			}
+			return true;
 		}
 	}
 }
