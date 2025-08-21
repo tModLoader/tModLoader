dc344c1ddbec0d1cd8a71b6dcb6a33e4a6cbc616
JavidPack <javidpack@gmail.com>
2025-08-21 13:33:49 -0600

    Update original code to 1.4.4 code

--- ExampleMod/Content/Projectiles/Minions/ExampleSentry.cs
+++ ExampleMod/Content/Projectiles/Minions/ExampleSentry.cs
@@ -1,88 +1,88 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ExampleMod.Projectiles.Minions
+namespace ExampleMod.Content.Projectiles.Minions
 {
 	internal class ExampleSentry : ModProjectile
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Example Sentry"); //The english name of the projectile
-			Main.projFrames[projectile.type] = 4;
-			ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
+			Main.projFrames[Type] = 4;
+			ProjectileID.Sets.MinionTargettingFeature[Type] = true;
 		}
 		public override void SetDefaults()
 		{
-			projectile.width = 54;
-			projectile.height = 30;
-			projectile.friendly = true;
-			projectile.magic = true;
-			projectile.sentry = true; //Sets the weapon as a sentry for sentry accessories to properly work.
-			projectile.timeLeft = Projectile.SentryLifeTime;
-			projectile.ignoreWater = true; //If this is set to false, the projectile will be slowed in water.
-			projectile.tileCollide = false;
-			projectile.penetrate = -1;
+			Projectile.width = 54;
+			Projectile.height = 30;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.sentry = true; // Sets the weapon as a sentry for sentry accessories to properly work.
+			Projectile.timeLeft = Projectile.SentryLifeTime;
+			Projectile.ignoreWater = true; // If this is set to false, the projectile will be slowed in water.
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
 		}
 
 		public override Color? GetAlpha(Color lightColor)
 		{
 			//return Color.White;
-			return new Color(255, 255, 255, 0) * (1f - projectile.alpha / 255f);
+			return new Color(255, 255, 255, 0) * (1f - Projectile.alpha / 255f);
 		}
 
 		public override void AI()
 		{
-			//This AI will function as a static sentry, and will not move. If you would like to know how to do more advanced minion AI, check out PurityWisp.cs.
+			// This AI will function as a static sentry, and will not move. If you would like to know how to do more advanced minion AI, check out PurityWisp.cs.
 
 			for (int k = 0; k < 1; k++) {
-				int dust = Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 132, projectile.oldVelocity.X * 0.5f, projectile.oldVelocity.Y * 0.5f);
-				Main.dust[dust].noGravity = true; //Disable the dust gravity.
-				Main.dust[dust].velocity *= 0.8f; //Dust velocity.
+				int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 132, Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
+				Main.dust[dust].noGravity = true; // Disable the dust gravity.
+				Main.dust[dust].velocity *= 0.8f; // Dust velocity.
 			}
 
-			int SentryRange = 40; //The sentry's range
-			int Speed = 20; //How fast the sentry can shoot the projectile.
-			float FireVelocity = 15f; //The velocity the sentry's shot projectile will travel. Slows down the closer the NPC is.
+			int SentryRange = 40; // The sentry's range
+			int Speed = 20; // How fast the sentry can shoot the projectile.
+			float FireVelocity = 15f; // The velocity the sentry's shot projectile will travel. Slows down the closer the NPC is.
 
 			
-			Main.player[projectile.owner].UpdateMaxTurrets(); //This makes the sentry be able to spawn more if your sentry cap is greater than one.
+			Main.player[Projectile.owner].UpdateMaxTurrets(); // This makes the sentry be able to spawn more if your sentry cap is greater than one.
 			for (int t = 0; t < Main.maxNPCs; t++) {
 				NPC npc = Main.npc[t];
 
-				float distance = projectile.Distance(npc.Center); //Set the distance from the NPC and the sentry projectile.
+				float distance = Projectile.Distance(npc.Center); // Set the distance from the NPC and the sentry projectile.
 
-				//Convert distance to tile position, and continue if the following NPC parameters are met.
+				// Convert distance to tile position, and continue if the following NPC parameters are met.
 				if (distance / 16 < SentryRange && Main.npc[t].active && !Main.npc[t].dontTakeDamage && !Main.npc[t].friendly && Main.npc[t].lifeMax > 5 && Main.npc[t].type != NPCID.TargetDummy) {
-					projectile.ai[1] = npc.whoAmI;
+					Projectile.ai[1] = npc.whoAmI;
 				}
 			}
 
-			NPC target = Main.npc[(int)projectile.ai[1]] ?? new NPC();
+			NPC target = Main.npc[(int)Projectile.ai[1]] ?? new NPC();
 
-			projectile.ai[0]++;
-			if (target.active && projectile.Distance(target.Center) / 16 < SentryRange && projectile.ai[0] % Speed == 5) {
+			Projectile.ai[0]++;
+			if (target.active && Projectile.Distance(target.Center) / 16 < SentryRange && Projectile.ai[0] % Speed == 5) {
 
-				Vector2 direction = target.Center - projectile.Center; //The direction the projectile will fire.
+				Vector2 direction = target.Center - Projectile.Center; // The direction the projectile will fire.
 
-				direction.Normalize(); //Normalizes the direction vector.
-				direction.X *= FireVelocity; //Multiply direction by fireVelocity so the sentry can fire the projectile faster the farther the NPC is away.
-				direction.Y *= FireVelocity; //Same as above, but with Y velocity.
+				direction.Normalize(); // Normalizes the direction vector.
+				direction.X *= FireVelocity; // Multiply direction by fireVelocity so the sentry can fire the projectile faster the farther the NPC is away.
+				direction.Y *= FireVelocity; // Same as above, but with Y velocity.
 
-				Main.PlaySound(SoundID.Item, projectile.Center, 102); //Play a sound.
-
-				int damage = 50; //How much damage the projectile shot from the sentry will do.
-				int type = ProjectileID.LostSoulFriendly; //The type of projectile the sentry will shoot. Use ModContent.ProjectileType<>() to fire a modded projectile.
-				Projectile.NewProjectile(projectile.Center.X - 4f, projectile.Center.Y, direction.X, direction.Y, type, damage, 3, projectile.owner);
+				SoundEngine.PlaySound(SoundID.Item102, Projectile.Center); // Play a sound.
+				
+				int damage = 50; // How much damage the projectile shot from the sentry will do.
+				int type = ProjectileID.LostSoulFriendly; // The type of projectile the sentry will shoot. Use ModContent.ProjectileType<>() to fire a modded projectile.
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X - 4f, Projectile.Center.Y, direction.X, direction.Y, type, damage, 3, Projectile.owner);
 			}
-			//Animate the projectile.
-			projectile.frameCounter++;
-			if (projectile.frameCounter % 10 == 0) {
-				projectile.frame++;
-				projectile.frameCounter = 0;
-				if (projectile.frame >= 4)
-					projectile.frame = 0;
+			// Animate the projectile.
+			Projectile.frameCounter++;
+			if (Projectile.frameCounter % 10 == 0) {
+				Projectile.frame++;
+				Projectile.frameCounter = 0;
+				if (Projectile.frame >= 4)
+					Projectile.frame = 0;
 			}
 		}
 	}
--- ExampleMod/Content/Projectiles/Minions/ExampleSentryItem.cs
+++ ExampleMod/Content/Projectiles/Minions/ExampleSentryItem.cs
@@ -1,40 +1,38 @@
-using ExampleMod.Projectiles.Minions;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ExampleMod.Items.Weapons
+namespace ExampleMod.Content.Projectiles.Minions
 {
 	public class ExampleSentryItem : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("Summons an example sentry to fight for you");
-			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
-			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+			ItemID.Sets.GamepadWholeScreenUseRange[Type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[Type] = true;
 		}
 
 		public override void SetDefaults()
 		{
-			item.damage = 50;
-			item.sentry = true;
-			item.mana = 10; //How much mana this weapon takes to use.
-			item.width = 26; //Item width hitbox.
-			item.height = 28; //Item height hitbox.
-			item.useTime = 36;
-			item.useAnimation = 36;
-			item.useStyle = ItemUseStyleID.SwingThrow;
-			item.noMelee = true; //Restricts this weapon dealing melee damage.
-			item.knockBack = 3;
-			item.value = Item.buyPrice(0, 30, 0, 0); //How much this item is sold for.
-			item.rare = ItemRarityID.Cyan;
-			item.UseSound = SoundID.Item83;
-			item.shoot = ModContent.ProjectileType<ExampleSentry>();
+			Item.damage = 50;
+			Item.sentry = true;
+			Item.mana = 10;
+			Item.width = 26;
+			Item.height = 28;
+			Item.useTime = 36;
+			Item.useAnimation = 36;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.noMelee = true;
+			Item.knockBack = 3;
+			Item.value = Item.buyPrice(gold: 30);
+			Item.rare = ItemRarityID.Cyan;
+			Item.UseSound = SoundID.Item83;
+			Item.shoot = ModContent.ProjectileType<ExampleSentry>();
 		}
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
 			position = Main.MouseWorld;
 			return true;
 		}
