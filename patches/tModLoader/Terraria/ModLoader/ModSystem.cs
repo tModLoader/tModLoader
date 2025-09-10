6b1ff864562ddf3e3e0375025c21e0b28255542e
direwolf420 <amakejk1@web.de>
2025-09-10 21:30:59 +0200

    Add ModSystem.PostWorldLoad and TileID.Sets.ClearedOnWorldLoad (#4776)
    * add ModSystem.PostWorldLoad and TileID.Sets.ClearedOnWorldLoad
    
    * fix wrong HookList
    
    * fix item drop, document ClearedOnWorldLoad more
    
    * Some doc changes
    
    ---------
    
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- patches/tModLoader/Terraria/ModLoader/ModSystem.cs
+++ patches/tModLoader/Terraria/ModLoader/ModSystem.cs
@@ -94,6 +94,13 @@ public abstract partial class ModSystem : ModType
 	public virtual void OnWorldLoad() { }
 
 	/// <summary>
+	/// Called whenever a world is loaded, after <see cref="LoadWorldData"/>. <br/>
+	/// At this point, the world is ready to be entered by players, and all modded world data is loaded. <br/>
+	/// Can be used to migrate modded world data across different <see cref="ModSystem"/>s, or to modify <see cref="Main.tile"/>. For deleting temporary tiles, use <see cref="TileID.Sets.ClearedOnWorldLoad"/> instead.
+	/// </summary>
+	public virtual void PostWorldLoad() { }
+
+	/// <summary>
 	/// Called whenever a world is unloaded.
 	/// </summary>
 	public virtual void OnWorldUnload() { }
