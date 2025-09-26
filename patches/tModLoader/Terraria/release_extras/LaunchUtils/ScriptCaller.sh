0492badfbafcf522f1596e8270dcfe75a3e8e24b
Chicken-Bones <mehvids@gmail.com>
2025-09-26 17:50:11 +1000

    Add -arm launch arg for Mac

--- patches/tModLoader/Terraria/release_extras/LaunchUtils/ScriptCaller.sh
+++ patches/tModLoader/Terraria/release_extras/LaunchUtils/ScriptCaller.sh
@@ -10,9 +10,14 @@ echo "You are on platform: \"$_uname\" arch: \"$_arch\""
 
 # Detect arm64 launches and the presence of Rosetta (oahd running on the system is how the dotnet official install script does it)
 if [ "$_arch" = "arm64" ] && [ "$(/usr/bin/pgrep oahd >/dev/null 2>&1;echo $?)" -eq 0 ]; then
-	echo "arm64 environment with Rosetta detected, restarting under arch -x86_64"
-	# Note this only changes the environment, so that dotnet install scripts download an x86 version. Launching an x86 process from an arm shell or vice versa does not require using arch, or otherwise intentionally invoking Rosetta.
-	exec arch -x86_64 ./ScriptCaller.sh "$@"
+	echo "arm64 environment with Rosetta detected"
+	arm_flag=false
+	for arg in "$@"; do [[ "$arg" = "-arm" ]] && { arm_flag=true; break; }; done
+	if [ "$arm_flag" = false ]; then
+		echo "restarting under arch -x86_64"
+		# Note this only changes the environment, so that dotnet install scripts download an x86 version. Launching an x86 process from an arm shell or vice versa does not require using arch, or otherwise intentionally invoking Rosetta.
+		exec arch -x86_64 ./ScriptCaller.sh "$@"
+	fi
 fi
 
 LaunchLogs="$root_dir/tModLoader-Logs"
