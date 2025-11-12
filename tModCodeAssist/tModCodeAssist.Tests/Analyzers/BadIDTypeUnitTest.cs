797574e63230a39accc4c3a2125d70ce58f9aeba
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-12 02:50:06 -0600

    feat: addrecipegroup checks for recipegroupid
    (cherry picked from commit c013f07bed697d5719635a1d30c0fa54d654eaf7)

--- tModCodeAssist/tModCodeAssist.Tests/Analyzers/BadIDTypeUnitTest.cs
+++ tModCodeAssist/tModCodeAssist.Tests/Analyzers/BadIDTypeUnitTest.cs
@@ -52,6 +52,7 @@ public class BadIDTypeUnitTest
 
 			var recipe = Recipe.Create(ItemID.CobaltBrickWall);
 			recipe.AddIngredient({|BadIDType:TileID.Dirt|});
+			recipe.AddRecipeGroup({|BadIDType:ItemID.Wood|});
 			"""
 			);
 	}
