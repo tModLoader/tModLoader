d46974971d065398914448c4c0a8c1a3bf8859b4
JavidPack <javidpack@gmail.com>
2025-12-02 17:47:00 -0700

    ExampleExposedGem tile example

---
+++ ExampleMod/Content/Items/Placeable/ExampleGem.cs
@@ -0,0 +1,15 @@
+﻿using ExampleMod.Content.Tiles;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Placeable
+{
+	public class ExampleGem : ModItem
+	{
+		public override void SetDefaults() {
+			Item.DefaultToPlaceableTile(ModContent.TileType<ExampleExposedGem>(), 0);
+			Item.alpha = 50;
+			Item.value = 7500;
+		}
+	}
+}
