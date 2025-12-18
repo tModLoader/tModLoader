e55639327f44e736a2cbcc7084267e4ec346a4c5
Nan_Ting <a3380991070@163.com>
2025-12-18 14:00:06 +0800

    ExampleMod to IEntitySource

--- ExampleMod/Common/Players/ExamplePickTilePlayer.cs
+++ ExampleMod/Common/Players/ExamplePickTilePlayer.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+﻿using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,12 +6,20 @@ namespace ExampleMod.Common.Players
 {
 	public class ExamplePickTilePlayer : ModPlayer
 	{
-		public override void ModifyPickTile(Item item, int x, int y, ref int pick) {
-			base.ModifyPickTile(item, x, y, ref pick);
+		public override void ModifyPickTile(IEntitySource source, int x, int y, ref int pick) {
+			base.ModifyPickTile(source, x, y, ref pick);
 			//if item is copperpickaxe then modify pick to 999
 			//So he could dig up everything
-			if (item.type == ItemID.CopperPickaxe) {
-				pick = 999;
+			if(source is EntitySource_ItemUse itemSource) {
+				if (itemSource.Item.type == ItemID.CopperPickaxe) {
+					pick = 999;
+				}
+			}
+
+			if(source is EntitySource_Mount mountSource) {
+				if(mountSource.MountId == MountID.Drill) {
+					pick = 1; // modify drill pick to 1
+				}
 			}
 		}
 	}
