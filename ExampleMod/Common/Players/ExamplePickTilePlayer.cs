450e9723405beec0197c240449569e75b256a6d4
Nan_Ting <a3380991070@163.com>
2026-01-01 15:14:47 +0800

    ExamplePickTilePlayer add ModifyPickWall

--- ExampleMod/Common/Players/ExamplePickTilePlayer.cs
+++ ExampleMod/Common/Players/ExamplePickTilePlayer.cs
@@ -7,6 +7,23 @@ namespace ExampleMod.Common.Players
 {
 	public class ExamplePickTilePlayer : ModPlayer
 	{
+		public override void ModifyPickWall(IEntitySource source, int x, int y, ref int damage) {
+			base.ModifyPickWall(source, x, y, ref damage);
+			// If the destruction originates from an item and the item is a wooden hammer, then set the damage to 999.
+			if (source is EntitySource_ItemUse itemSource) {
+				if(itemSource.Item.type == ItemID.WoodenHammer) {
+					damage = 999;
+				}
+			}
+
+			// If the destruction originates from a drill, then set the damage to 1, making it difficult to destroy walls.
+			if (source is EntitySource_Mount mountSource) {
+				if (mountSource.MountId == MountID.Drill) {
+					damage = 1; // modify drill damage to 1
+				}
+			}
+		}
+
 		public override void ModifyPickTile(IEntitySource source, int x, int y, ref int pick) {
 			base.ModifyPickTile(source, x, y, ref pick);
 			// if item is copperpickaxe then modify pick to 999
