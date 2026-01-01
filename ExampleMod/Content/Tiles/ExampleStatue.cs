63a005d614fd33f3a30dc88f41781d08eb5de16d
Konomi <konomik14@gmail.com>
2026-01-01 13:04:08 +1100

    Make ExampleStatue glow with spelunker potion

--- ExampleMod/Content/Tiles/ExampleStatue.cs
+++ ExampleMod/Content/Tiles/ExampleStatue.cs
@@ -16,6 +16,7 @@ namespace ExampleMod.Content.Tiles
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
 			Main.tileObsidianKill[Type] = true;
+			Main.tileSpelunker[Type] = true;
 			TileID.Sets.DisableSmartCursor[Type] = true;
 			TileID.Sets.IsAMechanism[Type] = true; // Ensures that this tile and connected pressure plate won't be removed during the "Remove Broken Traps" worldgen step
 
