4287ac7f46936ea80e129558e9b3950ebd70cba9
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-02 01:03:29 -0600

    feat: add some branding

---
+++ patches/Unified/Terraria/Main.Unified.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Terraria.DataStructures;
+
+namespace Terraria;
+
+partial class Main
+{
+	internal static string UnifiedVersion => "0.1.0";
+	internal static bool UnifiedBranding => true;
+
+	internal static List<TitleLinkButton> UnifiedLinks { get; } = [];
+}
