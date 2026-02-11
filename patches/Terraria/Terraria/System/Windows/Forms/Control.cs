a4dc6a7f16d38c64eb953f59cbdb77105af97306
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-10 20:14:50 -0600

    feat: simplify terraria patches

---
+++ patches/Terraria/Terraria/System/Windows/Forms/Control.cs
@@ -0,0 +1,13 @@
+﻿#if !WINDOWS
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Windows.Forms;
+
+internal class Control
+{
+	internal static Form FromHandle(IntPtr handle)
+	{
+		throw new NotImplementedException();
+	}
+}
+#endif
