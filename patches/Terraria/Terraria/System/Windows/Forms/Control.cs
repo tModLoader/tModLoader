1ce92bf51fc4e0b7c1beff3b9a1983e5de3b6a59
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-11 19:05:03 -0600

    feat: simplify dummy winforms namespace definition

--- patches/Terraria/Terraria/System/Windows/Forms/Control.cs
+++ patches/Terraria/Terraria/System/Windows/Forms/Control.cs
@@ -1,8 +1,10 @@
-﻿#if !WINDOWS
-
-#pragma warning disable IDE0130 // Namespace does not match folder structure
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace System.Windows.Forms;
 
+// Keep the namespace declaration outside of the conditional so the game will
+// still compile nicely with references to the namespace, even if no types in
+// it are present.
+#if !WINDOWS
 internal class Control
 {
 	internal static Form FromHandle(IntPtr handle)
