587eb7ccbbd5b0541412888a134f436d70c6ffd6
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-09 18:16:24 -0500

    Bump Terraria/TerrariaNetCore to 1.4.5.6 (#5028)
    * setup: bump to 1.4.5.6
    
    (cherry picked from commit 1c416d0887b5223bba2d1377ef16fd35e66e05c4)
    
    * feat: update terraria patches to 1.4.5.6
    
    (cherry picked from commit a9779a912de9b47dcc65373af6aea1d59ad6be77)
    
    * feat: bump netcore 1.4.5.6 patches
    
    (cherry picked from commit cc84b47470c9d0bbb814916868cf0666d0bd29f7)

--- setup/Core/TerrariaDecompileExecutableProvider.cs
+++ setup/Core/TerrariaDecompileExecutableProvider.cs
@@ -8,8 +8,8 @@ namespace Terraria.ModLoader.Setup.Core;
 
 public sealed class TerrariaDecompileExecutableProvider
 {
-	private static readonly Version ClientVersion = new("1.4.5.5");
-	private static readonly Version ServerVersion = new("1.4.5.5");
+	private static readonly Version ClientVersion = new("1.4.5.6");
+	private static readonly Version ServerVersion = new("1.4.5.6");
 
 	private readonly WorkspaceInfo workspaceInfo;
 	private readonly HttpClient httpClient;
