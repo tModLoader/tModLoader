4a7ec753f353f90aaa9affdda8854750d4c5f66e
Solxan <59670736+Solxanich@users.noreply.github.com>
2026-03-18 19:30:43 -0600

    Steam workshop eula update (#5041)
    * Proof Of Concept - Steam Workshop Custom Rules
    
    * Add ModUploadRules.md
    
    * Add bbcode formatting to ModUploadRules
    
    * Update Steam.cs

--- patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
+++ patches/tModLoader/Terraria/Social/Steam/SteamedWraps.cs
@@ -136,6 +136,41 @@ public static class SteamedWraps
 		return deps;
 	}
 
+	public static bool HasAcceptedTmodWorkshopEula()
+	{
+		if (!SteamClient)
+			return false;
+
+		WorkshopEULAStatus_t result = default;
+
+		using var _eulaHook = CallResult<WorkshopEULAStatus_t>.Create((WorkshopEULAStatus_t pCallback, bool bIOFailure) => {
+			result = pCallback;
+		});
+
+		_eulaHook.Set(SteamUGC.GetWorkshopEULAStatus());
+
+		// This probably should align better via a refactor of WorkshopHelper.WaitForQueryResultAsync
+		while (true) {
+			RunCallbacks();
+			if (result.m_eResult != EResult.k_EResultNone)
+				break;
+		}
+
+		// TODO: An exception here doesn't tell the user anything about what's going on. Just looks like button not working
+		if (result.m_eResult != EResult.k_EResultOK)
+			throw new SocialBrowserException("Failed to retreive EULA status");
+
+		return !result.m_bNeedsAction;
+	}
+
+	public static void ShowWorkshopEula()
+	{
+		if (!SteamClient)
+			return;
+
+		SteamUGC.ShowWorkshopEULA();
+	}
+
 	private static void ModifyQueryHandle(ref UGCQueryHandle_t qHandle, QueryParameters qP)
 	{
 		FilterByText(ref qHandle, qP.searchGeneric);
