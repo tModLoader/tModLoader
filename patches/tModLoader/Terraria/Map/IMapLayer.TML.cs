76abd037e00fde0899f62cfe850dee0ef05e21f0
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2026-03-29 22:57:00 -0400

    added 1.4.5 MapLayers

--- patches/tModLoader/Terraria/Map/IMapLayer.TML.cs
+++ patches/tModLoader/Terraria/Map/IMapLayer.TML.cs
@@ -2,7 +2,9 @@
 
 public partial interface IMapLayer
 {
+	public static IMapLayer BossBag { get; private set; } = new BossBagMapLayer();
 	public static IMapLayer Spawn { get; private set; } = new SpawnMapLayer();
+	public static IMapLayer TeamBasedSpawn { get; private set; } = new TeamBasedSpawnMapLayer();
 	public static IMapLayer Pylons { get; private set; } = new TeleportPylonsMapLayer();
 	public static IMapLayer Pings { get; private set; } = new PingMapLayer();
 
