a1604f7b590bc5e9c5aaf707f527a6a2bfe94554
Chicken-Bones <mehvids@gmail.com>
2026-04-02 16:27:34 +1000

    Upgrade Microsoft.CodeAnalysis and Newtonsoft.Json packages

--- patches/tModLoader/Terraria/ModLoader/Exceptions/BuildException.cs
+++ patches/tModLoader/Terraria/ModLoader/Exceptions/BuildException.cs
@@ -1,12 +1,10 @@
 using System;
-using System.CodeDom.Compiler;
 using Terraria.ModLoader.Engine;
 
 namespace Terraria.ModLoader.Exceptions;
 
 internal class BuildException : Exception
 {
-	public CompilerErrorCollection compileErrors;
 	public ErrorReporting.TMLErrorCode errorCode = ErrorReporting.TMLErrorCode.TML002;
 
 	public BuildException(string message, ErrorReporting.TMLErrorCode errorCode = ErrorReporting.TMLErrorCode.TML002) : base(message) {
