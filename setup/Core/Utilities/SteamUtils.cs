c26e7159b75cb4cd67fa67ec9254eb1c116ff584
Mark <138571922+RoNiN-shadow@users.noreply.github.com>
2026-04-06 03:02:29 +0200

    Add Debian Steam Installation path to Linux auto-detection (#5078)
    * Add Debian Steam Installation path to Linux auto-detection
    
    * add empty line for readability based on review

--- setup/Core/Utilities/SteamUtils.cs
+++ setup/Core/Utilities/SteamUtils.cs
@@ -90,6 +90,9 @@ namespace Terraria.ModLoader.Setup.Core.Utilities
 				path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local/share/Steam");
 				if (!Directory.Exists(path))
 					path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".steam/steam");
+
+                if (!Directory.Exists(path))
+					path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".steam/debian-installation");
 			}
 
 			return path != null && Directory.Exists(path);
