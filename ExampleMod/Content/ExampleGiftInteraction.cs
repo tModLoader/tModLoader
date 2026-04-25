21a3908d767cf98053db9323ccc0f4098111bbfe
末泠 <3238792828@qq.com>
2026-04-25 22:46:43 +0800

    Add ModNPCInteraction system for custom NPC interactions

---
+++ ExampleMod/Content/ExampleGiftInteraction.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content;
+
+/// <summary>
+/// Example NPC interaction that gives the player an item when clicked.
+/// </summary>
+public class ExampleGiftInteraction : ModNPCInteraction
+{
+	public override int ForNPCType => NPCID.Guide; // Applies to the Guide
+
+	public override string GetText() => "Give me a gift";
+
+	public override void Interact()
+	{
+		// Give the player an ExampleItem
+		var entitySource = Main.LocalPlayer.GetSource_GiftOrReward("Example Gift");
+		Item.NewItem(entitySource, Main.LocalPlayer.Center, ModContent.ItemType<Items.ExampleItem>(), 1);
+
+		// Show a message
+		Main.NewText("The Guide gave you an Example Item!", 255, 200, 100);
+
+		// Play a sound
+		Terraria.Audio.SoundEngine.PlaySound(SoundID.Coins);
+	}
+}
---
+++ patches/tModLoader/Terraria/ModLoader/ModNPCInteraction.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.GameContent;
+
+namespace Terraria.ModLoader;
+
+/// <summary>
+/// Base class for Mod NPC interactions. Automatically registered when the mod loads.
+/// </summary>
+public abstract class ModNPCInteraction : NPCInteraction, ILoadable
+{
+	public Mod Mod { get; internal set; }
+
+	/// <summary>
+	/// The NPC type this interaction applies to.
+	/// </summary>
+	public abstract int ForNPCType { get; }
+
+	/// <summary>
+	/// Whether to show an exclamation mark icon next to this interaction button.
+	/// </summary>
+	public override bool ShowExcalmation => false;
+
+	/// <summary>
+	/// Condition for when this interaction should appear. Default checks if talking to target NPC.
+	/// </summary>
+	public override bool Condition() => TalkNPCType == ForNPCType;
+
+	/// <summary>
+	/// The text displayed on the interaction button.
+	/// </summary>
+	public abstract override string GetText();
+
+	/// <summary>
+	/// What happens when the player clicks the interaction button.
+	/// </summary>
+	public abstract override void Interact();
+
+	/// <summary>
+	/// Optional: Display a coin value next to the interaction button.
+	/// </summary>
+	public override bool TryAddCoins(ref Color chatColor, out int coinValue)
+	{
+		coinValue = 0;
+		return false;
+	}
+
+	void ILoadable.Load(Mod mod)
+	{
+		Mod = mod;
+		NPCInteractions.All.Add(this);
+	}
+
+	void ILoadable.Unload() { }
+}
