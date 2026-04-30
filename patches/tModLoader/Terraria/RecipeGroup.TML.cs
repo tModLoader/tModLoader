f433828d6bfac15440a0cac03836c5fb9ab65682
JavidPack <javidpack@gmail.com>
2026-04-30 15:38:30 -0600

    Feedback, just use reflection approach to set Key

--- patches/tModLoader/Terraria/RecipeGroup.TML.cs
+++ patches/tModLoader/Terraria/RecipeGroup.TML.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Terraria.GameContent;
+using System.Linq;
+using System.Reflection;
 using Terraria.ID;
 
 namespace Terraria;
@@ -36,46 +37,11 @@ public partial class RecipeGroup
 
 	public static void AssignKeysToVanillaGroups()
 	{
-		/* These RecipeGroup don't register, so they aren't suitable for use in recipes anyway since they have no RegisteredId. If we wanted, we'd need to register and assign a key, which might look similar to this.
-		ConditionalDialogue.ItemGroups.Ore = new RecipeGroup("RecipeGroups.Ore", 699, 12, 11, 700, 14, 701, 13, 702) { Key = "Ore" }.Register();
-		ConditionalDialogue.ItemGroups.Ore.Key = "Ore";
-		ConditionalDialogue.ItemGroups.Bars.Key = "Bars";
-		ConditionalDialogue.ItemGroups.Anvils.Key = "Anvils";
-		ConditionalDialogue.ItemGroups.Whips.Key = "Whips";
-		ConditionalDialogue.ItemGroups.Mounts.Key = "Mounts";
-		*/
+		// The RecipeGroup in ConditionalDialogue don't register, so they aren't suitable for use in recipes anyway since they have no RegisteredId. They also are not currently reinitialized, so changes will persist between mod reloads.
+		// If we wanted to support them, they'd have to be registered and we'd need to make sure they are reset on mod reload, but they aren't used anywhere anyway yet.
 
-		// Could use reflection, but this should be faster.
-		RecipeGroups.Birds.Key = "Birds";
-		RecipeGroups.Scorpions.Key = "Scorpion";
-		RecipeGroups.Squirrels.Key = "Squirrel";
-		RecipeGroups.Bugs.Key = "Bugs";
-		RecipeGroups.Ducks.Key = "Ducks";
-		RecipeGroups.Butterflies.Key = "Butterflies";
-		RecipeGroups.Fireflies.Key = "Fireflies";
-		RecipeGroups.Snails.Key = "Snails";
-		RecipeGroups.Dragonflies.Key = "Dragonflies";
-		RecipeGroups.Turtles.Key = "Turtles";
-		RecipeGroups.Macaws.Key = "Macaws";
-		RecipeGroups.Cockatiels.Key = "Cockatiels";
-		RecipeGroups.CloudBalloons.Key = "CloudBalloons";
-		RecipeGroups.BlizzardBalloons.Key = "BlizzardBalloons";
-		RecipeGroups.SandstormBalloons.Key = "SandstormBalloons";
-		RecipeGroups.CritterGuides.Key = "CritterGuides";
-		RecipeGroups.NatureGuides.Key = "NatureGuides";
-		RecipeGroups.Seashells.Key = "Seashells";
-		RecipeGroups.Fruit.Key = "Fruit";
-		RecipeGroups.Balloons.Key = "Balloons";
-		RecipeGroups.CobaltBar.Key = "CobaltBar";
-		RecipeGroups.MythrilBar.Key = "MythrilBar";
-		RecipeGroups.GemCritter.Key = "GemCritter";
-		RecipeGroups.MagicMirror.Key = "MagicMirror";
-		RecipeGroups.Wood.Key = "Wood";
-		RecipeGroups.Stone.Key = "Stone";
-		RecipeGroups.Sand.Key = "Sand";
-		RecipeGroups.IronBar.Key = "IronBar";
-		RecipeGroups.Fragment.Key = "Fragment";
-		RecipeGroups.PressurePlate.Key = "PressurePlate";
-		RecipeGroups.Jellyfish.Key = "Jellyfish";
+		foreach (var fieldInfo in typeof(RecipeGroups).GetFields(BindingFlags.Static | BindingFlags.Public).Where(f => f.FieldType == typeof(RecipeGroup))) {
+			(fieldInfo.GetValue(null) as RecipeGroup).Key = fieldInfo.Name;
+		}
 	}
 }
