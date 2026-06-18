344ad8ffecdabd71d2907ee324748797d1157e13
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2026-06-19 07:42:10 +1200

    Refactor mod icon code (#4937)
    * Refactor mod icon code
    
    * logger.info to error
    
    * fix: move GetModIcon
    
    * fix: incorrect GetModIcon
    
    * more changes
    
    * fix
    
    * update
    
    * Doc update
    
    ---------
    
    Co-authored-by: Destructor-Ben <destructorben208@gmail.com>
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- patches/tModLoader/Terraria/ModLoader/ModLoader.cs
+++ patches/tModLoader/Terraria/ModLoader/ModLoader.cs
@@ -8,6 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using ReLogic.OS;
 using Terraria.GameContent.Liquid;
@@ -433,6 +434,38 @@ public static class ModLoader
 			Interface.modsMenu.sortMode = modsMenuSortMode;
 
 		Main.Configuration.Get("LiquidSlopeFix", ref LiquidEdgeRenderer.Enabled);
+	}
+
+	internal static Asset<Texture2D> GetModIcon(TmodFile modFile, out string error, string fileName = "icon.png", int iconSize = 80)
+	{
+		error = null;
+
+		if (modFile.HasFile(fileName)) {
+			try {
+				using (modFile.Open())
+				using (var s = modFile.GetStream(fileName)) {
+					var iconTexture = Main.Assets.CreateUntracked<Texture2D>(s, fileName);
+
+					if (iconTexture.Width() == iconSize && iconTexture.Height() == iconSize) {
+						return iconTexture;
+					}
+
+					error = $"{fileName} is not {iconSize}x{iconSize}";
+					return null;
+				}
+			}
+			catch (Exception e) {
+				Logging.tML.Error("Unknown error", e);
+			}
+		}
+
+		error = $"{fileName} does not exist";
+		return null;
+	}
+
+	internal static Asset<Texture2D> GetModIcon(TmodFile modFile, string fileName = "icon.png", int iconSize = 80)
+	{
+		return GetModIcon(modFile, out string _, fileName, iconSize);
 	}
 
 	internal static void MigrateSettings()
