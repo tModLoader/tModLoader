9c0a6b5e88c536b541bac5188fb3197ee3f71e93
Zeyad Emad <145834368+0ZeYaD0@users.noreply.github.com>
2026-06-27 20:28:21 +0300

    Refactor WallDefinitionElement for improved clarity

--- patches/tModLoader/Terraria/ModLoader/Config/UI/WallDefinitionElement.cs
+++ patches/tModLoader/Terraria/ModLoader/Config/UI/WallDefinitionElement.cs
@@ -12,69 +12,64 @@ internal class WallDefinitionElement : DefinitionElement<WallDefinition>
 {
 	protected override DefinitionOptionElement<WallDefinition> CreateDefinitionOptionElement() => new WallDefinitionOptionElement(Value, 0.5f);
 
-	protected override List<DefinitionOptionElement<WallDefinition>> GetPassedOptionElements()
-	{
-		var passed = new List<DefinitionOptionElement<WallDefinition>>();
-		foreach (var option in Options) {
-			// Filter by the text in the search bar
-			if (option.Definition.DisplayName.IndexOf(ChooserFilter.CurrentString, StringComparison.OrdinalIgnoreCase) == -1)
-				continue;
-
-			passed.Add(option);
-		}
-		return passed;
-	}
-
 	protected override List<DefinitionOptionElement<WallDefinition>> CreateDefinitionOptionElementList()
-	{
-		return WallDefinitionOptionElement.GetWallOptionsSetup();
-	}
-}
-
-internal class WallDefinitionOptionElement : DefinitionOptionElement<WallDefinition>
-{
-	public WallDefinitionOptionElement(WallDefinition definition, float scale) : base(definition, scale) { }
-
-	public static List<DefinitionOptionElement<WallDefinition>> GetWallOptionsSetup()
 	{
 		var options = new List<DefinitionOptionElement<WallDefinition>>();
 
-		options.Add(new WallDefinitionOptionElement(new WallDefinition(0), 0.5f));
-
-		for (int i = 1; i < WallLoader.WallCount; i++) {
-			options.Add(new WallDefinitionOptionElement(new WallDefinition(i), 0.5f));
+		for (int i = 0; i < WallLoader.WallCount; i++) {
+			var optionElement = new WallDefinitionOptionElement(new WallDefinition(i), OptionScale);
+			optionElement.OnLeftClick += (a, b) => {
+				Value = optionElement.Definition;
+				UpdateNeeded = true;
+				SelectionExpanded = false;
+			};
+			options.Add(optionElement);
 		}
 
 		return options;
 	}
 
+	protected override List<DefinitionOptionElement<WallDefinition>> GetPassedOptionElements()
+	{
+		var passed = new List<DefinitionOptionElement<WallDefinition>>();
+
+		foreach (var option in Options) {
+			if (!(option.Definition?.DisplayName ?? "").Contains(ChooserFilter.CurrentString, StringComparison.OrdinalIgnoreCase))
+				continue;
+
+			passed.Add(option);
+		}
+
+		return passed;
+	}
+}
+
+internal class WallDefinitionOptionElement : DefinitionOptionElement<WallDefinition>
+{
+	public WallDefinitionOptionElement(WallDefinition definition, float scale = 0.5f) : base(definition, scale) { }
+
 	protected override void DrawSelf(SpriteBatch spriteBatch)
 	{
-		base.DrawSelf(spriteBatch);
 		CalculatedStyle dimensions = GetInnerDimensions();
 
-		if (IsMouseHovering) {
+		spriteBatch.Draw(BackgroundTexture.Value, dimensions.Position(), null, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+
+		if (Definition != null && Definition.Type > 0) {
+			Main.instance.LoadWall(Definition.Type);
+			Texture2D wallTexture = TextureAssets.Wall[Definition.Type].Value;
+
+			if (wallTexture != null) {
+				int size = 32;
+				Rectangle sourceRectangle = new Rectangle(0, 0, size, size);
+				Vector2 position = new Vector2(
+					dimensions.X + dimensions.Width / 2f - size / 2f * Scale,
+					dimensions.Y + dimensions.Height / 2f - size / 2f * Scale
+				);
+				spriteBatch.Draw(wallTexture, position, sourceRectangle, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+			}
+		}
+
+		if (IsMouseHovering)
 			UIModConfig.Tooltip = Definition.DisplayName;
-		}
-
-		if (Definition.Type <= 0)
-			return;
-
-		Main.instance.LoadWall(Definition.Type);
-		Texture2D wallTexture = TextureAssets.Wall[Definition.Type].Value;
-
-		if (wallTexture != null) {
-			// Walls use 32x32 frames typically, we grab the first frame
-			int size = 32;
-			Rectangle sourceRectangle = new Rectangle(0, 0, size, size);
-
-			// Center the texture snippet inside the UI box
-			Vector2 position = new Vector2(
-				dimensions.X + dimensions.Width / 2f - size / 2f,
-				dimensions.Y + dimensions.Height / 2f - size / 2f
-			);
-
-			spriteBatch.Draw(wallTexture, position, sourceRectangle, Color.White);
-		}
 	}
 }
