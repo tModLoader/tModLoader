tModLoader/tModLoader

commit c8a40211e7e66c392b9f7564c1b90dcb5e9e1b51

Refactor mod icon code (#4937)
* Refactor mod icon code

* logger.info to error

* fix: move GetModIcon

* fix: incorrect GetModIcon

* more changes

* fix

* update

* Doc update

---------

Co-authored-by: Destructor-Ben <destructorben208@gmail.com>
Co-authored-by: JavidPack <javidpack@gmail.com>

patches/tModLoader/Terraria/ModLoader/Mod.Internals.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria.Localization;
 using Terraria.ModLoader.Core;
 using Terraria.ModLoader.Exceptions;
@@ -46,6 +48,18 @@ partial class Mod
 	{
 		if (Code == null)
 			return;
+
+		// Load icon.png and icon_small.png
+		// The embedded mod for tModLoader doesn't have File initialized
+		if (File != null) {
+			ModIcon = ModLoader.GetModIcon(File, out string modIconError)
+			       ?? throw new MissingResourceException($"Failed to load icon.png. Reason: {modIconError}.");
+
+			SmallModIcon = ModLoader.GetModIcon(File, out string smallIconError, "icon_small.png", 30);
+			if (SmallModIcon is null) {
+				Logger.Warn($"Failed to load icon_small.png. Reason: {smallIconError}.");
+			}
+		}
 
 		LocalizationLoader.Autoload(this);
 

patches/tModLoader/Terraria/GameContent/Bestiary/ModSourceBestiaryInfoElement.cs
@@ -16,18 +16,15 @@ public class ModSourceBestiaryInfoElement : ModBestiaryInfoElement
 
 	public override UIElement GetFilterImage()
 	{
-		Asset<Texture2D> asset;
-		if (_mod.HasAsset("icon_small")) {
-			asset = _mod.Assets.Request<Texture2D>("icon_small");
-			if (asset.Size() == new Vector2(30)) {
-				return new UIImage(asset) {
-					HAlign = 0.5f,
-					VAlign = 0.5f
-				};
-			}
-			_mod.Logger.Info("icon_small needs to be 30x30 pixels.");
+		Asset<Texture2D> asset = _mod.SmallModIcon;
+		if (asset != null) {
+			return new UIImage(asset) {
+				HAlign = 0.5f,
+				VAlign = 0.5f
+			};
 		}
-		asset = Main.Assets.Request<Texture2D>("Images/UI/Bestiary/Icon_Tags_Shadow");
+
+		asset = Main.Assets.Request<Texture2D>("Images/UI/Bestiary/Icon_Tags_Shadow", AssetRequestMode.ImmediateLoad);
 		return new UIImageFramed(asset, asset.Frame(16, 5, 0, 4)) {
 			HAlign = 0.5f,
 			VAlign = 0.5f

patches/tModLoader/Terraria/ModLoader/Config/UI/UIModConfigList.cs
@@ -215,7 +215,7 @@ internal class UIModConfigList : UIState
 
 		void AddSmallIcon(Mod mod, UIButton<string> modPanel)
 		{
-			var iconTexture = GetSmallIcon(mod);
+			var iconTexture = mod.SmallModIcon;
 			if (iconTexture == null) {
 				return;
 			}
@@ -224,7 +224,7 @@ internal class UIModConfigList : UIState
 			float iconPadding = 2;
 			modPanel.PaddingLeft += iconOffset + iconPadding;
 
-			var sideIndicator = new UIImage(iconTexture) {
+			var modIcon = new UIImage(iconTexture) {
 				VAlign = 0.5f,
 				HAlign = 0f,
 				Color = Color.White,
@@ -232,7 +232,7 @@ internal class UIModConfigList : UIState
 				MarginTop = -2, // 40 - 30 is 10, padding is 6, so -2 would make 5 pixels top and bottom since VAlign is 0.5
 			};
 
-			modPanel.Append(sideIndicator);
+			modPanel.Append(modIcon);
 		}
 	}
 
@@ -291,18 +291,6 @@ internal class UIModConfigList : UIState
 			configPanel.Append(sideIndicator);
 			configList.Add(configPanel);
 		}
-	}
-
-	private Asset<Texture2D> GetSmallIcon(Mod mod)
-	{
-		if (mod.HasAsset("icon_small")) {
-			var asset = mod.Assets.Request<Texture2D>("icon_small");
-			if (asset.Size() == new Vector2(30)) {
-				return asset;
-			}
-			mod.Logger.Info("icon_small needs to be 30x30 pixels.");
-		}
-		return null;
 	}
 
 	public override void Draw(SpriteBatch spriteBatch)

patches/tModLoader/Terraria/ModLoader/Mod.cs
@@ -52,6 +52,22 @@ public partial class Mod
 	/// </summary>
 	public virtual Version Version => File.Version;
 
+	/// <summary>
+	/// The icon for this mod, sourced from <c>icon.png</c>.
+	/// <br/><br/> Must be 80x80.
+	/// </summary>
+	public Asset<Texture2D> ModIcon { get; internal set; }
+	/// <summary>
+	/// The small icon for this mod, sourced from <c>icon_small.png</c>.
+	/// <br/><br/> Must be 30x30.
+	/// <br/><br/> Will be null if a suitable icon doesn't exist.
+	/// </summary>
+	public Asset<Texture2D> SmallModIcon { get; internal set; }
+	/// <summary>
+	/// A placeholder mod icon, for use when a mod doesn't have a valid icon.
+	/// </summary>
+	public static Asset<Texture2D> PlaceholderModIcon => Main.Assets.Request<Texture2D>("Images/UI/DefaultResourcePackIcon");
+
 	public List<string> TranslationForMods { get; internal set; }
 
 	/// <summary>

patches/tModLoader/Terraria/ModLoader/ModLoader.cs
@@ -8,6 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using ReLogic.OS;
 using Terraria.GameContent.Liquid;
@@ -433,6 +434,38 @@ public static class ModLoader
 			Interface.modsMenu.sortMode = modsMenuSortMode;
 
 		Main.Configuration.Get("LiquidSlopeFix", ref LiquidEdgeRenderer.Enabled);
+	}
+
+	internal static Asset<Texture2D> GetModIcon(TmodFile modFile, out string error, string fileName = "icon.png", int iconSize = 80)
+	{
+		error = null;
+
+		if (modFile.HasFile(fileName)) {
+			try {
+				using (modFile.Open())
+				using (var s = modFile.GetStream(fileName)) {
+					var iconTexture = Main.Assets.CreateUntracked<Texture2D>(s, fileName);
+
+					if (iconTexture.Width() == iconSize && iconTexture.Height() == iconSize) {
+						return iconTexture;
+					}
+
+					error = $"{fileName} is not {iconSize}x{iconSize}";
+					return null;
+				}
+			}
+			catch (Exception e) {
+				Logging.tML.Error("Unknown error", e);
+			}
+		}
+
+		error = $"{fileName} does not exist";
+		return null;
+	}
+
+	internal static Asset<Texture2D> GetModIcon(TmodFile modFile, string fileName = "icon.png", int iconSize = 80)
+	{
+		return GetModIcon(modFile, out string _, fileName, iconSize);
 	}
 
 	internal static void MigrateSettings()

patches/tModLoader/Terraria/ModLoader/UI/UIModItem.cs
@@ -83,24 +83,8 @@ internal class UIModItem : UIPanel
 		base.OnInitialize();
 
 		string text = _mod.DisplayName + " v" + _mod.modFile.Version;
-		var modIcon = Main.Assets.Request<Texture2D>("Images/UI/DefaultResourcePackIcon", AssetRequestMode.ImmediateLoad);
+		var modIcon = ModLoader.GetModIcon(_mod.modFile) ?? Mod.PlaceholderModIcon;
 		_modIconAdjust += 85;
-
-		if (_mod.modFile.HasFile("icon.png")) {
-			try {
-				using (_mod.modFile.Open())
-				using (var s = _mod.modFile.GetStream("icon.png")) {
-					var iconTexture = Main.Assets.CreateUntracked<Texture2D>(s, ".png");
-
-					if (iconTexture.Width() == 80 && iconTexture.Height() == 80) {
-						modIcon = iconTexture;
-					}
-				}
-			}
-			catch (Exception e) {
-				Logging.tML.Error("Unknown error", e);
-			}
-		}
 
 		_modIcon = new UIImage(modIcon) {
 			Left = { Percent = 0f },

patches/tModLoader/Terraria/ModLoader/UI/UIServerModsDifferMessage.cs
@@ -202,22 +202,7 @@ internal class UIServerModsDifferMessage : UIState, IHaveBackButtonCommand
 			};
 			reason.IsWrapped = true;
 
-			var modIcon = Main.Assets.Request<Texture2D>("Images/UI/DefaultResourcePackIcon", AssetRequestMode.ImmediateLoad);
-			if (entry.localMod != null) {
-				try {
-					using (entry.localMod.modFile.Open())
-					using (var s = entry.localMod.modFile.GetStream("icon.png")) {
-						var iconTexture = Main.Assets.CreateUntracked<Texture2D>(s, ".png");
-
-						if (iconTexture.Width() == 80 && iconTexture.Height() == 80) {
-							modIcon = iconTexture;
-						}
-					}
-				}
-				catch (Exception e) {
-					Logging.tML.Error("Unknown error", e);
-				}
-			}
+			var modIcon = ModLoader.GetModIcon(entry.localMod.modFile) ?? Mod.PlaceholderModIcon;
 			UIImage modIconImage = new UIImage(modIcon) {
 				Left = { Percent = 0f },
 				Top = { Percent = 0f },

commit 591f009b15fbc944cc6f5a04dc42076ee36499df

Fix servers not loading mods regression from #4937

patches/tModLoader/Terraria/ModLoader/Mod.Internals.cs
@@ -51,7 +51,7 @@ partial class Mod
 
 		// Load icon.png and icon_small.png
 		// The embedded mod for tModLoader doesn't have File initialized
-		if (File != null) {
+		if (File != null && !Main.dedServ) {
 			ModIcon = ModLoader.GetModIcon(File, out string modIconError)
 			       ?? throw new MissingResourceException($"Failed to load icon.png. Reason: {modIconError}.");
 