df45015ef777504ab0bb6077f7b6ed362d32a878
Reisenburger <lianghaoyu@sjtu.edu.cn>
2026-09-01 07:08:50 +0800

    [1.4.5] Fixed modded crafting stations in the modern crafting UI (#5334)
    * Fixed IndexOutOfRangeException in the modern crafting UI with modded crafting stations
    
    * Fixed modded crafting stations not opening the crafting UI
    
    * Add ExampleWorkbench highlight and migration guide info about crafting stations
    
    ---------
    
    Co-authored-by: JavidPack <javidpack@gmail.com>

--- ExampleMod/Content/Tiles/Furniture/ExampleWorkbench.cs
+++ ExampleMod/Content/Tiles/Furniture/ExampleWorkbench.cs
@@ -17,6 +17,7 @@ namespace ExampleMod.Content.Tiles.Furniture
 			Main.tileLavaDeath[Type] = true;
 			Main.tileFrameImportant[Type] = true;
 			TileID.Sets.DisableSmartCursor[Type] = true;
+			TileID.Sets.HasOutlines[Type] = true; // Crafting stations usually have interact outlines
 			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
 
 			DustType = ModContent.DustType<Dusts.Sparkle>();
