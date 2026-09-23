3f06b51aee43b28e549cb704d77ff3ba3a3b2b61
inb4banishment <potpem1@gmail.com>
2026-09-23 23:39:25 +0300

    Add tModPorter rules for ItemVariants world conditions

--- PortingNotes_1.4.5.md
+++ PortingNotes_1.4.5.md
@@ -226,8 +226,6 @@ Once all patches are fixed, these items need to be fixed or double checked:
 
 # tModPorter TODOs
 
-- ItemVariants.EverythingWorld renamed to MechdusaWorld
-- Main.GameModeInfo.IsJourneyMode -> Main.IsJourneyMode
 
 # ExampleMod TODOs
 - Verify that ExampleZombieThief still works with changes
--- tModPorter/tModPorter.Tests/TestData/DamageClassPlayerFields.Expected.cs
+++ tModPorter/tModPorter.Tests/TestData/DamageClassPlayerFields.Expected.cs
@@ -1,5 +1,6 @@
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.GameContent.Items;
 
 public class DamageClassPlayerFields : ModPlayer {
 	public void MethodA() {
@@ -8,6 +9,9 @@ public class DamageClassPlayerFields : ModPlayer {
 		Player.GetArmorPenetration(DamageClass.Generic) += 1;
 		Player.GetArmorPenetration(DamageClass.Melee) += 1;
 		Player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) = 2;
+		Condition.RemixWorld.ToString();
+		Condition.ForTheWorthyWorld.ToString();
+		Condition.ZenithWorld.ToString();
 	}
 
 	public override void UpdateEquips() {
